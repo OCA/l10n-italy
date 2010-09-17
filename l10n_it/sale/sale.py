7457c8abd5bfa40941d47edd1acc5b5916788d8a
Davide Corio <enlightx@sirah>
2010-09-17 14:28:19 +0200

    removed company_id and added validity date field to sale order

--- l10n_it/sale/sale.py
+++ l10n_it/sale/sale.py
@@ -10,10 +10,7 @@ from osv import fields, osv
 class sale_order(osv.osv):
     _inherit = "sale.order"
     _columns =  {
-        'company_id': fields.many2one('res.company', 'Company', required=True),
-    }
-    _defaults = {
-        'company_id': lambda self, cr, uid, context: self.pool.get('res.users').browse(cr, uid, uid,context=context).company_id.id,
+        'validity': fields.date('Validity'),
     }
   
 sale_order()
