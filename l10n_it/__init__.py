31870d28b98901ff54c2305e07d2eddd9b159fe2
eLBati <lorenzo.battistini@domsense.com>
2010-11-05 09:13:00 +0100

    l10n_it: FIX naming and removing dependency from other modules

--- l10n_it/__init__.py
+++ l10n_it/__init__.py
@@ -19,34 +19,4 @@
 #
 ##############################################################################
 
-
-{
-    "name" : "Italy - Generic Chart of Accounts",
-    "version" : "0.1",
-    "depends" : [
-                 "l10n_it_account", 
-        ],
-    "author" : "OpenERP Italian Community",
-    "description": """
-    Piano dei conti italiano di un'impresa generica, comprendente riclassificazione IV normativa UE
-    """,
-    "license": "AGPL-3",
-    "category" : "Localisation/Account Charts",
-    'website': 'http://www.openerp-italia.org/',
-    'init_xml': [
-        ],
-    'update_xml': [
-        'data/account.account.type.csv',
-        'data/account.account.template.csv',
-        'data/account.tax.code.template.csv',
-        'account_chart.xml',
-        'data/account.tax.template.csv',
-        'data/account.fiscal.position.template.csv',
-        'l10n_chart_it_generic.xml',
-        ],
-    'demo_xml': [
-        ],
-    'installable': True,
-    'active': False,
-}
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+import account
