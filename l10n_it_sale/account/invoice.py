e7a7e30300cb16421e436a7a549a1dbe329064fb
eLBati <lorenzo.battistini@agilebg.com>
2011-01-24 17:38:07 +0100

    FIX l10n_it_sale: onchange_partner_id

--- l10n_it_sale/account/invoice.py
+++ l10n_it_sale/account/invoice.py
@@ -38,13 +38,14 @@ class account_invoice(osv.osv):
         }
 
     def onchange_partner_id(self, cr, uid, ids, type, partner_id,
-            date_invoice=False, payment_term=False, partner_bank=False):
+            date_invoice=False, payment_term=False, partner_bank_id=False, company_id=False):
         result = super(account_invoice, self).onchange_partner_id(cr, uid, ids, type, partner_id,
-            date_invoice, payment_term, partner_bank)
-        partner = self.pool.get('res.partner').browse(cr, uid, partner_id)
-        result['value']['carriage_condition_id'] = partner.carriage_condition_id.id
-        result['value']['goods_description_id'] = partner.goods_description_id.id
-        result['value']['transportation_reason_id'] = partner.transportation_reason_id.id
+            date_invoice, payment_term, partner_bank_id, company_id)
+        if partner_id:
+            partner = self.pool.get('res.partner').browse(cr, uid, partner_id)
+            result['value']['carriage_condition_id'] = partner.carriage_condition_id.id
+            result['value']['goods_description_id'] = partner.goods_description_id.id
+            result['value']['transportation_reason_id'] = partner.transportation_reason_id.id
         return result
     
 account_invoice()
