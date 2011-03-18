643c006c8a4a58b03c512cbaa39ebcf2cbe605db
eLBati <lorenzo.battistini@agilebg.com>
2011-03-18 10:21:18 +0100

    FIX l10n_chart_it_cc2424 dependency

--- l10n_chart_it_cc2424/__terp__.py
+++ l10n_chart_it_cc2424/__terp__.py
@@ -25,7 +25,7 @@
     "author" : "OpenERP Italian Community",
     "category" : "Localisation/Account Charts",
     "description": "This is the module to manage the accounting chart for Italy (CC2424 Profile) in Open ERP.",
-    "depends" : ["l10n_it_account"],
+    "depends" : ["account_chart"],
     "demo_xml" : [],
     "update_xml" : ['account_tax_code.xml',"account_chart.xml",
                     'account_tax.xml','l10n_chart_it_cc2424_wizard.xml'],
