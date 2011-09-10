36591bf3ffe90d239ba01a65986586dd1dfd98ff
eLBati <lorenzo.battistini@agilebg.com>
2011-09-10 19:14:53 +0200

    [FIX] dependency

--- account_invoice_entry_date/__openerp__.py
+++ account_invoice_entry_date/__openerp__.py
@@ -27,7 +27,7 @@
     'author': 'OpenERP Italian Community',
     'website': 'http://www.openerp-italia.org',
     'license': 'AGPL-3',
-    "depends" : ['l10n_it_account',],
+    "depends" : ['account',],
     "init_xml" : [],
     "update_xml" : ['invoice_view.xml'],
     "demo_xml" : [],
