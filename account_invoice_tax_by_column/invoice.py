f1de94a15a7a89f7585a9bb120090f06cdc2425e
eLBati <lorenzo.battistini@agilebg.com>
2011-10-21 15:53:32 +0200

    [IMP] covering more cases

--- account_invoice_tax_by_column/invoice.py
+++ account_invoice_tax_by_column/invoice.py
@@ -26,25 +26,30 @@ from osv import fields, osv
 class account_invoice_tax(osv.osv):
     _inherit = "account.invoice.tax"
 
-    def compute_taxes_by_rate(self, cr, uid, lines=[], precision=2):
-        # lines has the form [{'price_unit': 100, 'discount': 0, 'quantity': 1, 'taxes': [account.tax]}]
+    def compute_taxes_by_rate(self, cr, uid, lines=[], precision=2, address_id=None, partner=None):
+
+        # lines has the form
+        # [{'price_unit': 100, 'discount': 0, 'quantity': 1, 'taxes': [account.tax], 'product': product.product}]
         # returns a dictionary like {0.2: 20}
 
+        tax_obj = self.pool.get('account.tax')
         tax_by_rate = {}
         # collect the base amount grouped by tax rate
         for line in lines:
             for tax in line['taxes']:
-                # TODO manage include_base_amount, applicable taxes and multi currency
-                if not tax_by_rate.get(tax['amount'], False):
-                    tax_by_rate[tax['amount']] = 0
-                price_unit=line['price_unit']
-                if tax['price_include']:
-                    price_unit = price_unit / (1 + tax['amount'])
-                tax_by_rate[tax['amount']] += round((price_unit* (1-(line['discount'] or 0.0)/100.0))
-                    * line['quantity'], precision)
-        # compute the tax amount grouped by rate
-        for rate in tax_by_rate:
-            tax_by_rate[rate] = round(rate * tax_by_rate[rate], precision)
+                # TODO manage multi currency
+                if not tax_by_rate.get(tax['id'], False):
+                    tax_by_rate[tax['id']] = {'base_amount': 0.0, 'product': None}
+                tax_by_rate[tax['id']]['base_amount'] += (line['price_unit'] * (1-(line['discount'] or 0.0)/100.0)) \
+                    * line['quantity']
+                if line.get('product', False):
+                    tax_by_rate[tax['id']]['product']  = line['product']
+        # compute the tax amount grouped by tax
+        for tax_id in tax_by_rate:
+            tax = tax_obj.browse(cr, uid, tax_id)
+            total = tax_obj.compute_all(cr, uid, [tax], tax_by_rate[tax_id]['base_amount'], 1,
+                address_id=address_id, product=tax_by_rate[tax_id]['product'], partner=partner)
+            tax_by_rate[tax_id] = total['total_included'] - total['total']
 
         return tax_by_rate
 
@@ -64,41 +69,44 @@ class account_invoice_tax(osv.osv):
 
             lines = []
             for line in inv.invoice_line:
-                line_dic = {'price_unit': line.price_unit, 'discount': line.discount, 'quantity': line.quantity, 'taxes': []}
+                line_dic = {'price_unit': line.price_unit, 'discount': line.discount, 'quantity': line.quantity,
+                    'taxes': [], 'product': line.product_id}
                 for tax in line.invoice_line_tax_id:
                     line_dic['taxes'].append(tax)
                 lines.append(line_dic)
 
-            tax_by_rate = self.compute_taxes_by_rate(cr, uid, lines=lines, precision=precision)
+            tax_by_rate = self.compute_taxes_by_rate(cr, uid, lines=lines, precision=precision,
+                address_id=inv.address_invoice_id.id, partner=inv.partner_id)
 
             # compute the tax amount of tax_grouped (old wrong amount), grouped by tax rate
             wrong_tax_by_rate = {}
             for inv_tax in tax_grouped.values():
-                if not wrong_tax_by_rate.get(inv_tax['tax_rate'], False):
-                    wrong_tax_by_rate[inv_tax['tax_rate']] = 0
-                wrong_tax_by_rate[inv_tax['tax_rate']] += inv_tax['tax_amount']
+                if not wrong_tax_by_rate.get(inv_tax['tax_id'], False):
+                    wrong_tax_by_rate[inv_tax['tax_id']] = 0
+                wrong_tax_by_rate[inv_tax['tax_id']] += inv_tax['tax_amount']
 
             # compute the difference between correct and wrong amount
             difference_by_rate = {}
-            for rate in tax_by_rate:
-                difference_by_rate[rate] = tax_by_rate[rate] - wrong_tax_by_rate[rate]
+            for tax_id in tax_by_rate:
+                difference_by_rate[tax_id] = tax_by_rate[tax_id] - wrong_tax_by_rate[tax_id]
 
-            for rate in difference_by_rate:
+            for tax_id in difference_by_rate:
+                tax = tax_obj.browse(cr, uid, tax_id)
                 value_set = False
                 # first try to add difference to non deductible tax
                 for inv_tax in tax_grouped.values():
-                    if inv_tax['tax_rate'] == rate:
+                    if inv_tax['tax_rate'] == tax.amount:
                         if inv_tax['base_code_id'] == False:
-                            inv_tax['tax_amount'] =  inv_tax['tax_amount'] + difference_by_rate[rate]
-                            inv_tax['amount'] =  inv_tax['amount'] + difference_by_rate[rate]
+                            inv_tax['tax_amount'] =  inv_tax['tax_amount'] + difference_by_rate[tax_id]
+                            inv_tax['amount'] =  inv_tax['amount'] + difference_by_rate[tax_id]
                             value_set = True
                             break
                 # else add it to the normal tax
                 if not value_set:
                     for inv_tax in tax_grouped.values():
-                        if inv_tax['tax_rate'] == rate:
-                            inv_tax['tax_amount'] =  inv_tax['tax_amount'] + difference_by_rate[rate]
-                            inv_tax['amount'] =  inv_tax['amount'] + difference_by_rate[rate]
+                        if inv_tax['tax_rate'] == tax.amount:
+                            inv_tax['tax_amount'] =  inv_tax['tax_amount'] + difference_by_rate[tax_id]
+                            inv_tax['amount'] =  inv_tax['amount'] + difference_by_rate[tax_id]
                             value_set = True
                             break
 
