ced7e6026813a6acf06b2ce15c927af634f80033
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2012-05-21 10:20:57 +0200

    [ADD] l10n_it_ricevute_bancarie moving from https://code.launchpad.net/~openobject-italia-core-devs/openobject-italia/l10n_it_ricevute_bancarie

---
+++ l10n_it_ricevute_bancarie/account/__init__.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Addon realizzata da Andrea Cometa
+#    Compatible with OpenERP release 6.x
+#    Copyright (C) 2012 Andrea Cometa. All Rights Reserved.
+#    Email: info@andreacometa.it
+#    Web site: http://www.andreacometa.it
+#
+##############################################################################
+
+import account
+
