9c1aea7ec8969f72004b8e04cce1b814e956e80b
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2012-06-06 08:37:00 +0200

    [IMP] copyright

--- l10n_it_ricevute_bancarie/account/__init__.py
+++ l10n_it_ricevute_bancarie/account/__init__.py
@@ -1,11 +1,23 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
-#
-#    Addon realizzata da Andrea Cometa
-#    Compatible with OpenERP release 6.x
-#    Copyright (C) 2012 Andrea Cometa. All Rights Reserved.
+#    
+#    Copyright (C) 2012 Andrea Cometa.
 #    Email: info@andreacometa.it
 #    Web site: http://www.andreacometa.it
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
