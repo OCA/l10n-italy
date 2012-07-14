d506066a7a24ca2976193efc6f87e15d7f9111c6
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2012-07-14 09:50:34 +0200

    [REM] l10n_it_CEE_balance

--- l10n_it_CEE_balance/account.py
+++
@@ -1,8 +0,0 @@
-from osv import fields, osv
-
-class account_account(osv.osv):
-    _inherit = "account.account"
-    _columns =  {
-        'parent_consol_ids': fields.many2many('account.account', 'account_account_consol_rel', 'parent_id', 'child_id', 'Consolidated Parents'),
-    }
-account_account()
