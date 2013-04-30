e18817a7a6c819d399f923602a7e0a62c0910d3c
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-04-30 15:15:03 +0200

    [FIX] wrong invoice date

--- l10n_it_vat_registries/invoice.py
+++ l10n_it_vat_registries/invoice.py
@@ -154,6 +154,7 @@ class Parser(report_sxw.rml_parse):
                         str_non_deductible or '',
                     'index': index,
                     'amount_total': invoice_amount_total,
+                    'invoice_date': move_line.invoice and move_line.invoice.date_invoice or '',
                     }
                 res.append(tax_item)
                 iva = cur_pool.round(
