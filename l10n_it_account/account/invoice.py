c99f3f92dbfb2891cd321174d9ddf1b92f2ac676
unknown <sergiocorato@gmail.com>
2013-09-05 22:23:28 +0200

    [FIX] little bugs

--- l10n_it_account/account/invoice.py
+++ l10n_it_account/account/invoice.py
@@ -28,6 +28,9 @@ from tools.translate import _
 class account_invoice(osv.osv):
     
     _inherit = 'account.invoice'
+    _columns = {
+        'supplier_invoice_number': fields.char('Supplier invoice nr', size=16),
+        }
 
     def action_number(self, cr, uid, ids, context=None):
         super(account_invoice, self).action_number(cr, uid, ids, context)
@@ -38,14 +41,50 @@ class account_invoice(osv.osv):
             number = obj_inv.number
             date_invoice = obj_inv.date_invoice
             journal = obj_inv.journal_id.id
+            date_start = obj_inv.registration_date or obj_inv.date_invoice or time.strftime('%Y-%m-%d')
+            date_stop = obj_inv.registration_date or obj_inv.date_invoice or time.strftime('%Y-%m-%d')
+            period_ids = self.pool.get('account.period').search(
+                cr, uid, [('date_start','<=',date_start),('date_stop','>=',date_stop), 
+                ('company_id', '=', obj_inv.company_id.id)])
             res = self.search(cr, uid, [('type','=',inv_type),('date_invoice','>',date_invoice), 
-                ('number', '<', number), ('journal_id','=',journal)])
+                ('number', '<', number), ('journal_id','=',journal),('period_id','in',period_ids)])
             if res:
                 raise osv.except_osv(_('Date Inconsistency'),
                         _('Cannot create invoice! Post the invoice with a greater date'))
         return True
-    
-account_invoice()
+
+    def action_move_create(self, cr, uid, ids, context=None):
+        super(account_invoice,self).action_move_create(cr, uid, ids, context=context)
+        for inv in self.browse(cr, uid, ids):
+            date_invoice=inv.date_invoice
+            reg_date = inv.registration_date
+
+            #periodo
+            date_start = inv.registration_date or inv.date_invoice or time.strftime('%Y-%m-%d')
+            date_stop = inv.registration_date or inv.date_invoice or time.strftime('%Y-%m-%d')
+            period_ids = self.pool.get('account.period').search(
+                cr, uid, [('date_start','<=',date_start),('date_stop','>=',date_stop), 
+                ('company_id', '=', inv.company_id.id)])
+
+            inv_type = inv.type
+            journal = inv.journal_id.id
+            res = self.search(cr, uid, [('type','=',inv_type),('registration_date','>',reg_date), 
+                ('journal_id','=',journal),('period_id','in',period_ids)], context=context)
+            if res:
+                raise orm.except_orm(_('Date Inconsistency'),
+                    _('Cannot create invoice! Post the invoice with a greater date'))
+
+            #check duplication (only supplier's invoices)
+            if inv_type == 'in_invoice' or inv_type == 'in_refund':
+                supplier_invoice_number = inv.supplier_invoice_number
+                partner_id = inv.partner_id.id
+                res = self.search(cr, uid, [('type','=',inv_type),('date_invoice','=',date_invoice), 
+                    ('journal_id','=',journal),('supplier_invoice_number','=',supplier_invoice_number),
+                    ('partner_id','=',partner_id),('state','not in',('draft','cancel'))], context=context)
+                if res:
+                    raise osv.except_osv(_('Invoice Duplication'),
+                        _('Invoice already posted!'))
+        return True
 
 
 
