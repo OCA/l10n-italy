2f468b692063f6376715d4a4907a244ad688fa09
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-12-16 16:22:58 +0100

    [REM] reverting PEP8 improvements. Moving to lp:~agilebg/openobject-italia/riba_v7_style

--- l10n_it_ricevute_bancarie/__openerp__.py
+++ l10n_it_ricevute_bancarie/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#
+#    
 #    Copyright (C) 2012 Andrea Cometa.
 #    Email: info@andreacometa.it
 #    Web site: http://www.andreacometa.it
@@ -33,45 +33,23 @@
     "description": """
 Gestione delle Ricevute Bancarie
 --------------------------------
-Nella configurazione delle Ri.Ba. è possibile specificare se si tratti di
-'salvo buon fine' o 'al dopo incasso'. I due tipi di Ri.Ba. hanno un flusso
-completamente diverso. In caso di 'al dopo incasso', nessuna registrazione
-verrà effettuata automaticamente e le fatture risulteranno pagate solo al
-momento dell'effettivo incasso.
+Nella configurazione delle Ri.Ba. è possibile specificare se si tratti di 'salvo buon fine' o 'al dopo incasso'. I due tipi di Ri.Ba. hanno un flusso completamente diverso. In caso di 'al dopo incasso', nessuna registrazione verrà effettuata automaticamente e le fatture risulteranno pagate solo al momento dell'effettivo incasso.
 
-E' possibile specificare diverse configurazioni (dal menù
-configurazioni -> varie -> Ri.Ba.). Per ognuna, in caso di 'salvo buon fine',
-è necessario specificare almeno il sezionale ed il conto da utilizzare al
-momento dell'accettazione della distinta da parte della banca.
-La configurazione relativa alla fase di accredito, verrà usata nel momento in
-cui la banca accredita l'importo della distinta. Mentre quella relativa
-all'insoluto verrà utilizzato in caso di mancato pagamento da parte del
-cliente.
+E' possibile specificare diverse configurazioni (dal menù configurazioni -> varie -> Ri.Ba.). Per ognuna, in caso di 'salvo buon fine', è necessario specificare almeno il sezionale ed il conto da utilizzare al momento dell'accettazione della distinta da parte della banca.
+La configurazione relativa alla fase di accredito, verrà usata nel momento in cui la banca accredita l'importo della distinta. Mentre quella relativa all'insoluto verrà utilizzato in caso di mancato pagamento da parte del cliente.
 
-Per utilizzare il meccanismo delle Ri.Ba. è necessario configurare un termine
-di pagamento di tipo 'Ri.Ba.'.
+Per utilizzare il meccanismo delle Ri.Ba. è necessario configurare un termine di pagamento di tipo 'Ri.Ba.'.
 
-Per emettere una distinta bisogna andare su Ri.Ba. -> emetti Ri.Ba. e
-selezionare i pagamenti per i quali emettere la distinta.
-Se per il cliente è stato abilitato il raggruppo, i pagamenti dello stesso
-cliente e con la stessa data di scadenza andranno a costituire un solo elemento
-della distinta.
+Per emettere una distinta bisogna andare su Ri.Ba. -> emetti Ri.Ba. e selezionare i pagamenti per i quali emettere la distinta.
+Se per il cliente è stato abilitato il raggruppo, i pagamenti dello stesso cliente e con la stessa data di scadenza andranno a costituire un solo elemento della distinta.
 
-I possibili stati della distinta sono: bozza, accettata, accreditata, pagata,
-insoluta, annullata.
-Ad ogni passaggio di stato sarà possibile generare le relative registrazioni
-contabili, le quali verranno riepilogate nel tab 'contabilità'.
-Questo tab è presente sia sulla distinta che sulle sue righe.
+I possibili stati della distinta sono: bozza, accettata, accreditata, pagata, insoluta, annullata.
+Ad ogni passaggio di stato sarà possibile generare le relative registrazioni contabili, le quali verranno riepilogate nel tab 'contabilità'. Questo tab è presente sia sulla distinta che sulle sue righe.
 
-Qui
-https://docs.google.com/document/d/1xCqeTcY6CF-Dgk_Avthhy7iwg_aG86WzNv3E_HHQkt4/edit#
-abbiamo un esempio delle tipiche registrazioni generate da un flusso
-'salvo buon fine'.
-""",
+Qui https://docs.google.com/document/d/1xCqeTcY6CF-Dgk_Avthhy7iwg_aG86WzNv3E_HHQkt4/edit# abbiamo un esempio delle tipiche registrazioni generate da un flusso 'salvo buon fine'.
+    """,
     'images': [],
-    'depends': [
-        'account', 'account_voucher', 'l10n_it_fiscalcode', 'account_due_list'
-        ],
+    'depends': ['account','account_voucher', 'l10n_it_fiscalcode', 'account_due_list'],
     'init_xml': [],
     'data': [
         "partner/partner_view.xml",
