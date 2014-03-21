111a1585d8948edbd6a53f6ddfd7e8262ff411b9
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-03-21 18:28:48 +0100

    [FIX] l10n_it_partially_deductible_vat - wrong test

--- l10n_it_partially_deductible_vat/__openerp__.py
+++ l10n_it_partially_deductible_vat/__openerp__.py
@@ -2,9 +2,10 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2011
+#    Copyright (C) 2011-2014
 #    Associazione OpenERP Italia (<http://www.openerp-italia.org>)
-#    Copyright (C) 2012 Agile Business Group sagl (<http://www.agilebg.com>)
+#    Copyright (C) 2012-2014 Agile Business Group sagl
+#    (<http://www.agilebg.com>)
 #    Copyright (C) 2012 Domsense srl (<http://www.domsense.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
