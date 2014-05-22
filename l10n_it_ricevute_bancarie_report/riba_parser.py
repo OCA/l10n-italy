5aa8b0c22834126b64118c73445b77d34224e7fa
Andrea Cometa <info@andreacometa.it>
2014-05-22 11:51:06 +0200

    [imp] ri.ba. report some fixes

--- l10n_it_ricevute_bancarie_report/riba_parser.py
+++ l10n_it_ricevute_bancarie_report/riba_parser.py
@@ -41,17 +41,6 @@ class report_riba_webkit(report_sxw.rml_parse):
         print res
         return res
 
-    def page_number(self, lines, row_limit):
-        if not lines or not row_limit:
-            return 0
-        division = float(len(lines)) / float(row_limit)
-        pn = int(ceil(division))
-        return pn
-
-    def newlined_text(self, text):
-        if text:
-            return text.replace('\n', '<br/>')
-
     def __init__(self, cr, uid, name, context):
         super(report_riba_webkit, self).__init__(cr, uid, name,
                                                  context=context)
@@ -61,8 +50,6 @@ class report_riba_webkit(report_sxw.rml_parse):
             'cr': cr,
             'uid': uid,
             'group_riba_by_date': self.group_riba_by_date,
-            'newlined_text': self.newlined_text,
-            'page_number': self.page_number,
         })
 
 report_sxw.report_sxw(
