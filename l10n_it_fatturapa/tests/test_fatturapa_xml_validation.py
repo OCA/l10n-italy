fa109650560ba5d32b5fab64a99e1d4b05f2aebc
Davide Corio <davide.corio@lsweb.it>
2014-07-23 13:35:49 +0200

    [FIX] copyright

--- l10n_it_fatturapa/tests/test_fatturapa_xml_validation.py
+++ l10n_it_fatturapa/tests/test_fatturapa_xml_validation.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2014 L.S. Advanced Software (<http://www.lsweb.it>)
+#    Copyright (C) 2014 Davide Corio <davide.corio@lsweb.it>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
