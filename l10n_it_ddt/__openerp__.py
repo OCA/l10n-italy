b2521c5565f6b3e5de6e76f00787fc49c98df17b
Davide Corio <enlightx@gmail.com>
2014-08-09 09:14:17 +0200

    [ADD] l10n_it_ddt

---
+++ l10n_it_ddt/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Davide Corio <davide.corio@lsweb.it>
+#    Copyright (C) 2014 L.S. Advanced Software srl
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'DdT',
+    'version': '1.0',
+    'category': 'Localization/Italy',
+    'summary': 'Documento di Trasporto',
+    'description': """
+Customizations needed to manage the italian DdT (delivery order).
+
+Class, method and fields name are the same of l10n_it_sale, in order
+to guarantee compatibility.
+
+    """,
+    'author': 'L.S. Advanced Software',
+    'website': 'http://www.lsweb.it',
+    'depends': ['account', 'base', 'stock', 'stock_account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/stock_data.xml',
+        'views/account_view.xml',
+        'views/partner_view.xml',
+        'views/stock_view.xml',
+    ],
+    'installable': True,
+    'active': False,
+}
---
+++ l10n_it_ddt/account.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Davide Corio <davide.corio@lsweb.it>
+#    Copyright (C) 2014 L.S. Advanced Software srl
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+
+
+class account_invoice(orm.Model):
+    _inherit = 'account.invoice'
+
+    _columns = {
+        'carriage_condition_id': fields.many2one(
+            'stock.picking.carriage_condition', 'Carriage Condition'),
+        'goods_description_id': fields.many2one(
+            'stock.picking.goods_description', 'Description of Goods'),
+        'transportation_reason_id': fields.many2one(
+            'stock.picking.transportation_reason',
+            'Reason for Transportation'),
+        'transportation_method_id': fields.many2one(
+            'stock.picking.transportation_method',
+            'Method of Transportation'),
+        }
+
+    def onchange_partner_id(
+            self, cr, uid, ids, type, partner_id, date_invoice=False,
+            payment_term=False, partner_bank_id=False, company_id=False):
+        result = super(account_invoice, self).onchange_partner_id(
+            cr, uid, ids, type, partner_id, date_invoice, payment_term,
+            partner_bank_id, company_id)
+        if partner_id:
+            partner = self.pool.get('res.partner').browse(cr, uid, partner_id)
+            result['value'][
+                'carriage_condition_id'] = partner.carriage_condition_id.id
+            result['value'][
+                'goods_description_id'] = partner.goods_description_id.id
+            result['value'][
+                'transportation_reason_id'
+                ] = partner.transportation_reason_id.id
+            result['value'][
+                'transportation_method_id'
+                ] = partner.transportation_method_id.id
+        return result
