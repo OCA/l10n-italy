c01574e993c5009c59b79381f460ea04f9ee5ea2
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-09-16 16:52:24 +0200

    [FIX] useless file and copyright

--- l10n_it_pec/model/__init__.py
+++ l10n_it_pec/model/__init__.py
@@ -1,11 +1,8 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2010 OpenERP Italian Community
+#    Copyright (C) 2014 Associazione Odoo Italia
 #    (<http://www.openerp-italia.org>).
-#
-#    All Rights Reserved
-#    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
