6c964d23cea9cbc38db02b6318aaf27f3d1b55c4
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-10-18 12:50:53 +0200

    [FIX] line length

--- account_vat_period_end_statement/report/vat_period_end_statement.py
+++ account_vat_period_end_statement/report/vat_period_end_statement.py
@@ -80,7 +80,9 @@ class print_vat_period_end_statement(report_sxw.rml_parse):
                     child_code, res=res, context=context)
         return res
 
-    def _get_tax_codes_amounts(self, period_id, tax_code_ids=None, context=None):
+    def _get_tax_codes_amounts(
+        self, period_id, tax_code_ids=None, context=None
+    ):
         if tax_code_ids is None:
             tax_code_ids = []
         if context is None:
