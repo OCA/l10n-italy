4e0af742775a830ccb881723c38a361b0d27a29d
Francesco Apruzzese <cescoap@gmail.com>
2014-10-28 17:59:35 +0100

    [IMP] stock, sale and account view [FIX] On change for partner in invoice class [ADD] Number of packages in stock, invoice and sale

--- l10n_it_ddt/models/account.py
+++ l10n_it_ddt/models/account.py
@@ -38,14 +38,16 @@ class account_invoice(orm.Model):
         'transportation_method_id': fields.many2one(
             'stock.picking.transportation_method',
             'Method of Transportation'),
+        'number_of_packages': fields.integer('Number of Packages'),
         }
 
     def onchange_partner_id(
             self, cr, uid, ids, type, partner_id, date_invoice=False,
-            payment_term=False, partner_bank_id=False, company_id=False):
+            payment_term=False, partner_bank_id=False, company_id=False,
+            context=None):
         result = super(account_invoice, self).onchange_partner_id(
             cr, uid, ids, type, partner_id, date_invoice, payment_term,
-            partner_bank_id, company_id)
+            partner_bank_id, company_id, context)
         if partner_id:
             partner = self.pool.get('res.partner').browse(cr, uid, partner_id)
             result['value'][
--- l10n_it_ddt/models/stock.py
+++ l10n_it_ddt/models/stock.py
@@ -90,6 +90,7 @@ class StockDdT(models.Model):
     transportation_method_id = fields.Many2one(
         'stock.picking.transportation_method',
         'Method of Transportation')
+    number_of_packages = fields.Integer()
     state = fields.Selection(
         [('draft', 'Draft'),
          ('confirmed', 'Confirmed'),
@@ -178,6 +179,7 @@ class StockPicking(models.Model):
     transportation_method_id = fields.Many2one(
         'stock.picking.transportation_method',
         string='Method of Transportation')
+    number_of_packages = fields.Integer()
     ddt_id = fields.Many2one('stock.ddt', string='DdT', readonly=True)
     ddt_type = fields.Selection(
         string="DdT Type", related='picking_type_id.code')
@@ -204,5 +206,6 @@ class StockPicking(models.Model):
                 'transportation_method_id':
                 picking.transportation_method_id and
                 picking.transportation_method_id.id,
+                'number_of_packages': picking.number_of_packages,
                 })
         return res
