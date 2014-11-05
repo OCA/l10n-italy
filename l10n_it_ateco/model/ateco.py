978d36499465cc16e1c3093cfeb930f8527427cc
Giorgio Borelli <casaborelli@gmail.com>
2014-11-05 15:11:30 +0100

    fix abstract.it web site

--- l10n_it_ateco/model/ateco.py
+++ l10n_it_ateco/model/ateco.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Copyright (C) 2014 Abstract
-#    (<http://abstrat.it).
+#    (<http://abstract.it>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
