c1a4b6f0c3c0bdfb4301d0376a5fcf67e855a1cb
Andrea Cometa <info@andreacometa.it>
2014-12-02 16:52:31 +0100

    [fiscalcode] join e rejoin
    [fiscalcode] remove useless join
    
    [fiscalcode] change website to www.odoo-italia.org
    
    [fiscalcode] added codicefiscale dipendence
    
    [fiscalcode] added codicefiscale dependence
    
    [fiscalcode] default fields new way(tm)
    
    [fiscalcode] default fields new way(tm)

--- l10n_it_fiscalcode/wizard/__init__.py
+++ l10n_it_fiscalcode/wizard/__init__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Copyright (C) 2014 Associazione Odoo Italia
-#    (<http://www.openerp-italia.org>).
+#    (<http://www.odoo-italia.org>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
