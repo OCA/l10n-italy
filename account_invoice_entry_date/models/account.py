e942edadd292d22a18058766dbdda094a05acb4b
Davide Corio <davide.corio@abstract.it>
2014-12-03 17:09:01 +0100

    [FIX] pep8

--- account_invoice_entry_date/models/account.py
+++ account_invoice_entry_date/models/account.py
@@ -59,8 +59,8 @@ class AccountInvoice(models.Model):
 
             if date_invoice and reg_date:
                 if (date_invoice > reg_date):
-                    raise Warning(_("The invoice date cannot be later than the "
-                                    "date of registration!"))
+                    raise Warning(_("The invoice date cannot be later than the"
+                                    " date of registration!"))
 
             date_start = inv.registration_date or inv.date_invoice \
                 or time.strftime('%Y-%m-%d')
