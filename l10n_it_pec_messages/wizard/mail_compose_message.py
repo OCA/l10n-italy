cf470588c3905bb1b9b410bc1997b1b2929a981f
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-12-15 18:11:47 +0100

    [FIX] right use of force_email

--- l10n_it_pec_messages/wizard/mail_compose_message.py
+++ l10n_it_pec_messages/wizard/mail_compose_message.py
@@ -23,30 +23,41 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
 
-from openerp import tools
 from openerp.osv import osv, fields
+from openerp.tools.translate import _
+
 
 class mail_compose_message(osv.TransientModel):
     _inherit = 'mail.compose.message'
 
     def _get_def_server(self, cr, uid, context=None):
         res = self.pool.get('fetchmail.server').search(
-            cr, uid, [('user_ids','in',uid)], context=context)
+            cr, uid, [('user_ids', 'in', uid)], context=context)
         return res and res[0] or False
 
     _columns = {
-        'server_id': fields.many2one('fetchmail.server', 'Server', required=True),
+        'server_id': fields.many2one(
+            'fetchmail.server', 'Server', required=True),
     }
     _defaults = {
         'server_id': _get_def_server
     }
 
     def send_mail(self, cr, uid, ids, context=None):
-        """ Override of send_mail to duplicate attachments linked to the email.template.
-            Indeed, basic mail.compose.message wizard duplicates attachments in mass
-            mailing mode. But in 'single post' mode, attachments of an email template
+        """ Override of send_mail to duplicate attachments linked to the
+        email.template.
+            Indeed, basic mail.compose.message wizard duplicates attachments
+            in mass
+            mailing mode. But in 'single post' mode, attachments of an
+            email template
             also have to be duplicated to avoid changing their ownership. """
         for wizard in self.browse(cr, uid, ids, context=context):
-            if  context.get('new_pec_mail') :
-                context['new_pec_server_id']=wizard.server_id
-        return super(mail_compose_message, self).send_mail(cr, uid, ids, context=context)
+            if context.get('new_pec_mail'):
+                context['new_pec_server_id'] = wizard.server_id
+                for partner in wizard.partner_ids:
+                    if not partner.pec_mail:
+                        raise osv.except_osv(
+                            _('Error'),
+                            _('No PEC mail for partner %s') % partner.name)
+        return super(mail_compose_message, self).send_mail(
+            cr, uid, ids, context=context)
