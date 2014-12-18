e2218e70b394ba32ab2a13b25c9b2a59552215fd
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-12-18 12:53:20 +0100

    [IMP] copyright

--- l10n_it_vat_registries/__init__.py
+++ l10n_it_vat_registries/__init__.py
@@ -3,6 +3,8 @@
 #
 #    Copyright (C) 2011-2013 Associazione OpenERP Italia
 #    (<http://www.openerp-italia.org>).
+#    Copyright (C) 2014 Agile Business Group sagl
+#    (<http://www.agilebg.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
