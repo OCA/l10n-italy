5cca1f068f31b79f60f1512bae4ba4ca9c91b126
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2015-01-28 10:19:41 +0100

    [ADD] copyright

--- l10n_it_ddt/wizard/ddt_create_invoice.py
+++ l10n_it_ddt/wizard/ddt_create_invoice.py
@@ -2,6 +2,7 @@
 ##############################################################################
 #
 #    Copyright (C) 2014 Abstract (http://www.abstract.it)
+#    Copyright (C) 2014 Agile Business Group (http://www.agilebg.com)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
