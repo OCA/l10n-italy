cefcbab52e16a8718b2ae3cde7b9cab8d7c5ca40
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2015-02-10 08:54:39 +0100

    [FIX] license

--- __unported__/l10n_it_DDT_webkit/__openerp__.py
+++ __unported__/l10n_it_DDT_webkit/__openerp__.py
@@ -8,16 +8,16 @@
 #    @author Lorenzo Battistini
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
@@ -31,7 +31,7 @@ This module adds the webkit DDT report
     """,
     'author': 'Agile Business Group',
     'website': 'http://www.agilebg.com',
-    'license': 'GPL-3 or any later version',
+    'license': 'AGPL-3',
     'depends': ['report_webkit', 'l10n_it_sale', 'base_headers_webkit'],
     'data': ['report.xml'],
     'installable': False,
