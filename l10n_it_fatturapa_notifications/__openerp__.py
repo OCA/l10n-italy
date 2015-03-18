06d988b3865a0f123255cad8243c6bb978fa7ed5
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2015-03-18 19:03:43 +0100

    [IMP] description

--- l10n_it_fatturapa_notifications/__openerp__.py
+++ l10n_it_fatturapa_notifications/__openerp__.py
@@ -39,7 +39,9 @@ Usage
 =====
 
 The module adds 'fatturapa.notification' model and exposes 'parse_xml' method
-to be called by other modules
+to be called by other modules.
+It also adds the 'Import notification' wizard that allows to load XML
+notification files and link them to fatturaPA
 
 
 Credits
