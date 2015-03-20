5df843d7aa70ee92dbe1c4896bb5c2415643cfc5
hurrinico <nicola.malcontenti@agilebg.com>
2015-03-20 10:54:56 +0100

    [FIX] add unidecode

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -22,7 +22,7 @@
 import base64
 import re
 import tempfile
-from unicodedata import normalize
+from unidecode import unidecode
 from pyxb.exceptions_ import SimpleFacetValueError
 from openerp.osv import orm
 from openerp import addons
@@ -600,7 +600,7 @@ class WizardExportFatturapa(orm.TransientModel):
                 PrezzoUnitario='%.2f' % line.price_unit,
                 Quantita='%.2f' % line.quantity,
                 UnitaMisura=line.uos_id and (
-                    normalize('NFD', line.uos_id.name)[0]) or None,
+                    unidecode(line.uos_id.name)) or None,
                 PrezzoTotale='%.2f' % line.price_subtotal,
                 AliquotaIVA='%.2f' % (
                     line.invoice_line_tax_id[0].amount*100)
