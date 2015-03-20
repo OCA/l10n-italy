aa9752ca184e49b67cc726bd6d32af289b1569db
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2015-03-20 16:16:09 +0100

    [FIX] module description

--- l10n_it_ipa/__openerp__.py
+++ l10n_it_ipa/__openerp__.py
@@ -27,6 +27,40 @@
     "category": "Localisation/Italy",
     "author": "KTec S.r.l, Odoo Community Association (OCA)",
     "website": "http://www.ktec.it",
+    "description": """
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License
+
+IPA Code (IndicePA)
+===================
+
+This module adds IPA (IndicePA) field to partner
+http://www.indicepa.gov.it
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Luigi Di Naro <luigi.dinaro@ktec.it>
+* Alex Comba <alex.comba@agilebg.com>
+* Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose mission is to support the collaborative development of Odoo features and promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
+""",
     "license": "AGPL-3",
     "depends": ['base'],
     "data": [
