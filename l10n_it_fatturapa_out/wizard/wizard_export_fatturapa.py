36537f95f5d19c0c956747ac5e0bf493180895bd
hurrinico <nicola.malcontenti@agilebg.com>
2015-03-20 10:42:11 +0100

    [FIX] ADD normalize
    [FIX] unidecode instead of normalize
    
    [FIX] change toxml

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -23,6 +23,7 @@ import base64
 import re
 import tempfile
 from pyxb.exceptions_ import SimpleFacetValueError
+from unidecode import unidecode
 from openerp.osv import orm
 from openerp import addons
 from openerp.addons.l10n_it_fatturapa.bindings.fatturapa_v_1_1 import (
@@ -94,7 +95,7 @@ class WizardExportFatturapa(orm.TransientModel):
         attach_vals = {
             'name': '%s_%s.xml' % (company.vat, str(number)),
             'datas_fname': '%s_%s.xml' % (company.vat, str(number)),
-            'datas': base64.encodestring(self.fatturapa.toxml("utf-8")),
+            'datas': base64.encodestring(self.fatturapa.toxml("latin1")),
         }
         attach_id = attach_obj.create(cr, uid, attach_vals, context=context)
 
@@ -594,7 +595,8 @@ class WizardExportFatturapa(orm.TransientModel):
                 Descrizione=line.name,
                 PrezzoUnitario='%.2f' % line.price_unit,
                 Quantita='%.2f' % line.quantity,
-                UnitaMisura=line.uos_id and line.uos_id.name or None,
+                UnitaMisura=line.uos_id and (
+                    unidecode(line.uos_id.name)) or None,
                 PrezzoTotale='%.2f' % line.price_subtotal,
                 AliquotaIVA='%.2f' % (
                     line.invoice_line_tax_id[0].amount*100)
