607696e03c5b62c555ddaa6108ba2bdbd9cac7b6
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2015-03-20 16:24:57 +0100

    [FIX] description and deps

--- l10n_it_fatturapa/__openerp__.py
+++ l10n_it_fatturapa/__openerp__.py
@@ -25,9 +25,59 @@
     'summary': 'Electronic invoices',
     'author': 'Davide Corio, Agile Business Group, Innoviu',
     'website': 'http://www.odoo-italia.org',
+    'description': """
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License
+
+Italian Localization - FatturaPA
+================================
+
+Base module to handle FatturaPA data.
+See l10n_it_fatturapa_out and l10n_it_fatturapa_in.
+
+
+Configuration
+=============
+
+ * Edit the FatturaPA fields of the partners who will send/receive the
+   electronic invoices
+ * Configure payment terms filling the fatturaPA fields related to payment
+   terms and payment methods
+ * Configure FatturaPA data in Accounting Configuration
+
+Credits
+=======
+
+Contributors
+------------
+
+* Davide Corio <davide.corio@abstract.it>
+* Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+* Roberto Onnis <roberto.onnis@innoviu.com>
+
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
+""",
     'license': 'AGPL-3',
     "depends": [
-        'account', 'l10n_it_base', 'l10n_it_fiscalcode', 'document'],
+        'account',
+        'l10n_it_base',
+        'l10n_it_fiscalcode',
+        'document',
+        'l10n_it_ipa',
+        ],
     "data": [
         'data/fatturapa_data.xml',
         'views/account_view.xml',
