693b50a6115569b96e99029477f3b49ce77753d8
hurrinico <nicola.malcontenti@agilebg.com>
2015-03-20 15:11:57 +0100

    [FIX] apply various commit

--- l10n_it_fatturapa/models/account_tax.py
+++ l10n_it_fatturapa/models/account_tax.py
@@ -24,12 +24,40 @@ from openerp.osv import fields, orm
 class AccountTax(orm.Model):
     _inherit = 'account.tax'
     _columns = {
-        'natura': fields.selection([
+        'non_taxable_nature': fields.selection([
             ('N1', 'escluse ex art. 15'),
             ('N2', 'non soggette'),
             ('N3', 'non imponibili'),
             ('N4', 'esenti'),
             ('N5', 'regime del margine'),
             ('N6', 'inversione contabile (reverse charge)'),
-            ], string="Natura"),
+            ], string="Non taxable nature"),
     }
+    def get_tax_by_invoice_tax(self, cr, uid, invoice_tax, context=None):
+        if ' - ' in invoice_tax:
+            tax_descr = invoice_tax.split(' - ')[0]
+            tax_ids = self.search(cr, uid, [
+                ('description', '=', tax_descr),
+                ], context=context)
+            if not tax_ids:
+                raise orm.except_orm(
+                    _('Error'), _('No tax %s found') %
+                    tax_descr)
+            if len(tax_ids) > 1:
+                raise orm.except_orm(
+                    _('Error'), _('Too many tax %s found') %
+                    tax_descr)
+        else:
+            tax_name = invoice_tax
+            tax_ids = self.search(cr, uid, [
+                ('name', '=', tax_name),
+                ], context=context)
+            if not tax_ids:
+                raise orm.except_orm(
+                    _('Error'), _('No tax %s found') %
+                    tax_name)
+            if len(tax_ids) > 1:
+                raise orm.except_orm(
+                    _('Error'), _('Too many tax %s found') %
+                    tax_name)
+        return tax_ids[0]
