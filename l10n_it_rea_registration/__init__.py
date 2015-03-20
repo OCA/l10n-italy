b5acdfc13f0baeb7a50499deb308cdd5eba8b75b
archetipo <alessio.gerace@gmail.com>
2015-03-20 12:31:23 +0100

    [FIX] mail address

--- l10n_it_rea_registration/__init__.py
+++ l10n_it_rea_registration/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2015 Alessio Gerace<alessio.gerace@agilebg.com>
+#    Copyright (C) 2015 Alessio Gerace <alessio.gerace@agilebg.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
