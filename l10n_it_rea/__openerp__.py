c1ba53e65ebbc6a2fbd898dbf34156839e7f2c2f
hurrinico <nicola.malcontenti@agilebg.com>
2015-03-24 17:36:48 +0100

    [FIX] rem descr from __openerp__

--- l10n_it_rea/__openerp__.py
+++ l10n_it_rea/__openerp__.py
@@ -24,46 +24,6 @@
     'summary': 'Manage fields for  Economic Administrative catalogue',
     'author': 'Agile Business Group, Odoo Community Association (OCA)',
     'website': 'http://www.agilebg.com',
-    'description': """
-.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
-    :alt: License
-
-REA Registration
-=======================
-
-The module implement fields of REA data in partner
-http://www.registroimprese.it/il-registro-imprese-e-altre-banche-dati#page=registro-imprese
-
-Usage
-=====
-
-The module adds fields in page Accounting of partner form, where you can
-add data of registry of businesses
-
-
-Credits
-=======
-
-Contributors
-------------
-
-* Alessio Gerace <alessio.gerace@agilebg.com>
-
-Maintainer
-----------
-
-.. image:: http://odoo-community.org/logo.png
-   :alt: Odoo Community Association
-   :target: http://odoo-community.org
-
-This module is maintained by the OCA.
-
-OCA, or the Odoo Community Association, is a nonprofit organization whose
-mission is to support the collaborative development of Odoo features and
-promote its widespread use.
-
-To contribute to this module, please visit http://odoo-community.org.
-""",
     'license': 'AGPL-3',
     "depends": [
         'l10n_it_base', 'account'
