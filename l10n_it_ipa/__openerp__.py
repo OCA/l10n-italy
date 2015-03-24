d70a8c017838a0a228e6bae82e2571b35cc85056
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2015-03-24 12:43:27 +0100

    [FIX] PEP8

--- l10n_it_ipa/__openerp__.py
+++ l10n_it_ipa/__openerp__.py
@@ -57,7 +57,9 @@ Maintainer
 
 This module is maintained by the OCA.
 
-OCA, or the Odoo Community Association, is a nonprofit organization whose mission is to support the collaborative development of Odoo features and promote its widespread use.
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
 
 To contribute to this module, please visit http://odoo-community.org.
 """,
