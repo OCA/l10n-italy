63b2ecd6b1b203d417cc8f0ccec56605b9f9a03f
Davide Corio <davide.corio@abstract.it>
2015-05-05 15:20:51 +0200

    [ADD] initial rev

---
+++ l10n_it_regions/models/res_region.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Abstract (http://www.abstract.it)
+#    @author Davide Corio <davide.corio@abstract.it>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class ResCountryRegion(models.Model):
+    _name = 'res.country.region'
+    _description = 'Region'
+
+    name = fields.Char('Name')
+    code = fields.Char('Code')
+
+
+class ResCountryState(models.Model):
+    _inherit = 'res.country.state'
+
+    region_id = fields.Many2one('res.country.region', 'Region')
