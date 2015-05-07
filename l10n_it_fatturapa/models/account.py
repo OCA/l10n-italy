f65e149c7801c5e422bfa81ea62ba906c59aec87
archetipo <alessio.gerace@gmail.com>
2015-05-07 16:25:16 +0200

    [IMP] demo data for Nature of tax with amount equal 0
    [FIX] DiscRisePriceLine
    
    [FIX] xml demo data
    [IMP] new xml demo data fatturaPA with attachment
    [IMP] new xml file to check export wizard
    [IMP] handle related attachment in wizard export FatturaPA
    [IMP] tests export with attachments
    [FIX] Flake8
    
    [IMP] handle fiscalyear for test
    
    [FIX] size length of cig and cup
    
    [FIX] pylint
    
    Conflicts:
    	l10n_it_fatturapa/bindings/fatturapa_v_1_1.py
    
    [FIX] test fails https://travis-ci.org/OCA/l10n-italy/jobs/63965642

--- l10n_it_fatturapa/models/account.py
+++ l10n_it_fatturapa/models/account.py
@@ -206,7 +206,7 @@ class discount_rise_price(orm.Model):
         'percentage': fields.float('Percentage'),
         'amount': fields.float('Amount'),
         'invoice_id': fields.many2one(
-            'account.invoice', 'Related Invoice',
+            'account.invoice.line', 'Related Invoice',
             ondelete='cascade', select=True
         ),
     }
@@ -239,8 +239,8 @@ class fatturapa_related_document_type(orm.Model):
         'date': fields.date('Date'),
         'numitem': fields.char('NumItem', size=20),
         'code': fields.char('Order Agreement Code', size=100),
-        'cig': fields.char('CIG Code', size=14),
-        'cup': fields.char('CUP Code', size=14),
+        'cig': fields.char('CIG Code', size=15),
+        'cup': fields.char('CUP Code', size=15),
     }
 
     def create(self, cr, uid, vals, context=None):
