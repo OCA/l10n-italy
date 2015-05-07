c4462f33ae00079336dd36d8b064507b9812c1da
archetipo <alessio.gerace@gmail.com>
2015-05-07 16:37:39 +0200

    [FIX] DiscRisePriceLine

--- l10n_it_fatturapa/models/account.py
+++ l10n_it_fatturapa/models/account.py
@@ -206,7 +206,7 @@ class discount_rise_price(orm.Model):
         'percentage': fields.float('Percentage'),
         'amount': fields.float('Amount'),
         'invoice_id': fields.many2one(
-            'account.invoice', 'Related Invoice',
+            'account.invoice.line', 'Related Invoice',
             ondelete='cascade', select=True
         ),
     }
