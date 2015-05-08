02f5d35d484fccf9ca79deafa773c78febd6b232
Alex Comba <alex.comba@agilebg.com>
2015-05-08 11:43:56 +0200

    Update copyright year for agilebg

--- account_vat_period_end_statement/report/__init__.py
+++ account_vat_period_end_statement/report/__init__.py
@@ -3,6 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2011 Domsense s.r.l. (<http://www.domsense.com>).
+#    Copyright (C) 2012-15 Agile Business Group sagl (<http://www.agilebg.com>)
 #    Copyright (C) 2015 Associazione Odoo Italia
 #    (<http://www.odoo-italia.org>).
 #
