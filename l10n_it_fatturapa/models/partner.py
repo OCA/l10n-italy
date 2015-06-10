82c89603e8fb1d3adfcf7229153c65dcd6929d38
archetipo <alessio.gerace@gmail.com>
2015-06-10 10:13:28 +0200

    [FIX] DataFatturaType init
    [FIX] PEP8
    
    Conflicts:
    	.travis.yml
    	l10n_it_fatturapa/bindings/fatturapa_v_1_1.py
    
    [FIX] FLAKE8
    
    [FIX] end FIX PEP8
    
    [FIX] Flake8
    
    Conflicts:
    	l10n_it_fatturapa/models/partner.py
    	l10n_it_fatturapa_in/models/account.py
    	l10n_it_fatturapa_in/tests/test_import_fatturapa_xml.py
    	l10n_it_fatturapa_in/wizard/wizard_import_fatturapa.py
    	l10n_it_fatturapa_out/__openerp__.py
    
    [FIX] handled RiferimenoAmministrazione 2.2.1.15 filed for in and out
    [IMP] tests for in and out
    
    Conflicts:
    	l10n_it_fatturapa_in/tests/data/IT05979361218_004.xml
    	l10n_it_fatturapa_in/tests/test_import_fatturapa_xml.py
    	l10n_it_fatturapa_in/wizard/wizard_import_fatturapa.py
    	l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
    
    [FIX] views
    [FIX] translation
    
    Conflicts:
    	l10n_it_fatturapa_in/views/account_view.xml
    
    [FIX] account_invoice_line view
    
    Conflicts:
    	l10n_it_fatturapa_in/views/account_view.xml

--- l10n_it_fatturapa/models/partner.py
+++ l10n_it_fatturapa/models/partner.py
@@ -27,18 +27,18 @@ class res_partner(orm.Model):
     _columns = {
         'eori_code': fields.char('EORI Code', size=20),
         'license_number': fields.char('License Code', size=20),
-        #1.2.6 RiferimentoAmministrazione
+        # 1.2.6 RiferimentoAmministrazione
         'pa_partner_code': fields.char('PA Code for partner', size=20),
-        #1.2.1.4
+        # 1.2.1.4
         'register': fields.char('Professional Register', size=60),
-        #1.2.1.5
+        # 1.2.1.5
         'register_province': fields.many2one(
             'res.country.state', string='Register Province'),
         #1.2.1.6
         'register_code': fields.char('Register Code', size=60),
-        #1.2.1.7
+        # 1.2.1.7
         'register_regdate': fields.date('Register Registration Date'),
-        #1.2.1.8
+        # 1.2.1.8
         'register_fiscalpos': fields.many2one(
             'fatturapa.fiscal_position',
             string="Register Fiscal Position"),
