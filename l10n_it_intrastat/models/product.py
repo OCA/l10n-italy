7f4c8a035da8cd4501c3eb5edbb60e2522d0b0fe
alessandrocamilli <alessandrocamilli@openforce.it>
2015-07-20 13:34:47 +0000

    Intrastat code on product category

--- l10n_it_intrastat/models/product.py
+++ l10n_it_intrastat/models/product.py
@@ -6,16 +6,5 @@ from openerp.exceptions import except_orm, Warning, RedirectWarning
 class product_category(models.Model):
     _inherit = 'product.category'
 
-    intrastat_code_good = fields.Many2one('account.intrastat.code.good',
-        string='INTRASTAT Code for goods')
-    intrastat_code_service = fields.Many2one('account.intrastat.code.service',
-        string='INTRASTAT Code for services')
-
-
-class product_template(models.Model):
-    _inherit = 'product.template'
-
-    intrastat_code_good = fields.Many2one('account.intrastat.code.good',
-        string='INTRASTAT Code for goods')
-    intrastat_code_service = fields.Many2one('account.intrastat.code.service',
-        string='INTRASTAT Code for services')
+    intrastat_id = fields.Many2one('report.intrastat.code',
+        string='Intrastat Code')
