42dca0a780da9105a212039c969122c5a822afa5
Francesco Apruzzese <cescoap@gmail.com>
2015-07-20 15:46:11 +0200

    [ADD] Copyright for product file

--- l10n_it_intrastat/models/product.py
+++ l10n_it_intrastat/models/product.py
@@ -1,10 +1,30 @@
-from openerp import models, fields, api, _
-import openerp.addons.decimal_precision as dp
-from openerp.exceptions import except_orm, Warning, RedirectWarning
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Apruzzese Francesco (f.apruzzese@apuliasoftware.it)
+#    Copyright (C) 2015
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from openerp import models, fields
 
 
 class product_category(models.Model):
     _inherit = 'product.category'
 
     intrastat_id = fields.Many2one('report.intrastat.code',
-        string='Intrastat Code')
+                                   string='Intrastat Code')
