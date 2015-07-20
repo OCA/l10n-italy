33e236ac1fe8a5239bad529933ec86bf49f553a3
Francesco Apruzzese <cescoap@gmail.com>
2015-07-20 18:04:34 +0200

    [FIX] import api in product

--- l10n_it_intrastat/models/product.py
+++ l10n_it_intrastat/models/product.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class product_category(models.Model):
