597f2d07a9fa46fa14e2cad487b815abbbd9935e
Francesco Apruzzese <cescoap@gmail.com>
2015-07-20 16:15:25 +0200

    [IMP] PEP8 refactor

--- l10n_it_intrastat/models/account.py
+++ l10n_it_intrastat/models/account.py
@@ -7,30 +7,26 @@ class account_fiscal_position(models.Model):
     _inherit = 'account.fiscal.position'
 
     intrastat = fields.Boolean(string="Subject to Intrastat")
-    intrastat_code_type = fields.Selection([
-            ('service', 'Service'),
-            ('good', 'Good'),
-            ], string='Code Type')
-    intrastat_move_type = fields.Selection([
-            ('sale', 'Sales'),
-            ('purchase', 'Purchases'),
-            ('refund_sale', 'Refund Sale'),
-            ('refund_purchase', 'Refund Purchase')
-            ], string='Move Type')
+    intrastat_code_type = fields.Selection([('service', 'Service'),
+                                            ('good', 'Good'),
+                                            ], string='Code Type')
+    intrastat_move_type = fields.Selection(
+        [('sale', 'Sales'), ('purchase', 'Purchases'),
+         ('refund_sale', 'Refund Sale'), ('refund_purchase', 'Refund Purchase')
+         ], string='Move Type')
 
 
 class account_invoice(models.Model):
     _inherit = "account.invoice"
 
     intrastat = fields.Boolean(string="Subject to Intrastat",
-        states={'draft': [('readonly', False)]})
-    intrastat_line_ids = fields.One2many('account.invoice.intrastat',
-             'invoice_id', string='Intrastat',
+                               states={'draft': [('readonly', False)]})
+    intrastat_line_ids = fields.One2many(
+        'account.invoice.intrastat', 'invoice_id', string='Intrastat',
         readonly=True, states={'draft': [('readonly', False)]}, copy=True)
 
     @api.multi
     def action_move_create(self):
-
         super(account_invoice, self).action_move_create()
         for inv in self:
             total_amount = 0
@@ -64,30 +60,30 @@ class account_invoice_intrastat(models.Model):
         ('good', 'Good')
         ], 'Code Type', required=True, default='good')
     intrastat_code_good = fields.Many2one('account.intrastat.code.good',
-        string='INTRASTAT Code for goods')
-    intrastat_code_service = fields.Many2one('account.intrastat.code.service',
-        string='INTRASTAT Code for services')
-    amount_euro = fields.Float(string='Amount Euro', compute='_compute_amount_euro',
+                                          string='INTRASTAT Code for goods')
+    intrastat_code_service = fields.Many2one(
+        'account.intrastat.code.service', string='INTRASTAT Code for services')
+    amount_euro = fields.Float(
+        string='Amount Euro', compute='_compute_amount_euro',
         digits=dp.get_precision('Account'), store=True, readonly=True)
-    amount_currency = fields.Float(string='Amount Currency',
-        digits=dp.get_precision('Account'))
+    amount_currency = fields.Float(
+        string='Amount Currency', digits=dp.get_precision('Account'))
     transation_nature = fields.Many2one('account.intrastat.transation.nature',
-        string='Transation Nature')
+                                        string='Transation Nature')
     weight_kg = fields.Float(string='Weight kg')
     additional_units = fields.Float(string='Additional Units')
     statistic_amount_euro = fields.Float(string='Statistic Amount Euro',
-        digits=dp.get_precision('Account'))
-
+                                         digits=dp.get_precision('Account'))
     delivery_code = fields.Many2one('stock.incoterms',
-        string='Delivery')
+                                    string='Delivery')
     transport_code = fields.Many2one('account.intrastat.transport',
-        string='Transport')
+                                     string='Transport')
     country_origin_id = fields.Many2one('res.country',
-        string='Country Origin')
+                                        string='Country Origin')
     country_good_origin_id = fields.Many2one('res.country',
-        string='Country Goods Origin')
+                                             string='Country Goods Origin')
     province_destination_id = fields.Many2one('res.country.state',
-        string='rovince Destination')
+                                              string='rovince Destination')
 
 
 class account_payment_term(models.Model):
