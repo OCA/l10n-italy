2ac8ad802ae756df81f98ba6487aa07fb57274ce
Davide Corio <davide.corio@abstract.it>
2015-07-21 12:21:59 +0200

    [ADD] l10n_it_account

---
+++ l10n_it_account/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Abstract srl (<http://www.abstract.it>)
+#    Copyright (C) 2015 Agile Business Group sagl (<http://www.agilebg.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Italian Localization - Account',
+    'version': '0.1',
+    'category': 'Localization/Italy',
+    'description': "Accounting-related modules helpers""",
+    'author': "Agile Business Group,Abstract,Odoo Community Association (OCA)",
+    'website': 'http://www.odoo-italia.org',
+    'license': 'AGPL-3',
+    "depends": ['base', 'account'],
+    "data": [
+        'view/account_view.xml',
+    ],
+    "qweb": [],
+    "demo": [],
+    "test": [],
+    "active": False,
+    'installable': True
+}
