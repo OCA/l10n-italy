ef716c7e0de130d9c440f1a7f5c080e848c7ea7a
eLBati <lorenzo.battistini@agilebg.com>
2015-08-12 17:37:57 +0200

    [REF] tax constraints. See https://github.com/OCA/l10n-italy/pull/124#discussion_r35435767

--- l10n_it_account/models/account_tax.py
+++ l10n_it_account/models/account_tax.py
@@ -4,9 +4,7 @@
 #    Copyright (C) 2013 Associazione OpenERP Italia
 #    (<http://www.openerp-italia.org>).
 #    Copyright (C) 2015 Agile Business Group <http://www.agilebg.com>
-#    (<http://www.openerp-italia.org>).
 #    Copyright (C) 2015 Link It Spa <http://www.linkgroup.it/>
-#    (<http://www.openerp-italia.org>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -26,6 +24,7 @@
 from openerp.osv import orm
 from openerp import fields
 from openerp.tools.translate import _
+from openerp.exceptions import except_orm
 
 
 class AccountTaxCode(orm.Model):
@@ -54,93 +53,71 @@ class account_tax(orm.Model):
         default=tmp_default, context=context)
 
     def create(self, cr, uid, vals, context=None):
-
-        if vals['type_tax_use'] == 'purchase':
-            if 'base_code_id' in vals:
-                if vals['base_code_id']:
-                    if self.exist(cr, uid, 'base_code_id',
-                         vals['base_code_id']):
-                        raise orm.except_orm(_('Error!'),
-                                             _('Base code invoice already used '
-                                             'in other purchase tax'))
-            if 'tax_code_id' in vals:
-                if vals['tax_code_id']:
-                    if self.exist(cr, uid, 'tax_code_id', vals['tax_code_id']):
-                        raise orm.except_orm(_('Error!'),
-                                             _('Tax code invoice already '
-                                              'used in other purchase tax.'))
-            if 'ref_base_code_id' in vals:
-                if vals['ref_base_code_id']:
-                    if self.exist(cr, uid, 'ref_base_code_id',
-                    vals['ref_base_code_id']):
-                        raise orm.except_orm(_('Error!'),
-                                             _('Base code refunds already used '
-                                             'in other purchase tax.'))
-            if 'ref_tax_code_id' in vals:
-                if vals['ref_tax_code_id']:
-                    if self.exist(cr, uid, 'ref_tax_code_id',
-                    vals['ref_tax_code_id']):
-                        raise orm.except_orm(_('Error!'),
-                                             _('Tax code refunds already used '
-                                             'in other purchase tax.'))
-
-        return super(account_tax, self).create(cr, uid, vals, context=context)
+        res = super(account_tax, self).create(cr, uid, vals, context=context)
+        tax = self.browse(cr, uid, res, context)
+        self.check_tax(cr, uid, tax, context=context)
+        return res
 
     def write(self, cr, uid, ids, vals, context=None):
-        for tax_code in self.browse(cr, uid, ids, context):
-            type_tax_use = tax_code.type_tax_use
-            base_code_id = tax_code.base_code_id.id
-            tax_code_id = tax_code.tax_code_id.id
-            ref_base_code_id = tax_code.ref_base_code_id.id
-            ref_tax_code_id = tax_code.ref_tax_code_id.id
-            if 'type_tax_use' in vals:
-                type_tax_use = vals['type_tax_use']
-            if 'base_code_id' in vals:
-                base_code_id = vals['base_code_id']
-            if 'tax_code_id' in vals:
-                tax_code_id = vals['tax_code_id']
-            if 'ref_base_code_id' in vals:
-                ref_base_code_id = vals['ref_base_code_id']
-            if 'ref_tax_code_id' in vals:
-                ref_tax_code_id = vals['ref_tax_code_id']
+        res = super(account_tax, self).write(
+            cr, uid, ids, vals, context=context)
+        for tax in self.browse(cr, uid, ids, context):
+            self.check_tax(cr, uid, tax, context=context)
+        return res
 
-            if type_tax_use == 'purchase':
-                if base_code_id:
-                    if self.exist(cr, uid, 'base_code_id', base_code_id,
-                         tax_code.id):
-                        raise orm.except_orm(_('Error!'),
-                                            _('Base code invoices already used '
-                                            'in other purchase tax.'))
-                if tax_code_id:
-                    if self.exist(cr, uid, 'tax_code_id', tax_code_id,
-                    tax_code.id):
-                        raise orm.except_orm(_('Error!'),
-                                                 _('Tax code invoices already '
-                                                 'used in other purchase tax.'))
-                if ref_base_code_id:
-                    if self.exist(cr, uid, 'ref_base_code_id', ref_base_code_id,
-                         tax_code.id):
-                        raise orm.except_orm(_('Error!'),
-                                            _('Base code refunds already '
-                                                 'used in other purchase tax.'))
-                if ref_tax_code_id:
-                    if self.exist(cr, uid, 'ref_tax_code_id', ref_tax_code_id,
-                         tax_code.id):
-                        raise orm.except_orm(_('Error!'),
-                                            _('Tax code refunds already used '
-                                            'in other purchase tax.'))
+    def check_tax(self, cr, uid, tax, context=None):
+        """
+        This is used to check that every (purchase) tax code is linked to
+        only one (main) tax.
+        This allows to print a VAT register summary where
+        tax amount, base amount, deductible amount and non deductible amount
+        are all printed on the same line
+        """
+        if tax.type_tax_use == 'purchase' and not tax.parent_id:
+            if tax.base_code_id:
+                if self.exist(
+                    cr, uid, 'base_code_id', tax.base_code_id.id, tax.id,
+                    context=context
+                ):
+                    raise except_orm(
+                        _('Error!'),
+                        _('Tax code %s already used '
+                          'in other purchase tax') % tax.base_code_id.name)
+            if tax.tax_code_id:
+                if self.exist(
+                    cr, uid, 'tax_code_id', tax.tax_code_id.id, tax.id,
+                    context=context
+                ):
+                    raise except_orm(
+                        _('Error!'),
+                        _('Tax code %s already used '
+                          'in other purchase tax') % tax.tax_code_id.name)
+            if tax.ref_base_code_id:
+                if self.exist(
+                    cr, uid, 'ref_base_code_id', tax.ref_base_code_id.id,
+                    tax.id, context=context
+                ):
+                    raise except_orm(
+                        _('Error!'),
+                        _('Tax code %s already used '
+                          'in other purchase tax') % tax.ref_base_code_id.name)
+            if tax.ref_tax_code_id:
+                if self.exist(
+                    cr, uid, 'ref_tax_code_id', tax.ref_tax_code_id.id,
+                    tax.id, context=context
+                ):
+                    raise except_orm(
+                        _('Error!'),
+                        _('Tax code %s already used '
+                          'in other purchase tax') % tax.ref_tax_code_id.name)
 
-        return super(account_tax, self).write(cr, uid, ids, vals,
-             context=context)
-
-    def exist(self, cr, uid, field, value, id_doc=None):
-        if id_doc:
-            res = self.pool.get('account.tax').search(cr, uid,
-            [(field, '=', value), ('id', '!=', id_doc)])
-        else:
-            res = self.pool.get('account.tax').search(cr, uid,
-            [(field, '=', value)])
-
+    def exist(self, cr, uid, field, value, id_doc, context=None):
+        res = self.pool.get('account.tax').search(
+            cr, uid, [
+                (field, '=', value),
+                ('id', '!=', id_doc),
+                ('parent_id', '=', False),
+                ], context=context)
         if res:
             return True
         else:
