7e5fbe3fef17ecc97f08f6844b4ff8c109edd7b6
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:01:34 +0200

    [UPD] prefix versions with 8.0

--- l10n_it_ateco/__openerp__.py
+++ l10n_it_ateco/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Ateco codes",
-    "version": "1.0",
+    "version": "8.0.1.0.0",
     "category": "Localisation/Italy",
     "description": """Italian Localisation module - Ateco codes
 
