466a24c975d0435fa4c0384338aa0289e588f615
eLBati <lorenzo.battistini@agilebg.com>
2015-10-22 15:39:58 +0200

    remove statement about split payment journal, as it is not present anymore
    FIX journal entry of refund

--- l10n_it_split_payment/models/account.py
+++ l10n_it_split_payment/models/account.py
@@ -58,7 +58,7 @@ class AccountInvoice(models.Model):
             raise UserError(
                 _("Please set 'Split Payment Write-off Account' field in"
                   " accounting configuration"))
-        return {
+        vals = {
             'name': _('Split Payment Write Off'),
             'partner_id': self.partner_id.id,
             'account_id': self.company_id.sp_account_id.id,
@@ -68,6 +68,10 @@ class AccountInvoice(models.Model):
             'debit': self.amount_sp,
             'credit': 0,
             }
+        if self.type == 'out_refund':
+            vals['debit'] = 0
+            vals['credit'] = self.amount_sp
+        return vals
 
     @api.multi
     def _compute_split_payments(self):
@@ -76,10 +80,16 @@ class AccountInvoice(models.Model):
             move_line_pool = self.env['account.move.line']
             for payment_line in move_line_pool.browse(payment_line_ids):
                 inv_total = invoice.amount_sp + invoice.amount_total
-                payment_line_debit = (
-                    invoice.amount_total * payment_line.debit) / inv_total
-                payment_line.write(
-                    {'debit': payment_line_debit}, update_check=False)
+                if invoice.type == 'out_invoice':
+                    payment_line_amount = (
+                        invoice.amount_total * payment_line.debit) / inv_total
+                    payment_line.write(
+                        {'debit': payment_line_amount}, update_check=False)
+                elif invoice.type == 'out_refund':
+                    payment_line_amount = (
+                        invoice.amount_total * payment_line.credit) / inv_total
+                    payment_line.write(
+                        {'credit': payment_line_amount}, update_check=False)
 
     @api.multi
     def action_move_create(self):
@@ -89,6 +99,9 @@ class AccountInvoice(models.Model):
                 invoice.fiscal_position and
                 invoice.fiscal_position.split_payment
             ):
+                if invoice.type in ['in_invoice', 'in_refund']:
+                    raise UserError(
+                        _("Can't handle supplier invoices with split payment"))
                 self._compute_split_payments()
                 line_model = self.env['account.move.line']
                 write_off_line_vals = invoice._build_debit_line()
