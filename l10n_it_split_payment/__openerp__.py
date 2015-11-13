99aa12e76466bb1628b45c28ae25661bda8b4699
eLBati <lorenzo.battistini@agilebg.com>
2015-11-13 16:54:44 +0100

    port description into __openerp__.py

--- l10n_it_split_payment/__openerp__.py
+++ l10n_it_split_payment/__openerp__.py
@@ -25,7 +25,67 @@
     'version': '8.0.1.0.0',
     'category': 'Localization/Italy',
     'summary': 'Split Payment',
-    'author': 'Abstract, Odoo Community Association (OCA)',
+    'description': """
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+====================================
+Italian Localization - split payment
+====================================
+
+Law: http://goo.gl/1riAwt (Articolo 17 ter)
+
+Module to generate Split Payment accounting entries
+
+Configuration
+=============
+
+To configure this module, you need to:
+
+* go to Settings, Configuration, Accounting and configure
+'Split Payment Write-off account' (like 'IVA n/debito sospesa SP').
+Write-off account should be different from standard debit VAT,
+in order to separately add it in VAT statement.
+* configure the fiscal position used for split payment, setting 'Split Payment'
+flag. In fiscal position, map standard VAT with SP VAT, like the following:
+
+.. image:: /l10n_it_split_payment/static/fiscal_position.png
+
+
+-------------------------------------------------------------------------------
+
+22SPL is configured like the following:
+
+
+.. image:: /l10n_it_split_payment/static/SP.png
+
+Credits
+=======
+
+Contributors
+------------
+
+* Davide Corio <davide.corio@abstract.it>
+* Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
+""",
+    'author': 'Abstract, Agile Business Group, '
+              'Odoo Community Association (OCA)',
     'website': 'http://www.abstract.it',
     'license': 'AGPL-3',
     'depends': [
