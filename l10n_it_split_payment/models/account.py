4a81b4d2ae2d63709b774a5f11e32ed540a9eb6e
eLBati <lorenzo.battistini@agilebg.com>
2015-11-13 16:53:41 +0100

    ADD l10n_it_split_payment from 8.0

---
+++ l10n_it_split_payment/models/account.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Abstract (http://www.abstract.it)
+#    Author: Davide Corio <davide.corio@abstract.it>
+#    Copyright 2015 Lorenzo Battistini - Agile Business Group
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import openerp.addons.decimal_precision as dp
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning as UserError
+
+
+class AccountFiscalPosition(models.Model):
+    _inherit = 'account.fiscal.position'
+
+    split_payment = fields.Boolean('Split Payment')
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    amount_sp = fields.Float(
+        string='Split Payment',
+        digits=dp.get_precision('Account'),
+        store=True,
+        readonly=True,
+        compute='_compute_amount')
+    split_payment = fields.Boolean(
+        'Split Payment',
+        related='fiscal_position.split_payment')
+
+    @api.one
+    @api.depends('invoice_line.price_subtotal', 'tax_line.amount')
+    def _compute_amount(self):
+        super(AccountInvoice, self)._compute_amount()
+        if self.fiscal_position.split_payment:
+            self.amount_sp = self.amount_tax
+            self.amount_tax = 0
+        self.amount_total = self.amount_untaxed + self.amount_tax
+
+    def _build_debit_line(self):
+        if not self.company_id.sp_account_id:
+            raise UserError(
+                _("Please set 'Split Payment Write-off Account' field in"
+                  " accounting configuration"))
+        vals = {
+            'name': _('Split Payment Write Off'),
+            'partner_id': self.partner_id.id,
+            'account_id': self.company_id.sp_account_id.id,
+            'journal_id': self.journal_id.id,
+            'period_id': self.period_id.id,
+            'date': self.date_invoice,
+            'debit': self.amount_sp,
+            'credit': 0,
+            }
+        if self.type == 'out_refund':
+            vals['debit'] = 0
+            vals['credit'] = self.amount_sp
+        return vals
+
+    @api.multi
+    def _compute_split_payments(self):
+        for invoice in self:
+            payment_line_ids = invoice.move_line_id_payment_get()
+            move_line_pool = self.env['account.move.line']
+            for payment_line in move_line_pool.browse(payment_line_ids):
+                inv_total = invoice.amount_sp + invoice.amount_total
+                if invoice.type == 'out_invoice':
+                    payment_line_amount = (
+                        invoice.amount_total * payment_line.debit) / inv_total
+                    payment_line.write(
+                        {'debit': payment_line_amount}, update_check=False)
+                elif invoice.type == 'out_refund':
+                    payment_line_amount = (
+                        invoice.amount_total * payment_line.credit) / inv_total
+                    payment_line.write(
+                        {'credit': payment_line_amount}, update_check=False)
+
+    @api.multi
+    def action_move_create(self):
+        res = super(AccountInvoice, self).action_move_create()
+        for invoice in self:
+            if (
+                invoice.fiscal_position and
+                invoice.fiscal_position.split_payment
+            ):
+                if invoice.type in ['in_invoice', 'in_refund']:
+                    raise UserError(
+                        _("Can't handle supplier invoices with split payment"))
+                self._compute_split_payments()
+                line_model = self.env['account.move.line']
+                write_off_line_vals = invoice._build_debit_line()
+                write_off_line_vals['move_id'] = invoice.move_id.id
+                line_model.create(write_off_line_vals)
+        return res
---
+++ l10n_it_split_payment/tests/test_splitpayment.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    About license, see __openerp__.py
+#
+##############################################################################
+
+from openerp.tests.common import TransactionCase
+
+
+class TestSP(TransactionCase):
+
+    def setUp(self):
+        super(TestSP, self).setUp()
+        self.tax_model = self.env['account.tax']
+        self.invoice_model = self.env['account.invoice']
+        self.term_model = self.env['account.payment.term']
+        self.inv_line_model = self.env['account.invoice.line']
+        self.fp_model = self.env['account.fiscal.position']
+        self.tax22 = self.tax_model.create({
+            'name': '22%',
+            'amount': 0.22,
+            })
+        self.sp_fp = self.fp_model.create({
+            'name': 'Split payment',
+            'split_payment': True,
+            })
+        self.company = self.env.ref('base.main_company')
+        self.company.sp_account_id = self.env.ref('account.ova')
+        self.a_recv = self.env.ref('account.a_recv')
+        self.sales_journal = self.env.ref('account.sales_journal')
+        self.sales_journal.update_posted = True
+        self.term_15_30 = self.term_model.create({
+            'name': '15 30',
+            'line_ids': [
+                (0, 0, {
+                    'value': 'procent',
+                    'value_amount': 0.5,
+                    'days': 15,
+                }),
+                (0, 0, {
+                    'value': 'balance',
+                    'days': 30,
+                })]})
+
+    def test_invoice(self):
+        invoice = self.invoice_model.create({
+            'partner_id': self.env.ref('base.res_partner_3').id,
+            'journal_id': self.sales_journal.id,
+            'account_id': self.a_recv.id,
+            'fiscal_position': self.sp_fp.id,
+            })
+        inv_line = self.inv_line_model.create({
+            'invoice_id': invoice.id,
+            'name': 'service',
+            'account_id': self.env.ref('account.o_income').id,
+            'quantity': 1,
+            'price_unit': 100,
+            })
+        inv_line.invoice_line_tax_id = self.tax22
+        self.assertTrue(invoice.split_payment)
+        invoice.signal_workflow('invoice_open')
+        self.assertEqual(invoice.amount_sp, 22)
+        self.assertEqual(invoice.amount_total, 100)
+        self.assertEqual(invoice.residual, 100)
+        self.assertEqual(invoice.amount_tax, 0)
+        vat_line = False
+        credit_line = False
+        for line in invoice.move_id.line_id:
+            if line.account_id.id == self.company.sp_account_id.id:
+                vat_line = True
+                self.assertEqual(line.debit, 22)
+            if line.account_id.id == self.a_recv.id:
+                credit_line = True
+                self.assertEqual(line.debit, 100)
+        self.assertTrue(vat_line)
+        self.assertTrue(credit_line)
+        invoice.action_cancel()
+
+        # invoice with payment term
+        invoice2 = self.invoice_model.create({
+            'partner_id': self.env.ref('base.res_partner_3').id,
+            'journal_id': self.sales_journal.id,
+            'account_id': self.a_recv.id,
+            'fiscal_position': self.sp_fp.id,
+            'payment_term': self.term_15_30.id,
+            })
+        inv_line2 = self.inv_line_model.create({
+            'invoice_id': invoice2.id,
+            'name': 'service',
+            'account_id': self.env.ref('account.o_income').id,
+            'quantity': 1,
+            'price_unit': 100,
+            })
+        inv_line2.invoice_line_tax_id = self.tax22
+        invoice2.signal_workflow('invoice_open')
+        self.assertEqual(invoice2.amount_sp, 22)
+        self.assertEqual(invoice2.amount_total, 100)
+        self.assertEqual(invoice2.residual, 100)
+        self.assertEqual(invoice2.amount_tax, 0)
+        vat_line = False
+        credit_line_count = 0
+        for line in invoice2.move_id.line_id:
+            if line.account_id.id == self.company.sp_account_id.id:
+                vat_line = True
+                self.assertEqual(line.debit, 22)
+            if line.account_id.id == self.a_recv.id:
+                credit_line_count += 1
+                self.assertEqual(line.debit, 50)
+        self.assertTrue(vat_line)
+        self.assertEqual(credit_line_count, 2)
+
+        # refund
+        invoice3 = self.invoice_model.create({
+            'partner_id': self.env.ref('base.res_partner_3').id,
+            'journal_id': self.sales_journal.id,
+            'account_id': self.a_recv.id,
+            'fiscal_position': self.sp_fp.id,
+            'type': 'out_refund',
+            })
+        inv_line3 = self.inv_line_model.create({
+            'invoice_id': invoice3.id,
+            'name': 'service',
+            'account_id': self.env.ref('account.o_income').id,
+            'quantity': 1,
+            'price_unit': 100,
+            })
+        inv_line3.invoice_line_tax_id = self.tax22
+        self.assertTrue(invoice3.split_payment)
+        invoice3.signal_workflow('invoice_open')
+        self.assertEqual(invoice3.amount_sp, 22)
+        self.assertEqual(invoice3.amount_total, 100)
+        self.assertEqual(invoice3.residual, 100)
+        self.assertEqual(invoice3.amount_tax, 0)
+        vat_line = False
+        credit_line = False
+        for line in invoice3.move_id.line_id:
+            if line.account_id.id == self.company.sp_account_id.id:
+                vat_line = True
+                self.assertEqual(line.credit, 22)
+            if line.account_id.id == self.a_recv.id:
+                credit_line = True
+                self.assertEqual(line.credit, 100)
+        self.assertTrue(vat_line)
+        self.assertTrue(credit_line)
