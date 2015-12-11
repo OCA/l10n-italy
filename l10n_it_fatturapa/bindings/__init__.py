cb2935d2a227e2753b308fe5f84ff8669cc18283
Leonardo Donelli <learts92@gmail.com>
2015-12-11 14:34:47 +0100

    fatturapa: Log import errors, comments

--- l10n_it_fatturapa/bindings/__init__.py
+++ l10n_it_fatturapa/bindings/__init__.py
@@ -17,7 +17,13 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+import logging
+
+_logger = logging.getLogger(__name__)
+
+# pyxb is referenced in several in top-level statements in
+# fatturapa_v_1_1, so we guard the import of the entire file
 try:
     from . import fatturapa_v_1_1
 except ImportError:
-    pass # Module is not installed, avoid init error
+    _logger.debug('Cannot `import pyxb`.')  # Avoid init error if not installed
