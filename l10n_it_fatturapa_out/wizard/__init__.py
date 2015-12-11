b5905628c860d0185cd4ab3b03a6fcc914871ede
Leonardo Donelli <learts92@gmail.com>
2015-12-11 14:34:47 +0100

    fatturapa: Log import errors, comments

--- l10n_it_fatturapa_out/wizard/__init__.py
+++ l10n_it_fatturapa_out/wizard/__init__.py
@@ -17,8 +17,13 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+import logging
 
+_logger = logging.getLogger(__name__)
+
+# there are multiple statements that should be guarded
+# in wizard_export_fatturapa, so we guard the entire file
 try:
     from . import wizard_export_fatturapa
 except ImportError:
-    pass  # Module not installed, avoid init error!
+    _logger.debug('Cannot `import pyxb`.')  # Avoid init error if not installed
