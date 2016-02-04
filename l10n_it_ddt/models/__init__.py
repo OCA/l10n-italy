41bcd683c24436056865589f60003f32af82575d
eLBati <lorenzo.battistini@agilebg.com>
2016-02-04 11:26:27 +0100

    copying l10n_it_ddt form 8.0 as is

---
+++ l10n_it_ddt/models/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Abstract (http://www.abstract.it)
+#    @author Davide Corio <davide.corio@abstract.it>
+#    Copyright (C) 2014 Agile Business Group (http://www.agilebg.com)
+#    Copyright (C) 2015 Apulia Software s.r.l. (http://www.apuliasoftware.it)
+#    @author Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
+#
+#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+#
+##############################################################################
+
+
+from . import stock_picking_package_preparation
+from . import partner
+from . import account
+from . import stock
+from . import sale
---
+++ l10n_it_ddt/models/partner.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Apulia Software s.r.l. (http://www.apuliasoftware.it)
+#    @author Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
+#
+#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+#
+##############################################################################
+
+
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+
+    _inherit = 'res.partner'
+
+    carriage_condition_id = fields.Many2one(
+        'stock.picking.carriage_condition', 'Carriage Condition')
+    goods_description_id = fields.Many2one(
+        'stock.picking.goods_description', 'Description of Goods')
+    transportation_reason_id = fields.Many2one(
+        'stock.picking.transportation_reason',
+        'Reason for Transportation')
+    transportation_method_id = fields.Many2one(
+        'stock.picking.transportation_method',
+        'Method of Transportation')
