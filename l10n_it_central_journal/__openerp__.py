fb62185c80f39fd3f81effb096cf3d05d9a71b82
eLBati <lorenzo.battistini@agilebg.com>
2016-02-12 15:00:34 +0100

    ADD OCA author

--- l10n_it_central_journal/__openerp__.py
+++ l10n_it_central_journal/__openerp__.py
@@ -13,7 +13,7 @@
     'name': 'Italian Localization - Account central journal',
     'version': '8.0.2.0.0',
     'category': 'Localization/Italy',
-    'author': 'Link It Spa',
+    'author': 'Link It Spa, Odoo Community Association (OCA)',
     'website': 'http://www.linkgroup.it',
     'license': 'AGPL-3',
     "depends": [
