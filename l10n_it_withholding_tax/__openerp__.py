9646033a3f8da211eaafba4ce6d3ded8b857dad1
eLBati <lorenzo.battistini@agilebg.com>
2016-02-16 18:28:43 +0100

    FIX copyright headers

--- l10n_it_withholding_tax/__openerp__.py
+++ l10n_it_withholding_tax/__openerp__.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#    
-#    Copyright (C) 2015 Alessandro Camilli 
-#    (<http://www.openforce.it>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright © 2015 Alessandro Camilli (<http://www.openforce.it>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     'name': 'Withholding tax',
     'version': '0.2',
@@ -26,7 +10,8 @@
     Withholding tax
 
 """,
-    'author': 'Openforce di Alessandro Camilli',
+    'author': 'Openforce di Alessandro Camilli, '
+              'Odoo Community Association (OCA)',
     'website': 'http://www.openforce.it',
     'license': 'AGPL-3',
     "depends" : ['account', 'account_voucher'],
@@ -38,7 +23,6 @@
         'security/ir.model.access.csv',
         'workflow.xml',
         ],
-    "demo" : [],
     "active": False,
     "installable": True
 }
