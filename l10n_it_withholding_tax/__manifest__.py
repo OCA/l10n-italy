d38d06458d75c4ab9d852ad8ca8295a039a16d96
alessandrocamilli <a.camilli@yahoo.it>
2016-02-25 11:33:44 +0100

    Compute withholding tax from invoice payment directly

--- l10n_it_withholding_tax/__manifest__.py
+++ l10n_it_withholding_tax/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Italian Withholding Tax',
-    'version': '8.0.2.0.0',
+    'version': '8.0.3.0.0',
     'category': 'Account',
     'author': 'Openforce di Alessandro Camilli, '
               'Odoo Community Association (OCA)',
