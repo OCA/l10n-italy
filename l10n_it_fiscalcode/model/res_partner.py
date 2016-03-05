b0912959fb0d1fc075113e6f3c5e4e1d27dafa1f
gigidn <gigidn@gmail.com>
2016-03-05 07:41:27 +0100

    l10n_it_fiscalcode refactor and port to v9

--- l10n_it_fiscalcode/model/res_partner.py
+++ l10n_it_fiscalcode/model/res_partner.py
@@ -19,29 +19,40 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api
+from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+try:
+    import codicefiscale
+except ImportError:
+    _logger.warning(
+        'codicefiscale library not found. '
+        'If you plan to use it, please install the codicefiscale library '
+        'from https://pypi.python.org/pypi/codicefiscale')
 
 
 class res_partner(models.Model):
     _inherit = 'res.partner'
 
-    @api.multi
-    def check_fiscalcode(self):
-        for partner in self:
-            if not partner.fiscalcode:
-                return True
-            elif len(partner.fiscalcode) != 16 and partner.individual:
-                return False
-            else:
-                return True
-
     fiscalcode = fields.Char(
-        'Fiscal Code', size=16, help="Italian Fiscal Code")
+        string='Fiscal Code', size=16, help="Italian Fiscal Code")
     individual = fields.Boolean(
-        'Individual', default=False,
+        string='Individual', default=False,
         help="If checked the C.F. is referred to a Individual Person")
 
-    _constraints = [
-        (check_fiscalcode,
-         "The fiscal code doesn't seem to be correct.", ["fiscalcode"])
-    ]
+    @api.one
+    @api.constrains('fiscalcode')
+    def _check_fiscalcode(self):
+
+        if self.fiscalcode:
+            if self.is_company and len(self.fiscalcode) != 11 and not self.individual and not self.fiscalcode.isdigit():
+                raise ValidationError(
+                    _("Company fiscal code must be 11 digts lenght.")
+                )
+            elif len(self.fiscalcode) != 16 and not codicefiscale.isvalid(self.fiscalcode):
+                raise ValidationError(
+                    _("The fiscal code doesn't seem to be correct.")
+                )
