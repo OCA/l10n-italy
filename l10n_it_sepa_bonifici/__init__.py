0f889602f87a03b09b185d0d2066d48f18b00daf
Alessandro Camilli <a.camilli@yahoo.it>
2016-04-30 10:26:47 +0200

    Added xsd schemes
    Added Cross Border payment
    
    Fix country data
    
    Removed commented lines
    
    New ensure_one to secure one export at a time.
    
    tag image:: replaced by figure::
    
    travis warning
    
    travis warning (2)
    
    travis warning (3)
    
    Pass test_flake8
    
    test_flake8 (2)
    
    Changes for test_pylint
    
    improved syntax

--- l10n_it_sepa_bonifici/__init__.py
+++ l10n_it_sepa_bonifici/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Alessandro Camilli <alessandro.camilli@openforce.it>
+# © 2016 Alessandro Camilli <alessandrocamilli@openforce.it>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import wizard
+from . import wizard
