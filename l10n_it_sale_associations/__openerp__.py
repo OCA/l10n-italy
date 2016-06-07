29735c7909a42557d837bf6e923fe18297ee9df0
Nicola Malcontenti <nicola.malcontenti@agilebg.com>
2016-06-07 09:06:28 +0200

    [FIX] Add 2 modules l10n_it_sale_associations l10n_it_website_sale_fiscalcode

---
+++ l10n_it_sale_associations/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Nicola Malcontenti - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Association',
+    'category': 'Sale',
+    'author': "Agile Business Group,"
+              "Odoo Community Association (OCA)",
+    'version': '8.0.1.0.0',
+    'author': 'Agile Business Group',
+    'website': 'http://www.agilebg.com',
+    'depends': [],
+    'data': [
+        'views/partner_view.xml'
+    ],
+    'installable': True,
+}
---
+++ l10n_it_website_sale_fiscalcode/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 Nicola Malcontenti - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Website Sale FiscalCode',
+    'category': 'e-commerce',
+    'author': "Agile Business Group,"
+              "Odoo Community Association (OCA)",
+    'version': '8.0.1.0.0',
+    'author': 'Agile Business Group',
+    'website': 'http://www.agilebg.com',
+    'depends': [
+        'website_sale_partner_type',
+        'l10n_it_fiscalcode',
+        'l10n_it_sale_associations'
+    ],
+    'data': [
+        'views/templates.xml',
+        'views/assets.xml',
+    ],
+    'installable': True,
+}
