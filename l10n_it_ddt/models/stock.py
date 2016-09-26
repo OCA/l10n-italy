59fd50a1e971064b7dbc13bdd8d5632230635bb3
eLBati <lorenzo.battistini@agilebg.com>
2016-09-26 11:22:09 +0200

    FIX
    odoo-ocb/openerp/fields.py", line 627, in _add_trigger
        field = model._fields[name]
    KeyError: 'ul_id'
    
    while installing
    
    FIX l10n_it_ddt: local variable 'res' is assigned to but never used
    
    FIX l10n_it_ddt using correct method for sequence
    
    IMP l10n_it_ddt tests with full coverage
    
    FIX l10n_it_ddt: error message

--- l10n_it_ddt/models/stock.py
+++ l10n_it_ddt/models/stock.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Apulia Software s.r.l. (http://www.apuliasoftware.it)
-#    @author Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
-#    Copyright 2015 Lorenzo Battistini - Agile Business Group
-#
-#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-#
-##############################################################################
+# Copyright 2015 Apulia Software s.r.l. (http://www.apuliasoftware.it)
+# @author Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
+# Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
 from openerp import fields, models, api
