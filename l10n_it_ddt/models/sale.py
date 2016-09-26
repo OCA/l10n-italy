cb0e2d92aec1aa8d72e57b1b9ba7c070b43bf908
eLBati <lorenzo.battistini@agilebg.com>
2016-09-26 11:22:09 +0200

    FIX
    odoo-ocb/openerp/fields.py", line 627, in _add_trigger
        field = model._fields[name]
    KeyError: 'ul_id'
    
    while installing
    
    FIX l10n_it_ddt: local variable 'res' is assigned to but never used
    
    FIX l10n_it_ddt using correct method for sequence
    
    IMP l10n_it_ddt tests with full coverage
    
    FIX l10n_it_ddt: error message

--- l10n_it_ddt/models/sale.py
+++ l10n_it_ddt/models/sale.py
@@ -19,13 +19,14 @@ class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    @api.one
-    def _get_ddt_ids(self):
-        ddt_ids = []
-        for picking in self.picking_ids:
-            for ddt in picking.ddt_ids:
-                ddt_ids.append(ddt.id)
-        self.ddt_ids = ddt_ids
+    @api.multi
+    def _compute_ddt_ids(self):
+        for so in self:
+            ddt_ids = []
+            for picking in so.picking_ids:
+                for ddt in picking.ddt_ids:
+                    ddt_ids.append(ddt.id)
+            so.ddt_ids = ddt_ids
 
     carriage_condition_id = fields.Many2one(
         'stock.picking.carriage_condition', string='Carriage Condition')
@@ -42,7 +43,7 @@ class SaleOrder(models.Model):
     ddt_ids = fields.Many2many(
         'stock.picking.package.preparation',
         string='Related DdTs',
-        compute='_get_ddt_ids')
+        compute='_compute_ddt_ids')
     create_ddt = fields.Boolean('Automatically create the DDT')
 
     @api.multi
@@ -94,18 +95,19 @@ class SaleOrder(models.Model):
                 ddt_model.create(ddt_data)
         return res
 
-    def action_view_ddt(self, cr, uid, ids, context=None):
-        mod_obj = self.pool.get('ir.model.data')
-        act_obj = self.pool.get('ir.actions.act_window')
+    @api.multi
+    def action_view_ddt(self):
+        mod_obj = self.env['ir.model.data']
+        act_obj = self.env['ir.actions.act_window']
 
         result = mod_obj.get_object_reference(
-            cr, uid, 'stock_picking_package_preparation',
+            'stock_picking_package_preparation',
             'action_stock_picking_package_preparation')
         id = result and result[1] or False
-        result = act_obj.read(cr, uid, [id], context=context)[0]
+        result = act_obj.browse(id).read()[0]
 
         ddt_ids = []
-        for so in self.browse(cr, uid, ids, context=context):
+        for so in self:
             ddt_ids += [ddt.id for ddt in so.ddt_ids]
 
         if len(ddt_ids) > 1:
@@ -113,7 +115,7 @@ class SaleOrder(models.Model):
                 map(str, ddt_ids)) + "])]"
         else:
             res = mod_obj.get_object_reference(
-                cr, uid, 'stock_picking_package_preparation',
+                'stock_picking_package_preparation',
                 'stock_picking_package_preparation_form')
             result['views'] = [(res and res[1] or False, 'form')]
             result['res_id'] = ddt_ids and ddt_ids[0] or False
