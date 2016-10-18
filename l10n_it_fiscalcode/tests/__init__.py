af49e768180c8916d0939ae2eeb5e876306eb3f4
andrea4ever <4everamd@gmail.com>
2016-10-18 15:14:21 +0200

    [FIX] Make module installable
    [MIG] Migrate code to new api
    
    [MIG] Replace YAML test with unittest
    
    [MIG] Onchange with new api, upgrade XML
    
    [MIG] Update README.rst
    
    [MIG] Update Copyright
    
    [MIG] Fix Readme and description
    
    [FIX] Remove useless key in manifest
    
    Update Manifest and Readme info
    
    OCA Transbot updated translations from Transifex
    
    [FIX] © -> Copyright
    
    [FIX] Remove useless api
    [FIX] Use % instead of positional in string
    [FIX] Readme link's to python depends
    
    [FIX] Pylint

--- l10n_it_fiscalcode/tests/__init__.py
+++ l10n_it_fiscalcode/tests/__init__.py
@@ -3,5 +3,4 @@
 # Copyright 2016 Andrea Gallina (Apulia Software)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import model
-from . import wizard
+from . import test_fiscalcode
