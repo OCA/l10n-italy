777171230d3acfd9a56e837790af5a2ddef12e80
eLBati <lorenzo.battistini@agilebg.com>
2016-10-19 14:52:44 +0200

    IMP l10n_it_fatturapa_out copyright and imports

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -1,33 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Davide Corio <davide.corio@lsweb.it>
-#    Copyright (C) 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2014 Davide Corio
+# Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import base64
 from unidecode import unidecode
 
 from pyxb.exceptions_ import SimpleFacetValueError, SimpleTypeValueError
 
-from openerp.osv import orm
-from openerp.tools.translate import _
+from odoo.osv import orm
+from odoo.tools.translate import _
 
-from openerp.addons.l10n_it_fatturapa.bindings.fatturapa_v_1_1 import (
+from odoo.addons.l10n_it_fatturapa.bindings.fatturapa_v_1_1 import (
     FatturaElettronica,
     FatturaElettronicaHeaderType,
     DatiTrasmissioneType,
@@ -52,7 +36,7 @@ from openerp.addons.l10n_it_fatturapa.bindings.fatturapa_v_1_1 import (
     DettaglioPagamentoType,
     AllegatiType
 )
-from openerp.addons.l10n_it_fatturapa.models.account import (
+from odoo.addons.l10n_it_fatturapa.models.account import (
     RELATED_DOCUMENT_TYPES)
 
 
