e8c0a5030d503f76a1a2415215945b6bd20b53f6
eLBati <lorenzo.battistini@agilebg.com>
2016-10-19 16:28:32 +0200

    l10n_it_fatturapa: porting to 10 and new API enable module porting views
    l10n_it_fatturapa_out porting to 10

--- l10n_it_fatturapa/models/partner.py
+++ l10n_it_fatturapa/models/partner.py
@@ -2,28 +2,26 @@
 # Copyright 2014 Davide Corio <davide.corio@abstract.it>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.osv import fields, orm
+from odoo import fields, models
 
 
-class res_partner(orm.Model):
+class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    _columns = {
-        'eori_code': fields.char('EORI Code', size=20),
-        'license_number': fields.char('License Code', size=20),
-        # 1.2.6 RiferimentoAmministrazione
-        'pa_partner_code': fields.char('PA Code for partner', size=20),
-        # 1.2.1.4
-        'register': fields.char('Professional Register', size=60),
-        # 1.2.1.5
-        'register_province': fields.many2one(
-            'res.country.state', string='Register Province'),
-        # 1.2.1.6
-        'register_code': fields.char('Register Code', size=60),
-        # 1.2.1.7
-        'register_regdate': fields.date('Register Registration Date'),
-        # 1.2.1.8
-        'register_fiscalpos': fields.many2one(
-            'fatturapa.fiscal_position',
-            string="Register Fiscal Position"),
-    }
+    eori_code = fields.Char('EORI Code', size=20)
+    license_number = fields.Char('License Code', size=20)
+    # 1.2.6 RiferimentoAmministrazione
+    pa_partner_code = fields.Char('PA Code for partner', size=20)
+    # 1.2.1.4
+    register = fields.Char('Professional Register', size=60)
+    # 1.2.1.5
+    register_province = fields.Many2one(
+        'res.country.state', string='Register Province')
+    # 1.2.1.6
+    register_code = fields.Char('Register Code', size=60)
+    # 1.2.1.7
+    register_regdate = fields.Date('Register Registration Date')
+    # 1.2.1.8
+    register_fiscalpos = fields.Many2one(
+        'fatturapa.fiscal_position',
+        string="Register Fiscal Position")
