5be3170070d5c8b54b945b51f673dcd8d7b2c468
andrea4ever <4everamd@gmail.com>
2016-10-19 12:54:48 +0200

    [FIX] © -> Copyright, and make module installable

--- l10n_it_rea/__init__.py
+++ l10n_it_rea/__init__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
-# © 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
-# © 2015 Alessio Gerace <alessio.gerace@agilebg.com>
-# © 2016 Andrea Gallina (Apulia Software)
+# Copyright 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
+# Copyright 2015 Alessio Gerace <alessio.gerace@agilebg.com>
+# Copyright 2016 Andrea Gallina (Apulia Software)
 
 from . import models
