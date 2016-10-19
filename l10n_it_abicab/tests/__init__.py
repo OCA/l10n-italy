c6ea72d59fa551a797199bd4fc13b62e2d1e74b3
Davide Corio <davide.corio@abstract.it>
2016-10-19 13:46:17 +0200

    [MIG] l10n_it_abicab: Migrated to 10.0
    [FIX] authors
    
    [FIX] copyright
    
    [FIX] README syntax
    
    [FIX] README syntax
    
    [FIX] contributors

--- l10n_it_abicab/tests/__init__.py
+++ l10n_it_abicab/tests/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 Associazione Odoo Italia (<http://www.odoo-italia.org>)
 # Copyright 2016 Davide Corio (Abstract)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import test_abicab
