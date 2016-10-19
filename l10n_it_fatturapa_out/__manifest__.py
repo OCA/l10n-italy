90eeae671c24deb83d0fb178f3f8c655eebb516c
eLBati <lorenzo.battistini@agilebg.com>
2016-10-19 14:52:44 +0200

    IMP l10n_it_fatturapa_out copyright and imports
    l10n_it_fatturapa_out porting to 10
    
    IMP l10n_it_fatturapa_out: handle ScontoMaggiorazione
    
    FIX l10n_it_fatturapa LINT for pyXB
    and avoid flake8 for pyxb generated files
    
    FIX l10n_it_fatturapa metadata
    
    FIX l10n_it_fatturapa: The model fatturapa.format has no access rules, consider adding one

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -1,31 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Davide Corio
-#    Copyright 2015 Agile Business Group <http://www.agilebg.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014 Davide Corio
+# Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 {
     'name': 'Italian Localization - FatturaPA - Emission',
-    'version': '8.0.0.1.1',
+    'version': '10.0.1.0.0',
     'category': 'Localization/Italy',
     'summary': 'Electronic invoices emission',
-    'author': 'Davide Corio, Agile Business Group, Innoviu',
+    'author': 'Davide Corio, Agile Business Group, Innoviu,'
+              'Odoo Community Association (OCA)',
     'website': 'http://www.agilebg.com',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     "depends": [
         'l10n_it_fatturapa',
         'l10n_it_split_payment',
@@ -36,8 +22,7 @@
         'views/account_view.xml',
         'security/ir.model.access.csv',
     ],
-    "test": [],
-    'installable': False,
+    'installable': True,
     'external_dependencies': {
         'python': ['unidecode'],
     }
--- l10n_it_fatturapa_out/models/attachment.py
+++ l10n_it_fatturapa_out/models/attachment.py
@@ -1,36 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Davide Corio <davide.corio@lsweb.it>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014 Davide Corio
+# Copyright 2016 Lorenzo Battistini - Agile Business Group
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from openerp.osv import fields, orm
+from odoo import fields, models
 
 
-class FatturaPAAttachment(orm.Model):
+class FatturaPAAttachment(models.Model):
     _name = "fatturapa.attachment.out"
     _description = "FatturaPA Export File"
     _inherits = {'ir.attachment': 'ir_attachment_id'}
     _inherit = ['mail.thread']
 
-    _columns = {
-        'ir_attachment_id': fields.many2one(
-            'ir.attachment', 'Attachment', required=True, ondelete="cascade"),
-        'out_invoice_ids': fields.one2many(
-            'account.invoice', 'fatturapa_attachment_out_id',
-            string="Out Invoices", readonly=True),
-    }
+    ir_attachment_id = fields.Many2one(
+        'ir.attachment', 'Attachment', required=True, ondelete="cascade")
+    out_invoice_ids = fields.One2many(
+        'account.invoice', 'fatturapa_attachment_out_id',
+        string="Out Invoices", readonly=True)
