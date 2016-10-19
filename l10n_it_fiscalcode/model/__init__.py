db9c78e1f73bf332bc60348998dd44aaf73c629c
andrea4ever <4everamd@gmail.com>
2016-10-19 12:58:51 +0200

    [FIX] © -> Copyright

--- l10n_it_fiscalcode/model/__init__.py
+++ l10n_it_fiscalcode/model/__init__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
-# © 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
-# © 2016 Andrea Gallina (Apulia Software)
+# Copyright 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
+# Copyright 2016 Andrea Gallina (Apulia Software)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import res_partner
