a6ddb52ee02a9358c84997adbab4cdfc27588fcd
Davide Corio <davide.corio@abstract.it>
2016-10-19 13:53:14 +0200

    [FIX] authors
    [FIX] copyright
    
    [FIX] README syntax
    
    [FIX] README syntax
    
    [FIX] contributors

--- l10n_it_abicab/__manifest__.py
+++ l10n_it_abicab/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# © 2015 Associazione Odoo Italia (<http://www.odoo-italia.org>)
-# © 2016 Davide Corio (Abstract)
+# Copyright 2015 Associazione Odoo Italia (<http://www.odoo-italia.org>)
+# Copyright 2016 Davide Corio (Abstract)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Base Bank ABI/CAB codes',
     'version': '10.0.1.0.0',
     'category': 'Localization/Italy',
-    'author': "Odoo Italian Community, Odoo Community Association (OCA)",
+    'author': "Odoo Italia Network, Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'depends': ['account'],
     'website': 'http://www.odoo-community.org',
