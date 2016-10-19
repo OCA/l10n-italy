d6cfe15394319c9b4e575a7db97f610472b2def6
eLBati <lorenzo.battistini@agilebg.com>
2016-10-19 16:28:32 +0200

    l10n_it_fatturapa: porting to 10 and new API enable module porting views
    l10n_it_fatturapa_out porting to 10

--- l10n_it_fatturapa/models/account_tax.py
+++ l10n_it_fatturapa/models/account_tax.py
@@ -2,55 +2,31 @@
 # Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.osv import fields, orm
+from odoo import fields, models, api
 from odoo.tools.translate import _
+from odoo.exceptions import UserError
 
 
-class AccountTax(orm.Model):
+class AccountTax(models.Model):
     _inherit = 'account.tax'
-    _columns = {
-        'non_taxable_nature': fields.selection([
-            ('N1', 'escluse ex art. 15'),
-            ('N2', 'non soggette'),
-            ('N3', 'non imponibili'),
-            ('N4', 'esenti'),
-            ('N5', 'regime del margine'),
-            ('N6', 'inversione contabile (reverse charge)'),
-            ], string="Non taxable nature"),
-        'payability': fields.selection([
-            ('I', 'Immediate payability'),
-            ('D', 'Deferred payability'),
-            ('S', 'Split payment'),
-            ], string="VAT payability"),
-        'law_reference': fields.char(
-            'Law reference', size=128),
-    }
-
-    def get_tax_by_invoice_tax(self, cr, uid, invoice_tax, context=None):
-        if ' - ' in invoice_tax:
-            tax_descr = invoice_tax.split(' - ')[0]
-            tax_ids = self.search(cr, uid, [
-                ('description', '=', tax_descr),
-                ], context=context)
-            if not tax_ids:
-                raise orm.except_orm(
-                    _('Error'), _('No tax %s found') %
-                    tax_descr)
-            if len(tax_ids) > 1:
-                raise orm.except_orm(
-                    _('Error'), _('Too many tax %s found') %
-                    tax_descr)
-        else:
-            tax_name = invoice_tax
-            tax_ids = self.search(cr, uid, [
-                ('name', '=', tax_name),
-                ], context=context)
-            if not tax_ids:
-                raise orm.except_orm(
-                    _('Error'), _('No tax %s found') %
-                    tax_name)
-            if len(tax_ids) > 1:
-                raise orm.except_orm(
-                    _('Error'), _('Too many tax %s found') %
-                    tax_name)
-        return tax_ids[0]
+    non_taxable_nature = fields.Selection([
+        ('N1', 'escluse ex art. 15'),
+        ('N2', 'non soggette'),
+        ('N3', 'non imponibili'),
+        ('N4', 'esenti'),
+        ('N5', 'regime del margine'),
+        ('N6', 'inversione contabile (reverse charge)'),
+        ('N7', 'IVA assolta in altro stato UE'),
+        ], string="Non taxable nature",
+        help="N7: (vendite a distanza ex art. 40 c. 3 e 4 e art. 41 c. 1 lett."
+             "b, DL 331/93; prestazione di servizi di telecomunicazioni, "
+             "tele-radiodiffusione ed elettronici ex art. 7-sexies lett. f, g,"
+             "art. 74-sexies DPR 633/72)"
+    )
+    payability = fields.Selection([
+        ('I', 'Immediate payability'),
+        ('D', 'Deferred payability'),
+        ('S', 'Split payment'),
+        ], string="VAT payability")
+    law_reference = fields.Char(
+        'Law reference', size=128)
