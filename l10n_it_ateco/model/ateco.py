a5672d2b43213940b9f8ade5f7c0b5b4da1566f4
Ciro Urselli <c.urselli@apuliasoftware.it>
2016-10-20 15:57:24 +0200

    [FIX] delete duplicate id in ateco_data.xml and change encoding utf-8 with coding-utf8
    [REF] pep8

--- l10n_it_ateco/model/ateco.py
+++ l10n_it_ateco/model/ateco.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
 #    Copyright (C) 2014 Abstract (<http://abstract.it>).
 #    Copyright (C) 2016 Ciro Urselli (<http://www.apuliasoftware.it>).
@@ -21,6 +21,6 @@ class AtecoCategory(models.Model):
     child_ids = fields.One2many(
         'ateco.category', 'parent_id', 'Child Categories')
     partner_ids = fields.Many2many(
-        'res.partner','ateco_category_partner_rel',
+        'res.partner', 'ateco_category_partner_rel',
         'ateco_id', 'partner_id', 'Partners'
     )
