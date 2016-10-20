33f336d40bb16143469d0cf475e48dfc887ccf24
Ciro Urselli <c.urselli@apuliasoftware.it>
2016-10-20 15:45:58 +0200

    [MIG] l10n_it_ateco: Migrated to 10.0

--- l10n_it_ateco/__manifest__.py
+++ l10n_it_ateco/__manifest__.py
@@ -1,40 +1,19 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
 #
-#    Copyright (C) 2014 Abstract
-#    (<http://abstract.it>).
+#    Copyright (C) 2014 Abstract (<http://abstract.it>).
+#    Copyright (C) 2016 Ciro Urselli (<http://www.apuliasoftware.it>).
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     "name": "Ateco codes",
-    "version": "8.0.1.0.0",
-    "category": "Localisation/Italy",
-    "description": """Italian Localisation module - Ateco codes
-
-    Funcionalities:
-
-    - Add Ateco codes model
-    - Reference Ateco codes to partner model
-
-    """,
-    "author": "Abstract,Odoo Community Association (OCA)",
+    "version": "10.0.1.0.0",
+    "category": "Localization/Italy",
+    "author": "Abstract,Odoo Community Association (OCA),Odoo Italia Network",
     "website": "http://abstract.it",
     "license": "AGPL-3",
     "depends": [
-        "base"
+        "sales_team"
     ],
     "data": [
         "security/ir.model.access.csv",
@@ -42,6 +21,5 @@
         "view/partner_view.xml",
         "data/ateco_data.xml"
     ],
-    "active": False,
-    'installable': False
+    'installable': True
 }
--- l10n_it_ateco/model/ateco.py
+++ l10n_it_ateco/model/ateco.py
@@ -1,57 +1,26 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
 #
-#    Copyright (C) 2014 Abstract
-#    (<http://abstract.it>).
+#    Copyright (C) 2014 Abstract (<http://abstract.it>).
+#    Copyright (C) 2016 Ciro Urselli (<http://www.apuliasoftware.it>).
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models
-from openerp import fields
+#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
 
 
-class ateco_category(models.Model):
+class AtecoCategory(models.Model):
+
     _name = 'ateco.category'
     _description = 'ATECO Code'
 
-    name = fields.Char(
-        'Name',
-        required=True
-    )
-    code = fields.Char(
-        'ATECO Code',
-        size=9,
-        required=False
-    )
-    description = fields.Text(
-        'Description'
-    )
+    name = fields.Char('Name', required=True)
+    code = fields.Char('ATECO Code', size=9, required=False)
+    description = fields.Text('Description')
     parent_id = fields.Many2one(
-        'ateco.category',
-        'Parent Category',
-        select=True
-    )
+        'ateco.category', 'Parent Category', select=True)
     child_ids = fields.One2many(
-        'ateco.category',
-        'parent_id',
-        'Child Categories'
-    )
+        'ateco.category', 'parent_id', 'Child Categories')
     partner_ids = fields.Many2many(
-        'res.partner',
-        'ateco_category_partner_rel',
-        'ateco_id',
-        'partner_id',
-        'Partners'
+        'res.partner','ateco_category_partner_rel',
+        'ateco_id', 'partner_id', 'Partners'
     )
