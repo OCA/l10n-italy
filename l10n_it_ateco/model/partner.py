42e511a69b74313f0ec7d111e6d0968572a0b18e
Ciro Urselli <c.urselli@apuliasoftware.it>
2016-10-20 15:45:58 +0200

    [MIG] l10n_it_ateco: Migrated to 10.0

--- l10n_it_ateco/model/partner.py
+++ l10n_it_ateco/model/partner.py
@@ -1,28 +1,15 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
 #
-#    Copyright (C) 2014 Abstract
-#    (<http://abstract.it>).
+#    Copyright (C) 2014 Abstract (<http://abstract.it>).
+#    Copyright (C) 2016 Ciro Urselli (<http://www.apuliasoftware.it>).
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models
-from openerp import fields
+#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
 
 
-class res_partner(models.Model):
+class ResPartner(models.Model):
+
     _inherit = 'res.partner'
 
     ateco_category_ids = fields.Many2many(
