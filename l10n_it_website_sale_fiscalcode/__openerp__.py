086355fb5bd9fc939cc2f6ba16b80bd649f7cbe3
Nicola Malcontenti <nicola.malcontenti@agilebg.com>
2016-10-24 18:27:42 +0200

    [ADD] Refactoring of module l10n_it_website_sale_fiscalcode

---
+++ l10n_it_website_sale_fiscalcode/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Nicola Malcontenti - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Website Sale FiscalCode',
+    'category': 'e-commerce',
+    'author': "Agile Business Group,"
+              "Odoo Community Association (OCA)",
+    'version': '9.0.1.0.0',
+    'author': 'Agile Business Group',
+    'website': 'http://www.agilebg.com',
+    'depends': [
+        'l10n_it_fiscalcode',
+        'website_sale'
+    ],
+    'data': ['views/templates.xml'],
+    'installable': True,
+}
