f3a551113d7ca160b39b154c1a3888659c24af82
Andrea Cometa <info@andreacometa.it>
2016-10-28 15:48:45 +0200

    [add] account_invoice_report_ddt_group, fixed headers, deps and imports
    [add] group by ddt in report
    
    [fix] group by line method
    
    [fix] grouping bug
    
    [del] _create_invoice_line_from_vals

---
+++ account_invoice_report_ddt_group/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2015 Nicola Malcontenti - Agile Business Group
+# © 2016 Andrea Cometa - Apulia Software
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+{
+    'name': "Account invoice report grouped by DDT",
+    'version': '9.0.1.0.0',
+    'description': """
+This module sets invoice line origin as ddt name.
+""",
+    'author': 'Agile Business Group, Apulia Software',
+    'website': 'http://www.agilebg.com',
+    'license': 'GPL-3',
+    'depends': [
+        'account', 'sale_layout', 'stock_picking_invoice_link', 'l10n_it_ddt',
+    ],
+    "data": [
+        'views/invoice_ddt.xml',
+    ],
+    "installable": True
+}
