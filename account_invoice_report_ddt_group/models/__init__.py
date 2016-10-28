43bafc887cb13abe9d9a54a55e38563e930e2559
Andrea Cometa <info@andreacometa.it>
2016-10-28 15:48:45 +0200

    [add] account_invoice_report_ddt_group, fixed headers, deps and imports

---
+++ account_invoice_report_ddt_group/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2015 Nicola Malcontenti - Agile Business Group
+# © 2016 Andrea Cometa - Apulia Software
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from . import stock_move
---
+++ account_invoice_report_ddt_group/models/stock_move.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2015 Nicola Malcontenti - Agile Business Group
+# © 2016 Andrea Cometa - Apulia Software
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from openerp.osv import fields, osv
+
+
+class stock_move(osv.osv):
+    _inherit = "stock.move"
+
+    def _create_invoice_line_from_vals(
+            self, cr, uid, move, invoice_line_vals, context=None):
+
+        invoice_line_vals['origin'] = move.picking_id.ddt_ids.ddt_number
+        return super(stock_move, self)._create_invoice_line_from_vals(
+            cr, uid, move, invoice_line_vals, context)
