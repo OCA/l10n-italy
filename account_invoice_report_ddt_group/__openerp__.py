13a1e8966b82bc8915e3dccb5604c7c0c8ce318c
eLBati <lorenzo.battistini@agilebg.com>
2016-11-03 15:01:45 +0100

    FIX handle multiple DDTs for the same line: if you generate several DDT from the same sale order and you invoice the whole SO, the module must not print the same invoice line several times

--- account_invoice_report_ddt_group/__openerp__.py
+++ account_invoice_report_ddt_group/__openerp__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2015 Nicola Malcontenti - Agile Business Group
 # © 2016 Andrea Cometa - Apulia Software
+# © 2016 Lorenzo Battistini - Agile Business Group
 # License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
 {
