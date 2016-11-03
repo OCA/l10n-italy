1dd970d1e4cecbeeb7de1071af59b96d392b00bb
eLBati <lorenzo.battistini@agilebg.com>
2016-11-03 15:01:45 +0100

    FIX handle multiple DDTs for the same line: if you generate several DDT from the same sale order and you invoice the whole SO, the module must not print the same invoice line several times

--- account_invoice_report_ddt_group/models/account_invoice.py
+++ account_invoice_report_ddt_group/models/account_invoice.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2015 Alex Comba - Agile Business Group
 # © 2016 Andrea Cometa - Apulia Software
+# © 2016 Lorenzo Battistini - Agile Business Group
 # License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
 from itertools import groupby
@@ -14,26 +15,37 @@ class AccountInvoice(models.Model):
     def grouped_lines_by_ddt(self):
         """
         Returns invoice lines from a specified invoice grouped by ddt
-
-        :Parameters:
-            -'invoice_id' (int): specify the concerned invoice.
         """
         all_lines = self.invoice_line_ids
-        ddt_lines_dict = []
+        ddt_dict = {}
         for line in all_lines:
+            # group by recordset (that can be empty or bigger then 1)
             ddt_lines = line.get_ddt_lines()
-            for ddt_line in ddt_lines:
-                ddt_lines_dict.append(
-                    {'ddt': ddt_line.package_preparation_id.ddt_number,
-                     'line': line})
-            if not ddt_lines:
-                ddt_lines_dict.append({'ddt': '', 'line': line})
-
-        lines = sorted(ddt_lines_dict, key=lambda x: x['ddt'])
+            ddts = ddt_lines.mapped('package_preparation_id')
+            if ddts not in ddt_dict:
+                ddt_dict[ddts] = [line]
+            else:
+                ddt_dict[ddts].append(line)
+        # convert recordset to string.
         group = {}
-        for key, valuesiter in groupby(lines, key=lambda x: x['ddt']):
-            group[key] = list(v['line'] for v in valuesiter)
-
+        for key in ddt_dict:
+            if not key:
+                # empty recordset
+                string_key = ''
+            else:
+                string_key = ''
+                for ddt in key:
+                    if ddt.ddt_number:
+                        if string_key:
+                            string_key += ', %s' % ddt.ddt_number
+                        else:
+                            string_key = ddt.ddt_number
+            # group dict can be different from ddt_dict,
+            # e.g. when DDT does not have a number yet
+            if string_key not in group:
+                group[string_key] = ddt_dict[key]
+            else:
+                group[string_key].append(ddt_dict[key])
         return group
 
 
