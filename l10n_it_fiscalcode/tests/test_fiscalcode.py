21ab8659cfe6e8c190f0e5738dfdb5c70d6e6774
Giuliano59 <giuliano.lotta@gmail.com>
2016-11-16 09:30:27 +0100

    modified:   l10n_it_fiscalcode/tests/test_fiscalcode.py - moved test code from fiscalcode check in wizard, to validation of constrains in partner record creation

--- l10n_it_fiscalcode/tests/test_fiscalcode.py
+++ l10n_it_fiscalcode/tests/test_fiscalcode.py
@@ -12,20 +12,48 @@ class TestFiscalCode(TransactionCase):
     # is rolled back after each.
     def setUp(self):
         super(TestFiscalCode, self).setUp()
-        self.partner = self.env.ref('base.partner_root')
-        self.partner.email = "foo@gmail.com"
 
+    def test_newpartner_fiscalcode(self):
+        """ save a new partner with a standard fiscalcode,
+        omocodia fiscalcode, and VAT fiscalcode (compamy)
+        constrain method will check it and grant right to be saved
+        """
+        # fix test error because no email found during partner creation
+        # modification rolled back after TransactionCase end
+        partner_root = self.env.ref('base.partner_root')
+        partner_root.email = u"foo@gmail.com"
 
-    def test_fiscalcode_compute(self):
-        wizard = self.env['wizard.compute.fc'].with_context(
-            active_id=self.partner.id).create({
-                'fiscalcode_surname': 'ROSSI',
-                'fiscalcode_firstname': 'MARIO',
-                'birth_date': '1984-06-04',
-                'sex': 'M',
-                'birth_city': 10048,
-                'birth_province': 10048,
-            })
-        # ---- Compute FiscalCode
-        wizard.compute_fc()
-        self.assertEqual(self.partner.fiscalcode, 'RSSMRA84H04H501X')
+        partner_model = self.env['res.partner'].sudo(
+            self.ref('base.partner_root'))
+
+        # get Italian country_id for Italian citizen/companies creation
+        italy_id = self.env['res.country'].search('code', '=', 'IT')
+
+        # normal fiscalcode for private citizen
+        self.partner = partner_model.create(
+            {'name': u'Test1 Private Italian Citizen',
+             'email': u"foo@gmail.com",
+             'is_company': 'False',
+             'country_id': italy_id,
+             'fiscalcode': 'BNZVCN32S10E573Z',
+             })
+
+        # special fiscalcode with omocodia for private citizen
+        # last "3" character, become a "P" because of omocodia
+        # CRC code changes accordingly
+        self.partner = partner_model.create(
+            {'name': u'Test2 Private Italian Citizen',
+             'email': u"foo@gmail.com",
+             'is_company': 'False',
+             'country_id': italy_id,
+             'fiscalcode': 'BNZVCN32S10E57PV',
+             })
+
+        # Italian company has the fiscalcode like a VAT number
+        self.partner = partner_model.create(
+            {'name': u'Test3 Italian Company',
+             'email': u"foo@gmail.com",
+             'is_company': 'True',
+             'country_id': italy_id,
+             'fiscalcode': '08106710158',
+             })
