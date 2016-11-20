4b163f02c8d0428a469cd139524f604731715fcb
Giuliano59 <giuliano.lotta@gmail.com>
2016-11-20 14:01:15 +0100

    modified:   l10n_it_fiscalcode/static/description/icon.png - changed icon to the Italian fiscal code card 	modified:   l10n_it_fiscalcode/tests/test_fiscalcode_wizard.py - changed environment (partner) before wizard call

--- l10n_it_fiscalcode/tests/test_fiscalcode_wizard.py
+++ l10n_it_fiscalcode/tests/test_fiscalcode_wizard.py
@@ -8,12 +8,28 @@ from odoo.tests.common import TransactionCase
 
 
 class TestFiscalCodeWizard(TransactionCase):
+    """ Each test method is run independently and the database transaction
+    is rolled back after each.
+    """
 
     def setUp(self):
+        """initializes the self.env attribute The test runner will
+        run then the tests one after the other with a call to setUp()
+        before each test method and a call to tearDown() after each
+        To test the fiscal code for a private citizen, se need to create
+        a transient partner with is_company = False
+        """
         super(TestFiscalCodeWizard, self).setUp()
-        self.partner = self.env.ref('base.res_partner_2')
+        partner_model = self.env['res.partner']
+        self.partner = partner_model.create(
+            {'name': u'Rossi Mario',
+             'email': u"mario.rossi@gmail.com",
+             'is_company': False, })
 
     def test_fiscalcode_compute(self):
+        """ test that for a private citizen the fiscal code is
+        computed correctly
+        """
         wizard = self.env['wizard.compute.fc'].with_context(
             active_id=self.partner.id).create({
                 'fiscalcode_surname': 'ROSSI',
