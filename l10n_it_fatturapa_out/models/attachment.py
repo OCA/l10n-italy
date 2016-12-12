760c134a097780edc363be19aaf34f87e699e307
eLBati <lorenzo.battistini@agilebg.com>
2016-12-12 16:40:35 +0100

    l10n_it_fatturapa_out porting to 10

--- l10n_it_fatturapa_out/models/attachment.py
+++ l10n_it_fatturapa_out/models/attachment.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2014 Davide Corio
+# Copyright 2016 Lorenzo Battistini - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.osv import fields, orm
+from odoo import fields, models
 
 
-class FatturaPAAttachment(orm.Model):
+class FatturaPAAttachment(models.Model):
     _name = "fatturapa.attachment.out"
     _description = "FatturaPA Export File"
     _inherits = {'ir.attachment': 'ir_attachment_id'}
     _inherit = ['mail.thread']
 
-    _columns = {
-        'ir_attachment_id': fields.many2one(
-            'ir.attachment', 'Attachment', required=True, ondelete="cascade"),
-        'out_invoice_ids': fields.one2many(
-            'account.invoice', 'fatturapa_attachment_out_id',
-            string="Out Invoices", readonly=True),
-    }
+    ir_attachment_id = fields.Many2one(
+        'ir.attachment', 'Attachment', required=True, ondelete="cascade")
+    out_invoice_ids = fields.One2many(
+        'account.invoice', 'fatturapa_attachment_out_id',
+        string="Out Invoices", readonly=True)
