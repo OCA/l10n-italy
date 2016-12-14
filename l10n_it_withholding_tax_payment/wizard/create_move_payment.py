af67aa03feb02274391226daccc685d04f47b4a5
alessandrocamilli <a.camilli@yahoo.it>
2016-12-14 19:39:00 +0100

    WT move and account move creation from reconcile
    [FIX] 10.0 api compat
    
    First beta
    
    Travis check
    
    Ref name
    
    [FIX] Compute WT only if the invoice has wt lines
    
    Added feature to unreconcile the wt move created by payment, and after it possible to re-reconcile

---
+++ l10n_it_withholding_tax_payment/wizard/create_move_payment.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2015 Alessandro Camilli (<http://www.openforce.it>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp import models, fields, api
+
+
+class WizardWtMovePaymentCreate(models.TransientModel):
+    _name = 'wizard.wt.move.payment.create'
+    _description = 'WT wizard create move payment'
+
+    @api.model
+    def default_get(self, fields):
+        res = super(WizardWtMovePaymentCreate, self).default_get(fields)
+        active_ids = self._context.get('active_ids', [])
+        res = {
+            'wt_move_ids': active_ids
+        }
+        return res
+
+    wt_move_ids = fields.Many2many(
+        'withholding.tax.move', 'wiz_wt_move_payment_create_rel',
+        'wizard_id', 'wt_move_id', 'Wt Moves', readonly=True)
+
+    def generate(self):
+        wt_move_payment_obj = self.env['withholding.tax.move.payment']
+        wt_move_payment_obj.generate_from_moves(self.wt_move_ids)
+
+        return {'type': 'ir.actions.act_window_close'}
