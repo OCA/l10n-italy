bb576569e8694b7c4235a19e01b4a038b7e92542
eLBati <lorenzo.battistini@agilebg.com>
2017-01-10 17:36:46 +0100

    IMP l10n_it_fatturapa_out: handle ScontoMaggiorazione
    FIX l10n_it_fatturapa LINT for pyXB
    and avoid flake8 for pyxb generated files
    
    FIX l10n_it_fatturapa metadata
    
    FIX l10n_it_fatturapa: The model fatturapa.format has no access rules, consider adding one

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2014 Davide Corio
+# Copyright 2014 Davide Corio
 # Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 import base64
-from unidecode import unidecode
-
-from pyxb.exceptions_ import SimpleFacetValueError, SimpleTypeValueError
+import logging
 
 from odoo import models
 from odoo.tools.translate import _
@@ -35,10 +33,19 @@ from odoo.addons.l10n_it_fatturapa.bindings.fatturapa_v_1_2 import (
     ContattiType,
     DatiPagamentoType,
     DettaglioPagamentoType,
-    AllegatiType
+    AllegatiType,
+    ScontoMaggiorazioneType
 )
 from odoo.addons.l10n_it_fatturapa.models.account import (
     RELATED_DOCUMENT_TYPES)
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from unidecode import unidecode
+    from pyxb.exceptions_ import SimpleFacetValueError, SimpleTypeValueError
+except ImportError as err:
+    _logger.debug(err)
 
 
 class WizardExportFatturapa(models.TransientModel):
@@ -63,7 +70,6 @@ class WizardExportFatturapa(models.TransientModel):
     def setProgressivoInvio(self, fatturapa):
 
         company = self.env.user.company_id
-        sequence_obj = self.pool['ir.sequence']
         fatturapa_sequence = company.fatturapa_sequence_id
         if not fatturapa_sequence:
             raise UserError(
@@ -488,6 +494,12 @@ class WizardExportFatturapa(models.TransientModel):
                     unidecode(line.uom_id.name)) or None,
                 PrezzoTotale='%.2f' % line.price_subtotal,
                 AliquotaIVA=AliquotaIVA)
+            if line.discount:
+                ScontoMaggiorazione = ScontoMaggiorazioneType(
+                    Tipo='SC',
+                    Percentuale='%.2f' % line.discount
+                )
+                DettaglioLinea.ScontoMaggiorazione.append(ScontoMaggiorazione)
             if aliquota == 0.0:
                 if not line.invoice_line_tax_ids[0].non_taxable_nature:
                     raise UserError(
@@ -504,7 +516,6 @@ class WizardExportFatturapa(models.TransientModel):
 
             # el.remove(el.find('DataInizioPeriodo'))
             # el.remove(el.find('DataFinePeriodo'))
-            # el.remove(el.find('ScontoMaggiorazione'))
             # el.remove(el.find('Ritenuta'))
             # el.remove(el.find('AltriDatiGestionali'))
 
@@ -513,7 +524,6 @@ class WizardExportFatturapa(models.TransientModel):
         return True
 
     def setDatiRiepilogo(self, invoice, body):
-        tax_pool = self.pool['account.tax']
         for tax_line in invoice.tax_line_ids:
             tax = tax_line.tax_id
             riepilogo = DatiRiepilogoType(
