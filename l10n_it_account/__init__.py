dabf5b7570a7d8e0f0a8f67de7143a87de62aee5
eLBati <lorenzo.battistini@agilebg.com>
2017-01-11 19:44:02 +0100

    porting l10n_it_vat_registries to 10: 2nd part
    ADD date_range support
    
    FIX missing only_totals
    
    IMP translations
    
    REF _tax_amounts_by_tax_id: do not depend on invoices
    
    ADD account_accountant dep

--- l10n_it_account/__init__.py
+++ l10n_it_account/__init__.py
@@ -1,25 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2010 OpenERP Italian Community
-#    (<http://www.openerp-italia.org>).
-#    Copyright (C) 2014 Associazione Odoo Italia
-#    (<http://www.openerp-italia.org>).
-#    Copyright (C) 2015 Agile Business Group (<http://www.agilebg.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-
+# Copyright 2010 OpenERP Italian Community
+# (<http://www.openerp-italia.org>).
+# Copyright 2014 Associazione Odoo Italia
+# (<http://www.openerp-italia.org>).
+# Copyright 2015-2017 Agile Business Group (<http://www.agilebg.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
