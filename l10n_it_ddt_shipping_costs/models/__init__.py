cfba40c82d0b4cf1462e9a8dc84dbe99ddb05009
Andrea Cometa <a.cometa@apuliasoftware.it>
2017-03-17 18:52:46 +0100

    [imp] automatically adds shipment costs on put in pack

--- l10n_it_ddt_shipping_costs/models/__init__.py
+++ l10n_it_ddt_shipping_costs/models/__init__.py
@@ -2,4 +2,6 @@
 # Copyright 2017 Andrea Cometa - Apulia Software
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+
 from . import company
+from . import stock_picking_package_preparation
---
+++ l10n_it_ddt_shipping_costs/models/stock_picking_package_preparation.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Andrea Cometa - Apulia Software
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api, exceptions, _
+
+
+class StockPickingPackagePreparation(models.Model):
+
+    _inherit = 'stock.picking.package.preparation'
+    _rec_name = 'display_name'
+
+    @api.multi
+    def action_put_in_pack(self):
+        # we add a the shipping cost
+        for cost in self.company_id.shipment_cost_ids:
+            if cost.lower_limit < self.amount_untaxed < cost.upper_limit:
+                self.env['stock.picking.package.preparation.line'].create({
+                    'package_preparation_id': self.id,
+                    'product_id': cost.shipment_cost_product_id.id,
+                    'product_uom_qty': 1.0,
+                    'product_uom': cost.shipment_cost_product_id.uom_id.id,
+                    'name': cost.shipment_cost_product_id.name,
+                })
+        return super(StockPickingPackagePreparation, self).action_put_in_pack()
