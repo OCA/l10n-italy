7fd395cb70aa4d824ef5e16681a190964a1f3936
Andrea Cometa <a.cometa@apuliasoftware.it>
2017-03-18 08:33:37 +0100

    [add] translation

--- l10n_it_ddt_shipping_costs/__openerp__.py
+++ l10n_it_ddt_shipping_costs/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'DDT Shipment Costs',
+    'name': 'DDT Shipping Costs',
     'version': '8.0.1.0.0',
     'category': 'Localization/Italy',
     'summary': 'DDT Spese di trasporto',
