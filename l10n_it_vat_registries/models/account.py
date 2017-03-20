27b19c60548bf3cb98e748087be11e9bda745045
Lara Baggio <lbaggio@linkgroup.it>
2017-03-20 15:59:32 +0100

    Aggiunta flag cee_type all'imposta figlia per la corretta visualizzazione nel registro IVA
    aggiunte traduzioni e calcolo sul totale registro iva

--- l10n_it_vat_registries/models/account.py
+++ l10n_it_vat_registries/models/account.py
@@ -16,6 +16,13 @@ class AccountTax(models.Model):
     parent_tax_ids = fields.Many2many(
         'account.tax', 'account_tax_filiation_rel', 'child_tax', 'parent_tax',
         string='Parent Taxes')
+    
+    cee_type = fields.Selection([
+            ('sale', 'Sale'),
+            ('purchase', 'Purchase')
+            ], string='Include in VAT registries',
+            help="""Use in the case of tax on 'VAT integration'. specifies the VAT
+             register (sales / purchases) it has to be computed the tax.""")
 
     def get_balance_domain(self, state_list, type_list):
         domain = super(AccountTax, self).get_balance_domain(
