ab2929a123049b8f17c420bd6e8b0eef82b95860
eLBati <lorenzo.battistini@agilebg.com>
2017-03-28 10:20:14 +0200

    FIX l10n_it_ricevute_bancarie: clean up files porting to new API porting to new accounting porting views porting tests
    FIX l10n_it_ricevute_bancarie
    wrong computation of payment_ids
    and tests
    
    ADD OCA dep for account_due_list

--- l10n_it_ricevute_bancarie/wizard/wizard_riba_issue.py
+++ l10n_it_ricevute_bancarie/wizard/wizard_riba_issue.py
@@ -1,30 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2012 Andrea Cometa.
-#    Email: info@andreacometa.it
-#    Web site: http://www.andreacometa.it
-#    Copyright (C) 2012-2015 Agile Business Group <http://www.agilebg.com>
-#    Copyright (C) 2012 Domsense srl (<http://www.domsense.com>)
-#    Copyright (C) 2012 Associazione OpenERP Italia
-#    (<http://www.odoo-italia.org>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2012 Andrea Cometa.
+# Email: info@andreacometa.it
+# Web site: http://www.andreacometa.it
+# Copyright (C) 2012 Associazione OpenERP Italia
+# (<http://www.odoo-italia.org>).
+# Copyright (C) 2012-2017 Lorenzo Battistini - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models, exceptions, api, _
+from odoo import fields, models, exceptions, api, _
 
 
 # -------------------------------------------------------
@@ -62,7 +45,7 @@ class RibaIssue(models.TransientModel):
 
         # create distinta
         rd = {
-            'name': self.env['ir.sequence'].get('seq.riba.distinta'),
+            'name': self.env['ir.sequence'].next_by_code('seq.riba.distinta'),
             'config_id': self.configuration_id.id,
             'user_id': self._uid,
             'date_created': fields.Date.context_today(self),
@@ -90,7 +73,6 @@ class RibaIssue(models.TransientModel):
                 bank_id = move_line.partner_id.bank_ids[0]
             else:
                 raise exceptions.Warning(
-                    _('Attention!'),
                     _('Partner %s has not bank!!!') %
                     move_line.partner_id.name)
             if move_line.partner_id.group_riba:
