417f0a674205b6e3214596f2f9cab23bdb6b00a4
eLBati <lorenzo.battistini@agilebg.com>
2017-04-20 16:13:10 +0200

    FIX l10n_it_ddt PEP8 field names authors related field allow to invoice a DDT not linked to a SO ADD tests for invoicing
    IMP  to_be_invoiced help

--- l10n_it_ddt/__manifest__.py
+++ l10n_it_ddt/__manifest__.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
-##############################################################################
 #
 #    Copyright (C) 2014 Abstract (http://www.abstract.it)
 #    @author Davide Corio <davide.corio@abstract.it>
-#    Copyright (C) 2014-2015 Agile Business Group (http://www.agilebg.com)
+#    Copyright (C) 2014-2017 Agile Business Group (http://www.agilebg.com)
 #    Copyright (C) 2015 Apulia Software s.r.l. (http://www.apuliasoftware.it)
 #    @author Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
 #
 #    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 #
-##############################################################################
 
 {
     'name': 'DDT',
-    'version': '10.0.1.0.2',
+    'version': '10.0.1.1.0',
     'category': 'Localization/Italy',
     'summary': 'Documento di Trasporto',
     'author': 'Davide Corio, Odoo Community Association (OCA),'
-              'Agile Business Group, Francesco Apruzzese, Apulia Software, '
+              'Agile Business Group, Francesco Apruzzese, '
               'Openforce di Camilli Alessandro',
-    'website': 'http://www.odoo-italia.org/',
+    'website': 'https://odoo-community.org/',
     'license': 'AGPL-3',
     'depends': [
         'sale_stock',
