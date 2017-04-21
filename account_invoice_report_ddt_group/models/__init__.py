78427b2faa6926765d4cf7f1174b423d3cb1b28b
eLBati <lorenzo.battistini@agilebg.com>
2017-04-21 15:59:43 +0200

    porting account_invoice_report_ddt_group to 10.0

--- account_invoice_report_ddt_group/models/__init__.py
+++ account_invoice_report_ddt_group/models/__init__.py
@@ -2,6 +2,6 @@
 # © 2015 Nicola Malcontenti - Agile Business Group
 # © 2016 Andrea Cometa - Apulia Software
 # © 2016 Lorenzo Battistini - Agile Business Group
-# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from . import account_invoice
--- account_invoice_report_ddt_group/tests/__init__.py
+++ account_invoice_report_ddt_group/tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # © 2016 Andrea Cometa - Apulia Software
-# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from . import test_invoice_ddt_group
