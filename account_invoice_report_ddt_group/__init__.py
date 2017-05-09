82754d8e5fbe21207f7c536684d9e9991c074c8f
Alex Comba <alex.comba@agilebg.com>
2017-05-09 15:21:15 +0200

    [IMP] account_invoice_report_ddt_group: adapt code to latest OCA conventions

--- account_invoice_report_ddt_group/__init__.py
+++ account_invoice_report_ddt_group/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015 Nicola Malcontenti - Agile Business Group
-# © 2016 Andrea Cometa - Apulia Software
+# Copyright 2015 Nicola Malcontenti - Agile Business Group
+# Copyright 2016 Andrea Cometa - Apulia Software
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from . import models
--- account_invoice_report_ddt_group/models/account_invoice.py
+++ account_invoice_report_ddt_group/models/account_invoice.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2015 Alex Comba - Agile Business Group
-# © 2016 Andrea Cometa - Apulia Software
-# © 2016-2017 Lorenzo Battistini - Agile Business Group
+# Copyright 2015 Alex Comba - Agile Business Group
+# Copyright 2016 Andrea Cometa - Apulia Software
+# Copyright 2016-2017 Lorenzo Battistini - Agile Business Group
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from openerp import models, api, fields
