7a8b1c1a4757e2929773eb245a3da1ca0c4ce5ee
Alex Comba <alex.comba@agilebg.com>
2017-05-09 15:33:02 +0200

    [IMP] account_invoice_report_ddt_group: avoid get_ddt_lines as useless

--- account_invoice_report_ddt_group/__manifest__.py
+++ account_invoice_report_ddt_group/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': "Account invoice report grouped by DDT",
-    'version': '10.0.0.2.0',
+    'version': '10.0.0.3.0',
     'category': 'Localization/Italy',
     'author': 'Agile Business Group, Apulia Software, '
               'Odoo Community Association (OCA)',
