ee759011480a6013ce949c975864e852d643e980
eLBati <lorenzo.battistini@agilebg.com>
2017-05-31 09:42:46 +0200

    l10n_it_ddt ADD weight_manual to force weght when different from the computed one Ported from d3c2e74727583f48d535774943a7a41369b4f654

--- l10n_it_ddt/tests/test_ddt.py
+++ l10n_it_ddt/tests/test_ddt.py
@@ -184,6 +184,9 @@ class TestDdt(TransactionCase):
         ddt2 = self.ddt_model.browse(res['res_id'])
         ddt1.action_put_in_pack()
         ddt1.action_done()
+        self.assertEqual(ddt1.weight, 0)
+        ddt1.weight_manual = 10
+        self.assertEqual(ddt1.weight, 10)
         ddt2.action_put_in_pack()
         ddt2.action_done()
         wizard = self._create_invoice_wizard([ddt1.id, ddt2.id])
