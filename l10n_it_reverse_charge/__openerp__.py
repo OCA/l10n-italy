dde71b8a53a60f906ab4ae426a56cbb0241cc736
eLBati <lorenzo.battistini@agilebg.com>
2017-06-13 17:53:52 +0200

    l10n_it_reverse_charge ADD "With additional supplier self invoice" to enable the creation of an additional supplier self invoice. This is tipically used for extraUE suppliers, in order to show, in supplier register, an invoice to the specified partner (tipically, my company), instead of the extraUE partner
    Also FIX
    ValueError: "local variable 'tax_code_id' referenced before assignment" while evaluating
    u'invoice_validate()'
    when tax mapping not found

--- l10n_it_reverse_charge/__openerp__.py
+++ l10n_it_reverse_charge/__openerp__.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Davide Corio
 # Copyright 2017 Alex Comba - Agile Business Group
+# Copyright 2017 Lorenzo Battistini - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Reverse Charge IVA',
-    'version': '8.0.1.0.1',
+    'version': '8.0.2.0.0',
     'category': 'Localization/Italy',
     'summary': 'Reverse Charge for Italy',
     'author': 'Abstract, Agile Business Group,'
               'Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'website': 'http://www.abstract.it',
+    'website': 'https://www.odoo-italia.net',
     'depends': [
-        'account',
+        'account_accountant',
         'account_invoice_entry_date',
         'account_cancel',
     ],
