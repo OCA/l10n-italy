40a101b392b4b623f6a14b9d64ebb379962c9b38
alessandrocamilli <a.camilli@yahoo.it>
2017-06-23 09:58:00 +0200

    Order group by ddt number
    Label DDT if doesn't exist in ref
    
    Fix test_flake8
    
    Added option in the partner to print shipping address of ddt in the invoice report
    
    Added author
    
    Fix view position

--- account_invoice_report_ddt_group/models/__init__.py
+++ account_invoice_report_ddt_group/models/__init__.py
@@ -4,4 +4,4 @@
 # Copyright 2016 Lorenzo Battistini - Agile Business Group
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from . import account_invoice
+from . import account_invoice, partner
