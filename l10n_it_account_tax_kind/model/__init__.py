e4636015bbef5d162ecc46c602d42e21c74b4a34
Francesco Apruzzese <cescoap@gmail.com>
2017-09-11 17:28:43 +0200

    [ADD] l10n_it_account_tax_kind
    [FIX] Depends for display_name compute field
    
    [ADD] Readme file
    
    [ADD] Italian translation

---
+++ l10n_it_account_tax_kind/model/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2017 Apulia Software s.r.l. (http://www.apuliasoftware.it)
+#    @author Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
+#
+#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import account_tax_kind
+from . import account
