c761c339dbe48d16ca0525fb4ebfc4f4943eea52
eLBati <lorenzo.battistini@agilebg.com>
2017-09-26 12:03:26 +0200

    IMP l10n_it_esigibilita_iva metadata

--- l10n_it_esigibilita_iva/__openerp__.py
+++ l10n_it_esigibilita_iva/__openerp__.py
@@ -2,17 +2,13 @@
 # Copyright 2017 Alessandro Camilli - Openforce
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-
 {
     'name': 'Esigibilità IVA',
-    'summary':
-        'Aggiunge alla tassa il campo esigibilità da usare nei dichiarativi'
-        ' fiscali italiani',
     'version': '8.0.1.0.0',
     'category': 'Account',
     'author': "Openforce di Camilli Alessandro, "
-        "Odoo Community Association (OCA)",
-    'website': 'http://odoo-italia.net',
+              "Odoo Community Association (OCA)",
+    'website': 'https://www.odoo-italia.net',
     'license': 'LGPL-3',
     'depends': [
         'account',
