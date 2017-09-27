6b08b935b7fb2961960ac0931ee5ac85e84a62dc
eLBati <lorenzo.battistini@agilebg.com>
2017-09-27 17:35:54 +0200

    porting l10n_it_account_tax_kind to 10.0
    ADD law_reference

--- l10n_it_account_tax_kind/model/account_tax_kind.py
+++ l10n_it_account_tax_kind/model/account_tax_kind.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-#
-#    Copyright (C) 2017 Apulia Software s.r.l. (http://www.apuliasoftware.it)
-#    @author Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
-#
-#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from openerp import api, models, fields
+from odoo import api, models, fields
 
 
 class AccountTaxKind(models.Model):
