8d673db2b81b07d200d349c9ef96ca92af304c32
eLBati <lorenzo.battistini@agilebg.com>
2017-09-27 17:35:54 +0200

    porting l10n_it_account_tax_kind to 10.0
    ADD law_reference

--- l10n_it_account_tax_kind/__manifest__.py
+++ l10n_it_account_tax_kind/__manifest__.py
@@ -3,17 +3,17 @@
 #    Copyright (C) 2017 Apulia Software s.r.l. (http://www.apuliasoftware.it)
 #    @author Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
 #
-#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+#    License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'Italian Localisation - Natura delle aliquote IVA',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Localisation/Italy',
     'author': "Odoo Community Association (OCA), Apulia Software s.r.l",
-    'website': 'https://odoo-community.org/',
-    'license': 'AGPL-3',
+    'website': 'https://www.odoo-italia.net/',
+    'license': 'LGPL-3',
     'depends': [
-        'account',
+        'l10n_it_account',
         ],
     'data': [
         'view/account_tax_kind_view.xml',
--- l10n_it_account_tax_kind/model/account.py
+++ l10n_it_account_tax_kind/model/account.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-#
-#    Copyright (C) 2017 Apulia Software s.r.l. (http://www.apuliasoftware.it)
-#    @author Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
-#
-#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class AccountTax(models.Model):
@@ -13,3 +9,4 @@ class AccountTax(models.Model):
     _inherit = 'account.tax'
 
     kind_id = fields.Many2one('account.tax.kind', string="Kind")
+    law_reference = fields.Char('Law reference')
