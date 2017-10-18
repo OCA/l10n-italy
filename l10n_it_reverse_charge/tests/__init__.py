cc341c95e049189595990211bbc197d1f8cd30a3
marco <mcalcagni@dinamicheaziendali.it>
2017-10-18 11:23:50 +0200

    porting l10n_it_reverse_charge to 10.0

--- l10n_it_reverse_charge/tests/__init__.py
+++ l10n_it_reverse_charge/tests/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Davide Corio
+# © 2016 Davide Corio (Abstract)
 # Copyright 2017 Alex Comba - Agile Business Group
 # Copyright 2017 Lorenzo Battistini - Agile Business Group
 # Copyright 2017 Marco Calcagni - Dinamiche Aziendali srl
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from . import models
+from . import test_rc
