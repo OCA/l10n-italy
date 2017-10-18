d6de8f60219477e52fafdc26c3cd1d9ba1411979
marco <mcalcagni@dinamicheaziendali.it>
2017-10-18 11:23:50 +0200

    porting l10n_it_reverse_charge to 10.0

--- l10n_it_reverse_charge/models/account_fiscal_position.py
+++ l10n_it_reverse_charge/models/account_fiscal_position.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Davide Corio
+# Copyright 2016 Davide Corio
 # Copyright 2017 Alex Comba - Agile Business Group
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2017 Lorenzo Battistini - Agile Business Group
+# Copyright 2017 Marco Calcagni - Dinamiche Aziendali srl
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class AccountFiscalPosition(models.Model):
