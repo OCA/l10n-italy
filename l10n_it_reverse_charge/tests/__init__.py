ba3712a06451fbae12fad4adb6294204171f3b30
Simone Rubino <simone.rubino@agilebg.com>
2017-10-19 15:10:00 +0200

    [FIX] l10n_it_reverse_charge: added check for invoice of 0

--- l10n_it_reverse_charge/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 Davide Corio (Abstract)
-# Copyright 2017 Alex Comba - Agile Business Group
-# Copyright 2017 Lorenzo Battistini - Agile Business Group
-# Copyright 2017 Marco Calcagni - Dinamiche Aziendali srl
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
-from . import test_rc
