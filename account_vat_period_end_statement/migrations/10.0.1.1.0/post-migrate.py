b899cb94fd49bfd7cf88df7942737e935f47e837
Alex Comba <alex.comba@agilebg.com>
2017-11-16 15:32:28 +0100

    [ADD] account_vat_period_end_statement: add migration script for copying account.period (with vat statement) to date.range (#408)

---
+++ account_vat_period_end_statement/migrations/10.0.1.1.0/post-migrate.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Alex Comba - Agile Business Group
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+from openerp import api, SUPERUSER_ID
+
+
+def migrate(cr, version):
+    """Copy account.period to data.range
+      only for those periods with vat statement"""
+    if not version:
+        return
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        date_range_model = env['date.range']
+        date_range_type = env['date.range.type']
+        env.cr.execute(
+            """SELECT vat_statement_id, company_id, name, date_start, date_stop
+            from account_period """)
+        periods = env.cr.fetchall()
+        for period in periods:
+            # if period has vat statement
+            if period[0]:
+                date_range = date_range_model.search(
+                    [('date_start', '=', period[3]),
+                     ('date_end', '=', period[4])
+                     ])
+                if date_range:
+                    date_range.write({'vat_statement_id': period[0]})
+                else:
+                    if not date_range_type:
+                        date_range_type = env['date.range.type'].create(
+                            {'name': 'periodo fiscale'})
+                    date_range_type.company_id = period[1]
+                    date_range_vals = {
+                        'vat_statement_id': period[0],
+                        'company_id': period[1],
+                        'name': period[2],
+                        'type_id': date_range_type.id,
+                        'date_start': period[3],
+                        'date_end': period[4]}
+                    date_range_model.create(date_range_vals)
