5931d0127d9179a9a6a9fd3e889fe3abcfecfe14
labaggio <lbaggio@linkgroup.it>
2017-12-04 15:55:17 +0100

    moduli per la gestione del registro iva con iva per cassa (#385)

---
+++ l10n_it_vat_registries_cash_basis/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Lara Baggio - Link IT srl
+# (<http://www.linkgroup.it/>)
+# Copyright 2014-2017 Lorenzo Battistini - Agile Business Group
+# (<http://www.agilebg.com>)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Italian Localization - VAT Registries - Cash Basis',
+    'version': '10.0.1.0.0',
+    'category': 'Hidden',
+    "author": "LinkIt Srl, Agile Business Group"
+              ", Odoo Community Association (OCA)",
+    'website': 'http://www.linkgroup.it/',
+    'license': 'LGPL-3',
+    "depends": [
+        'l10n_it_vat_registries',
+        'account_tax_cash_basis'],
+    "data": [
+    ],
+    'installable': True,
+    'auto_install': True,
+}
