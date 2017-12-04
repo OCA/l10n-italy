2d58b71f8b55731a47eb40c7aa7d5c4cf8dbba16
labaggio <lbaggio@linkgroup.it>
2017-12-04 15:55:17 +0100

    moduli per la gestione del registro iva con iva per cassa (#385)

--- l10n_it_vat_registries/models/vat_registry.py
+++ l10n_it_vat_registries/models/vat_registry.py
@@ -5,6 +5,8 @@
 from odoo import api, models
 from odoo.tools.misc import formatLang
 from odoo.tools.translate import _
+from odoo.exceptions import Warning as UserError
+
 import time
 
 
@@ -16,11 +18,7 @@ class ReportRegistroIva(models.AbstractModel):
         # docids required by caller but not used
         # see addons/account/report/account_balance.py
 
-        lang_code = self._context.get('company_id',
-                                      self.env.user.company_id.partner_id.lang)
-        lang = self.env['res.lang']
-        lang_id = lang._lang_get(lang_code)
-        date_format = lang_id.date_format
+        date_format = data['form']['date_format']
 
         docargs = {
             'doc_ids': data['ids'],
@@ -61,16 +59,19 @@ class ReportRegistroIva(models.AbstractModel):
         return self.env['account.invoice'].search([
             ('move_id', '=', move.id)])
 
-    def _tax_amounts_by_tax_id(self, move, registry_type):
+    def _get_move_line(self, move, data):
+        return [move_line for move_line in move.line_ids]
+
+    def _tax_amounts_by_tax_id(self, move, move_lines, registry_type):
         res = {}
 
-        for move_line in move.line_ids:
+        for move_line in move_lines:
             set_cee_absolute_value = False
             if not(move_line.tax_line_id or move_line.tax_ids):
                 continue
 
             if move_line.tax_ids and len(move_line.tax_ids) != 1:
-                    raise Exception(
+                    raise UserError(
                         _("Move line %s has too many base taxes")
                         % move_line.name)
 
@@ -151,8 +152,13 @@ class ReportRegistroIva(models.AbstractModel):
         else:
             invoice_type = "FA"
 
+        move_lines = self._get_move_line(move, data)
+
         amounts_by_tax_id = self._tax_amounts_by_tax_id(
-            move, data['registry_type'])
+            move,
+            move_lines,
+            data['registry_type'])
+
         for tax_id in amounts_by_tax_id:
             tax = self.env['account.tax'].browse(tax_id)
             tax_item = {
