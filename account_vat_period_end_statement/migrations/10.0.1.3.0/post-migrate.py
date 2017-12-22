d8f4f6c135cd1df6987c1e1e289f607c0c1f3a0e
Alex Comba <alex.comba@agilebg.com>
2017-12-22 08:26:57 +0100

    [FIX] account_vat_period_end_statement: get migration script multi company compliant (#428)
    [FIX] account_vat_period_end_statement: check move_id exists to get statement_draft properly working (#435)
    
    move_id needs to be checked if exists in the computed fields, otherwise the method unlink, during the fields recomputation, raises a MissingError for them
    
    [FIX] account_vat_period_end_statement: post move + cancel move (#436)
    
    [IMP] account_vat_period_end_statement: allow to adviser to manage date.range and date.range.type
    Otherwise a user who belongs to adviser group cannot add a date.range to vat statement
    
    [FIX] Move menu related to date range to l10n_it_account module

--- account_vat_period_end_statement/migrations/10.0.1.3.0/post-migrate.py
+++ account_vat_period_end_statement/migrations/10.0.1.3.0/post-migrate.py
@@ -23,22 +23,29 @@ def migrate(cr, version):
             """SELECT vat_statement_id, company_id, name, date_start, date_stop
             from account_period """)
         date_range_model = env['date.range']
-        date_range_type = env['date.range.type']
+        date_range_type_model = env['date.range.type']
         periods = env.cr.fetchall()
         for period in periods:
             # if period has vat statement
             if period[0]:
+                date_range_type = date_range_type_model.search(
+                    [('name', '=', 'periodo fiscale'),
+                     ('company_id', '=', period[1])
+                     ])
+                if not date_range_type:
+                    date_range_type = date_range_type_model.create(
+                        {'name': 'periodo fiscale',
+                         'company_id': period[1]})
                 date_range = date_range_model.search(
                     [('date_start', '=', period[3]),
-                     ('date_end', '=', period[4])
+                     ('date_end', '=', period[4]),
+                     ('company_id', '=', period[1])
                      ])
                 if date_range:
-                    date_range.write({'vat_statement_id': period[0]})
+                    date_range.write(
+                        {'vat_statement_id': period[0],
+                         'type_id': date_range_type.id})
                 else:
-                    if not date_range_type:
-                        date_range_type = env['date.range.type'].create(
-                            {'name': 'periodo fiscale'})
-                    date_range_type.company_id = period[1]
                     date_range_vals = {
                         'vat_statement_id': period[0],
                         'company_id': period[1],
