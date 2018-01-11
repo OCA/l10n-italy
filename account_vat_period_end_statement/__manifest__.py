94b725d8ed3fe772948e9ea6de902402a2358fdf
Alex Comba <alex.comba@agilebg.com>
2018-01-11 12:45:12 +0100

    [FIX] account_vat_period_end_statement: check move_id exists to get statement_draft properly working move_id needs to be checked if exists in the computed fields, otherwise the method unlink, during the fields recomputation, raises a MissingError for them

--- account_vat_period_end_statement/__manifest__.py
+++ account_vat_period_end_statement/__manifest__.py
@@ -26,7 +26,7 @@
 
 {
     "name": "Period End VAT Statement",
-    "version": "10.0.1.3.0",
+    "version": "10.0.1.3.1",
     'category': 'Generic Modules/Accounting',
     'license': 'AGPL-3',
     "depends": [
