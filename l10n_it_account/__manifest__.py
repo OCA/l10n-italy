45088c4cb096feaaef5a614b490168ea14d0504e
Alex Comba <alex.comba@agilebg.com>
2018-01-19 12:59:37 +0100

    [FIX] Move menu related to date range to l10n_it_account module

--- l10n_it_account/__manifest__.py
+++ l10n_it_account/__manifest__.py
@@ -6,14 +6,18 @@
 
 {
     'name': 'Italian Localization - Account',
-    'version': '10.0.1.0.1',
+    'version': '10.0.1.1.1',
     'category': 'Hidden',
     'author': "Agile Business Group,Abstract,Odoo Community Association (OCA)",
     'website': 'http://www.odoo-italia.net',
     'license': 'AGPL-3',
-    "depends": ['account'],
+    "depends": [
+        'account',
+        'date_range',
+    ],
     "data": [
         'views/account_setting.xml',
+        'views/account_view.xml',
         'reports/account_reports_view.xml',
     ],
     'installable': True,
