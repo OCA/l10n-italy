e072e13bd07cccae7edd6de569fe151869131ae6
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2018-02-28 16:28:02 +0100

    FIX VAT registries and VAT statement when invoices with negative lines (#452)
    Bug solved:
     - Create an invoice with a positive line and a negative line
     - Print VAT register
    Get wrong totals

--- l10n_it_account/__manifest__.py
+++ l10n_it_account/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Italian Localization - Account',
-    'version': '10.0.1.1.1',
+    'version': '10.0.1.1.2',
     'category': 'Hidden',
     'author': "Agile Business Group,Abstract,Odoo Community Association (OCA)",
     'website': 'http://www.odoo-italia.net',
