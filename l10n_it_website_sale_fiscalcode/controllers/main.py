04d672e7e7944963fada7ccbe1cd0d1b71c8c640
hurrinico <nicola.malcontenti@agilebg.com>
2018-03-30 11:15:07 +0200

    [ADD] Add module website sale Fiscalcode

---
+++ l10n_it_website_sale_fiscalcode/controllers/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Nicola Malcontenti - Agile Business Group
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSaleFiscalCode(WebsiteSale):
+
+    def _checkout_form_save(self, mode, checkout, all_values):
+        res = super(WebsiteSaleFiscalCode, self)._checkout_form_save(
+            mode, checkout, all_values)
+        if 'fiscalcode' not in checkout and 'fiscalcode' in all_values:
+            request.env['res.partner'].browse(res).sudo().write(
+                {'fiscalcode': all_values['fiscalcode']})
+        return res
