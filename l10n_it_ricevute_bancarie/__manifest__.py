360080cc628a4999dcb8a6a911b9124ec18200ca
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2018-04-13 12:26:38 +0200

    FIX l10n_it_ricevute_bancarie quando , nella registrazione di accettazione, viene usato sempre lo stesso conto. (#472)
    Allora per la riconciliazione prendiamo comunque la riga che ci interessa, quella con 'debit'

--- l10n_it_ricevute_bancarie/__manifest__.py
+++ l10n_it_ricevute_bancarie/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': "Ricevute Bancarie",
-    'version': "10.0.1.0.0",
+    'version': "10.0.1.0.1",
     'author': "Odoo Community Association (OCA)",
     'category': "Accounting & Finance",
     'website': "https://odoo-community.org/",
