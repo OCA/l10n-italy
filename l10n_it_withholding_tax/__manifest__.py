fa6d583c86c4fe9ff222d17f308bb077b5e35757
eLBati <lorenzo.battistini@agilebg.com>
2018-04-16 10:59:45 +0200

    FIX typo Withhoulding

--- l10n_it_withholding_tax/__manifest__.py
+++ l10n_it_withholding_tax/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Italian Withholding Tax',
-    'version': '10.0.1.2.0',
+    'version': '10.0.1.2.1',
     'category': 'Account',
     'author': 'Openforce, Odoo Italia Network, '
               'Odoo Community Association (OCA)',
