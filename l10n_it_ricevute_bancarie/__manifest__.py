8d0e52e0d65daddcfadb781de23b028567b6b862
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2018-05-04 15:29:22 +0200

    IMP l10n_it_ricevute_bancarie README (#478)

--- l10n_it_ricevute_bancarie/__manifest__.py
+++ l10n_it_ricevute_bancarie/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': "Ricevute Bancarie",
-    'version': "10.0.1.0.1",
+    'version': "10.0.1.0.2",
     'author': "Odoo Community Association (OCA)",
     'category': "Accounting & Finance",
     'website': "https://odoo-community.org/",
