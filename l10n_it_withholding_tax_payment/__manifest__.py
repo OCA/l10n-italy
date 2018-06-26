07a6a8c6cf64b3e7738a0a551ccabaf93f826895
alex_cam <alessandrocamilli@openforce.it>
2018-06-26 18:33:39 +0200

    porting l10n_it_withholding_tax to v11

--- l10n_it_withholding_tax_payment/__manifest__.py
+++ l10n_it_withholding_tax_payment/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Alessandro Camilli (<http://www.openforce.it>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Italian Withholding Tax Payment',
-    'version': '10.0.1.1.0',
+    'version': '11.0.1.0.0',
     'category': 'Account',
     'author': 'Openforce, Odoo Italia Network, '
               'Odoo Community Association (OCA)',
@@ -14,7 +13,6 @@
                 'l10n_it_withholding_tax'],
     "data": [
         'views/withholding_tax.xml',
-        'workflow.xml',
         'security/ir.model.access.csv',
         'data/sequence.xml',
         'wizard/create_move_payment_view.xml',
