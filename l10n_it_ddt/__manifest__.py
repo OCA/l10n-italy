964e198201c7bedef6d7c3c2b670495fb87816e7
Simone Rubino <simone.rubino@agilebg.com>
2018-07-02 18:00:48 +0200

    [FIX] fix delivery address for generated invoices
    [FIX] call sale's prepare_invoice when preparing ddt's invoice

--- l10n_it_ddt/__manifest__.py
+++ l10n_it_ddt/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     'name': 'DDT',
-    'version': '10.0.1.5.0',
+    'version': '10.0.1.5.1',
     'category': 'Localization/Italy',
     'summary': 'Documento di Trasporto',
     'author': 'Davide Corio, Odoo Community Association (OCA),'
