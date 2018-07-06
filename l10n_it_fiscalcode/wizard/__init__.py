af9a469d67ecd29d99c2c0a2ff76ce8c204639b6
Giacomo Grasso <giacomo.grasso.82@gmail.com>
2018-07-06 11:07:27 +0200

    Corrected LINT CHECK=1 of merged modules for PEP8 (#544)

--- l10n_it_fiscalcode/wizard/__init__.py
+++ l10n_it_fiscalcode/wizard/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
 # Copyright 2016 Andrea Gallina (Apulia Software)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
