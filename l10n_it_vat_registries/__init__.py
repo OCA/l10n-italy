203ec9537b2c8abbaf6e2d346c3b6415080e0ade
eLBati <lorenzo.battistini@agilebg.com>
2018-07-13 15:32:49 +0200

    copy l10n_it_vat_registries from 10.0 branch due to conflits with git AM

---
+++ l10n_it_vat_registries/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2011-2013 Associazione OpenERP Italia
+# (<http://www.openerp-italia.org>).
+# Copyright 2014-2017 Lorenzo Battistini - Agile Business Group
+# (<http://www.agilebg.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
+from . import wizard
