f04119cd6afa8649d4052839b453be4e8ccd93c2
Roberto Fichera <robyf@tekno-soft.it>
2018-07-16 19:38:31 +0200

    [10.0][DDT] Add email template and send button in DDT view

--- l10n_it_ddt/models/stock_picking_package_preparation.py
+++ l10n_it_ddt/models/stock_picking_package_preparation.py
@@ -439,6 +439,44 @@ class StockPickingPackagePreparation(models.Model):
         return [inv.id for inv in invoices.values()]
 
     @api.multi
+    def action_send_ddt_mail(self):
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = ir_model_data.\
+                get_object_reference('l10n_it_ddt',
+                                     'email_template_edi_ddt')[1]
+        except ValueError:
+            template_id = False
+
+        try:
+            compose_form_id = ir_model_data.\
+                get_object_reference('mail',
+                                     'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+
+        ctx = {
+            'default_model': 'stock.picking.package.preparation',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'mark_so_as_sent': True,
+            'custom_layout':
+                "l10n_it_ddt.mail_template_data_notification_email_ddt"
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
+
+    @api.multi
     def unlink(self):
         for ddt in self:
             if ddt.invoice_id:
