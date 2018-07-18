ee5a2be49ae1279d1b33817f2cc2c2c85f0c1721
jackjack <giacomo.grasso.82@gmail.com>
2018-07-18 07:34:40 +0200

    [11][MIG] Migrating account vat statement to 11

--- account_vat_period_end_statement/__manifest__.py
+++ account_vat_period_end_statement/__manifest__.py
@@ -1,45 +1,26 @@
-# -*- coding: utf-8 -*-
-#
-#
-#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2011-2012 Domsense s.r.l. (<http://www.domsense.com>).
 #    Copyright (C) 2012-17 Agile Business Group (<http://www.agilebg.com>)
 #    Copyright (C) 2012-15 LinkIt Spa (<http://http://www.linkgroup.it>)
 #    Copyright (C) 2015 Associazione Odoo Italia
 #    (<http://www.odoo-italia.org>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-
 
 {
-    "name": "Period End VAT Statement",
-    "version": "10.0.1.4.2",
-    'category': 'Generic Modules/Accounting',
+    "name": "Liquidazione IVA",
+    "version": "11.0.1.0.0",
+    'category': 'Localization/Italy',
     'license': 'AGPL-3',
-    "depends": [
-        "l10n_it_account",
-        "report",
-        "l10n_it_fiscalcode",
-        "date_range",
-        "account_accountant",
-        "account_tax_balance",
-        ],
     "author": "Agile Business Group, Odoo Community Association (OCA)"
               ", LinkIt Spa",
-    'website': 'http://www.agilebg.com',
+    'website': 'https://github.com/OCA/l10n-italy/tree/11.0/'
+               'account_vat_period_end_statement',
+    "depends": [
+        "account_invoicing",
+        "account_tax_balance",
+        "date_range",
+        "l10n_it_account",
+        "l10n_it_fiscalcode",
+        "web",
+        ],
     'data': [
         'wizard/add_period.xml',
         'wizard/remove_period.xml',
