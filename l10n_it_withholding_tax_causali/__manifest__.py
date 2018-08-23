86657cde5a8494aab08189e2ac9359fa05e4f0db
eLBati <lorenzo.battistini@agilebg.com>
2018-08-23 19:51:07 +0200

    ADD l10n_it_causali_pagamento and l10n_it_withholding_tax_causali

---
+++ l10n_it_withholding_tax_causali/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Lorenzo Battistini - Agile Business Group
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Causali pagamento per ritenute d'acconto",
+    "version": "10.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Hidden",
+    "website": "https://github.com/OCA/l10n-italy",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_it_withholding_tax",
+        "l10n_it_causali_pagamento",
+    ],
+    "data": [
+        "views/withholding_tax.xml",
+    ],
+    'auto_install': True,
+}
