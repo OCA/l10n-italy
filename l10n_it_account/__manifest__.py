0d7ca31b89bffe9443755ae05aacde3c11ab4f1f
sergio <sergiocorato@gmail.com>
2018-08-28 16:32:01 +0200

    [10.0][FIX] l10n_it_account:avoid menu menu_date_range duplication

--- l10n_it_account/__manifest__.py
+++ l10n_it_account/__manifest__.py
@@ -6,14 +6,14 @@
 
 {
     'name': 'Italian Localization - Account',
-    'version': '10.0.1.1.2',
+    'version': '10.0.1.2.2',
     'category': 'Hidden',
-    'author': "Agile Business Group,Abstract,Odoo Community Association (OCA)",
+    'author': "Agile Business Group, Abstract, "
+              "Odoo Community Association (OCA)",
     'website': 'http://www.odoo-italia.net',
     'license': 'AGPL-3',
     "depends": [
-        'account',
-        'date_range',
+        'account_fiscal_year',
     ],
     "data": [
         'views/account_setting.xml',
