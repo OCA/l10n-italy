6aa9bc801537610496ecb405366fe23765824a6d
eLBati <lorenzo.battistini@agilebg.com>
2018-08-31 16:00:22 +0200

    FIX api.constrains gets a recordset https://www.odoo.com/documentation/10.0/howtos/backend.html#model-constraints

--- l10n_it_codici_carica/models/codice_carica.py
+++ l10n_it_codici_carica/models/codice_carica.py
@@ -12,11 +12,12 @@ class CodiceCarica(models.Model):
 
     @api.constrains('code')
     def _check_code(self):
-        domain = [('code', '=', self.code)]
-        elements = self.search(domain)
-        if len(elements) > 1:
-            raise ValidationError(
-                _("The element with this code already exists"))
+        for codice in self:
+            domain = [('code', '=', codice.code)]
+            elements = self.search(domain)
+            if len(elements) > 1:
+                raise ValidationError(
+                    _("The element with code %s already exists") % codice.code)
 
     code = fields.Char(string='Code', size=2)
     name = fields.Char(string='Name')
