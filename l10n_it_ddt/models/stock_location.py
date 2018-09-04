a80e3e365d6f5d7b077b72b064ef8de943dd0dad
SimoRubi <simone.rubino@agilebg.com>
2018-09-04 14:54:16 +0200

    [MIG] l10n_it_ddt: Migration to 11.0
    [l10n_it_ddt] Migrated module l10n_it_ddt to 11.0
    
    [10.0][FIX] l10n_it_ddt: Fix https://github.com/OCA/l10n-italy/issues/654 (#656)
    
    * [FIX] l10n_it_ddt: Fix https://github.com/OCA/l10n-italy/issues/654
    
    * Do not exclude Picking processing out of the DDT
    
    * [IMP] l10n_it_ddt: Open picking in separate form

--- l10n_it_ddt/models/stock_location.py
+++ l10n_it_ddt/models/stock_location.py
@@ -1,6 +1,9 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Dinamiche Aziendali s.r.l. (http://www.dinamicheaziendali.it)
-# @author Gianmarco Conte <gconte@dinamicheaziendali.it>
+# Copyright 2014 Abstract (http://www.abstract.it)
+# Copyright Davide Corio <davide.corio@abstract.it>
+# Copyright 2014-2018 Agile Business Group (http://www.agilebg.com)
+# Copyright 2015 Apulia Software s.r.l. (http://www.apuliasoftware.it)
+# Copyright Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
+# Copyright 2018 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
--- l10n_it_ddt/wizard/ddt_invoicing.py
+++ l10n_it_ddt/wizard/ddt_invoicing.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Alessandro Camilli - Openforce
-# Copyright 2017 Gianmarco Conte - Dinamiche Aziendali
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# Copyright 2014 Abstract (http://www.abstract.it)
+# Copyright Davide Corio <davide.corio@abstract.it>
+# Copyright 2014-2018 Agile Business Group (http://www.agilebg.com)
+# Copyright 2015 Apulia Software s.r.l. (http://www.apuliasoftware.it)
+# Copyright Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
+# Copyright 2018 Simone Rubino - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models, api, _
 from odoo.exceptions import Warning as UserError
