f19bda2db37ca53eb714edf8e2c7721aa22fd919
eLBati <lorenzo.battistini@agilebg.com>
2018-09-04 11:32:50 +0200

    FIX migration script and openerp XML tag

--- l10n_it_fiscal_payment_term/__init__.py
+++ l10n_it_fiscal_payment_term/__init__.py
@@ -8,10 +8,9 @@ from . import models
 
 def pre_init_hook(cr):
     cr.execute("SELECT state from ir_module_module where name = "
-               "'l10n_it_fatturapa' and state = 'installed'")
+               "'l10n_it_fatturapa' and state in ('installed', 'to upgrade')")
     l10n_it_fatturapa_is_installed = cr.fetchone()
-    if l10n_it_fatturapa_is_installed and \
-            l10n_it_fatturapa_is_installed[0] == 'installed':
+    if l10n_it_fatturapa_is_installed:
         cr.execute(
             "UPDATE ir_model_data SET module = 'l10n_it_fiscal_payment_term' "
             "WHERE model = 'fatturapa.payment_term'")
