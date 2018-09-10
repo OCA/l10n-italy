faeeb3f6d5af594f4f8a9d018c57675abbe57159
eLBati <lorenzo.battistini@agilebg.com>
2018-09-10 12:27:49 +0200

    l10n_it_ipa ADD is_pa bool field ADD PECDestinatario, CodiceDestinatario , FormatoTrasmissione handling ADD Lotto di fatture verso soggetto privato ADD gestione prodotti nelle fatture passive ADD l10n_it_fatturapa_in_purchase ADD einvoice.line model IMP form fatture ADD Livello di dettaglio Fatture elettroniche ADD PDF preview dell'XML ADD Link to existing supplier invoice and handle "registered" XML ADD rappresentante fiscale e stabile organizzazione in emissione gestione prodotti DatiBollo , DatiCassaPrevidenziale e ScontoMaggiorazione CodiceArticolo, ftpa_line_number
    IMP "show preview" as link
    FIX invoice duplication
    
    ADD constraints and Nome Cognome
    
    Avoid to cancel invoice with XML
    
    Mostra i dati fattura elettronica solo se il cliente è soggetto
    IMP READMEs
    
    Gestione IVA inclusa nel prezzo
    
    ADD l10n_it_fatturapa_out_ddt
    
    IMP PEP8
    
    IMP decimal precision
    ADD Export E-invoice button
    IMP README

--- l10n_it_fatturapa_out/models/account.py
+++ l10n_it_fatturapa_out/models/account.py
@@ -4,7 +4,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 
-from odoo import fields, models
+from odoo import fields, models, api
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
 
@@ -14,7 +14,7 @@ class AccountInvoice(models.Model):
 
     fatturapa_attachment_out_id = fields.Many2one(
         'fatturapa.attachment.out', 'FatturaPA Export File',
-        readonly=True)
+        readonly=True, copy=False)
 
     def preventive_checks(self):
         for line in self.invoice_line_ids:
@@ -22,3 +22,14 @@ class AccountInvoice(models.Model):
                 raise UserError(_(
                     "Invoice line [%s] must not contain new line character"
                 ) % line.name)
+
+    @api.multi
+    def action_invoice_cancel(self):
+        for invoice in self:
+            if invoice.fatturapa_attachment_out_id:
+                raise UserError(_(
+                    "Invoice %s has XML and can't be canceled. "
+                    "Delete the XML before"
+                ) % invoice.number)
+        res = super(AccountInvoice, self).action_invoice_cancel()
+        return res
