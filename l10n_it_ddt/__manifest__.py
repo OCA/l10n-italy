b73f1d8a57edc07c1e2822499fa164e7cbd2ac45
Simone Rubino <simone.rubino@agilebg.com>
2018-09-10 14:17:46 +0200

    [IMP] l10n_it_ddt: DDT type multi-company (#591)

--- l10n_it_ddt/__manifest__.py
+++ l10n_it_ddt/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     'name': 'DDT',
-    'version': '10.0.1.5.1',
+    'version': '10.0.1.6.0',
     'category': 'Localization/Italy',
     'summary': 'Documento di Trasporto',
     'author': 'Davide Corio, Odoo Community Association (OCA),'
@@ -26,6 +26,7 @@
     ],
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'data/ddt_data.xml',
         'views/stock_picking_package_preparation.xml',
         'views/stock_picking.xml',
