3ae79c39ad451b549edcce6183e3800bcf655863
Simone Rubino <simone.rubino@agilebg.com>
2018-09-10 17:18:12 +0200

    [FIX] l10n_it_corrispettivi: default for journal
    _default_journal did not correctly override the default function for field journal_id.
    Everything was *luckily* working thanks to the method _default_currency that called self._default_journal and this kind of call respects inheritance

--- l10n_it_corrispettivi/models/account.py
+++ l10n_it_corrispettivi/models/account.py
@@ -17,12 +17,6 @@ class AccountInvoice(models.Model):
             return False
         return self.env.ref('base.public_user').partner_id.id
 
-    # set default option on inherited field
-    corrispettivo = fields.Boolean(
-        string='Corrispettivo', related="journal_id.corrispettivi",
-        readonly=True, store=True)
-    partner_id = fields.Many2one(default=_default_partner_id)
-
     @api.model
     def _default_journal(self):
         if not self._context.get('default_corrispettivi', False):
@@ -32,6 +26,13 @@ class AccountInvoice(models.Model):
             'company_id', self.env.user.company_id)
         return self.env['account.journal'] \
             .get_corr_journal(company_id)
+
+    # set default option on inherited field
+    corrispettivo = fields.Boolean(
+        string='Corrispettivo', related="journal_id.corrispettivi",
+        readonly=True, store=True)
+    partner_id = fields.Many2one(default=_default_partner_id)
+    journal_id = fields.Many2one(default=_default_journal)
 
     @api.onchange('company_id')
     def onchange_company_id_corrispettivi(self):
