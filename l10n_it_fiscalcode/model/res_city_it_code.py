e6a006575b3f5b59559d9ba45fd0f56bc95c2f2c
luca-vercelli <luca.vercelli.to@gmail.com>
2018-09-28 19:50:41 +0000

    bugfix encoding

--- l10n_it_fiscalcode/model/res_city_it_code.py
+++ l10n_it_fiscalcode/model/res_city_it_code.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
 # Copyright 2016 Andrea Gallina (Apulia Software)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
