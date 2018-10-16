8f44bed8bba3f18ffbccb26819cb075072561f81
eLBati <lorenzo.battistini@agilebg.com>
2018-10-16 11:23:57 +0200

    FIX REA fields order ADD REA fields to copany and compute company_registry accordingly

--- l10n_it_rea/__manifest__.py
+++ l10n_it_rea/__manifest__.py
@@ -2,21 +2,24 @@
 # Copyright 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
 # Copyright 2015 Alessio Gerace <alessio.gerace@agilebg.com>
 # Copyright 2016 Andrea Gallina (Apulia Software)
+# Copyright 2018 Lorenzo Battistini
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'REA Register',
-    'version': '10.0.1.0.0',
+    'name': 'Italian localization - Registro REA',
+    'version': '10.0.1.1.0',
     'category': 'Localisation/Italy',
-    'summary': 'Manage fields for  Economic Administrative catalogue',
+    'summary': 'Manage fields for Economic Administrative catalogue',
     'author': 'Agile Business Group, Odoo Italia Network,'
               'Odoo Community Association (OCA)',
-    'website': 'http://www.agilebg.com',
+    'website': 'https://github.com/OCA/l10n-italy/tree/10.0/l10n_it_rea',
     'license': 'AGPL-3',
     "depends": [
         'account'
     ],
     "data": [
         'views/partner_view.xml',
+        'views/company_view.xml',
     ],
     'installable': True,
 }
