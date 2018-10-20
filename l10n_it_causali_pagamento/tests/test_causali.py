01f8a835be525c721cfc6acc740f8632e107fc39
gianmarco <gconte@dinamicheaziendali.it>
2018-10-20 10:14:33 +0200

    [ADD] Aggiunto modulo l10n_it_causali_pagamento

---
+++ l10n_it_causali_pagamento/tests/test_causali.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from openerp.tests.common import TransactionCase
+from openerp.exceptions import ValidationError
+
+
+class TestCausali(TransactionCase):
+
+    def setUp(self):
+        super(TestCausali, self).setUp()
+        self.causale_model = self.env['causale.pagamento']
+        self.causale_b = self.env.ref('l10n_it_causali_pagamento.b')
+
+    def test_causali(self):
+        with self.assertRaises(ValidationError):
+            self.causale_model.create({
+                'code': 'B',
+                'name': 'Test'
+            })
+        name = self.causale_b.name_get()
+        self.assertEqual(name, [(
+            self.causale_b.id,
+            u"B - Utilizzazione economica, da parte dell'autore ..."
+        )])
