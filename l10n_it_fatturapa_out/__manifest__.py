895845ca9dd905822b3ffef37097619bae0b4c04
SimoRubi <simone.rubino@agilebg.com>
2018-10-22 17:53:32 +0200

    [FIX] l10n_it_fatturapa_out: wizard popup
    [ADD] l10n_it_fatturapa_out: generate a report for invoice and include in generated XML
    
    [FIX] l10n_it_fatturapa_out: attachments must be encoded only once

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Italian Localization - Fattura Elettronica - Emission',
-    'version': '10.0.1.0.3',
+    'version': '10.0.1.1.0',
     'category': 'Localization/Italy',
     'summary': 'Electronic invoices emission',
     'author': 'Davide Corio, Agile Business Group, Innoviu,'
