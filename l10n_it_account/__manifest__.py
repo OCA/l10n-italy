4a8f6af7b7393da06fa5183de97be266762379a9
Sergio Zanchetta <primes2h@gmail.com>
2018-10-27 09:37:29 +0200

    [FIX] l10n_it_account: add Accounting labels (#620)

--- l10n_it_account/__manifest__.py
+++ l10n_it_account/__manifest__.py
@@ -1,15 +1,16 @@
 # Copyright 2015 Abstract srl (<http://www.abstract.it>)
 # Copyright 2015-2017 Agile Business Group (<http://www.agilebg.com>)
 # Copyright 2015 Link It Spa (<http://www.linkgroup.it/>)
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Italian Localization - Account',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.2.0',
     'category': 'Hidden',
     'author': "Agile Business Group, Abstract, "
               "Odoo Community Association (OCA)",
-    'website': 'http://www.odoo-italia.net',
+    'website': 'https://github.com/OCA/l10n-italy/tree/11.0/l10n_it_account',
     'license': 'AGPL-3',
     "depends": [
         'account_fiscal_year',
@@ -18,6 +19,10 @@
     "data": [
         'views/account_setting.xml',
         'views/account_view.xml',
+        'views/account_menuitem.xml',
+        'views/partner_view.xml',
+        'views/product_view.xml',
+        'views/res_config_settings_views.xml',
         'reports/account_reports_view.xml',
     ],
     'installable': True,
