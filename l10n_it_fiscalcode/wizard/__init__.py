9213980dcba86afa177fd3762bd15a30e5546ebb
Matteo Bilotta <Byloth@users.noreply.github.com>
2018-10-30 17:08:23 +0100

    [IMP] A better wizard for fiscalcode generation. (#569)

--- l10n_it_fiscalcode/wizard/__init__.py
+++ l10n_it_fiscalcode/wizard/__init__.py
@@ -1,5 +1,6 @@
 # Copyright 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
 # Copyright 2016 Andrea Gallina (Apulia Software)
+# Copyright © 2018 Matteo Bilotta (Link IT s.r.l.)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import compute_fc
