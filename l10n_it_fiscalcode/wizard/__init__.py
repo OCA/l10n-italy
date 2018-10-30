c3c4ffd4d6d2aac893010f6a888f63b3febdf1fa
eLBati <lorenzo.battistini@agilebg.com>
2018-10-30 18:07:12 +0100

    [MIG] l10n_it_fiscalcode: Migration to 12.0

--- l10n_it_fiscalcode/wizard/__init__.py
+++ l10n_it_fiscalcode/wizard/__init__.py
@@ -1,6 +1,3 @@
-# Copyright 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
-# Copyright 2016 Andrea Gallina (Apulia Software)
-# Copyright © 2018 Matteo Bilotta (Link IT s.r.l.)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import compute_fc
