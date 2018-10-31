e71955b6098ade26c16c4ee11a819a9bb36c86c9
eLBati <lorenzo.battistini@agilebg.com>
2018-10-31 16:52:30 +0100

    [MIG] l10n_it_esigibilita_iva: Migration to 12.0

--- l10n_it_esigibilita_iva/__manifest__.py
+++ l10n_it_esigibilita_iva/__manifest__.py
@@ -1,13 +1,15 @@
 # Copyright 2017 Alessandro Camilli - Openforce
+# Copyright 2018 Lorenzo Battistini <https://github.com/eLBati>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Esigibilità IVA',
-    'version': '11.0.1.0.0',
+    'name': "Italian Localization - Esigibilita' IVA",
+    'version': '12.0.1.0.0',
+    'development_status': 'Beta',
     'category': 'Account',
     'author': "Openforce di Camilli Alessandro, "
               "Odoo Community Association (OCA)",
-    'website': 'https://www.odoo-italia.net',
+    'website': 'https://github.com/OCA/l10n-italy',
     'license': 'LGPL-3',
     'depends': [
         'account',
