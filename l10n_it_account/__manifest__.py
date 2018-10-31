fbb6905b0872bf1ece52e23c3e8b0b00ad7d2d2e
eLBati <lorenzo.battistini@agilebg.com>
2018-10-31 09:08:01 +0100

    porting l10n_it_account to 12

--- l10n_it_account/__manifest__.py
+++ l10n_it_account/__manifest__.py
@@ -2,23 +2,24 @@
 # Copyright 2015-2017 Agile Business Group (<http://www.agilebg.com>)
 # Copyright 2015 Link It Spa (<http://www.linkgroup.it/>)
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# Copyright 2018 Lorenzo Battistini <https://github.com/eLBati>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Italian Localization - Account',
-    'version': '11.0.1.2.0',
+    'version': '12.0.1.0.0',
     'category': 'Hidden',
     'author': "Agile Business Group, Abstract, "
               "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/l10n-italy/tree/11.0/l10n_it_account',
+    'website': 'https://github.com/OCA/l10n-italy/tree/12.0/l10n_it_account',
     'license': 'AGPL-3',
     "depends": [
         'account_fiscal_year',
+        'account_tax_balance',
         'web',
     ],
     "data": [
         'views/account_setting.xml',
-        'views/account_view.xml',
         'views/account_menuitem.xml',
         'views/partner_view.xml',
         'views/product_view.xml',
