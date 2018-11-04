c91fe4561d5324c8496c6d5496de9e683d4740df
Andrea Cometa <a.cometa@apuliasoftware.it>
2018-11-04 14:16:21 +0100

    add module l10n_it_account_tax_kind 6.1

--- l10n_it_account_tax_kind/__openerp__.py
+++ l10n_it_account_tax_kind/__openerp__.py
@@ -2,6 +2,7 @@
 #
 #    Copyright (C) 2017 Apulia Software s.r.l. (http://www.apuliasoftware.it)
 #    @author Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
+#   Andrea Cometa <a.cometa@apuliasoftware.it>
 #
 #    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
