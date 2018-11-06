4249d62e0911412b248cd2e27abec47d1e31b2b1
SimoRubi <simone.rubino@agilebg.com>
2018-11-06 17:17:58 +0100

    [IMP] l10n_it_fatturapa_out: Add flag to recognize which invoices have a PDF attached in the Fattura Elettronica XML

--- l10n_it_fatturapa_out/models/attachment.py
+++ l10n_it_fatturapa_out/models/attachment.py
@@ -3,7 +3,7 @@
 # Copyright 2016 Lorenzo Battistini - Agile Business Group
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class FatturaPAAttachment(models.Model):
@@ -18,3 +18,36 @@ class FatturaPAAttachment(models.Model):
     out_invoice_ids = fields.One2many(
         'account.invoice', 'fatturapa_attachment_out_id',
         string="Out Invoices", readonly=True)
+    has_pdf_invoice_print = fields.Boolean(
+        help="True if all the invoices have a printed "
+             "report attached in the XML, False otherwise.",
+        compute='_compute_has_pdf_invoice_print', store=True)
+
+    @api.multi
+    @api.depends(
+        'out_invoice_ids.fatturapa_doc_attachments.is_pdf_invoice_print')
+    def _compute_has_pdf_invoice_print(self):
+        """Check if all the invoices related to this attachment
+        have at least one attachment containing
+        the PDF report of the invoice"""
+        for attachment_out in self:
+            for invoice in attachment_out.out_invoice_ids:
+                invoice_attachments = invoice.fatturapa_doc_attachments
+                if any([ia.is_pdf_invoice_print
+                        for ia in invoice_attachments]):
+                    continue
+                else:
+                    attachment_out.has_pdf_invoice_print = False
+                    break
+            else:
+                # We have examined all the invoices and none of them
+                # has caused a break, this means all the invoices have at least
+                # one attachment having is_pdf_invoice_print = True
+                attachment_out.has_pdf_invoice_print = True
+
+
+class FatturaAttachments(models.Model):
+    _inherit = "fatturapa.attachments"
+
+    is_pdf_invoice_print = fields.Boolean(
+        help="This attachment contains the PDF report of the linked invoice")
