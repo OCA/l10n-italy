4983de892d6a4404be63b5ba134a8455c0d1ba12
francesca-bianchini <fbianchini@linkgroup.it>
2018-11-06 11:34:20 +0100

    [MIG] l10n_it_fatturapa_out: Migration to 11.0

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -1,22 +1,22 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 Davide Corio
 # Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Italian Localization - Fattura Elettronica - Emission',
-    'version': '10.0.1.2.2',
+    'version': '11.0.1.0.0',
+    'development_status': 'Beta',
     'category': 'Localization/Italy',
     'summary': 'Electronic invoices emission',
     'author': 'Davide Corio, Agile Business Group, Innoviu,'
               'Odoo Community Association (OCA)',
-    'website': 'http://www.agilebg.com',
+    'website': 'https://github.com/OCA/l10n_italy',
     'license': 'LGPL-3',
-    "depends": [
+    'depends': [
         'l10n_it_fatturapa',
         'l10n_it_split_payment',
         ],
-    "data": [
+    'data': [
         'wizard/wizard_export_fatturapa_view.xml',
         'views/attachment_view.xml',
         'views/account_view.xml',
--- l10n_it_fatturapa_out/tests/__init__.py
+++ l10n_it_fatturapa_out/tests/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 Davide Corio
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
