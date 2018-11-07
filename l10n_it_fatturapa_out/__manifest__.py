aacd1c76ec4da7bdf5b6c0e500f90728d3d686ee
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2018-11-07 14:10:23 +0100

    FIX l10n_it_fatturapa_out, steps: (#642)
    - Set 'Product Price' Decimal Accuracy with Digits = 4
     - Create an invoice with 1 line with quantity = 24 and price 1.2519
     - Export XML
    
    In the XML you get
    Quantità: 24.00
    Valore unitario: 1.25
    Valore totale: 30.05
    
    While you should have
    Valore unitario: 1.2519

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Italian Localization - Fattura Elettronica - Emission',
-    'version': '10.0.1.1.0',
+    'version': '10.0.1.1.1',
     'category': 'Localization/Italy',
     'summary': 'Electronic invoices emission',
     'author': 'Davide Corio, Agile Business Group, Innoviu,'
