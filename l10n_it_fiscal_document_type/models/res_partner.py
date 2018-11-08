755524e76429127d42959cebef49844c33488611
eLBati <lorenzo.battistini@agilebg.com>
2018-11-08 09:43:02 +0100

    [MIG] l10n_it_fiscal_document_type: Migration to 12.0

--- l10n_it_fiscal_document_type/models/res_partner.py
+++ l10n_it_fiscal_document_type/models/res_partner.py
@@ -5,6 +5,6 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     out_fiscal_document_type = fields.Many2one(
-        'fiscal.document.type', string="Fiscal Document Type",)
+        'fiscal.document.type', string="Out Fiscal Document Type",)
     in_fiscal_document_type = fields.Many2one(
-        'fiscal.document.type', string="Fiscal Document Type",)
+        'fiscal.document.type', string="In Fiscal Document Type",)
