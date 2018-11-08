1d90ae30928f27dc355fd8d0a074e7446f515ee8
eLBati <lorenzo.battistini@agilebg.com>
2018-11-08 09:43:02 +0100

    [MIG] l10n_it_fiscal_document_type: Migration to 12.0

--- l10n_it_fiscal_document_type/__manifest__.py
+++ l10n_it_fiscal_document_type/__manifest__.py
@@ -1,14 +1,15 @@
 # Copyright 2017 Alessandro Camilli
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# Copyright 2018 Lorenzo Battistini (https://github.com/eLBati)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Italian Localization - '
             'Tipi di documento fiscali per dichiarativi',
-    'version': '11.0.1.1.0',
+    'version': '12.0.1.0.0',
     'category': 'Localisation/Italy',
     'author': "Link It srl, Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/l10n-italy/tree/11.0/'
+    'website': 'https://github.com/OCA/l10n-italy/tree/12.0/'
                'l10n_it_fiscal_document_type',
     'license': 'LGPL-3',
     'depends': ['l10n_it_account'],
