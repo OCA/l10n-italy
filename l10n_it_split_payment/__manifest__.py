e7a3ec39683d4400aa8071c03e9116c55cfa1847
Ruben <ruben.tonetto@gmail.com>
2018-11-09 17:08:25 +0100

    [FIX] manifest website string
    [IMP] converted query to orm in get_receivable_line_ids()
    
    [IMP] check if split payment with related field
    
    [FIX] missing contributor name
    
    [ADD] depends to _compute_amount()

--- l10n_it_split_payment/__manifest__.py
+++ l10n_it_split_payment/__manifest__.py
@@ -1,6 +1,7 @@
 # Copyright 2015  Davide Corio <davide.corio@abstract.it>
 # Copyright 2015-2018  Lorenzo Battistini - Agile Business Group
 # Copyright 2016  Alessio Gerace - Agile Business Group
+# Copyright 2018  Ruben Tonetto (Associazione PNLUG - Gruppo Odoo)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -10,7 +11,8 @@
     'summary': 'Split Payment',
     'author': 'Abstract, Agile Business Group, '
               'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-italy/tree/11.0/l10n_it_split_payment',
+    'website': 'https://github.com/OCA/l10n-italy/tree/11.0/'
+               'l10n_it_split_payment',
     'license': 'AGPL-3',
     'depends': [
         'account',
