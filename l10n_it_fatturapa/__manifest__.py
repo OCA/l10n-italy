460a1cdc77a10ae511903e10424afcb30788e030
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2018-11-12 13:59:57 +0100

    IMP l10n_it_fatturapa codice destinatario default 0000000 (#657)

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Italian Localization - Fattura Elettronica - Base',
-    'version': '10.0.2.1.0',
+    'version': '10.0.2.2.0',
     'category': 'Localization/Italy',
     'summary': 'Electronic invoices',
     'author': 'Davide Corio, Agile Business Group, Innoviu, '
