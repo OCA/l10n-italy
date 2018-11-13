18a3eb4df23a8b743fa1cb123c3bb698caae34ca
eLBati <lorenzo.battistini@agilebg.com>
2018-11-13 15:12:09 +0100

    IMP README exceptions sending email Avoid to delete sent files Check duplicated files Send several files Reset to ready button Sending and delivery date Handling ACCETTAZIONE and CONSEGNA IMP 'link to existing invoice' wizard FIX invoice views within attachment view IMP attachments views IMP l10n_it_fatturapa_out export: allow multiple export , group by partner ALLOW to use newline char in invoice line description check sequence without date ranges Extract correcttle messages from NS

---
+++ l10n_it_fatturapa_pec/wizard/send_pec.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from odoo import models, api
+
+
+class SendPEC(models.TransientModel):
+    _name = 'wizard.fatturapa.send.pec'
+    _description = "Wizard to send multiple e-invoice PEC"
+
+    @api.multi
+    def send_pec(self):
+        if self.env.context.get('active_ids'):
+            attachments = self.env['fatturapa.attachment.out'].browse(
+                self.env.context['active_ids'])
+            attachments.send_via_pec()
