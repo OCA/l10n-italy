f2b67a0eaf18ed90391225601f64734b02b86356
eLBati <lorenzo.battistini@agilebg.com>
2018-11-13 15:12:09 +0100

    IMP README exceptions sending email Avoid to delete sent files Check duplicated files Send several files Reset to ready button Sending and delivery date Handling ACCETTAZIONE and CONSEGNA IMP 'link to existing invoice' wizard FIX invoice views within attachment view IMP attachments views IMP l10n_it_fatturapa_out export: allow multiple export , group by partner ALLOW to use newline char in invoice line description check sequence without date ranges Extract correcttle messages from NS

--- l10n_it_fatturapa/models/partner.py
+++ l10n_it_fatturapa/models/partner.py
@@ -19,7 +19,7 @@ class ResPartner(models.Model):
     register_province = fields.Many2one(
         'res.country.state', string='Register Province')
     # 1.2.1.6
-    register_code = fields.Char('Register Code', size=60)
+    register_code = fields.Char('Register Registration Number', size=60)
     # 1.2.1.7
     register_regdate = fields.Date('Register Registration Date')
     # 1.2.1.8
@@ -28,19 +28,19 @@ class ResPartner(models.Model):
         string="Register Fiscal Position")
     # 1.1.4
     codice_destinatario = fields.Char(
-        "Codice Destinatario",
-        help="Il codice, di 7 caratteri, assegnato dal Sdi ai soggetti che "
-             "hanno accreditato un canale; qualora il destinatario non abbia "
-             "accreditato un canale presso Sdi e riceva via PEC le fatture, "
-             "l'elemento deve essere valorizzato con tutti zeri ('0000000').",
+        "Addressee Code",
+        help="The code, 7 characters long, assigned by ES to subjects with an "
+             "accredited channel; if the addressee didn't accredit a channel "
+             "to ES and invoices are received by PEC, the field must be "
+             "filled with zeros ('0000000').",
         default='0000000')
     # 1.1.6
     pec_destinatario = fields.Char(
-        "PEC destinatario",
-        help="Indirizzo PEC al quale inviare la fattura elettronica. "
-             "Da valorizzare "
-             "SOLO nei casi in cui l'elemento informativo "
-             "<CodiceDestinatario> vale '0000000'"
+        "Addressee PEC",
+        help="PEC to which the Electronic Invoice will be sent. "
+             "Must be filled "
+             "ONLY when the information element "
+             "<CodiceDestinatario> is '0000000'"
     )
     electronic_invoice_subjected = fields.Boolean(
         "Subjected to electronic invoice")
@@ -54,58 +54,54 @@ class ResPartner(models.Model):
     )
     def _check_ftpa_partner_data(self):
         for partner in self:
-            if partner.electronic_invoice_subjected:
+            if partner.electronic_invoice_subjected and partner.customer:
+                # These checks must be done for customers only, as only
+                # needed for XML generation
                 if partner.is_pa and (
                     not partner.ipa_code or len(partner.ipa_code) != 6
                 ):
                     raise ValidationError(_(
-                        "Il partner %s, essendo una pubblica amministrazione "
-                        "deve avere il codice IPA lungo 6 caratteri"
+                        "As a Public Administration, partner %s IPA Code "
+                        "must be 6 characters long"
+                    ) % partner.name)
+                if partner.company_type == 'person' and (
+                    not partner.lastname or not partner.firstname
+                ):
+                    raise ValidationError(_(
+                        "As a natural person, partner %s "
+                        "must have Name and Surname"
                     ) % partner.name)
                 if not partner.is_pa and (
                     not partner.codice_destinatario or
                     len(partner.codice_destinatario) != 7
                 ):
                     raise ValidationError(_(
-                        "Il partner %s "
-                        "deve avere il Codice Destinatario lungo 7 caratteri"
+                        "Partner %s Addressee Code "
+                        "must be 7 characters long"
                     ) % partner.name)
-                if partner.company_type == 'person' and (
-                    not partner.lastname or not partner.firstname
-                ):
+                if not partner.vat and not partner.fiscalcode:
                     raise ValidationError(_(
-                        "Il partner %s, essendo persona "
-                        "deve avere Nome e Cognome"
+                        "Partner %s, must have VAT Number or Fiscal Code"
                     ) % partner.name)
-                if (
-                    not partner.is_pa and
-                    partner.codice_destinatario == '0000000'
-                ):
-                    if not partner.vat and not partner.fiscalcode:
-                        raise ValidationError(_(
-                            "Il partner %s, con Codice Destinatario '0000000',"
-                            " deve avere o P.IVA o codice fiscale"
-                        ) % partner.name)
-                if partner.customer:
-                    if not partner.street:
-                        raise ValidationError(_(
-                            'Customer %s: street is needed for XML generation.'
-                        ) % partner.name)
-                    if not partner.zip:
-                        raise ValidationError(_(
-                            'Customer %s: ZIP is needed for XML generation.'
-                        ) % partner.name)
-                    if not partner.city:
-                        raise ValidationError(_(
-                            'Customer %s: city is needed for XML generation.'
-                        ) % partner.name)
-                    if not partner.state_id:
-                        raise ValidationError(_(
-                            'Customer %s: province is needed for XML '
-                            'generation.'
-                        ) % partner.name)
-                    if not partner.country_id:
-                        raise ValidationError(_(
-                            'Customer %s: country is needed for XML'
-                            ' generation.'
-                        ) % partner.name)
+                if not partner.street:
+                    raise ValidationError(_(
+                        'Customer %s: street is needed for XML generation.'
+                    ) % partner.name)
+                if not partner.zip:
+                    raise ValidationError(_(
+                        'Customer %s: ZIP is needed for XML generation.'
+                    ) % partner.name)
+                if not partner.city:
+                    raise ValidationError(_(
+                        'Customer %s: city is needed for XML generation.'
+                    ) % partner.name)
+                if not partner.state_id:
+                    raise ValidationError(_(
+                        'Customer %s: province is needed for XML '
+                        'generation.'
+                    ) % partner.name)
+                if not partner.country_id:
+                    raise ValidationError(_(
+                        'Customer %s: country is needed for XML'
+                        ' generation.'
+                    ) % partner.name)
