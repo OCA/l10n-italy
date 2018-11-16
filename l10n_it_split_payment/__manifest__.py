37493f869b38d357bf553277b65c04c957787f71
eLBati <lorenzo.battistini@agilebg.com>
2018-11-16 12:30:13 +0100

    [MIG] l10n_it_split_payment: Migration to 12.0

--- l10n_it_split_payment/__manifest__.py
+++ l10n_it_split_payment/__manifest__.py
@@ -6,17 +6,17 @@
 
 {
     'name': 'Split Payment',
-    'version': '11.0.1.1.0',
+    'version': '12.0.1.0.0',
     'category': 'Localization/Italy',
     'summary': 'Split Payment',
     'author': 'Abstract, Agile Business Group, '
               'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-italy/tree/11.0/'
+    'website': 'https://github.com/OCA/l10n-italy/tree/12.0/'
                'l10n_it_split_payment',
     'license': 'AGPL-3',
     'depends': [
         'account',
-        'account_invoicing'],
+    ],
     'data': [
         'views/account_view.xml',
         'views/config_view.xml',
