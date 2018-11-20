e817d7b0b28de398d62fc494608112107a697591
eLBati <lorenzo.battistini@agilebg.com>
2018-11-20 18:27:03 +0100

    IMP l10n_it_sdi_channel interface ADD mechanism to check SDI PEC address after first sending ADD check valid PEC incoming server
    IMP attachment out filter

--- l10n_it_sdi_channel/models/sdi.py
+++ l10n_it_sdi_channel/models/sdi.py
@@ -15,7 +15,7 @@ class SdiChannel(models.Model):
     _name = "sdi.channel"
     _description = "SdI channel"
 
-    name = fields.Char(string='Name', required=True)
+    name = fields.Char(string='Name', required=True, default=_("PEC"))
     company_id = fields.Many2one(
         'res.company', string='Company', required=True,
         default=lambda self:
@@ -28,11 +28,25 @@ class SdiChannelPEC(models.Model):
     channel_type = fields.Selection(
         string='SdI channel type', selection=SDI_CHANNELS, required=True,
         help='PEC is the only implemented channel in this module. Other '
-             'channels (Web, Sftp) could be provided by external modules.')
+             'channels (Web, Sftp) could be provided by external modules.',
+        default='pec')
     pec_server_id = fields.Many2one(
-        'ir.mail_server', string='Pec mail server', required=False,
+        'ir.mail_server', string='Outgoing PEC server', required=False,
         domain=[('is_fatturapa_pec', '=', True)])
-    email_exchange_system = fields.Char("Exchange System Email Address")
+    # This is only used in configuration, to force the user to create one
+    fetch_pec_server_id = fields.Many2one(
+        'fetchmail.server', string='Incoming PEC server', required=False,
+        domain=[('is_fatturapa_pec', '=', True)])
+    email_exchange_system = fields.Char(
+        "Exchange System Email Address",
+        help="The first time you send a PEC to SDI, you must use the address "
+             "sdi01@pec.fatturapa.it . The system, with the first response "
+             "or notification, communicates the PEC address to be used for "
+             "future messages",
+        default=lambda self: self.env['ir.config_parameter'].get_param(
+            'sdi.pec.first.address')
+    )
+    first_invoice_sent = fields.Boolean("First invoice sent", readonly=True)
 
     @api.constrains('pec_server_id')
     def check_pec_server_id(self):
@@ -46,11 +60,34 @@ class SdiChannelPEC(models.Model):
 
     @api.constrains('email_exchange_system')
     def check_email_validity(self):
+        if self.env.context.get('skip_check_email_validity'):
+            return
         for channel in self:
             if not extract_rfc2822_addresses(channel.email_exchange_system):
                 raise exceptions.ValidationError(
                     _("Email %s is not valid")
                     % channel.email_exchange_system)
+
+    def check_first_pec_sending(self):
+        sdi_address = self.env['ir.config_parameter'].get_param(
+            'sdi.pec.first.address')
+        if not self.first_invoice_sent:
+            if self.email_exchange_system != sdi_address:
+                raise exceptions.UserError(_(
+                    "This is a first sending but SDI address is different "
+                    "from %s"
+                ) % sdi_address)
+        else:
+            if not self.email_exchange_system:
+                raise exceptions.UserError(_(
+                    "SDI PEC address not set. Please update it with the "
+                    "address indicated by SDI after the first sending"))
+
+    def update_after_first_pec_sending(self):
+        if not self.first_invoice_sent:
+            self.first_invoice_sent = True
+            self.with_context(
+                skip_check_email_validity=True).email_exchange_system = False
 
 
 class SdiChannelWEB(models.Model):
