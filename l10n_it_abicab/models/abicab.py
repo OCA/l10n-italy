0b20c62d169ed5616407d20cd7bd2773180d1663
Sergio Zanchetta <primes2h@gmail.com>
2018-11-21 23:27:00 +0100

    [MIG] l10n_it_abicab: Migration to 12.0
    [IMP] Set partner bank abi/cab related

--- l10n_it_abicab/models/abicab.py
+++ l10n_it_abicab/models/abicab.py
@@ -1,5 +1,6 @@
 # Copyright 2015 Associazione Odoo Italia (<http://www.odoo-italia.org>)
 # Copyright 2016 Davide Corio (Abstract)
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api
@@ -17,8 +18,16 @@ class ResPartnerBank(models.Model):
 
     _inherit = "res.partner.bank"
 
-    bank_abi = fields.Char(size=5, string='ABI')
-    bank_cab = fields.Char(size=5, string='CAB')
+    bank_abi = fields.Char(
+        size=5,
+        string='ABI',
+        related='bank_id.abi',
+        store=True)
+    bank_cab = fields.Char(
+        size=5,
+        string='CAB',
+        related='bank_id.cab',
+        store=True)
 
     @api.onchange('bank_id')
     def onchange_bank_id(self):
