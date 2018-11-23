59a50e35e4afaa261fdd1878bf6ecf2ee9aa6145
Alex Comba <alex.comba@agilebg.com>
2018-11-23 14:31:08 +0100

    [IMP] l10n_it_ddt: avoid print useless time part of field 'Date' (#670)

--- l10n_it_ddt/__manifest__.py
+++ l10n_it_ddt/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     'name': 'DDT',
-    'version': '10.0.1.7.0',
+    'version': '10.0.1.7.1',
     'category': 'Localization/Italy',
     'summary': 'Documento di Trasporto',
     'author': 'Davide Corio, Odoo Community Association (OCA),'
