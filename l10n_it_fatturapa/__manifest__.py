c8b092a10435f64bfdec990baf3a8876e8ebc54b
Sergio Zanchetta <primes2h@gmail.com>
2018-11-26 12:23:02 +0100

    [FIX] translation source strings l10n_it_fatturapa
    Ref. https://www.fatturapa.gov.it/export/fatturazione/sdi/Specifiche_tecniche_del_formato_FatturaPA_v1.1_EN.pdf
    
    [IMP] revamp README and manifest

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 # Copyright 2014 Davide Corio <davide.corio@abstract.it>
 # Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Italian Localization - Fattura Elettronica - Base',
-    'version': '10.0.2.2.1',
+    'version': '10.0.2.2.2',
     'category': 'Localization/Italy',
     'summary': 'Electronic invoices',
     'author': 'Davide Corio, Agile Business Group, Innoviu, '
               'Odoo Italia Network, Odoo Community Association (OCA)',
-    'website': 'https://odoo-community.org',
+    'website': 'https://github.com/OCA/l10n-italy/tree/10.0/'
+               'l10n_it_fatturapa',
     'license': 'LGPL-3',
     "depends": [
         'l10n_it_account',
--- l10n_it_fatturapa/readme/CONTRIBUTORS.rst
+++ l10n_it_fatturapa/readme/CONTRIBUTORS.rst
@@ -2,3 +2,4 @@
 * Lorenzo Battistini <https://github.com/eLBati>
 * Roberto Onnis
 * Alessio Gerace
+* Sergio Zanchetta <https://github.com/primes2h>
