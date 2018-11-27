bde6c17a8eaeee24fcba5ba51f2596f29dc40d27
Sergio Zanchetta <primes2h@gmail.com>
2018-11-27 08:37:02 +0100

    [FIX] change FatturaPA into E-invoice (#674)
    [IMP] revamp README and manifest

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 # Copyright 2014 Davide Corio
 # Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Italian Localization - Fattura Elettronica - Emission',
-    'version': '10.0.1.2.2',
+    'version': '10.0.1.2.3',
     'category': 'Localization/Italy',
     'summary': 'Electronic invoices emission',
     'author': 'Davide Corio, Agile Business Group, Innoviu,'
               'Odoo Community Association (OCA)',
-    'website': 'http://www.agilebg.com',
+    'website': 'https://github.com/OCA/l10n-italy/tree/10.0/'
+               'l10n_it_fatturapa_out',
     'license': 'LGPL-3',
     "depends": [
         'l10n_it_fatturapa',
