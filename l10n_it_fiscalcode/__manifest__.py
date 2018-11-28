add7a82037fd53bee7c7e378d89edaa8e4d71a87
Alfredo Salata <alfredo.salata@rdsmoulding.com>
2018-11-28 10:29:48 +0100

    [UPD] Updated Copyright Notices, moved Teuron to init.py, added RDS.

--- l10n_it_fiscalcode/__manifest__.py
+++ l10n_it_fiscalcode/__manifest__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+# Copyright 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
+# Copyright 2016 Andrea Gallina (Apulia Software)
+# Copyright 2018 Matteo Bilotta (Link IT s.r.l.)
+# Copyright 2018 Lorenzo Battistini (https://github.com/eLBati)
 # Copyright 2018 Teuron (<http://www.teuron.it>)
+# Copyright 2018 RDS S.p.A. (<http://www.rdsmoulding.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Italian Localisation - Fiscal Code',
