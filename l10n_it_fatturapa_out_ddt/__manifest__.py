02f2fdfe12e7783013ce65304eed1f18f26b5305
Sergio Zanchetta <primes2h@gmail.com>
2018-11-28 23:30:23 +0100

    [FIX] translation source strings l10n_it_fatturapa_out_ddt
    [IMP] revamp README and manifest

--- l10n_it_fatturapa_out_ddt/__manifest__.py
+++ l10n_it_fatturapa_out_ddt/__manifest__.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Lorenzo Battistini
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
-    "name": "Fattura Elettronica & DDT",
+    'name': 'Italian Localization - Fattura Elettronica & DDT',
     "summary": "Bridge module",
-    "version": "10.0.1.0.1",
+    "version": "10.0.1.0.2",
     "development_status": "Beta",
     "category": "Hidden",
-    "website": "https://github.com/OCA/l10n-italy",
+    'website': 'https://github.com/OCA/l10n-italy/tree/10.0/'
+               'l10n_it_fatturapa_out_ddt',
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "maintainers": [],
     "license": "LGPL-3",
