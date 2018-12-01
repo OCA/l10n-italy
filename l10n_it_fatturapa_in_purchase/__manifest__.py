7957fba6bf294a37939956d1a8d1f90a7c9920ed
Sergio Zanchetta <primes2h@gmail.com>
2018-12-01 16:56:56 +0100

    [IMP] l10n_it_fatturapa_in_purchase revamp manifest and README

--- l10n_it_fatturapa_in_purchase/__manifest__.py
+++ l10n_it_fatturapa_in_purchase/__manifest__.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Lorenzo Battistini
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 
 {
-    'name': 'Fattura Elettronica - Purchase integration',
+    'name': 'Italian Localization - '
+            'Fattura Elettronica - Purchase integration',
     'version': '10.0.1.0.0',
     'category': 'Hidden',
     'author': 'Agile Business Group, '
               'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-italy',
+    'website': 'https://github.com/OCA/l10n-italy/tree/10.0/'
+               'l10n_it_fatturapa_in_purchase',
     'license': 'LGPL-3',
     "depends": [
         'l10n_it_fatturapa_in',
