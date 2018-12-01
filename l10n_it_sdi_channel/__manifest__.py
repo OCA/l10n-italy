e04ece80ca323c843a9352ec7b509a4dbde4871d
Sergio Zanchetta <primes2h@gmail.com>
2018-12-01 17:50:00 +0100

    [IMP] l10n_it_fatturapa_pec revamp manifest and README
    [FIX] l10n_it_fatturapa_pec translation source strings
    
    [IMP] l10n_it_sdi_channel revamp README and manifest (#692)
    
    [FIX] l10n_it_sdi_channel FatturaPA/translation source strings
    
    [IMP] l10n_it_sdi_channel set ITA name and summary
    
    [IMP] fatturapa modules, set ITA name and summary (#694)
    
    [FIX] l10n_it_fatturapa_in typos in source translation strings

--- l10n_it_sdi_channel/__manifest__.py
+++ l10n_it_sdi_channel/__manifest__.py
@@ -1,14 +1,17 @@
 # Copyright 2018 Sergio Corato (https://efatto.it)
 # Copyright 2018 Lorenzo Battistini <https://github.com/eLBati>
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "SdI channel",
-    "summary": "Add channel to send-receice xml files to SdI.",
+    "name": "Italian Localization - Fattura Elettronica - Canale SdI",
+    "summary": "Aggiunge il canale di invio/ricezione dei file XML "
+               "attraverso lo SdI",
     "version": "12.0.1.0.0",
     "development_status": "Beta",
     "category": "Hidden",
-    "website": "https://github.com/OCA/l10n-italy",
+    'website': 'https://github.com/OCA/l10n-italy/tree/10.0/'
+               'l10n_it_sdi_channel',
     "author": "Efatto.it di Sergio Corato, Odoo Community Association (OCA)",
     "maintainers": ["sergiocorato"],
     "license": "AGPL-3",
