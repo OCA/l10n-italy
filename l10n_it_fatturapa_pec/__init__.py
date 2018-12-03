bbe0fb785b32dfdd4b011a47778833c5a050d5c5
Enrico Ganzaroli <enrico.gz@gmail.com>
2018-12-03 16:44:14 +0100

    l10n_it_fatturapa_pec backport first commit

---
+++ l10n_it_fatturapa_pec/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Italian Localization - FatturaPA - Emission - PEC Support
+#    See __openerp__.py file for copyright and licensing details.
+#
+##############################################################################
+
+from . import models
+from . import wizard
---
+++ l10n_it_fatturapa_pec/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Italian Localization - FatturaPA - Emission - PEC Support
+#    See __openerp__.py file for copyright and licensing details.
+#
+##############################################################################
+
+from . import account
+from . import fatturapa_attachment_out
+from . import mail_thread
+from . import fetchmail
