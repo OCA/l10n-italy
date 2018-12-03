81cc1157ca3178b15b25a0a80a74b99491e45853
eLBati <lorenzo.battistini@agilebg.com>
2018-12-03 17:47:27 +0100

    FIX l10n_it_fatturapa: province is not required in XML

--- l10n_it_fatturapa/models/partner.py
+++ l10n_it_fatturapa/models/partner.py
@@ -94,11 +94,6 @@ class ResPartner(models.Model):
                     raise ValidationError(_(
                         'Customer %s: city is needed for XML generation.'
                     ) % partner.name)
-                if not partner.state_id:
-                    raise ValidationError(_(
-                        'Customer %s: province is needed for XML '
-                        'generation.'
-                    ) % partner.name)
                 if not partner.country_id:
                     raise ValidationError(_(
                         'Customer %s: country is needed for XML'
