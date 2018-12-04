bd547470e259ede1d32e34092b6133c316f0042b
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2018-12-04 17:08:00 +0100

    FIX l10n_it_fatturapa: province is not required in XML (#693)

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -440,9 +440,6 @@ class WizardExportFatturapa(models.TransientModel):
         if not partner.city:
             raise UserError(
                 _('Customer city is not set.'))
-        if not partner.state_id:
-            raise UserError(
-                _('Customer province is not set.'))
         if not partner.country_id:
             raise UserError(
                 _('Customer country is not set.'))
@@ -453,8 +450,10 @@ class WizardExportFatturapa(models.TransientModel):
                 Indirizzo=partner.street,
                 CAP=partner.zip,
                 Comune=partner.city,
-                Provincia=partner.state_id.code,
                 Nazione=partner.country_id.code))
+        if partner.state_id:
+            fatturapa.FatturaElettronicaHeader.CessionarioCommittente.Sede.\
+                Provincia = partner.state_id.code
 
         return True
 
