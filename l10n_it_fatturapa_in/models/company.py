ac16dce3a53f64cef320b4e7429e4a2e39e12924
eLBati <lorenzo.battistini@agilebg.com>
2018-12-07 08:13:08 +0100

    FIX l10n_it_fatturapa_in: do not automatically add Bollo as invoice line; if charged to customer will be added as invoice line

--- l10n_it_fatturapa_in/models/company.py
+++ l10n_it_fatturapa_in/models/company.py
@@ -5,10 +5,6 @@ from odoo import fields, models, api
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    dati_bollo_product_id = fields.Many2one(
-        'product.product', 'Stamp Data Product',
-        help="Product used to model DatiBollo XML element on bills."
-        )
     cassa_previdenziale_product_id = fields.Many2one(
         'product.product', 'Welfare Fund Data Product',
         help="Product used to model DatiCassaPrevidenziale XML element "
@@ -23,12 +19,6 @@ class ResCompany(models.Model):
 class AccountConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    dati_bollo_product_id = fields.Many2one(
-        related='company_id.dati_bollo_product_id',
-        string="Stamp Data Product",
-        help='Product used to model DatiBollo XML element on bills',
-        readonly=False
-        )
     cassa_previdenziale_product_id = fields.Many2one(
         related='company_id.cassa_previdenziale_product_id',
         string="Welfare Fund Data Product",
@@ -48,10 +38,6 @@ class AccountConfigSettings(models.TransientModel):
         res = super(AccountConfigSettings, self).onchange_company_id()
         if self.company_id:
             company = self.company_id
-            self.dati_bollo_product_id = (
-                company.dati_bollo_product_id and
-                company.dati_bollo_product_id.id or False
-                )
             self.cassa_previdenziale_product_id = (
                 company.cassa_previdenziale_product_id and
                 company.cassa_previdenziale_product_id.id or False
@@ -61,7 +47,6 @@ class AccountConfigSettings(models.TransientModel):
                 company.sconto_maggiorazione_product_id.id or False
                 )
         else:
-            self.dati_bollo_product_id = False
             self.cassa_previdenziale_product_id = False
             self.sconto_maggiorazione_product_id = False
         return res
