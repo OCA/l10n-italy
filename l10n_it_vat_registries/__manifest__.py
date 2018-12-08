ea92794b4e702f1fe0bfc538f73a72f45abaf08b
Alex Comba <alex.comba@agilebg.com>
2018-12-08 08:28:20 +0100

    [FIX] l10n_it_vat_registries: change layout_type according to the selected tax_registry_id (#705)

--- l10n_it_vat_registries/__manifest__.py
+++ l10n_it_vat_registries/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Italian Localization - VAT Registries',
-    'version': '10.0.1.2.2',
+    'version': '10.0.1.2.3',
     'category': 'Localization/Italy',
     "author": "Agile Business Group, Odoo Community Association (OCA)"
               ", LinkIt Srl",
