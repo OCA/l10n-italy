cd107be80032981665402ded6d152f8a8c855b3a
sergiocorato <sergiocorato@gmail.com>
2018-12-12 10:43:27 +0100

    [IMP] use tax obj instead of tax code

--- l10n_it_account_stamp/__openerp__.py
+++ l10n_it_account_stamp/__openerp__.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-# Author(s): Ermanno Gnan (ermannognan@gmai.com)
+# Author(s): Ermanno Gnan (ermannognan@gmail.com)
 # Copyright © 2018 Sergio Corato (https://efatto.it)
 # Copyright © 2018 Enrico Ganzaroli (enrico.gz@gmail.com)
 # Copyright © 2018 Ermanno Gnan (ermannognan@gmail.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Account Stamp',
+    'name': 'Tax Stamp',
     'version': '8.0.0.0.1',
     'category': 'Localisation/Italy',
-    'summary': 'Account stamp automatic management',
+    'summary': 'Tax stamp automatic management',
     'description': """
-Account Stamp
+Tax Stamp
 
 Functionalities:
 
-- Adds account stamp support.
+- Adds tax stamp support.
 - Invoices stamp lines reports: "assolvimento dell’imposta di bollo ai sensi 
 dell’articolo 6, comma 2, del Dm 17 giugno 2014".
 
--- l10n_it_account_stamp/models/company.py
+++ l10n_it_account_stamp/models/company.py
@@ -7,8 +7,8 @@ from openerp import fields, models, api
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    account_stamp_product_id = fields.Many2one(
-        'product.product', 'Stamp Data Product',
+    tax_stamp_product_id = fields.Many2one(
+        'product.product', 'Tax Stamp Product',
         help="Product used to model DatiBollo XML element on bills."
         )
 
@@ -16,9 +16,9 @@ class ResCompany(models.Model):
 class AccountConfigSettings(models.TransientModel):
     _inherit = 'account.config.settings'
 
-    account_stamp_product_id = fields.Many2one(
-        related='company_id.account_stamp_product_id',
-        string="Stamp Data Product",
+    tax_stamp_product_id = fields.Many2one(
+        related='company_id.tax_stamp_product_id',
+        string="Tax Stamp Product",
         help="Product used to model DatiBollo XML element on bills."
         )
 
@@ -30,12 +30,12 @@ class AccountConfigSettings(models.TransientModel):
             company = self.pool.get('res.company').browse(
                 cr, uid, company_id, context=context)
             res['value'].update({
-                'account_stamp_product_id': (
-                    company.account_stamp_product_id and
-                    company.account_stamp_product_id.id or False
+                'tax_stamp_product_id': (
+                    company.tax_stamp_product_id and
+                    company.tax_stamp_product_id.id or False
                     )
             })
         else:
             res['value'].update({
-                'account_stamp_product_id': False})
+                'tax_stamp_product_id': False})
         return res
