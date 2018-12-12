d2294364ab2ccf9819eec66bbd7a08cc2c71552c
eLBati <lorenzo.battistini@agilebg.com>
2018-12-12 16:14:42 +0100

    ADD TD20 doc type IMP l10n_it_fiscal_document_type to be sure that doc type is always set FIX l10n_it_fatturapa_out to use the fiscal doc type

--- l10n_it_fiscal_document_type/models/account_invoice.py
+++ l10n_it_fiscal_document_type/models/account_invoice.py
@@ -11,8 +11,19 @@ class AccountInvoice(models.Model):
         dt = self._get_document_fiscal_type(
             self.type, self.partner_id, self.fiscal_position_id,
             self.journal_id)
-        if len(dt) == 1:
+        if dt:
             self.fiscal_document_type_id = dt[0]
+
+    @api.model
+    def create(self, vals):
+        invoice = super(AccountInvoice, self).create(vals)
+        if not invoice.fiscal_document_type_id:
+            dt = self._get_document_fiscal_type(
+                invoice.type, invoice.partner_id, invoice.fiscal_position_id,
+                invoice.journal_id)
+            if dt:
+                invoice.fiscal_document_type_id = dt[0]
+        return invoice
 
     def _get_document_fiscal_type(self, type=None, partner=None,
                                   fiscal_position=None, journal=None):
