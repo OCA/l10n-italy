6ddc4e4b1e9f448d7d2ff4c3ecbbf179e775985c
Sergio Corato <sergiocorato@users.noreply.github.com>
2018-12-15 17:47:16 +0100

    [FIX] latin type encode (#726)

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -1,6 +1,7 @@
 # Copyright 2014 Davide Corio
 # Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# Copyright 2018 Sergio Corato
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
