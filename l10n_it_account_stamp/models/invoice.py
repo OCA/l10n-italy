58d46fd14d4d18d58319c23691e86ec95318b7d3
sergiocorato <sergiocorato@gmail.com>
2018-12-17 16:39:17 +0100

    [FIX] license and terms

--- l10n_it_account_stamp/models/invoice.py
+++ l10n_it_account_stamp/models/invoice.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from openerp import fields, api, models, exceptions
 from openerp.tools.translate import _
