e7de01118895370d55808727075bbd5d2fcca5d3
eLBati <lorenzo.battistini@agilebg.com>
2018-12-17 17:58:58 +0100

    porting l10n_it_account_stamp to 10

--- l10n_it_account_stamp/models/__init__.py
+++ l10n_it_account_stamp/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from . import invoice
 from . import product
