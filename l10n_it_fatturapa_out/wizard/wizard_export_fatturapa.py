b5894fe52270023d768e554eca22d34207f2b4da
sergiocorato <sergiocorato@gmail.com>
2018-12-17 15:37:31 +0100

    [8.0][FIX] set max lenght for attachment name

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -707,7 +707,7 @@ class WizardExportFatturapa(models.TransientModel):
         if invoice.fatturapa_doc_attachments:
             for doc_id in invoice.fatturapa_doc_attachments:
                 AttachDoc = AllegatiType(
-                    NomeAttachment=doc_id.datas_fname,
+                    NomeAttachment=doc_id.datas_fname[:60],
                     Attachment=base64.decodestring(doc_id.datas)
                 )
                 body.Allegati.append(AttachDoc)
