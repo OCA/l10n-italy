618c3306843ae34d0eae91ef8e062abe5de2237c
sergiocorato <sergiocorato@gmail.com>
2018-12-19 15:50:48 +0100

    [IMP] add tests

---
+++ l10n_it_sdi_channel/tests/test_sdi_channel.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests.common import TransactionCase
+from openerp.exceptions import Warning as UserError
+
+
+class SdICase(TransactionCase):
+
+    def setUp(self):
+        super(SdICase, self).setUp()
+        self.sdi_model = self.env['sdi.channel']
+        self.mail_pec_server = self.env['ir.mail_server'].create({
+            'name': 'Pec mail',
+            'is_fatturapa_pec': True,
+            'email_from_for_fatturaPA': 'test@test.it',
+            'smtp_host': 'smtp.host',
+        })
+
+    def test_create_sdi_channel(self):
+        with self.assertRaises(UserError) as e:
+            self.sdi_model.create({
+                    'name': 'Sdi Channel',
+                    'channel_type': 'pec',
+                    'pec_server_id': self.mail_pec_server.id,
+                    'email_exchange_system': 'test%x@pec.it'
+                })
+        self.assertEqual(e.exception.message,
+                         "Email test%x@pec.it is not valid")
