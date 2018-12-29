3dd0ee1922af581586915347989596d926588b4d
sergiocorato <sergiocorato@gmail.com>
2018-12-29 17:16:14 +0100

    [FIX] test order

--- l10n_it_fatturapa_in/tests/__init__.py
+++ l10n_it_fatturapa_in/tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
+from . import test_import_fatturapa_xml2
 from . import test_import_fatturapa_xml
 from . import test_import_fatturapa_xml1
-from . import test_import_fatturapa_xml2
