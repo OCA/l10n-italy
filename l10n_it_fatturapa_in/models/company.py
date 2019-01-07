ac3d21c871e345149d3b1b5b4d2fe6586f43d88c
sergiocorato <sergiocorato@gmail.com>
2019-01-07 19:05:23 +0100

    [8.0] backport fix not add dati bollo automatically

--- l10n_it_fatturapa_in/models/company.py
+++ l10n_it_fatturapa_in/models/company.py
@@ -5,17 +5,6 @@ from openerp import fields, models, api
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
-
-    dati_bollo_product_id = fields.Many2one(
-        'product.product', 'Product for Dati Bollo',
-        help='Prodotto da utilizzare nelle fatture passive quando nell\'XML '
-             'viene valorizzato l\'elemento DatiBollo'
-        )
-    cassa_previdenziale_product_id = fields.Many2one(
-        'product.product', 'Product for Dati Cassa Previdenziale',
-        help='Prodotto da utilizzare nelle fatture passive quando nell\'XML '
-             'viene valorizzato l\'elemento DatiCassaPrevidenziale'
-        )
     sconto_maggiorazione_product_id = fields.Many2one(
         'product.product', 'Product for Sconto Maggiorazione',
         help='Prodotto da utilizzare nelle fatture passive quando nell\'XML '
@@ -25,19 +14,6 @@ class ResCompany(models.Model):
 
 class AccountConfigSettings(models.TransientModel):
     _inherit = 'account.config.settings'
-
-    dati_bollo_product_id = fields.Many2one(
-        related='company_id.dati_bollo_product_id',
-        string="Product for Dati Bollo",
-        help='Prodotto da utilizzare nelle fatture passive quando nell\'XML '
-             'viene valorizzato l\'elemento DatiBollo'
-        )
-    cassa_previdenziale_product_id = fields.Many2one(
-        related='company_id.cassa_previdenziale_product_id',
-        string="Product for Dati Cassa Previdenziale",
-        help='Prodotto da utilizzare nelle fatture passive quando nell\'XML '
-             'viene valorizzato l\'elemento DatiCassaPrevidenziale'
-        )
     sconto_maggiorazione_product_id = fields.Many2one(
         related='company_id.sconto_maggiorazione_product_id',
         string="Product for Sconto Maggiorazione",
@@ -53,14 +29,6 @@ class AccountConfigSettings(models.TransientModel):
             company = self.pool.get('res.company').browse(
                 cr, uid, company_id, context=context)
             res['value'].update({
-                'dati_bollo_product_id': (
-                    company.dati_bollo_product_id and
-                    company.dati_bollo_product_id.id or False
-                    ),
-                'cassa_previdenziale_product_id': (
-                    company.cassa_previdenziale_product_id and
-                    company.cassa_previdenziale_product_id.id or False
-                    ),
                 'sconto_maggiorazione_product_id': (
                     company.sconto_maggiorazione_product_id and
                     company.sconto_maggiorazione_product_id.id or False
@@ -68,8 +36,6 @@ class AccountConfigSettings(models.TransientModel):
             })
         else:
             res['value'].update({
-                'dati_bollo_product_id': False,
-                'cassa_previdenziale_product_id': False,
                 'sconto_maggiorazione_product_id': False,
             })
         return res
