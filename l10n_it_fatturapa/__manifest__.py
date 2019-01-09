9f410302de39490c321345b48b5c9974acd5ddab
eLBati <elbaddy@gmail.com>
2019-01-09 12:01:27 +0100

    FIX l10n_it_fatturapa allowing to import XML elements with blank spaces, as already done for String*LatinType.
    Strings of blank spaces are allowed by SDI.
    {1,60} would produce
    SimpleFacetValueError: Type
    {http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2}
    String60Type pattern constraint violated by value

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Base',
-    'version': '10.0.2.3.3',
+    'version': '10.0.2.3.4',
     'category': 'Localization/Italy',
     'summary': 'Fatture elettroniche',
     'author': 'Davide Corio, Agile Business Group, Innoviu, '
