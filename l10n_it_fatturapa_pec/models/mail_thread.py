eb23178e7e5ab3416eee23a61cc4587c53112b73
d-stragapede <d.stragapede@elvenstudio.it>
2019-01-16 10:16:33 +0100

    revisione file

--- l10n_it_fatturapa_pec/models/mail_thread.py
+++ l10n_it_fatturapa_pec/models/mail_thread.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Author(s): Andrea Colangelo (andreacolangelo@openforce.it)
 # Copyright 2018 Openforce Srls Unipersonale (www.openforce.it)
-# Copyright 2018 Sergio Corato (https://efatto.it)ftpa_out_cls
+# Copyright 2018 Sergio Corato (https://efatto.it)
 # Copyright 2018 Lorenzo Battistini <https://github.com/eLBati>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
