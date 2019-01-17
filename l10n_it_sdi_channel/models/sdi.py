82217d4c74cf5790438b07275db4b0f74fb1c61b
eLBati <elbaddy@gmail.com>
2019-01-17 15:15:53 +0100

    l10n_it_sdi_channel: IMP e-invoice modules: multi company

--- l10n_it_sdi_channel/models/sdi.py
+++ l10n_it_sdi_channel/models/sdi.py
@@ -38,6 +38,17 @@ class SdiChannelPEC(models.Model):
         domain=[('is_fatturapa_pec', '=', True)])
     email_exchange_system = fields.Char("Exchange System Email Address")
 
+    @api.constrains('fetch_pec_server_id')
+    def check_fetch_pec_server_id(self):
+        for channel in self:
+            domain = [
+                ('fetch_pec_server_id', '=', channel.fetch_pec_server_id.id)]
+            elements = self.search(domain)
+            if len(elements) > 1:
+                raise exceptions.ValidationError(
+                    _("The channel %s with pec server %s already exists")
+                    % (channel.name, channel.fetch_pec_server_id.name))
+
     @api.constrains('pec_server_id')
     def check_pec_server_id(self):
         for channel in self:
