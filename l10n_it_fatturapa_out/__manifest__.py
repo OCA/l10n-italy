cb303f3f059816ffe679f220405d125b1c9147ea
Davide <davide.gazzotti@gmail.com>
2019-01-17 10:34:04 +0100

    l10n_it_fatturapa_out: manage e-invoice for foreign customers That may have CAP and Proviancia not validating against SDI, and that may have both no VAT# and no FiscalCode.
    l10n_it_fatturapa_out: improve README. REF partner checks

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Emissione',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'development_status': 'Beta',
     'category': 'Localization/Italy',
     'summary': 'Emissione fatture elettroniche',
