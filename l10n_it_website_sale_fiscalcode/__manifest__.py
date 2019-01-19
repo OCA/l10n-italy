7d679211356fb9d38478699d3174ace38d8503d2
Simone <simone.rubino88@gmail.com>
2019-01-19 13:58:41 +0100

    [MIG] l10n_it_website_sale_fiscalcode: Migration to 12.0
    [IMP] l10n_it_website_sale_fiscalcode: Assume that users are always people (same assumption of Odoo core)

--- l10n_it_website_sale_fiscalcode/__manifest__.py
+++ l10n_it_website_sale_fiscalcode/__manifest__.py
@@ -1,15 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Nicola Malcontenti - Agile Business Group
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/Lgpl).
+# Copyright 2019 Simone Rubino
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Website Sale FiscalCode',
     'category': 'e-commerce',
     'author': "Agile Business Group,"
               "Odoo Community Association (OCA)",
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.0',
     'license': 'LGPL-3',
-    'website': 'http://www.agilebg.com',
+    'website': 'https://github.com/OCA/l10n-italy/tree/'
+               '12.0/l10n_it_website_sale_fiscalcode',
     'depends': [
         'website_sale',
         'l10n_it_fiscalcode'
---
+++ l10n_it_website_sale_fiscalcode/readme/DESCRIPTION.rst
@@ -0,0 +1,7 @@
+**Italiano**
+
+Questo modulo aggiunge il codice fiscale nel form del checkout.
+
+**English**
+
+This module adds fiscal code in the checkout form.
