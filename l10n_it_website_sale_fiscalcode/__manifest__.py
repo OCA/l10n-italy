654304509e5eebc331c401dbfe7796b90446e150
Simone <simone.rubino88@gmail.com>
2019-01-20 13:07:54 +0100

    [IMP] l10n_it_website_sale_fiscalcode: Allow to sell to people who don't have a Fiscal Code

--- l10n_it_website_sale_fiscalcode/__manifest__.py
+++ l10n_it_website_sale_fiscalcode/__manifest__.py
@@ -7,7 +7,7 @@
     'category': 'e-commerce',
     'author': "Agile Business Group,"
               "Odoo Community Association (OCA)",
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'license': 'LGPL-3',
     'website': 'https://github.com/OCA/l10n-italy/tree/'
                '12.0/l10n_it_website_sale_fiscalcode',
