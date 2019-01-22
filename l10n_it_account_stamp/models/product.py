a9e4479121c85342c5f74d646de04823ded60de8
Sergio Zanchetta <primes2h@gmail.com>
2019-01-22 17:07:22 +0100

    [FIX] move stamp checkbox to avoid misleading
    [FIX] extra blank line
    [FIX] typos in source strings
    [IMP] readme, adapt to v.12.0 and add italian section
    [IMP] __manifest__.py, set ITA name and summary

--- l10n_it_account_stamp/models/product.py
+++ l10n_it_account_stamp/models/product.py
@@ -12,7 +12,7 @@ class ProductTemplate(models.Model):
         for template in self:
             if template.stamp_apply_tax_ids and not template.is_stamp:
                 raise exceptions.ValidationError(
-                    _("The product %s must be a stamp to set apply taxes!")
+                    _("The product %s must be a stamp to apply set taxes!")
                     % template.name)
 
     stamp_apply_tax_ids = fields.Many2many(
@@ -20,6 +20,6 @@ class ProductTemplate(models.Model):
         'product_tax_account_tax__rel',
         'product_id', 'tax_id', string='Stamp taxes')
     stamp_apply_min_total_base = fields.Float(
-        'Stamp apply min total base',
+        'Stamp applicability min total base',
         digits=dp.get_precision('Account'))
-    is_stamp = fields.Boolean('Is stamp')
+    is_stamp = fields.Boolean('Is a stamp')
--- l10n_it_account_stamp/readme/DESCRIPTION.rst
+++ l10n_it_account_stamp/readme/DESCRIPTION.rst
@@ -1,1 +1,7 @@
-This module supports Italian Tax Stamp in invoices.
+**Italiano**
+
+Questo modulo aggiunge il supporto all'imposta di bollo italiana nelle fatture.
+
+**English**
+
+This module adds Italian Tax Stamp support in invoices.
