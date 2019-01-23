cc036466bbff50bcf8af0b5dc08d824962ad73aa
eLBati <elbaddy@gmail.com>
2019-01-23 08:30:34 +0100

    l10n_it_fatturapa: improve _check_ftpa_partner_data and onchange_country_id_e_inv for foreign customers
    l10n_it_fatturapa: add res_partner_fatturapa_4 to demo data

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Base',
-    'version': '12.0.1.2.1',
+    'version': '12.0.1.2.2',
     'category': 'Localization/Italy',
     'summary': 'Fatture elettroniche',
     'author': 'Davide Corio, Agile Business Group, Innoviu, '
