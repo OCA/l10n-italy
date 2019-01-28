728850107e90a4bcbcf6028eb10cecd77c106df8
Sergio Zanchetta <primes2h@gmail.com>
2019-01-28 16:36:10 +0100

    [FIX] decree month in source string

--- l10n_it_account_stamp/__manifest__.py
+++ l10n_it_account_stamp/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Italian Localization - Imposta di bollo',
-    'version': '12.0.1.1.0',
+    'version': '12.0.1.1.1',
     'category': 'Localization/Italy',
     'summary': "Gestione automatica dell'imposta di bollo",
     'author': 'Ermanno Gnan, Sergio Corato, Enrico Ganzaroli, '
