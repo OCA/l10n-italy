205f511af6db0e8039a07ad815278b87c4b26b80
tafaRU <alex.comba@agilebg.com>
2019-01-30 15:41:19 +0100

    [ADD] l10n_it_account_stamp_ddt module

---
+++ l10n_it_account_stamp_ddt/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Alex Comba - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Italian Localization - Imposta di bollo - DDT',
+    'summary': "Modulo ponte tra imposta di bollo e DDT",
+    'version': '10.0.1.0.0',
+    'category': 'Localization/Italy',
+    'license': 'AGPL-3',
+    'author': 'Agile Business Group, Odoo Community Association (OCA)',
+    'maintainers': ['tafaRU'],
+    'website': 'https://github.com/OCA/l10n-italy/tree/10.0/'
+               'l10n_it_account_stamp_ddt',
+    'depends': [
+        'l10n_it_account_stamp',
+        'l10n_it_ddt',
+    ],
+    'auto_install': True,
+    'installable': True,
+}
