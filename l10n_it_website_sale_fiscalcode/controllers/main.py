36ca41c1932a22f6b5ceacf8c3bd519f579427f0
Simone <simone.rubino88@gmail.com>
2019-01-31 20:39:25 +0100

    [IMP] l10n_it_website_sale_fiscalcode: Let the user decide if is a company in the checkout

--- l10n_it_website_sale_fiscalcode/controllers/main.py
+++ l10n_it_website_sale_fiscalcode/controllers/main.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Nicola Malcontenti - Agile Business Group
+# Copyright 2019 Simone Rubino
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+from odoo import _
 from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
@@ -18,7 +20,32 @@ class WebsiteSaleFiscalCode(WebsiteSale):
     def _checkout_form_save(self, mode, checkout, all_values):
         res = super(WebsiteSaleFiscalCode, self)._checkout_form_save(
             mode, checkout, all_values)
+        partner_values = dict()
+        # checkout contains some fields of the form
+        # (see WebsiteSale.values_postprocess).
+        # all_values instead contains all the values of the form.
+        # The fields in checkout have already been written in the partner
+        # by the super() call then we don't need to write them again.
         if 'fiscalcode' not in checkout and 'fiscalcode' in all_values:
-            request.env['res.partner'].browse(res).sudo().write(
-                {'fiscalcode': all_values['fiscalcode']})
+            partner_values['fiscalcode'] = all_values['fiscalcode']
+        if 'is_company' not in checkout and 'is_company' in all_values:
+            partner_values['is_company'] = all_values['is_company']
+            partner_values['individual'] = not all_values['is_company']
+        if partner_values:
+            request.env['res.partner'].browse(res).sudo().write(partner_values)
         return res
+
+    def checkout_form_validate(self, mode, all_form_values, data):
+        error, error_message = super(WebsiteSaleFiscalCode, self) \
+            .checkout_form_validate(mode, all_form_values, data)
+        data['individual'] = not data.get('is_company')
+        dummy_partner = request.env['res.partner'].new({
+            'fiscalcode': data.get('fiscalcode'),
+            'individual': data.get('individual'),
+            'is_company': data.get('is_company')
+        })
+        if not dummy_partner.check_fiscalcode():
+            error['fiscalcode'] = 'error'
+            error['is_company'] = 'error'
+            error_message.append(_('Fiscal Code not valid'))
+        return error, error_message
