8fe18a8c413dc89bf286ed34b78d03ea2025ab46
eLBati <elbaddy@gmail.com>
2019-02-01 20:16:17 +0100

    IMP l10n_it_fatturapa_out: use company_name for Denominazione, when present

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Italian Localization - Fattura Elettronica - Emissione',
-    "version": "11.0.1.0.1",
+    "version": "11.0.1.0.2",
     'development_status': 'Beta',
     'category': 'Localization/Italy',
     'summary': 'Emissione fatture elettroniche',
