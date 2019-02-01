7a0fb9baadec8f6583be98c83931dfa4893740cf
Simone <simone.rubino88@gmail.com>
2019-02-01 23:03:30 +0100

    [IMP] l10n_it_website_portal_fiscalcode: Use individual field from current partner to check fiscalcode

--- l10n_it_website_portal_fiscalcode/controllers/main.py
+++ l10n_it_website_portal_fiscalcode/controllers/main.py
@@ -6,8 +6,7 @@ from odoo import _
 from odoo.addons.website_portal.controllers.main import website_account
 from odoo.http import request
 
-website_account.OPTIONAL_BILLING_FIELDS.extend(
-    ['fiscalcode', 'individual', 'is_company'])
+website_account.OPTIONAL_BILLING_FIELDS.extend(['fiscalcode'])
 
 
 class WebsitePortalFiscalCode(website_account):
@@ -15,13 +14,13 @@ class WebsitePortalFiscalCode(website_account):
     def details_form_validate(self, data):
         error, error_message = \
             super(WebsitePortalFiscalCode, self).details_form_validate(data)
-        data['individual'] = not data.get('is_company')
+        # Check fiscalcode
+        partner_sudo = request.env.user.partner_id.sudo()
         dummy_partner = request.env['res.partner'].new({
             'fiscalcode': data.get('fiscalcode'),
-            'individual': data.get('individual')
+            'individual': partner_sudo.individual
         })
         if not dummy_partner.check_fiscalcode():
             error['fiscalcode'] = 'error'
-            error['is_company'] = 'error'
             error_message.append(_('Fiscal Code not valid'))
         return error, error_message
