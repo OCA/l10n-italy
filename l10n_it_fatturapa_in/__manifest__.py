17f5b6502a5a3f644bd1ed6a996ade09ad4e2770
sherpya <sherpya@netfarm.it>
2019-02-06 15:08:22 +0100

    [10.0] [FIX] l10n_it_fatturapa: allow xml files without xml declaration

--- l10n_it_fatturapa_in/__manifest__.py
+++ l10n_it_fatturapa_in/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Ricezione',
-    'version': '10.0.1.4.4',
+    'version': '10.0.1.4.5',
     'category': 'Localization/Italy',
     'summary': 'Ricezione fatture elettroniche',
     'author': 'Agile Business Group, Innoviu, '
