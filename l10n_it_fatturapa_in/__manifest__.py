e0229ff22de290f3e39c6e127e3b2e2cc1e12ed7
eLBati <elbaddy@gmail.com>
2019-02-07 18:19:51 +0100

    IMP l10n_it_fatturapa_in using supplier payment term to compute due date

--- l10n_it_fatturapa_in/__manifest__.py
+++ l10n_it_fatturapa_in/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Ricezione',
-    'version': '11.0.1.0.2',
+    'version': '11.0.1.0.3',
     "development_status": "Beta",
     'category': 'Localization/Italy',
     'summary': 'Ricezione fatture elettroniche',
