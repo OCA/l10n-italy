78bad190f3e7efdfddce75ba0c5d1a5cce3666f3
eLBati <elbaddy@gmail.com>
2019-02-08 08:26:48 +0100

    IMP l10n_it_fatturapa_out: hide 'Export E-invoice' for non subjected customers

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Emissione',
-    'version': '12.0.1.0.2',
+    'version': '12.0.1.0.3',
     'development_status': 'Beta',
     'category': 'Localization/Italy',
     'summary': 'Emissione fatture elettroniche',
