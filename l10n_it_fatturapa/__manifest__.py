2f3351bf90f6bdfd762c1063a20d5ff30b4bc8c3
eLBati <elbaddy@gmail.com>
2019-02-10 21:53:52 +0100

    IMP l10n_it_fatturapa better label 'Enable electronic invoicing' to identify partners

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Base',
-    'version': '12.0.1.2.3',
+    'version': '12.0.1.2.4',
     'category': 'Localization/Italy',
     'summary': 'Fatture elettroniche',
     'author': 'Davide Corio, Agile Business Group, Innoviu, '
