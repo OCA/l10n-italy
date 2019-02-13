0115f548b1e7f5ab67ffeff72053841c24d184f1
eLBati <elbaddy@gmail.com>
2019-02-13 09:46:05 +0100

    Restore DatiCassaPrevidenziale handling: when present, add it as invoice line. Reverting part of https://github.com/OCA/l10n-italy/pull/703 Adapting _prepare_generic_line_data to accept DatiCassaPrevidenzialeType as line

--- l10n_it_fatturapa_in/__manifest__.py
+++ l10n_it_fatturapa_in/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Ricezione',
-    'version': '10.0.1.4.9',
+    'version': '10.0.1.5.0',
     'category': 'Localization/Italy',
     'summary': 'Ricezione fatture elettroniche',
     'author': 'Agile Business Group, Innoviu, '
