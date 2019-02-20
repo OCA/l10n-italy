c9c4776198f72bf356f314cb3ef50336ef7a47cd
eLBati <elbaddy@gmail.com>
2019-02-20 16:41:46 +0100

    IMP l10n_it_fatturapa_export_zip meta data. REF exported_zip field IMP export wizard

--- l10n_it_fatturapa_export_zip/__manifest__.py
+++ l10n_it_fatturapa_export_zip/__manifest__.py
@@ -2,11 +2,11 @@
 # Copyright 2019 Sergio Corato (https://efatto.it)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Export fatturapa xml zip file",
-    "summary": "Add ability to export multiple xml of e-invoice in "
-               "a zip file.",
+    "name": "Italian Localization - Fattura elettronica - Export ZIP",
+    "summary": "Permette di esportare in uno ZIP diversi file XML di "
+               "fatture elettroniche",
     "version": "10.0.1.0.0",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "category": "other",
     "website": "https://github.com/OCA/l10n-italy",
     "author": "Efatto.it di Sergio Corato, Odoo Community Association (OCA)",
@@ -19,7 +19,7 @@
         "l10n_it_fatturapa_in",
     ],
     "data": [
-        "views/wizard.xml",
+        "wizard/export_fatturapa_view.xml",
         "views/attachment_view.xml",
     ],
 }
