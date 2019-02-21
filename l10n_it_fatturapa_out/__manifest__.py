106af704fe98708617b468e40ac1b24109b779d6
eLBati <elbaddy@gmail.com>
2019-02-21 15:15:37 +0100

    FIX l10n_it_fatturapa_out AttributeError: 'res.company' object has no attribute 'company'

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Emissione',
-    'version': '10.0.1.3.3',
+    'version': '10.0.1.3.4',
     'category': 'Localization/Italy',
     'summary': 'Emissione fatture elettroniche',
     'author': 'Davide Corio, Agile Business Group, Innoviu,'
