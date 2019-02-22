69ad69b530ed2c5a572b5f298ce02ec38b7b294f
gianmarco <gconte@dinamicheaziendali.it>
2019-02-22 14:03:48 +0100

    10.0 IMP Add xml supplier in search xml

--- l10n_it_fatturapa_in/__manifest__.py
+++ l10n_it_fatturapa_in/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Ricezione',
-    'version': '10.0.1.4.7',
+    'version': '10.0.1.4.8',
     'category': 'Localization/Italy',
     'summary': 'Ricezione fatture elettroniche',
     'author': 'Agile Business Group, Innoviu, '
