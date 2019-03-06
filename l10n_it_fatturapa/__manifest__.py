606168517cf97fe85659e778dc56184855def7d1
Simone <simone.rubino88@gmail.com>
2019-03-06 19:24:38 +0100

    [FIX] l10n_it_fatturapa: Add cross check pec_destinatario and codice_destinatario and check empty codice_destinatario

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Base',
-    'version': '12.0.1.2.5',
+    'version': '12.0.1.2.6',
     'category': 'Localization/Italy',
     'summary': 'Fatture elettroniche',
     'author': 'Davide Corio, Agile Business Group, Innoviu, '
