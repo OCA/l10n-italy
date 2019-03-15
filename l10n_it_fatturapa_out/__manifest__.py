8bec762f241802cfa9925234606d8e169f60f599
Roberto Fichera <roberto.fichera@levelprime.com>
2019-03-15 10:30:22 +0100

    [10.0][IMP][l10n_it_fatturapa_out] Hook for extension to permit to export multiple discounts lines (#829)

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Emissione',
-    'version': '10.0.1.3.4',
+    'version': '10.0.1.3.5',
     'category': 'Localization/Italy',
     'summary': 'Emissione fatture elettroniche',
     'author': 'Davide Corio, Agile Business Group, Innoviu,'
