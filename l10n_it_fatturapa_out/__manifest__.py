ff268997d78b38a08e9e0d53c80f51d1855aa45e
Marco Beri <marcoberi@gmail.com>
2019-03-19 17:09:53 +0100

    [10.0] IMP l10n_it_fatturapa allowing to specify 'ODOO' code prefix in XML, fixed version and readme

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Emissione',
-    'version': '10.0.1.3.5',
+    'version': '10.0.1.3.6',
     'category': 'Localization/Italy',
     'summary': 'Emissione fatture elettroniche',
     'author': 'Davide Corio, Agile Business Group, Innoviu,'
