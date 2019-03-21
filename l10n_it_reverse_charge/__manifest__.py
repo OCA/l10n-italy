40ea1355419e9d4f29a93a716f61c9dbde439c66
tafaRU <alex.comba@agilebg.com>
2019-03-21 12:25:19 +0100

    l10n_it_reverse_charge: fix compute_rc_amount_tax to avoid difference between amount_tax and amount shown on payments_widget

--- l10n_it_reverse_charge/__manifest__.py
+++ l10n_it_reverse_charge/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Reverse Charge IVA',
-    'version': '10.0.1.1.5',
+    'version': '10.0.1.1.6',
     'category': 'Localization/Italy',
     'summary': 'Reverse Charge for Italy',
     'author': 'Odoo Italia Network,Odoo Community Association (OCA)',
