0c940049d692bf809aaa01418f161ea9787111a0
Roberto Fichera <robyf@tekno-soft.it>
2019-03-22 16:17:02 +0100

    [12.0][IMP][l10n_it_fatturapa_in] Add to a supplier invoice the validation of the amount when coming from a e-invoice source

--- l10n_it_fatturapa_in/wizard/wizard_import_fatturapa.py
+++ l10n_it_fatturapa_in/wizard/wizard_import_fatturapa.py
@@ -1117,12 +1117,6 @@ class WizardImportFatturapa(models.TransientModel):
         invoice.compute_taxes()
         return invoice_id
 
-    def compute_xml_amount_untaxed(self, DatiRiepilogo):
-        amount_untaxed = 0.0
-        for Riepilogo in DatiRiepilogo:
-            amount_untaxed += float(Riepilogo.ImponibileImporto)
-        return amount_untaxed
-
     def check_invoice_amount(self, invoice, FatturaElettronicaBody):
         if (
             FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.
@@ -1149,7 +1143,7 @@ class WizardImportFatturapa(models.TransientModel):
             # DatiGeneraliDocumento.ScontoMaggiorazione is not present,
             # because otherwise DatiRiepilogo and odoo invoice total would
             # differ
-            amount_untaxed = self.compute_xml_amount_untaxed(
+            amount_untaxed = invoice.compute_xml_amount_untaxed(
                 FatturaElettronicaBody.DatiBeniServizi.DatiRiepilogo)
             if not float_is_zero(
                 invoice.amount_untaxed-amount_untaxed, precision_digits=2
@@ -1225,6 +1219,8 @@ class WizardImportFatturapa(models.TransientModel):
                     )
                 new_invoices.append(invoice_id)
                 self.check_invoice_amount(invoice, fattura)
+
+                invoice.set_einvoice_amount(fattura)
 
                 if self.env.context.get('inconsistencies'):
                     invoice_inconsistencies = (
