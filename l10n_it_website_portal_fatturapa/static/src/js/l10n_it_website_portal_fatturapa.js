f48419d8b05db18696ef55456e9ff9b3314054a5
eLBati <elbaddy@gmail.com>
2019-04-04 21:34:55 +0200

    porting l10n_it_website_portal_fatturapa to 12
    FIX l10n_it_website_portal_fatturapa: Cannot read property 'checked' of undefined
    when not in /my/home page

--- l10n_it_website_portal_fatturapa/static/src/js/l10n_it_website_portal_fatturapa.js
+++ l10n_it_website_portal_fatturapa/static/src/js/l10n_it_website_portal_fatturapa.js
@@ -3,18 +3,22 @@ odoo.define('l10n_it_website_portal_fatturapa', function(require) {
     require('web.core');
 
     $(document).ready(function () {
-        var $details_div = $('.o_website_portal_details');
+        var $details_div = $('.o_portal_details');
+        if (!$details_div.length) {
+            return $.Deferred().reject(
+                "DOM doesn't contain '.o_website_portal_details'");
+        }
         var $electronic_invoice_subjected_input = $details_div.find("input[name='electronic_invoice_subjected'][type='checkbox']");
         var $div_codice_destinatario = $details_div.find(".div_codice_destinatario");
         var $div_pec_destinatario = $details_div.find(".div_pec_destinatario");
 
          var compute_e_inv_fields_visibility = function(){
             if ($electronic_invoice_subjected_input[0].checked) {
-                $div_codice_destinatario.removeClass("hidden");
-                $div_pec_destinatario.removeClass("hidden");
+                $div_codice_destinatario.show();
+                $div_pec_destinatario.show();
             } else {
-                $div_codice_destinatario.addClass("hidden");
-                $div_pec_destinatario.addClass("hidden");
+                $div_codice_destinatario.hide();
+                $div_pec_destinatario.hide();
             }
         };
         compute_e_inv_fields_visibility();
