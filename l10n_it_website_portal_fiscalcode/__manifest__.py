d140e3ecff15b66fce67586b59b2b26aa5f5c00f
eLBati <elbaddy@gmail.com>
2019-04-04 18:56:28 +0200

    porting l10n_it_website_portal_fiscalcode to 12

--- l10n_it_website_portal_fiscalcode/__manifest__.py
+++ l10n_it_website_portal_fiscalcode/__manifest__.py
@@ -1,20 +1,21 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Simone Rubino
+# Copyright 2019 Lorenzo Battistini
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Italian Localization - Website portal fiscalcode",
     "summary": "Add fiscal code to details of frontend user",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Odoo Community Association (OCA)",
     "category": "Localization/Italy",
     "website": "https://github.com/OCA/l10n-italy/tree/"
-               "10.0/l10n_it_website_portal_fiscalcode",
+               "12.0/l10n_it_website_portal_fiscalcode",
     "license": "AGPL-3",
     "depends": [
         "l10n_it_fiscalcode",
-        "website_portal"
+        "portal"
     ],
     "data": [
         "views/l10n_it_website_portal_fiscalcode_templates.xml"
-    ]
+    ],
+    "auto_install": True,
 }
