4e829c58badc9f311454b2c652f3f934731e170f
eLBati <elbaddy@gmail.com>
2019-04-04 21:34:55 +0200

    porting l10n_it_website_portal_fatturapa to 12
    FIX l10n_it_website_portal_fatturapa: Cannot read property 'checked' of undefined
    when not in /my/home page

--- l10n_it_website_portal_fatturapa/__manifest__.py
+++ l10n_it_website_portal_fatturapa/__manifest__.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Simone Rubino
+# Copyright 2019 Lorenzo Battistini
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Italian Localization - Fattura elettronica - Portale",
     "summary": "Add fatturapa fields and checks in frontend user's details",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Odoo Community Association (OCA)",
     "category": "Localization/Italy",
     "website": "https://github.com/OCA/l10n-italy/tree/"
-               "10.0/l10n_it_website_portal_fatturapa",
+               "12.0/l10n_it_website_portal_fatturapa",
     "license": "AGPL-3",
     "depends": [
         "l10n_it_fatturapa",
@@ -16,5 +16,6 @@
     ],
     "data": [
         "views/l10n_it_website_portal_fatturapa_templates.xml"
-    ]
+    ],
+    "auto_install": True,
 }
