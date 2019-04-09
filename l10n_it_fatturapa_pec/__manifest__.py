2d95c46ae083fc578f33f97d9cc69cf8c580a817
Sergio Corato <sergiocorato@users.noreply.github.com>
2019-04-09 11:31:37 +0200

    [FIX] valid other countries intermediary filename (#1133)
    [FIX] remove escape sequence from . used for file extension separation
    
    [FIX] escape . used for file extension separation

--- l10n_it_fatturapa_pec/__manifest__.py
+++ l10n_it_fatturapa_pec/__manifest__.py
@@ -1,11 +1,12 @@
 
 # Author(s): Andrea Colangelo (andreacolangelo@openforce.it)
 # Copyright 2018 Openforce Srls Unipersonale (www.openforce.it)
-# Copyright 2018 Sergio Corato (https://efatto.it)
+# Copyright 2018-2019 Sergio Corato (https://efatto.it)
 # Copyright 2018-2019 Lorenzo Battistini <https://github.com/eLBati>
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
+
     'name': 'Italian Localization - FatturaPA - Emission - PEC Support',
     'version': '11.0.1.0.1',
     "development_status": "Beta",
