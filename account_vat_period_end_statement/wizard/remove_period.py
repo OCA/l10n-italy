33ed3596cbdae6c1299e36fb4aad85b7de573e5e
SimoRubi <simone.rubino@agilebg.com>
2019-04-09 11:34:28 +0200

    [MIG] account_vat_period_end_statement: Migration to 12.0

--- account_vat_period_end_statement/wizard/remove_period.py
+++ account_vat_period_end_statement/wizard/remove_period.py
@@ -1,25 +1,7 @@
-#
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2012 Domsense s.r.l. (<http://www.domsense.com>).
-#    Copyright (C) 2012-15 Agile Business Group sagl (<http://www.agilebg.com>)
-#    Copyright (C) 2015 Associazione Odoo Italia
-#    (<http://www.odoo-italia.org>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+#  Copyright 2012 Domsense s.r.l. (<http://www.domsense.com>).
+#  Copyright 2012-15 Agile Business Group sagl (<http://www.agilebg.com>)
+#  Copyright 2015 Associazione Odoo Italia (<http://www.odoo-italia.org>)
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api
 from odoo.tools.translate import _
@@ -38,6 +20,8 @@ class RemovePeriod(models.TransientModel):
         return res
 
     _name = 'remove.period.from.vat.statement'
+    _description = "Remove period from VAT Statement"
+
     period_id = fields.Selection(_get_period_ids, 'Period', required=True)
 
     @api.multi
