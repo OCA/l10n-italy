99060fa840415281ec2cf60aaf4fe7f1b504ebac
Roberto Fichera <robyf@tekno-soft.it>
2019-04-12 17:29:47 +0200

    [l10n_it_rea] Use rea_office in sql_constraints to avoid 'false' uniqueness

--- l10n_it_rea/__manifest__.py
+++ l10n_it_rea/__manifest__.py
@@ -6,8 +6,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Italian Localization - Registro REA',
-    'version': '12.0.1.0.1',
+    'name': 'ITA - Registro REA',
+    'version': '12.0.1.0.2',
     'category': 'Localisation/Italy',
     'development_status': 'Production/Stable',
     'summary': 'Gestisce i campi del Repertorio Economico Amministrativo',
