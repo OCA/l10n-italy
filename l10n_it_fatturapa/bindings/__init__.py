6bd42a38052743e1c6266c4bb094a05f76245088
sergiocorato <sergiocorato@gmail.com>
2019-04-15 21:10:24 +0200

    [10.0] fix bad tz in data transport

--- l10n_it_fatturapa/bindings/__init__.py
+++ l10n_it_fatturapa/bindings/__init__.py
@@ -3,6 +3,12 @@ import logging
 _logger = logging.getLogger(__name__)
 
 try:
-    from . import fatturapa_v_1_2
-except Exception as e:  # ImportError or pyxb.PyXBVersionError
-    _logger.warning('%s: %s' % (e.__class__.__name__, e))
+    import pyxb
+except ImportError as e:
+    _logger.warning(e)
+else:
+    pyxb.PreserveInputTimeZone(True)
+    try:
+        from . import fatturapa_v_1_2
+    except pyxb.PyXBVersionError as e:
+        _logger.warning('%s: %s' % (e.__class__.__name__, e))
