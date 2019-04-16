26894827202e93f4e9ddd0006ee297ee64cce118
eLBati <elbaddy@gmail.com>
2019-04-16 22:00:23 +0200

    Compute tax amount considering correct rate date. Also fix extra UE case
    FIX l10n_it_reverse_charge: setting discount in supplier invoice would produce a wrong self invoice
    
    REF reconcile_rc_invoice to increase performance.
    Writing company_id would produce recomputation of every account.move.line linked to accounts involved in journal entry to be created.
    In case of thousands or milions of move lines, this would last hours

--- l10n_it_reverse_charge/__manifest__.py
+++ l10n_it_reverse_charge/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Reverse Charge IVA',
-    'version': '10.0.1.1.7',
+    'version': '10.0.1.1.8',
     'category': 'Localization/Italy',
     'summary': 'Reverse Charge for Italy',
     'author': 'Odoo Italia Network,Odoo Community Association (OCA)',
