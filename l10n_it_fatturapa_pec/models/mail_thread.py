f66da790a63da7f8df7d1f96281a823e7b15b27c
Domenico Stragapede <d.stragapede@elvenstudio.it>
2019-04-16 17:15:43 +0200

    [FIX] l10n_it_fatturapa_pec
     * Fix FATTURAPA_IN_REGEX to allow signed incoming invoices.

--- l10n_it_fatturapa_pec/models/mail_thread.py
+++ l10n_it_fatturapa_pec/models/mail_thread.py
@@ -17,7 +17,9 @@ _logger = logging.getLogger(__name__)
 
 FATTURAPA_IN_REGEX = '^(IT[a-zA-Z0-9]{11,16}|'\
                      '(?!IT)[A-Z]{2}[a-zA-Z0-9]{2,28})'\
-                     '_[a-zA-Z0-9]{1,5}[.](xml|XML|zip|ZIP)$'
+                     '_[a-zA-Z0-9]{1,5}'\
+                     '\\.(xml|XML|Xml|zip|ZIP|Zip|p7m|P7M|P7m)'\
+                     '(\\.(p7m|P7M|P7m))?$'
 RESPONSE_MAIL_REGEX = '(IT[a-zA-Z0-9]{11,16}|'\
                       '(?!IT)[A-Z]{2}[a-zA-Z0-9]{2,28})'\
                       '_[a-zA-Z0-9]{1,5}'\
