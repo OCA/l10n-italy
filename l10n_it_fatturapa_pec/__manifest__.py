d1f1428d7df6955817c276f1f623c2519e363a1f
Domenico Stragapede <d.stragapede@elvenstudio.it>
2019-04-16 17:15:43 +0200

    [FIX] l10n_it_fatturapa_pec
     * Fix FATTURAPA_IN_REGEX to allow signed incoming invoices.

--- l10n_it_fatturapa_pec/__manifest__.py
+++ l10n_it_fatturapa_pec/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Supporto PEC',
-    'version': '12.0.1.3.2',
+    'version': '12.0.1.3.3',
     'category': 'Localization/Italy',
     'summary': 'Invio fatture elettroniche tramite PEC',
     'author': 'Openforce Srls Unipersonale, Odoo Community Association (OCA)',
