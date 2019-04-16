3bad210f9c09c807d0cc88193079ff9e188419c1
Roberto Fichera <robyf@tekno-soft.it>
2019-04-16 20:58:45 +0200

    [11.0][IMP][l10n_it_fatturapa_in] Add to a supplier invoice the validation of the amount when coming from a e-invoice source

--- l10n_it_fatturapa_in/wizard/link_to_existing_invoice.py
+++ l10n_it_fatturapa_in/wizard/link_to_existing_invoice.py
@@ -17,3 +17,16 @@ class WizardLinkToInvoice(models.TransientModel):
         if len(active_ids) != 1:
             raise UserError(_("You can select only one XML file to link."))
         self.invoice_id.fatturapa_attachment_in_id = active_ids[0]
+        # extract pdf if attached
+        fatturapa_attachment_obj = self.env['fatturapa.attachment.in']
+        for fatturapa_attachment_id in active_ids:
+            fatturapa_attachment = fatturapa_attachment_obj.browse(
+                fatturapa_attachment_id)
+            fatt = self.get_invoice_obj(fatturapa_attachment)
+            for FatturaBody in fatt.FatturaElettronicaBody:
+                self.invoice_id.set_einvoice_amount(FatturaBody)
+                # 2.5
+                AttachmentsData = FatturaBody.Allegati
+                if AttachmentsData and self.invoice_id:
+                    fatturapa_attachment_obj.extract_attachments(
+                        AttachmentsData, self.invoice_id.id)
