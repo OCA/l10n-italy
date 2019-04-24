8cbfe753957d00e25a0eab8cfbd62e1315969ee0
SimoRubi <simone.rubino@agilebg.com>
2019-04-24 16:58:47 +0200

    [FIX] l10n_it_fatturapa_out: DatiRiepilogo is mandatory but tax_line_ids is not required Without this, when generating the e-invoice for an invoice that has no tax lines, the following exception is raised: IncompleteElementContentError: (<odoo.addons.l10n_it_fatturapa.bindings.fatturapa_v_1_2.DatiBeniServiziType object at 0x7f6fb8653ad0>, <pyxb.utils.fac.Configuration object at 0x7f6fb7d137d0>, [<pyxb.binding.basis.ElementContent object at 0x7f6fb8168410>, <pyxb.binding.basis.ElementContent object at 0x7f6fb8168350>, <pyxb.binding.basis.ElementContent object at 0x7f6fb87d8c90>], {})

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'ITA - Fattura elettronica - Emissione',
-    'version': '12.0.1.7.0',
+    'version': '12.0.1.7.1',
     'development_status': 'Beta',
     'category': 'Localization/Italy',
     'summary': 'Emissione fatture elettroniche',
