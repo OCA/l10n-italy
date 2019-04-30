96bb5e83e9afd2b324a9e75280e8c1eec0e7103c
eLBati <elbaddy@gmail.com>
2019-04-30 09:33:54 +0200

    IMP l10n_it_ddt error messages and do not block if SO has not shipping info

--- l10n_it_ddt/__manifest__.py
+++ l10n_it_ddt/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Italian Localization - DDT: documento di trasporto',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'category': 'Localization/Italy',
     'summary': 'Documento di Trasporto',
     'author': 'Davide Corio, Odoo Community Association (OCA),'
