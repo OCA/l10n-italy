adf20864f0e02a9e415682a1191843fcf87567ec
Gianluigi Tiesi <sherpya@netfarm.it>
2019-05-03 16:53:06 +0200

    l10n_it_fatturapa: xml doctor for fatturapa
    it fixes:
     - removes xs:dateTime if bogus and not mandatory
       i.e. 0001-01-01T00:00:00.000+02:00 where python raises
       OverflowError
     - removes timezone from xs:date to make pyxb able to compare with
       1-1-1970, it also removes the need of patching pyxb
     - removes space only strings if not mandatory, else replace with
       a dash
    
    breaking change:
      modules needs to import binding.fatturapa instead of
      bindings.fatturapa_v_1_2, this would be asl useful for
      new specs

--- l10n_it_fatturapa/bindings/__init__.py
+++ l10n_it_fatturapa/bindings/__init__.py
@@ -3,6 +3,11 @@ import logging
 _logger = logging.getLogger(__name__)
 
 try:
-    from . import fatturapa_v_1_2
-except Exception as e:  # ImportError or pyxb.PyXBVersionError
-    _logger.warning('%s: %s' % (e.__class__.__name__, e))
+    import pyxb
+except ImportError as e:
+    _logger.warning(e)
+else:
+    try:
+        from . import fatturapa  # noqa: F401
+    except pyxb.PyXBVersionError as e:
+        _logger.warning('%s: %s' % (e.__class__.__name__, e))
