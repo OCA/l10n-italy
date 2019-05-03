c5c1744d90fa9cd6e89f211d83cb6c1a8715a018
Gianluigi Tiesi <sherpya@netfarm.it>
2019-05-03 16:53:06 +0200

    l10n_it_fatturapa: xml doctor for fatturapa
    it fixes:
     - removes xs:dateTime if bogus and not mandatory
       i.e. 0001-01-01T00:00:00.000+02:00 where python raises
       OverflowError
     - removes timezone from xs:date to make pyxb able to compare with
       1-1-1970, it also removes the need of patching pyxb
     - removes space only strings if not mandatory, else replace with
       a dash
    
    breaking change:
      modules needs to import binding.fatturapa instead of
      bindings.fatturapa_v_1_2, this would be asl useful for
      new specs

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -13,7 +13,7 @@ from odoo import api, fields, models
 from odoo.tools.translate import _
 from odoo.exceptions import UserError
 
-from odoo.addons.l10n_it_fatturapa.bindings.fatturapa_v_1_2 import (
+from odoo.addons.l10n_it_fatturapa.bindings.fatturapa import (
     FatturaElettronica,
     FatturaElettronicaHeaderType,
     DatiTrasmissioneType,
