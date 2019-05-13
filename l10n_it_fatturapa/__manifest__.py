7556a481df385eeb54a75ad1debcbc01b7b0f630
jackjack82 <giacomo.grasso.82@gmail.com>
2019-05-13 18:41:43 +0200

    [11.0] Fattura elettronica per indirizzi di fatturazione

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Italian Localization - Fattura Elettronica - Base',
-    'version': '11.0.1.0.3',
+    'version': '11.0.1.0.4',
     'category': 'Localization/Italy',
     'summary': 'Electronic invoices',
     'author': 'Davide Corio, Agile Business Group, Innoviu, '
