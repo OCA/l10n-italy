abb3fbffa7a6d0e30628e002a0aaa9731d40f704
Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
2019-05-15 11:42:44 +0200

    [FIX] Use journals to filter move lines

--- l10n_it_central_journal/__manifest__.py
+++ l10n_it_central_journal/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Italian Localization - Account central journal',
-    'version': '10.0.0.0.1',
+    'version': '10.0.0.0.2',
     'category': 'Localization/Italy',
     'author': 'Dinamiche Aziendali, Odoo Community Association (OCA)',
     'website': 'http://www.dinamicheaziendali.it',
