e3d80c87d328081c44a985c5a3774357e5703803
eLBati <elbaddy@gmail.com>
2019-05-25 10:44:43 +0200

    porting l10n_it_vat_statement_communication_reverse_charge to 12

--- l10n_it_vat_statement_communication_reverse_charge/__manifest__.py
+++ l10n_it_vat_statement_communication_reverse_charge/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Lorenzo Battistini @ TAKOBI
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "ITA - Comunicazione liquidazione IVA e reverse charge",
     "summary": "Permette di calcolare correttamente gli importi da comunicare "
                "in presenza di reverse charge",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "development_status": "Beta",
     "category": "Accounting",
     "website": "https://github.com/OCA/l10n-italy",
