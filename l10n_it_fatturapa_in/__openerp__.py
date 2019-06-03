1c1cc0e17435e1647be4d1ef1d3874f483e89512
SGiuseppe <gstoduto@gmail.com>
2019-06-03 18:05:23 +0200

    [8.0]  l10n_it_fatturapa_in: Correct _get_tax_amount() function (#1252)
    * Correct _get_tax_amount() function
    
    Removed the _get_tax_amount () function because the calculation on "Tax on Children" occurs differently from odoo10
    
    * Update __openerp__.py

--- l10n_it_fatturapa_in/__openerp__.py
+++ l10n_it_fatturapa_in/__openerp__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Ricezione',
-    'version': '8.0.1.1.6',
+    'version': '8.0.1.1.7',
     'category': 'Localization/Italy',
     'summary': 'Electronic invoices reception',
     'author': 'Agile Business Group, Innoviu, '
