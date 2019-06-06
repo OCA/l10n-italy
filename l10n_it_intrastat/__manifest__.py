e446dcdd053afb715391ad1acdf34d1725912ba0
Lara Baggio <lbaggio@linkgroup.it>
2019-06-06 15:53:06 +0200

    rinominati e spostati file per pulizia

--- l10n_it_intrastat/__manifest__.py
+++ l10n_it_intrastat/__manifest__.py
@@ -1,48 +1,15 @@
-# -*- coding: utf-8 -*-
-#
-#    Author: Alessandro Camilli (a.camilli@openforce.it)
-#    Copyright (C) 2015
-#    Apulia Software srl - info@apuliasoftware.it - www.apuliasoftware.it
-#    Openforce di Camilli Alessandro - www.openforce.it
-#    LinkIt Srl (<http://http://www.linkgroup.it>)
-#
-# Odoo Proprietary License v1.0
-#
-# This software and associated files (the "Software") may only be used
-# (executed, modified, executed after modifications) if you have purchased a
-# valid license from the authors, typically via Odoo Apps, or if you have
-# received a written agreement from the authors of the Software
-#
-# You may develop Odoo modules that use the Software as a library (typically by
-# depending on it, importing it and using its resources), but without copying
-# any source code or material from the Software. You may distribute those
-# modules under the license of your choice, provided that this license is
-# compatible with the terms of the Odoo Proprietary License (For example:
-# LGPL, MIT, or proprietary licenses similar to this one).
-#
-# It is forbidden to publish, distribute, sublicense, or sell copies of the
-# Software or modified copies of the Software.
-#
-# The above copyright notice and this permission notice must be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 {
-    'name': 'Account - Intrastat',
-    'version': '10.0.1.0.1',
+    'name': 'ITA - Intrastat',
+    'version': '11.0.1.0.0',
     'category': 'Account',
-    'author': 'Odoo Italia Network',
-    'website': 'https://www.odoo-italia.net/',
-    'license': 'Other proprietary',
+    'summary': 'Riclassificazione merci e servizi per dichiarazioni Intrastat',
+    'author': 'Openforce'
+              ', Link IT srl, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-italy',
+    'license': 'LGPL-3',
     "depends": [
-        'account',
+        'sale_management',
         'product',
         'stock',
         'stock_account',
@@ -53,9 +20,13 @@
         'views/product.xml',
         'views/account.xml',
         'views/config.xml',
-        ],
+        'data/account.intrastat.transation.nature.csv',
+        'data/account.intrastat.transport.csv',
+        'data/account.intrastat.custom.csv',
+        'data/report.intrastat.code.csv',
+    ],
     "demo": [
         'demo/product_demo.xml'
-        ],
+    ],
     "installable": True
 }
--- l10n_it_intrastat/models/intrastat.py
+++ l10n_it_intrastat/models/intrastat.py
@@ -1,72 +1,63 @@
-# -*- coding: utf-8 -*-
-#
-#    Author: Alessandro Camilli (a.camilli@openforce.it)
-#    Copyright (C) 2015
-#    Apulia Software srl - info@apuliasoftware.it - www.apuliasoftware.it
-#    Openforce di Camilli Alessandro - www.openforce.it
-#
-
-
 from openerp import models, fields, api, _
-import openerp.addons.decimal_precision as dp
-from openerp.exceptions import except_orm, ValidationError
-from datetime import datetime, date, timedelta
+from openerp.exceptions import ValidationError
 
-class account_intrastat_custom(models.Model):
+
+class AccountIntrastatCustom(models.Model):
     _name = 'account.intrastat.custom'
-    _description = 'Account INTRASTAT - Customs'
-    
+    _description = 'Customs Sections'
+
     code = fields.Char(string='Code', size=6)
     name = fields.Char(string='Name')
     date_start = fields.Date(string='Date start')
     date_stop = fields.Date(string='Date stop')
 
 
-class report_intrastat_code(models.Model):
+class ReportIntrastatCode(models.Model):
 
     _inherit = 'report.intrastat.code'
 
     active = fields.Boolean(default=True)
-    additional_unit_required = fields.Boolean(default=False,
+    additional_unit_required = fields.Boolean(
+        default=False,
         string='Unit of Measure Additional Required')
     additional_unit_from = fields.Selection(
-        [('quantity', 'Quantity'),('weight', 'Weight'),('none', 'None')], 
+        [('quantity', 'Quantity'), ('weight', 'Weight'), ('none', 'None')],
         string='Additional Unit of Measure FROM')
-    additional_unit_uom_id = fields.Many2one('product.uom', 
+    additional_unit_uom_id = fields.Many2one(
+        'product.uom',
         string='Unit of Measure Additional')
     type = fields.Selection(
         [('good', 'Good'), ('service', 'Service')])
     description = fields.Char('Description', translate=True)
-    
-    
-class res_country(models.Model):
+
+
+class ResCountry(models.Model):
 
     _inherit = 'res.country'
-    
+
     @api.model
     def intrastat_validate(self):
         control_ISO_code = self._context.get('control_ISO_code', False)
         if not self:
             raise ValidationError(
-                _('Missing Country' ))
+                _('Missing Country'))
         if control_ISO_code and not self.code:
             raise ValidationError(
-                _('Country %s without ISO code') % (self.name,) )
+                _('Country %s without ISO code') % (self.name,))
         return True
 
 
-class account_intrastat_transport(models.Model):
+class AccountIntrastatTransport(models.Model):
     _name = 'account.intrastat.transport'
-    _description = 'Account INTRASTAT - Transport'
-    
+    _description = 'Transport mode'
+
     code = fields.Char(string='Code', size=1, required=True)
     name = fields.Char(string='Name')
 
 
-class account_intrastat_transation_nature(models.Model):
+class AccountIntrastatTransationNature(models.Model):
     _name = 'account.intrastat.transation.nature'
-    _description = 'Account INTRASTAT - Transation Nature'
-    
+    _description = 'Transaction Nature'
+
     code = fields.Char(string='Code', size=1, required=True)
     name = fields.Char(string='Name')
-
