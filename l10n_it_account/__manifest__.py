722ba4593b86f3f3c0b0d62861cbd3974ec65fb9
Silvio Gregorini <silviogregorini@openforce.it>
2019-06-06 16:51:35 +0200

    [UPD] [l10n_it_account] Added constraint upon accounts and account groups to prevent groups from having accounts of different types

--- l10n_it_account/__manifest__.py
+++ l10n_it_account/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Italian Localization - Account',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.2.0',
     'category': 'Hidden',
     'author': "Agile Business Group, Abstract, "
               "Odoo Community Association (OCA)",
