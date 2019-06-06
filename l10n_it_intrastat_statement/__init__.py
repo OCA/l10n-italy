9aec5327d835a024d76ae65b3e7ef36a372c72f4
Lara Baggio <lbaggio@linkgroup.it>
2019-06-06 15:53:06 +0200

    rinominati e spostati file per pulizia

--- l10n_it_intrastat_statement/__init__.py
+++ l10n_it_intrastat_statement/__init__.py
@@ -1,12 +1,2 @@
-# -*- coding: utf-8 -*-
-#
-#    Author: Alessandro Camilli (a.camilli@openforce.it)
-#    Copyright (C) 2015
-#    Apulia Software srl - info@apuliasoftware.it - www.apuliasoftware.it
-#    Openforce di Camilli Alessandro - www.openforce.it
-#
-
-
 from . import models
 from . import wizard
-
--- l10n_it_intrastat_statement/models/config.py
+++ l10n_it_intrastat_statement/models/config.py
@@ -1,18 +1,9 @@
-# -*- coding: utf-8 -*-
-#
-#    Author: Alessandro Camilli (a.camilli@openforce.it)
-#    Copyright (C) 2015
-#    Apulia Software srl - info@apuliasoftware.it - www.apuliasoftware.it
-#    Openforce di Camilli Alessandro - www.openforce.it
-#
-
-from openerp import models, fields, api
+from openerp import models, fields
 
 
-class res_company(models.Model):
+class ResCompany(models.Model):
     _inherit = 'res.company'
-    
+
     intrastat_custom_id = fields.Many2one(
         'account.intrastat.custom', string='Custom'
-        )
-    
+    )
