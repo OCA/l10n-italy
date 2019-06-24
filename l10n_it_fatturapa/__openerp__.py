3b3bdb670ffe58dac31e012c8b3099b7d2d819c2
sergiocorato <sergiocorato@gmail.com>
2019-06-24 11:09:23 +0200

    [IMP] mngt data ability in l10n_it_fatturapa

--- l10n_it_fatturapa/__openerp__.py
+++ l10n_it_fatturapa/__openerp__.py
@@ -2,6 +2,7 @@
 # Copyright 2014 Davide Corio <davide.corio@abstract.it>
 # Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
 # Copyright 2018 Gianmarco Conte, Marco Calcagni - Dinamiche Aziendali srl
+# Copyright 2019 Sergio Corato
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
--- l10n_it_fatturapa/models/account.py
+++ l10n_it_fatturapa/models/account.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2014 Davide Corio <davide.corio@abstract.it>
 # Copyright 2018 Gianmarco Conte, Marco Calcagni - Dinamiche Aziendali srl
+# Copyright 2019 Sergio Corato
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from openerp import fields, models, api
@@ -417,3 +418,32 @@ class AccountInvoice(models.Model):
     electronic_invoice_subjected = fields.Boolean(
         'Subjected to electronic invoice',
         related='partner_id.electronic_invoice_subjected', readonly=True)
+    related_mngt_data_ids = fields.One2many(
+        'fatturapa.mngt_data_type', 'invoice_id',
+        'Related management datas', copy=False
+    )
+
+
+class FatturapaMngtData(models.Model):
+    _name = 'fatturapa.mngt_data_type'
+    _description = 'FatturaPA Mngt Data Type'
+
+    name = fields.Char('Name', size=10, required=True)
+    lineRef = fields.Integer('LineRef')
+    invoice_id = fields.Many2one(
+        'account.invoice', 'Related Invoice',
+        ondelete='cascade', index=True)
+    invoice_line_id = fields.Many2one(
+        'account.invoice.line', 'Related Invoice Line',
+        ondelete='cascade', index=True)
+    text_ref = fields.Char('Reference Text', size=60)
+    number_ref = fields.Float('Reference Number')
+    date_ref = fields.Date('Reference Date')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('invoice_line_id'):
+            line_obj = self.env['account.invoice.line']
+            line = line_obj.browse(vals['invoice_line_id'])
+            vals['lineRef'] = line.sequence
+        return super(FatturapaMngtData, self).create(vals)
