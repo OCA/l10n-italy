51db17ec4bde680ca9606b06da1bdff4eff8f1fc
eLBati <elbaddy@gmail.com>
2019-06-26 18:46:15 +0200

    ADD test for invoicing services by DDT

--- l10n_it_ddt/__manifest__.py
+++ l10n_it_ddt/__manifest__.py
@@ -7,7 +7,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Italian Localization - DDT: documento di trasporto',
+    'name': 'ITA - DDT: documento di trasporto',
     'version': '12.0.1.3.0',
     'category': 'Localization/Italy',
     'summary': 'Documento di Trasporto',
