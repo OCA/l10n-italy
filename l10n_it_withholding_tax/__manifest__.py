dbe5d696e1a10b8042237d0414af341afc3f0301
Silvio Gregorini <silviogregorini@openforce.it>
2019-06-28 16:46:42 +0200

    [FIX] l10n_it_withholding_tax: Added condition upon 'date_end' column in where clause for _get_rate method

--- l10n_it_withholding_tax/__manifest__.py
+++ l10n_it_withholding_tax/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Italian Withholding Tax',
-    'version': '12.0.1.0.3',
+    'version': '12.0.1.0.4',
     'category': 'Account',
     'author': 'Openforce, Odoo Italia Network, '
               'Odoo Community Association (OCA)',
