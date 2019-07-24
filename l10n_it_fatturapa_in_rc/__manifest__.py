d88fdb1b34c0b6cebb9f352cfdf1edeed94bc8a7
eLBati <elbaddy@gmail.com>
2019-07-24 19:19:07 +0200

    IMP l10n_it_fatturapa_in_rc metadata ADD tests FIX RC flag and inconsistency message about totals

--- l10n_it_fatturapa_in_rc/__manifest__.py
+++ l10n_it_fatturapa_in_rc/__manifest__.py
@@ -1,12 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Sergio Corato (https://efatto.it)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Italian Localization - Fattura elettronica - integrazione fattura'
-            'in acquisto e reverse charge',
-    "summary": "Modulo di collegamento tra fattura in acquisto e reverse"
-               " charge",
-    "version": "10.0.1.0.0",
+    'name': 'ITA - Fattura elettronica - Inversione contabile',
+    "summary": "Modulo ponte tra e-fattura in acquisto e inversione"
+               " contabile",
+    "version": "12.0.1.1.0",
     "development_status": "Beta",
     "category": "Hidden",
     'website': 'https://github.com/OCA/l10n-italy/tree/10.0/'
@@ -20,7 +18,9 @@
     "depends": [
         "l10n_it_reverse_charge",
         "l10n_it_fatturapa_in",
+        "l10n_it_account_tax_kind",
     ],
     "data": [
+        "views/rc_type_view.xml",
     ],
 }
