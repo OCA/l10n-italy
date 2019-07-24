c34ba0c879dd78869ec5a2060f0af115c9a6ddc7
eLBati <elbaddy@gmail.com>
2019-07-24 19:16:18 +0200

    FIX l10n_it_fatturapa_in use fiscal position from partner FIX l10n_it_fatturapa_in: delete einvoice.line when invoice is deleted REF l10n_it_fatturapa_in tests with FatturapaCommon
    REF l10n_it_fatturapa_in extracting _compute_e_invoice_validation_error parts for override

--- l10n_it_fatturapa_in/wizard/wizard_import_fatturapa.py
+++ l10n_it_fatturapa_in/wizard/wizard_import_fatturapa.py
@@ -883,7 +883,8 @@ class WizardImportFatturapa(models.TransientModel):
             'currency_id': currency[0].id,
             'journal_id': purchase_journal.id,
             # 'origin': xmlData.datiOrdineAcquisto,
-            'fiscal_position_id': False,
+            'fiscal_position_id': (
+                partner.property_account_position_id.id or False),
             'payment_term_id': partner.property_supplier_payment_term_id.id,
             'company_id': company.id,
             'fatturapa_attachment_in_id': fatturapa_attachment.id,
