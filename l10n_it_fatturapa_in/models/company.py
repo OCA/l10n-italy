35ebe40ea61cc95b2c44e8a6f972898e11ae0bfc
Roberto Fichera <robyf@tekno-soft.it>
2019-07-29 17:01:31 +0200

    [10.0][l10n_it_fatturapa_in] Cleanup company model inheritage to redundant field declaration and useless onchange

--- l10n_it_fatturapa_in/models/company.py
+++ l10n_it_fatturapa_in/models/company.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, api
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
+
     cassa_previdenziale_product_id = fields.Many2one(
         'product.product', 'Welfare Fund Data Product',
         help="Product used to model DatiCassaPrevidenziale XML element "
@@ -33,17 +34,13 @@ class ResCompany(models.Model):
 
 class AccountConfigSettings(models.TransientModel):
     _inherit = 'account.config.settings'
+
     cassa_previdenziale_product_id = fields.Many2one(
         related='company_id.cassa_previdenziale_product_id',
-        string="Welfare Fund Data Product",
-        help="Product used to model DatiCassaPrevidenziale XML element "
-             "on bills."
     )
     sconto_maggiorazione_product_id = fields.Many2one(
         related='company_id.sconto_maggiorazione_product_id',
-        string="Discount Supplement Product",
-        help="Product used to model ScontoMaggiorazione XML element on bills."
-        )
+    )
     arrotondamenti_attivi_account_id = fields.Many2one(
         related='company_id.arrotondamenti_attivi_account_id',
     )
@@ -53,21 +50,3 @@ class AccountConfigSettings(models.TransientModel):
     arrotondamenti_tax_id = fields.Many2one(
         related='company_id.arrotondamenti_tax_id',
     )
-
-    @api.onchange('company_id')
-    def onchange_company_id(self):
-        res = super(AccountConfigSettings, self).onchange_company_id()
-        if self.company_id:
-            company = self.company_id
-            self.cassa_previdenziale_product_id = (
-                company.cassa_previdenziale_product_id and
-                company.cassa_previdenziale_product_id.id or False
-            )
-            self.sconto_maggiorazione_product_id = (
-                company.sconto_maggiorazione_product_id and
-                company.sconto_maggiorazione_product_id.id or False
-                )
-        else:
-            self.cassa_previdenziale_product_id = False
-            self.sconto_maggiorazione_product_id = False
-        return res
