844c8879956d0b5d6fa54f5898028563449f6f2c
Roberto Fichera <robyf@tekno-soft.it>
2019-08-01 18:52:24 +0200

    [12.0][l10n_it_fatturapa_in] Supporto per la rilevazione degli arrotondamenti in fatture elettroniche d'acquisto

--- l10n_it_fatturapa_in/models/company.py
+++ l10n_it_fatturapa_in/models/company.py
@@ -1,9 +1,10 @@
 
-from odoo import fields, models, api
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
+
     cassa_previdenziale_product_id = fields.Many2one(
         'product.product', 'Welfare Fund Data Product',
         help="Product used to model DatiCassaPrevidenziale XML element "
@@ -12,38 +13,45 @@ class ResCompany(models.Model):
     sconto_maggiorazione_product_id = fields.Many2one(
         'product.product', 'Discount Supplement Product',
         help="Product used to model ScontoMaggiorazione XML element on bills."
-        )
+    )
+
+    arrotondamenti_attivi_account_id = fields.Many2one(
+        'account.account', 'Round Up Account',
+        domain=[('deprecated', '=', False)],
+        help="Account used to round up bills amount."
+    )
+    arrotondamenti_passivi_account_id = fields.Many2one(
+        'account.account', 'Round Down Account',
+        domain=[('deprecated', '=', False)],
+        help="Account used to round down bills amount."
+    )
+    arrotondamenti_tax_id = fields.Many2one(
+        'account.tax', 'Rounding Tax',
+        domain=[('type_tax_use', '=', 'purchase'), ('amount', '=', 0.0)],
+        help="Tax used to both round up and down bills amount."
+    )
 
 
 class AccountConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
+
     cassa_previdenziale_product_id = fields.Many2one(
         related='company_id.cassa_previdenziale_product_id',
-        string="Welfare Fund Data Product",
-        help="Product used to model DatiCassaPrevidenziale XML element "
-             "on bills.", readonly=False
+        readonly=False
     )
     sconto_maggiorazione_product_id = fields.Many2one(
         related='company_id.sconto_maggiorazione_product_id',
-        string="Discount Supplement Product",
-        help='Product used to model ScontoMaggiorazione XML element on bills',
         readonly=False
-        )
-
-    @api.onchange('company_id')
-    def onchange_company_id(self):
-        res = super(AccountConfigSettings, self).onchange_company_id()
-        if self.company_id:
-            company = self.company_id
-            self.cassa_previdenziale_product_id = (
-                company.cassa_previdenziale_product_id and
-                company.cassa_previdenziale_product_id.id or False
-            )
-            self.sconto_maggiorazione_product_id = (
-                company.sconto_maggiorazione_product_id and
-                company.sconto_maggiorazione_product_id.id or False
-                )
-        else:
-            self.cassa_previdenziale_product_id = False
-            self.sconto_maggiorazione_product_id = False
-        return res
+    )
+    arrotondamenti_attivi_account_id = fields.Many2one(
+        related='company_id.arrotondamenti_attivi_account_id',
+        readonly=False
+    )
+    arrotondamenti_passivi_account_id = fields.Many2one(
+        related='company_id.arrotondamenti_passivi_account_id',
+        readonly=False
+    )
+    arrotondamenti_tax_id = fields.Many2one(
+        related='company_id.arrotondamenti_tax_id',
+        readonly=False
+    )
