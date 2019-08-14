1be3de8ebd3f0ce34b24c2dd79ed0756a241510b
SimoRubi <simone.rubino@agilebg.com>
2019-08-14 17:30:32 +0200

    [IMP] l10n_it_fatturapa_out: FatturaPA attachment names must be unique

--- l10n_it_fatturapa_out/models/attachment.py
+++ l10n_it_fatturapa_out/models/attachment.py
@@ -15,6 +15,10 @@ class FatturaPAAttachment(models.Model):
 
     ir_attachment_id = fields.Many2one(
         'ir.attachment', 'Attachment', required=True, ondelete="cascade")
+    att_name = fields.Char(
+        string="E-invoice file name",
+        related='ir_attachment_id.name',
+        store=True)
     out_invoice_ids = fields.One2many(
         'account.invoice', 'fatturapa_attachment_out_id',
         string="Out Invoices", readonly=True)
@@ -25,6 +29,11 @@ class FatturaPAAttachment(models.Model):
     invoice_partner_id = fields.Many2one(
         'res.partner', string='Customer', store=True,
         compute='_compute_invoice_partner_id')
+
+    _sql_constraints = [(
+        'ftpa_attachment_out_name_uniq',
+        'unique(att_name)',
+        'The name of the e-invoice file must be unique!')]
 
     @api.model
     def get_file_vat(self):
