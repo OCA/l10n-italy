bb6ab23c825051637bcae37e419ccb686ab4a356
Lara Baggio <lbaggio@linkgroup.it>
2019-08-20 11:04:09 +0200

    [MIG] l10n_it_vat_registries_cash_basis: Migration to 11.0

--- l10n_it_vat_registries_cash_basis/__manifest__.py
+++ l10n_it_vat_registries_cash_basis/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Lara Baggio - Link IT srl
 # (<http://www.linkgroup.it/>)
 # Copyright 2014-2017 Lorenzo Battistini - Agile Business Group
@@ -7,15 +6,16 @@
 
 {
     'name': 'Italian Localization - VAT Registries - Cash Basis',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
+    "development_status": "Production/Stable",
     'category': 'Hidden',
     "author": "LinkIt Srl, Agile Business Group"
               ", Odoo Community Association (OCA)",
-    'website': 'http://www.linkgroup.it/',
-    'license': 'LGPL-3',
+    'website': 'https://github.com/OCA/l10n-italy/tree/11.0/',
+    'license': 'AGPL-3',
     "depends": [
         'l10n_it_vat_registries',
-        'account_tax_cash_basis'],
+        'account_cash_basis_base_account'],
     "data": [
     ],
     'installable': True,
--- l10n_it_vat_registries_cash_basis/wizard/__init__.py
+++ l10n_it_vat_registries_cash_basis/wizard/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Lara Baggio - Link IT srl
 # (<http://www.linkgroup.it/>)
 # Copyright 2014-2017 Lorenzo Battistini - Agile Business Group
