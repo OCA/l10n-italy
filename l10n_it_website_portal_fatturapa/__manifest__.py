3d0fbcbb50d9f2b04d9948c0c5baa6dc2debc873
SimoRubi <simone.rubino@agilebg.com>
2019-08-22 12:41:43 +0200

    [FIX] l10n_it_website_portal_fatturapa: Fix electronic invoice label

--- l10n_it_website_portal_fatturapa/__manifest__.py
+++ l10n_it_website_portal_fatturapa/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Italian Localization - Fattura elettronica - Portale",
     "summary": "Add fatturapa fields and checks in frontend user's details",
-    "version": "12.0.1.2.0",
+    "version": "12.0.1.2.1",
     "author": "Odoo Community Association (OCA)",
     "category": "Localization/Italy",
     "website": "https://github.com/OCA/l10n-italy/tree/"
