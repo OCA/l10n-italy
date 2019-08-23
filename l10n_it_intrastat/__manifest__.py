f10830ba839c98e3ef4a54900b849817340b1317
gprina <gprina@linkgroup.it>
2019-08-23 10:06:38 +0200

    [MIG] l10n_it_intrastat: Migration to 12.0

--- l10n_it_intrastat/__manifest__.py
+++ l10n_it_intrastat/__manifest__.py
@@ -1,32 +1,35 @@
+# Copyright 2019 Simone Rubino - Agile Business Group
+# Copyright 2015 Camilli Alessandro - www.openforce.it
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'ITA - Intrastat',
-    'version': '11.0.1.0.0',
+    'name': "ITA - Intrastat",
+    'version': '12.0.1.0.0',
     'category': 'Account',
-    'summary': 'Riclassificazione merci e servizi per dichiarazioni Intrastat',
-    'author': 'Openforce'
-              ', Link IT srl, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-italy',
-    'license': 'LGPL-3',
-    "depends": [
+    'author': "Openforce, Link IT srl, Agile Business Group, "
+              "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/l10n-italy/tree/'
+               '12.0/l10n_it_intrastat',
+    'license': 'AGPL-3',
+    'depends': [
         'sale_management',
         'product',
         'stock',
         'stock_account',
-        'report_intrastat'],
-    "data": [
+        'uom',
+    ],
+    'data': [
         'security/ir.model.access.csv',
-        'views/intrastat.xml',
-        'views/product.xml',
-        'views/account.xml',
-        'views/config.xml',
         'data/account.intrastat.transation.nature.csv',
         'data/account.intrastat.transport.csv',
         'data/account.intrastat.custom.csv',
         'data/report.intrastat.code.csv',
+        'views/intrastat.xml',
+        'views/product.xml',
+        'views/account.xml',
+        'views/config.xml',
     ],
-    "demo": [
+    'demo': [
         'demo/product_demo.xml'
-    ],
-    "installable": True
+    ]
 }
