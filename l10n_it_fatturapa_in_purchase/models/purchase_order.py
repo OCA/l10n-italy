bab1f78d0eeaac20318d34008c073a57b5f593c0
SimoRubi <simone.rubino@agilebg.com>
2019-08-30 15:30:46 +0200

    [MIG] l10n_it_fatturapa_in_purchase: Migration to 12.0

--- l10n_it_fatturapa_in_purchase/models/purchase_order.py
+++ l10n_it_fatturapa_in_purchase/models/purchase_order.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+# Copyright 2018 Lorenzo Battistini
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import models, fields, api
 
