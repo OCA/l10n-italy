cbfbdc057422154bd5148b77d1d6528e14518156
SimoRubi <simone.rubino@agilebg.com>
2019-08-30 15:46:27 +0200

    [IMP] l10n_it_fatturapa_in_purchase: Compute to_invoice according to purchase.order._get_invoiced

--- l10n_it_fatturapa_in_purchase/models/purchase_order.py
+++ l10n_it_fatturapa_in_purchase/models/purchase_order.py
@@ -1,8 +1,10 @@
 # Copyright 2018 Lorenzo Battistini
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# Copyright 2019 Simone Rubino - Agile Business Group
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import models, fields, api
+from odoo.tools import float_compare
 
 
 class POLine(models.Model):
@@ -15,8 +17,16 @@ class POLine(models.Model):
         "invoice_lines.quantity", "invoice_lines.uom_id"
     )
     def _compute_to_invoice(self):
+        precision = self.env['decimal.precision'] \
+            .precision_get('Product Unit of Measure')
         for line in self:
-            if line.product_qty > line.qty_invoiced:
+            if line.product_id.purchase_method == 'purchase':
+                qty_to_invoice = line.product_qty
+            else:
+                qty_to_invoice = line.qty_received
+
+            if float_compare(line.qty_invoiced, qty_to_invoice,
+                             precision_digits=precision) == -1:
                 line.to_invoice = True
             else:
                 line.to_invoice = False
