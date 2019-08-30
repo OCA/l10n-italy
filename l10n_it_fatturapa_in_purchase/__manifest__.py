40ad411f1594f4f38fcf1089104721da3cc56b8e
SimoRubi <simone.rubino@agilebg.com>
2019-08-30 15:30:46 +0200

    [MIG] l10n_it_fatturapa_in_purchase: Migration to 12.0

--- l10n_it_fatturapa_in_purchase/__manifest__.py
+++ l10n_it_fatturapa_in_purchase/__manifest__.py
@@ -1,24 +1,22 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Lorenzo Battistini
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 
 {
-    'name': 'Italian Localization - '
-            'Fattura elettronica - Integrazione acquisti',
+    'name': 'ITA - Fattura elettronica - Integrazione acquisti',
     "summary": "Modulo ponte tra ricezione fatture elettroniche e acquisti",
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Hidden',
     'author': 'Agile Business Group, '
               'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-italy/tree/10.0/'
+    'website': 'https://github.com/OCA/l10n-italy/tree/12.0/'
                'l10n_it_fatturapa_in_purchase',
     'license': 'LGPL-3',
     "depends": [
         'l10n_it_fatturapa_in',
         'purchase',
-        ],
+    ],
     "data": [
         "views/invoice_view.xml",
     ],
