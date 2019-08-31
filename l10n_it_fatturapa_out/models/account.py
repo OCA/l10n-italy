3bcfcbd57c1360af90e1adcc5dce3533ed6cfc09
eLBati <elbaddy@gmail.com>
2019-08-31 12:19:07 +0200

    FIX l10n_it_fatturapa_out: invoice lines "Section" and "Note" can't use taxes not present in DatiRiepilogo

--- l10n_it_fatturapa_out/models/account.py
+++ l10n_it_fatturapa_out/models/account.py
@@ -33,3 +33,19 @@ class AccountInvoice(models.Model):
                 ) % invoice.number)
         res = super(AccountInvoice, self).action_invoice_cancel()
         return res
+
+    def get_first_non_zero_tax(self):
+        for line in self.invoice_line_ids:
+            if (
+                not line.display_type and line.price_subtotal and
+                len(line.invoice_line_tax_ids) == 1
+            ):
+                return line.invoice_line_tax_ids[0]
+        return False
+
+    def set_taxes_for_descriptive_lines(self):
+        for line in self.invoice_line_ids:
+            if line.display_type:
+                non_zero_tax = self.get_first_non_zero_tax()
+                if non_zero_tax:
+                    line.invoice_line_tax_ids = [(6, 0, [non_zero_tax.id])]
