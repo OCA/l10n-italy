b45968e1bfa17c7ffafcc745595f8807b709a352
eLBati <elbaddy@gmail.com>
2019-09-11 15:52:20 +0200

    FIX l10n_it_fatturapa_in when importing XML with 100% discount. See test case

--- l10n_it_fatturapa_in/tests/test_import_fatturapa_xml.py
+++ l10n_it_fatturapa_in/tests/test_import_fatturapa_xml.py
@@ -453,3 +453,11 @@ class TestFatturaPAXMLValidation(SingleTransactionCase):
         self.assertEqual(invoice.inconsistencies, '')
         self.assertEqual(invoice.invoice_line_ids[2].price_unit, 0.0)
         self.assertEqual(invoice.invoice_line_ids[2].discount, 0.0)
+
+    def test_31_xml_import(self):
+        res = self.run_wizard('test31', 'IT01234567890_FPR05.xml')
+        invoice_id = res.get('domain')[0][2][0]
+        invoice = self.invoice_model.browse(invoice_id)
+        self.assertEqual(invoice.invoice_line_ids[1].discount, 100)
+        self.assertEqual(invoice.invoice_line_ids[1].price_subtotal, 0)
+        self.assertEqual(invoice.amount_total, 12.2)
