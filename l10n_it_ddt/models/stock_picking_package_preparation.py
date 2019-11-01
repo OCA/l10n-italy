13b2ba2f8c2f715a686f1f45236bed7044b51759
Francesco Apruzzese <francescoapruzzese@openforce.it>
2019-11-01 19:21:27 +0100

    [FIX] missing translate boolean and order ddt ported from 8.0

--- l10n_it_ddt/models/stock_picking_package_preparation.py
+++ l10n_it_ddt/models/stock_picking_package_preparation.py
@@ -18,8 +18,9 @@ class StockPickingCarriageCondition(models.Model):
     _name = "stock.picking.carriage_condition"
     _description = "Carriage Condition"
 
-    name = fields.Char(string='Carriage Condition', required=True)
-    note = fields.Text(string='Note')
+    name = fields.Char(string='Carriage Condition', required=True,
+                       translate=True)
+    note = fields.Text(string='Note', translate=True)
 
 
 class StockPickingGoodsDescription(models.Model):
@@ -27,8 +28,9 @@ class StockPickingGoodsDescription(models.Model):
     _name = 'stock.picking.goods_description'
     _description = "Description of Goods"
 
-    name = fields.Char(string='Description of Goods', required=True)
-    note = fields.Text(string='Note')
+    name = fields.Char(string='Description of Goods', required=True,
+                       translate=True)
+    note = fields.Text(string='Note', translate=True)
 
 
 class StockPickingTransportationReason(models.Model):
@@ -36,9 +38,10 @@ class StockPickingTransportationReason(models.Model):
     _name = 'stock.picking.transportation_reason'
     _description = 'Reason for Transportation'
 
-    name = fields.Char(string='Reason For Transportation', required=True)
+    name = fields.Char(string='Reason For Transportation', required=True,
+                       translate=True)
     note = fields.Text(string='Note')
-    to_be_invoiced = fields.Boolean(string='To be Invoiced')
+    to_be_invoiced = fields.Boolean(string='To be Invoiced', translate=True)
 
 
 class StockPickingTransportationMethod(models.Model):
@@ -46,8 +49,9 @@ class StockPickingTransportationMethod(models.Model):
     _name = 'stock.picking.transportation_method'
     _description = 'Method of Transportation'
 
-    name = fields.Char(string='Method of Transportation', required=True)
-    note = fields.Text(string='Note')
+    name = fields.Char(string='Method of Transportation', required=True,
+                       translate=True)
+    note = fields.Text(string='Note', translate=True)
 
 
 class StockDdtType(models.Model):
@@ -79,7 +83,7 @@ class StockPickingPackagePreparation(models.Model):
 
     _inherit = 'stock.picking.package.preparation'
     _rec_name = 'display_name'
-    _order = 'date desc'
+    _order = 'ddt_number, date desc'
 
     @api.onchange('transportation_reason_id')
     def _onchange_to_be_invoiced(self):
