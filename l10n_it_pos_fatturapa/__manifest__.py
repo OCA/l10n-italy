845944189aa78c3a656a9700c35dc5b0c02e4043
eLBati <lb@takobi.online>
2019-11-25 07:23:03 +0100

    l10n_it_pos_fatturapa: porting to 12.0

--- l10n_it_pos_fatturapa/__manifest__.py
+++ l10n_it_pos_fatturapa/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Roberto Fichera
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
@@ -6,7 +5,7 @@
     "name": "ITA - POS - Fattura elettronica",
     "summary": "Gestione dati fattura elettronica del cliente all'interno "
                "dell'interfaccia del POS",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "development_status": "Beta",
     "category": "Point Of Sale",
     "website": "https://github.com/OCA/l10n-italy",
