6eed8c3b9cce44fd57827bc4a81b37003e795796
Francesco Apruzzese <francescoapruzzese@openforce.it>
2019-12-05 15:04:18 +0100

    [ADD] l10n_it_dichiarazione_intento
    [FIX] Check of invoice type searching valid declarations
    
    [FIX] flake8 on l10n_it_dichiarazione_intento
    
    [FIX] Import not used
    
    [FIX] Display name compute warning
    
    [FIX] FY display_name
    
    [FIX] Undefined name res
    
    [FIX] name_get result

---
+++ l10n_it_dichiarazione_intento/models/__init__.py
@@ -0,0 +1,6 @@
+from . import account
+from . import dichiarazione_intento
+from . import company
+from . import account_invoice
+from . import sale
+from . import product
---
+++ l10n_it_dichiarazione_intento/models/product.py
@@ -0,0 +1,18 @@
+# Copyright 2017 Francesco Apruzzese <f.apruzzese@apuliasoftware.it>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class ProductProduct(models.Model):
+
+    _inherit = 'product.product'
+
+    @api.model
+    def _convert_prepared_anglosaxon_line(self, line, partner):
+        res = super(ProductProduct, self)._convert_prepared_anglosaxon_line(
+            line, partner)
+        if line.get('force_dichiarazione_intento_id', False):
+            res['force_dichiarazione_intento_id'] = line[
+                'force_dichiarazione_intento_id']
+        return res
