c4c9126999b278b4c79aae35d35b217d255c7efa
As400it <mcalcagni@dinamicheaziendali.it>
2019-12-05 22:34:13 +0100

    Porting to 12.0

---
+++ l10n_it_ricevute_bancarie/models/account/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2012 Andrea Cometa.
+# Email: info@andreacometa.it
+# Web site: http://www.andreacometa.it
+# Copyright (C) 2012 Associazione OpenERP Italia
+# (<http://www.odoo-italia.org>).
+# Copyright (C) 2012-2017 Lorenzo Battistini - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import account
+from . import bank_statement
