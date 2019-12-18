d97dd3a099b7cc5c2511e474f62f34cb52e05d44
eLBati <lb@takobi.online>
2019-12-18 15:56:47 +0100

    IMP fiscal_epos_print: avoid to print again an order with a fiscal receipt number
    Lock receipt screen until printer communication ends.
    Prevent user to go to next order if receipt not correctly printed.
    Allow user to retry printing in case of errors
    
    ADD print fiscal closure widget (Z report)
    and X report
    
    ADD refund management from OrderListScreenWidget
    FIX Restaurant compatibility

--- fiscal_epos_print/static/src/js/chrome.js
+++ fiscal_epos_print/static/src/js/chrome.js
@@ -31,6 +31,70 @@ odoo.define("fiscal_epos_print.chrome", function (require) {
 
     });
 
+    var PrinterFiscalClosure = PosBaseWidget.extend({
+        template: 'PrinterFiscalClosure',
+
+        button_click: function () {
+            var self = this;
+            this.chrome.loading_show();
+            this.chrome.loading_message(_t('Connecting to the fiscal printer'));
+            var protocol = ((this.pos.config.use_https) ? 'https://' : 'http://');
+            var printer_url = protocol + this.pos.config.printer_ip + '/cgi-bin/fpmate.cgi';
+            var printer_options = {url: printer_url, requested_report: true};
+            var fp90 = new eposDriver(printer_options, this);
+            this.gui.show_popup('confirm',{
+                'title': _t('Confirm Printer Fiscal Closure (Report Z)?'),
+                'body': _t('Please confirm to execute the Printer Fiscal Closure'),
+                confirm: function(){
+                    fp90.printFiscalReport();
+                },
+                cancel: function(){
+                    self.chrome.loading_hide();
+                },
+            });
+        },
+
+        renderElement: function () {
+            var self = this;
+            this._super();
+            this.$el.click(function () {
+                self.button_click();
+            });
+        },
+    });
+
+    var PrinterFiscalXReport = PosBaseWidget.extend({
+        template: 'PrinterFiscalXReport',
+
+        button_click: function () {
+            var self = this;
+            this.chrome.loading_show();
+            this.chrome.loading_message(_t('Connecting to the fiscal printer'));
+            var protocol = ((this.pos.config.use_https) ? 'https://' : 'http://');
+            var printer_url = protocol + this.pos.config.printer_ip + '/cgi-bin/fpmate.cgi';
+            var printer_options = {url: printer_url, requested_report: true};
+            var fp90 = new eposDriver(printer_options, this);
+            this.gui.show_popup('confirm',{
+                'title': _t('Confirm Printer Daily Financial Report (Report X)?'),
+                'body': _t('Please confirm to execute the Printer Daily Financial Report'),
+                confirm: function(){
+                    fp90.printFiscalXReport();
+                },
+                cancel: function(){
+                    self.chrome.loading_hide();
+                },
+            });
+        },
+
+        renderElement: function () {
+            var self = this;
+            this._super();
+            this.$el.click(function () {
+                self.button_click();
+            });
+        },
+    });
+
     var widgets = chrome.Chrome.prototype.widgets;
     widgets.push({
         'name': _t('ADE files status'),
@@ -40,9 +104,27 @@ odoo.define("fiscal_epos_print.chrome", function (require) {
             'label': 'ADE files status',
         },
     });
+    widgets.push({
+        'name': _t('Printer Fiscal Closure'),
+        'widget': PrinterFiscalClosure,
+        'append': '.pos-rightheader',
+        'args': {
+            'label': 'Printer Fiscal Closure',
+        },
+    });
+    widgets.push({
+        'name': _t('Printer Fiscal X Report'),
+        'widget': PrinterFiscalXReport,
+        'append': '.pos-rightheader',
+        'args': {
+            'label': 'Printer Fiscal X Report',
+        },
+    });
 
     return {
         FiscalPrinterADEFilesButtonWidget: FiscalPrinterADEFilesButtonWidget,
+        PrinterFiscalClosure: PrinterFiscalClosure,
+        PrinterFiscalXReport: PrinterFiscalXReport,
     };
 
 });
