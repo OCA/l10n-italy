1a4fa4a3f58d5136f7bfa87ffe527eeef43c8d1c
Roberto Fichera <robyf@tekno-soft.it>
2019-12-18 19:51:20 +0100

    [10.0][l10n_it_fatturapa_out] Aggiunge la possibilità di specificare per cliente quante fatture si vogliono inserire in un file xml
    [10.0][l10n_it_fatturapa_pec] Permettere di rigenerare il file xml per le fatture elettroniche in stato di errore
    
    [10.0][l10n_it_fatturapa_out] Refactory del metodo di generazione delle fatture elettroniche, supporto per rigenerare gli xml e limite di fatture da inserire nel file xml generato

---
+++ l10n_it_fatturapa_out/models/partner.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Roberto Fichera <roberto.fichera@levelprime.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    max_invoice_in_xml = fields.Integer(
+        string='Max Invoice # in XML',
+        default=lambda self: self.env.user.company_id.max_invoice_in_xml,
+        help="Maximum number of invoices to group in a single "
+             "XML file. 0=Unlimited")
+
+    @api.constrains('max_invoice_in_xml')
+    def _validate_max_invoice_in_xml(self):
+        if self.max_invoice_in_xml < 0:
+            raise ValidationError(
+                _("The max number of invoice to group can't be negative"))
--- l10n_it_fatturapa_pec/__manifest__.py
+++ l10n_it_fatturapa_pec/__manifest__.py
@@ -3,6 +3,7 @@
 # Copyright 2018-2019 Sergio Corato (https://efatto.it)
 # Copyright 2018-2019 Lorenzo Battistini <https://github.com/eLBati>
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# Copyright 2019 Roberto Fichera (https://levelprime.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
@@ -22,9 +23,10 @@
     ],
     'data': [
         'security/groups.xml',
-        'views/account.xml',
         'views/fatturapa_attachment_out.xml',
         'wizard/send_pec_view.xml',
+        'wizard/wizard_export_fatturapa_view.xml',
+        'views/account.xml',
         'views/fetchmail_view.xml',
         'security/ir.model.access.csv',
         'data/fetchmail_data.xml',
