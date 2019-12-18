7fb057830e0af832f43c9c4deac5726e114e0df0
eLBati <lb@takobi.online>
2019-12-18 15:56:47 +0100

    IMP fiscal_epos_print: avoid to print again an order with a fiscal receipt number
    Lock receipt screen until printer communication ends.
    Prevent user to go to next order if receipt not correctly printed.
    Allow user to retry printing in case of errors
    
    ADD print fiscal closure widget (Z report)
    and X report
    
    ADD refund management from OrderListScreenWidget
    FIX Restaurant compatibility

--- fiscal_epos_print/readme/ROADMAP.rst
+++ fiscal_epos_print/readme/ROADMAP.rst
@@ -1,4 +1,3 @@
 * Resi:
    - Aggiungere controllo "rendibilità"
-   - Generare reso dalla lista di pos_order_mgmt
    - Stampare sullo scontrino un barcode identificativo, in modo da generare il reso facendone la scansione
--- fiscal_epos_print/static/src/js/pos_order_mgmt.js
+++ fiscal_epos_print/static/src/js/pos_order_mgmt.js
@@ -11,21 +11,20 @@ odoo.define("fiscal_epos_print.pos_order_mgmt", function (require) {
     OrderListScreenWidget.include({
         _prepare_order_from_order_data: function (order_data, action) {
             var order = this._super(order_data, action);
-            order.refund_report = order_data.refund_report;
-            order.refund_date = order_data.refund_date ? order_data.refund_date.substr(8, 2) +  // day
-                                                         order_data.refund_date.substr(5, 2) +  // month
-                                                         order_data.refund_date.substr(0, 4)    // year
-                                                       : null;
-            order.refund_doc_num = order_data.refund_doc_num;
-            order.refund_cash_fiscal_serial = order_data.refund_cash_fiscal_serial;
-
+            if (action === 'print') {
+                order.refund_report = order_data.refund_report;
+                order.refund_date = order_data.refund_date;
+                order.refund_doc_num = order_data.refund_doc_num;
+                order.refund_cash_fiscal_serial = order_data.refund_cash_fiscal_serial;
+            }
+            else if (action === 'return') {
+                order.refund_report = order_data.fiscal_z_rep_number;
+                order.refund_date = order_data.fiscal_receipt_date;
+                order.refund_doc_num = order_data.fiscal_receipt_number;
+                order.refund_cash_fiscal_serial = order_data.fiscal_printer_serial;
+            }
+            // for action === 'copy' we don't need to do anything
             return order;
-        },
-        // copiato da screens.PaymentScreenWidget
-        getPrinterOptions: function (){
-            var protocol = ((this.pos.config.use_https) ? 'https://' : 'http://');
-            var printer_url = protocol + this.pos.config.printer_ip + '/cgi-bin/fpmate.cgi';
-            return {url: printer_url};
         },
         // copiato da screens.PaymentScreenWidget
         sendToFP90Printer: function(receipt, printer_options) {
@@ -34,8 +33,17 @@ odoo.define("fiscal_epos_print.pos_order_mgmt", function (require) {
         },
         action_print: function (order_data, order) {
             if (this.pos.config.printer_ip) {
+                if (order_data.fiscal_receipt_number) {
+                    this.pos.gui.show_popup('error', {
+                        'title': _t('Order already printed'),
+                        'body': order_data.pos_reference + _t(": order already has a fiscal number, ") + order_data.fiscal_receipt_number,
+                    });
+                    return;
+                }
+                this.chrome.loading_show();
+                this.chrome.loading_message(_t('Connecting to the fiscal printer'));
                 var receipt = order.export_for_printing();
-                var printer_options = this.getPrinterOptions();
+                var printer_options = order.getPrinterOptions();
                 printer_options.order = order;
                 this.sendToFP90Printer(receipt, printer_options);
             }
