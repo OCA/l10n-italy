601f1f349742c82b838368ab360711c1a5ab2569
sergiocorato <sergiocorato@gmail.com>
2020-01-04 22:24:36 +0100

    [MIG] l10n_it_account_stamp_sale: Migration to 12.0

--- l10n_it_account_stamp_sale/__manifest__.py
+++ l10n_it_account_stamp_sale/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Alex Comba - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Italian Localization - Imposta di bollo - vendite',
+    'name': 'ITA - Imposta di bollo - Vendite',
     'summary': "Modulo ponte tra imposta di bollo e vendite",
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Localization/Italy',
     'license': 'AGPL-3',
     'author': 'Agile Business Group, Odoo Community Association (OCA)',
     'maintainers': ['tafaRU'],
-    'website': 'https://github.com/OCA/l10n-italy/tree/10.0/'
+    'website': 'https://github.com/OCA/l10n-italy/tree/12.0/'
                 'l10n_it_account_stamp_sale',
     'depends': [
         'l10n_it_account_stamp',
