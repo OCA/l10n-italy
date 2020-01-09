81f2784788930026cb2a516bca75942cac10f5c4
eLBati <lb@takobi.online>
2020-01-09 11:54:04 +0100

    ADD test for refund with positive total FIX importing refund XML with negative lines

--- l10n_it_fatturapa_in/wizard/wizard_import_fatturapa.py
+++ l10n_it_fatturapa_in/wizard/wizard_import_fatturapa.py
@@ -975,6 +975,8 @@ class WizardImportFatturapa(models.TransientModel):
 
         # compute the invoice
         invoice.compute_taxes()
+        # this can happen with refunds with negative amounts
+        invoice.process_negative_lines()
         return invoice_id
 
     def set_vendor_bill_data(self, FatturaBody, invoice):
