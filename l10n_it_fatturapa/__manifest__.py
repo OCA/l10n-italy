9297b07064798881ad9fbd3a7c597b3325f344a6
Giovanni <giovanni@gslab.it>
2020-01-15 18:14:33 +0100

    - add fatturapa_pub_administration_ref to res config settings view

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Italian Localization - Fattura elettronica - Base',
-    'version': '12.0.1.8.1',
+    'version': '12.0.1.9.0',
     'category': 'Localization/Italy',
     'summary': 'Fatture elettroniche',
     'author': 'Davide Corio, Agile Business Group, Innoviu, '
