50b689917397cf3b616358fb60ec75fc035391c9
Silvio Gregorini <silviogregorini@openforce.it>
2020-01-16 11:08:25 +0100

    [REF][l10n_it_vat_registries] Refractoring and improvement of views; removed leftovers of elder versions ('openerp' tags and such)

--- l10n_it_vat_registries/wizard/print_registro_iva.py
+++ l10n_it_vat_registries/wizard/print_registro_iva.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api, _
-from odoo.exceptions import Warning as UserError
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class WizardRegistroIva(models.TransientModel):
@@ -35,7 +35,7 @@ class WizardRegistroIva(models.TransientModel):
     @api.onchange('tax_registry_id')
     def on_change_tax_registry_id(self):
         self.journal_ids = self.tax_registry_id.journal_ids
-        self.layout_type = self.tax_registry_id.layout_type
+        return {"type": "ir.actions.do_nothing"}
 
     @api.onchange('date_range_id')
     def on_change_date_range_id(self):
@@ -46,14 +46,20 @@ class WizardRegistroIva(models.TransientModel):
     @api.onchange('from_date')
     def get_year_footer(self):
         if self.from_date:
-            self.year_footer = self.from_date.year
+            self.year_footer = fields.Datetime.from_string(self.from_date).year
 
     def _get_move_ids(self, wizard):
-        moves = self.env['account.move'].search([
-            ('date', '>=', self.from_date),
-            ('date', '<=', self.to_date),
-            ('journal_id', 'in', [j.id for j in self.journal_ids]),
-            ('state', '=', 'posted'), ], order='date, name')
+        moves = self.env['account.move'].search(
+            [('date', '>=', self.from_date),
+             ('date', '<=', self.to_date),
+             ('journal_id', 'in', self.journal_ids.ids),
+             ('state', '=', 'posted')],
+            order='date, name'
+        )
+
+        if not moves:
+            raise UserError(_('No documents found in the current selection'))
+
         return moves.ids
 
     @api.multi
@@ -65,30 +71,30 @@ class WizardRegistroIva(models.TransientModel):
                               'Please load them before to retry!'))
         move_ids = self._get_move_ids(wizard)
 
-        datas_form = {}
-        datas_form['from_date'] = wizard.from_date
-        datas_form['to_date'] = wizard.to_date
-        datas_form['journal_ids'] = [j.id for j in wizard.journal_ids]
-        datas_form['fiscal_page_base'] = wizard.fiscal_page_base
-        datas_form['registry_type'] = wizard.layout_type
-        datas_form['year_footer'] = wizard.year_footer
-
         lang_code = self.env.user.company_id.partner_id.lang
-        lang = self.env['res.lang']
-        lang_id = lang._lang_get(lang_code)
-        date_format = lang_id.date_format
-        datas_form['date_format'] = date_format
+        lang = self.env['res.lang']._lang_get(lang_code)
+        date_format = lang.date_format
 
         if wizard.tax_registry_id:
-            datas_form['tax_registry_name'] = wizard.tax_registry_id.name
+            tax_registry_name = wizard.tax_registry_id.name
         else:
-            datas_form['tax_registry_name'] = ''
-        datas_form['only_totals'] = wizard.only_totals
+            tax_registry_name = ''
+
         # report_name = 'l10n_it_vat_registries.report_registro_iva'
         report_name = 'l10n_it_vat_registries.action_report_registro_iva'
         datas = {
             'ids': move_ids,
             'model': 'account.move',
-            'form': datas_form
+            'form': {
+                'date_format': date_format,
+                'from_date': wizard.from_date,
+                'fiscal_page_base': wizard.fiscal_page_base,
+                'journal_ids': wizard.journal_ids.ids,
+                'only_totals': wizard.only_totals,
+                'registry_type': wizard.layout_type,
+                'tax_registry_name': tax_registry_name,
+                'to_date': wizard.to_date,
+                'year_footer': wizard.year_footer,
+            },
         }
         return self.env.ref(report_name).report_action(self, data=datas)
