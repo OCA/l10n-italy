fa0582155d2f7aa49d777156d055d8e04ca8116b
SimoRubi <simone.rubino@agilebg.com>
2020-03-26 10:51:40 +0100

    [ADD] l10n_it_fatturapa_sale

---
+++ l10n_it_fatturapa_sale/models/account_invoice.py
@@ -0,0 +1,15 @@
+#  Copyright 2020 Simone Rubino - Agile Business Group
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class AccountInvoice (models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def unlink(self):
+        related_documents = self.mapped('related_documents')
+        res = super().unlink()
+        related_documents.check_unlink().unlink()
+        return res
