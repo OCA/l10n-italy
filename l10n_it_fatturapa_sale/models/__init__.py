6f71232ffc11196cf4c558eaa02a5956f16bfcf2
SimoRubi <simone.rubino@agilebg.com>
2020-03-26 10:51:40 +0100

    [ADD] l10n_it_fatturapa_sale

---
+++ l10n_it_fatturapa_sale/models/__init__.py
@@ -0,0 +1,7 @@
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import account_invoice
+from . import account_invoice_line
+from . import related_document
+from . import sale_order
+from . import sale_order_line
