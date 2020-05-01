d7f5fd8bca4ff164694bfdf841184e811065b34b
alex_cam <alessandrocamilli@openforce.it>
2020-05-01 18:31:40 +0200

    [11.0][MIG] backport from 12

---
+++ l10n_it_vat_statement_communication/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2017-2020 Alessandro Camilli - Openforce
+# Copyright 2017-2019 Lorenzo Battistini
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    'name': 'ITA - Comunicazione liquidazione IVA',
+    'summary': 'Comunicazione liquidazione IVA ed esportazione file xml'
+               'conforme alle specifiche dell\'Agenzia delle Entrate',
+    'version': '11.0.1.0.0',
+    'category': 'Account',
+    'author': "Openforce di Camilli Alessandro, "
+              "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/l10n-italy',
+    'license': 'LGPL-3',
+    'depends': [
+        'account_vat_period_end_statement',
+        'l10n_it_codici_carica', 'l10n_it_fiscalcode'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/comunicazione_liquidazione.xml',
+        'views/config.xml',
+        'views/account.xml',
+        'wizard/export_file_view.xml',
+        'security/security.xml',
+    ],
+    'installable': True,
+}
---
+++ l10n_it_vat_statement_communication/wizard/export_file.py
@@ -0,0 +1,49 @@
+
+import base64
+from odoo import api, fields, models, exceptions, _
+
+
+class ComunicazioneLiquidazioneExportFile(models.TransientModel):
+    _name = "comunicazione.liquidazione.export.file"
+    _description = "Export VAT statement communication XML file"
+
+    file_export = fields.Binary('File', readonly=True)
+    name = fields.Char('File Name', readonly=True, default='liquidazione.xml')
+
+    @api.multi
+    def export(self):
+
+        comunicazione_ids = self._context.get('active_ids')
+        if not comunicazione_ids:
+            raise exceptions.Warning(_(
+                "No communication selected"
+            ))
+        if len(comunicazione_ids) > 1:
+            raise exceptions.Warning(_(
+                'You can export only 1 communication at a time'
+            ))
+
+        for wizard in self:
+            for comunicazione in self.env['comunicazione.liquidazione'].\
+                    browse(comunicazione_ids):
+                out = base64.encodestring(comunicazione.get_export_xml())
+                wizard.file_export = out
+                wizard.name = "%s_LI_%s.xml" % (
+                    comunicazione.declarant_fiscalcode,
+                    str(comunicazione.identificativo).rjust(5, '0'))
+            model_data_obj = self.env['ir.model.data']
+            view_rec = model_data_obj.get_object_reference(
+                'l10n_it_vat_statement_communication',
+                'wizard_liquidazione_export_file_exit'
+            )
+            view_id = view_rec and view_rec[1] or False
+
+            return {
+                'view_type': 'form',
+                'view_id': [view_id],
+                'view_mode': 'form',
+                'res_model': 'comunicazione.liquidazione.export.file',
+                'res_id': wizard.id,
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+            }
