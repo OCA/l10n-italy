c77478148b95ad22dd2d222e34504c4df02fda08
alex_cam <alessandrocamilli@openforce.it>
2020-05-05 20:57:56 +0200

    [FIX][l10n_it_dichiarazione_intento] Fix calcolo plafond in caso di nota credito

--- l10n_it_dichiarazione_intento/models/account_invoice.py
+++ l10n_it_dichiarazione_intento/models/account_invoice.py
@@ -82,7 +82,7 @@ class AccountInvoice(models.Model):
 
                 else:
                     continue
-            sign = 1 if invoice.type.startswith('out_') else -1
+            sign = 1 if invoice.type in ['out_invoice', 'in_refund'] else -1
             dichiarazioni_amounts = {}
             for tax_line in invoice.tax_line_ids:
                 amount = sign * tax_line.base
@@ -99,6 +99,18 @@ class AccountInvoice(models.Model):
                 raise UserError(_(
                     'Available plafond insufficent.\n'
                     'Excess value: %s') % (abs(dichiarazioni_residual)))
+            # Check se con nota credito ho superato il plafond
+            for dich in dichiarazioni_amounts:
+                dichiarazione = dichiarazione_model.browse(dich)
+                # dichiarazioni_amounts contains residual, so, if > limit_amount,
+                # used_amount went < 0
+                if dichiarazioni_amounts[dich] > dichiarazione.limit_amount:
+                    raise UserError(_(
+                        'Available plafond insufficent.\n'
+                        'Excess value: %s') % (
+                            abs(dichiarazioni_amounts[dich] -
+                                dichiarazione.limit_amount)
+                    ))
         # ----- Assign account move lines to dichiarazione for invoices
         for invoice in self:
             if invoice.dichiarazione_intento_ids:
