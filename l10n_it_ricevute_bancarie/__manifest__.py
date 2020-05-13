dbf0ff06227590d1ee812c5bc6b83b2636b34678
Lara Baggio <lbaggio@linkgroup.it>
2020-05-13 18:03:37 +0200

    merge PR 1264 1717 1728 1737 1739

--- l10n_it_ricevute_bancarie/__manifest__.py
+++ l10n_it_ricevute_bancarie/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': "ITA - Ricevute bancarie",
-    'version': "12.0.1.0.1",
+    'version': "12.0.1.0.2",
     'author': "Odoo Community Association (OCA)",
     'category': 'Localization/Italy',
     'summary': 'Ricevute bancarie',
