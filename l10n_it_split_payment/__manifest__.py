3986e65c432f6e4ff1b282cae31a0bff346d6633
Lara Baggio <lbaggio@linkgroup.it>
2020-05-13 18:03:37 +0200

    merge PR 1264 1717 1728 1737 1739

--- l10n_it_split_payment/__manifest__.py
+++ l10n_it_split_payment/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Split Payment',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Localization/Italy',
     'summary': 'Split Payment',
     'author': 'Abstract, Agile Business Group, '
