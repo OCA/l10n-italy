782b163887e586d6bec2cd2e89fc456cd392fb44
Lara Baggio <lbaggio@linkgroup.it>
2020-05-13 18:03:37 +0200

    merge PR 1264 1717 1728 1737 1739

--- l10n_it_vat_statement_split_payment/__manifest__.py
+++ l10n_it_vat_statement_split_payment/__manifest__.py
@@ -7,7 +7,7 @@
     'name': "ITA - Liquidazione IVA + Scissione dei pagamenti",
     'summary': "Migliora la liquidazione dell'IVA tenendo in"
                " considerazione la scissione dei pagamenti",
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'development_status': "Beta",
     'category': "Accounting & Finance",
     'website': 'https://github.com/OCA/l10n-italy',
