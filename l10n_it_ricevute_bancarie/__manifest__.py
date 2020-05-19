1cc560d66ed3b333eef672392a929b76238f9d52
sergiocorato <sergiocorato@gmail.com>
2020-05-19 18:12:17 +0200

    [12.0][FIX] remove unused files

--- l10n_it_ricevute_bancarie/__manifest__.py
+++ l10n_it_ricevute_bancarie/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': "ITA - Ricevute bancarie",
-    'version': "12.0.1.0.2",
+    'version': "12.0.1.0.3",
     'author': "Odoo Community Association (OCA)",
     'category': 'Localization/Italy',
     'summary': 'Ricevute bancarie',
