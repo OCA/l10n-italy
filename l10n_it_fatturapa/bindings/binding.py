ad4b79613fc2c72ad493e2965076874ae19c55de
SimoRubi <simone.rubino@agilebg.com>
2020-06-11 14:54:24 +0200

    [IMP] l10n_it_fatturapa: Manually apply bindings.diff

--- l10n_it_fatturapa/bindings/binding.py
+++ l10n_it_fatturapa/bindings/binding.py
@@ -6,14 +6,23 @@
 # Namespace http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2
 
 from __future__ import unicode_literals
-import pyxb
-import pyxb.binding
-import pyxb.binding.saxer
 import io
-import pyxb.utils.utility
-import pyxb.utils.domutils
-import sys
-import pyxb.utils.six as _six
+
+import logging
+
+_logger = logging.getLogger(__name__)
+try:
+    import pyxb
+    import pyxb.binding
+    import pyxb.binding.saxer
+    import pyxb.utils.utility
+    import pyxb.utils.domutils
+    import pyxb.utils.six as _six
+    # Import bindings for namespaces imported into schema
+    import pyxb.binding.datatypes
+except (ImportError) as err:
+    _logger.debug(err)
+
 # Unique identifier for bindings created at the same time
 _GenerationUID = pyxb.utils.utility.UniqueIdentifier('urn:uuid:03e3931c-abe0-11ea-9ec3-144f8a914443')
 
