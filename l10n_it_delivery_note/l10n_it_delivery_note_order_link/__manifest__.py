7d6241bfce9329950c7d2072bfa482b7e56cfd0c
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-07-06 16:49:49 +0200

    [ADD] Aggiunto modulo per collegare gli acquisti ai ddt

---
+++ l10n_it_delivery_note/l10n_it_delivery_note_order_link/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2014-2019 Dinamiche Aziendali srl
+# (http://www.dinamicheaziendali.it/)
+# @author: Andrea Piovesana <andrea.m.piovesana@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+# noinspection PyStatementEffect
+{
+    'name': 'ITA - Documento di Trasporto link from purchase order',
+    'summary': 'Delivery Note link from purchase order',
+    'author': 'Openindustry.it Sas',
+    'website': "https://github.com/OCA/l10n-italy/tree/12.0/"
+               "l10n_it_delivery_note_order_link",
+    'version': '12.0.1.0.0',
+    'category': 'Localization',
+    'depends': [
+        'purchase_stock',
+        'sale_stock',
+        'l10n_it_delivery_note',
+    ],
+    'data': [
+        'views/purchase_order.xml',
+        'views/sale_order.xml',
+    ],
+}
---
+++ l10n_it_delivery_note/l10n_it_delivery_note_order_link/readme/CONFIGURE.rst
@@ -0,0 +1,8 @@
+**English**
+
+To configure this module, you need only to install
+
+**Italiano**
+
+Per configurare questo modulo hai bisogno solo di installarlo
+
