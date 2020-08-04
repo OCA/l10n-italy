b2f5afd906553a6b449f27674c05827ca40b11fa
eLBati <lb@takobi.online>
2020-08-04 07:03:51 +0200

    FIX l10n_it_vat_statement_communication when setting declarant_fiscalcode_company File "/home/odoo/custom-addons/OCA/l10n-italy/l10n_it_vat_statement_communication/models/comunicazione_liquidazione.py", line 291, in _export_xml_get_frontespizio self.declarant_fiscalcode_company.code AttributeError: 'str' object has no attribute 'code'

--- l10n_it_vat_statement_communication/__manifest__.py
+++ l10n_it_vat_statement_communication/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'ITA - Comunicazione liquidazione IVA',
     'summary': 'Comunicazione liquidazione IVA ed esportazione file xml'
                'conforme alle specifiche dell\'Agenzia delle Entrate',
-    'version': '12.0.1.5.3',
+    'version': '12.0.1.5.4',
     'category': 'Account',
     'author': "Openforce di Camilli Alessandro, "
               "Odoo Community Association (OCA)",
