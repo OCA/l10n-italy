582f354d066d41a91850da9778fcd2739fed2c4e
eLBati <lb@takobi.online>
2020-08-26 16:48:31 +0200

    FIX translations after module renaming
    FIX readme and translations
    
    Co-authored-by: Sergio Zanchetta <primes2h@gmail.com>

--- l10n_it_mis_reports_pl_bs/__manifest__.py
+++ l10n_it_mis_reports_pl_bs/__manifest__.py
@@ -1,9 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'ITA - MIS builder - Bilancio civilistico',
-    'summary': """
-        MIS Builder templates for the Italian P&L
-        and Balance Sheets """,
+    'summary': 'Modelli "MIS Builder" per il conto economico e lo stato patrimoniale',
     'author': 'Dinamiche Aziendali srl,'
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/l10n-italy',
