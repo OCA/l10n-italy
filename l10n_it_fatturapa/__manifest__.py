4c9a6179e20827e9c1c105cfc16cdc2038dd4ba6
eLBati <lb@takobi.online>
2020-09-10 16:39:49 +0200

    FIX license compatibility

--- l10n_it_fatturapa/__manifest__.py
+++ l10n_it_fatturapa/__manifest__.py
@@ -2,18 +2,18 @@
 # Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
 # Copyright 2018-2019 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # Copyright 2019 Gianluigi Tiesi - Netfarm S.r.l.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'ITA - Fattura elettronica - Base',
-    'version': '12.0.1.12.1',
+    'version': '12.0.1.13.1',
     'category': 'Localization/Italy',
     'summary': 'Fatture elettroniche',
     'author': 'Davide Corio, Agile Business Group, Innoviu, '
               'Odoo Italia Network, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/l10n-italy/tree/12.0/'
                'l10n_it_fatturapa',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'excludes': ['l10n_it_edi'],
     "depends": [
         'l10n_it_account',
