bb2af6fda6dcfb08a4e4cd11b31ea4d22e097920
eLBati <lb@takobi.online>
2020-09-10 16:39:49 +0200

    FIX license compatibility

--- l10n_it_fatturapa_in_purchase/__manifest__.py
+++ l10n_it_fatturapa_in_purchase/__manifest__.py
@@ -1,19 +1,19 @@
 # Copyright 2018 Lorenzo Battistini
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # Copyright 2019 Simone Rubino - Agile Business Group
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
     'name': 'ITA - Fattura elettronica - Integrazione acquisti',
     "summary": "Modulo ponte tra ricezione fatture elettroniche e acquisti",
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'category': 'Hidden',
     'author': 'Agile Business Group, '
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/l10n-italy/tree/12.0/'
                'l10n_it_fatturapa_in_purchase',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     "depends": [
         'l10n_it_fatturapa_in',
         'purchase',
