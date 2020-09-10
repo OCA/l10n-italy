f504300cad05b41fb843699edf63ef375200fd9e
eLBati <lb@takobi.online>
2020-09-10 16:39:49 +0200

    FIX license compatibility

--- l10n_it_fatturapa_in_purchase/__manifest__.py
+++ l10n_it_fatturapa_in_purchase/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Lorenzo Battistini
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # Copyright 2019 Simone Rubino - Agile Business Group
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
@@ -13,7 +13,7 @@
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/l10n-italy/tree/12.0/'
                'l10n_it_fatturapa_in_purchase',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     "depends": [
         'l10n_it_fatturapa_in',
         'purchase',
