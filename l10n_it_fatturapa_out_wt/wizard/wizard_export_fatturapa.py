b9ba73b87731747c6d830806619d842b66400def
Ciro Urselli <c.urselli@apuliasoftware.it>
2020-09-11 14:19:00 +0200

    12.0 nuovi dati ritenuta fe (#1876)
    * [IMP] extend wt_types selection with new code and add tax_id for daticassa
    
    * [IMP] use causale_pagamento for all withholding tax type
    
    * [IMP] improve wizard_export_fatturapa with new 1.6 xml tack specific

--- l10n_it_fatturapa_out_wt/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out_wt/wizard/wizard_export_fatturapa.py
@@ -10,77 +10,102 @@ from odoo.addons.l10n_it_fatturapa.bindings.fatturapa import (
     DatiRiepilogoType
 )
 
+WT_TAX_CODE = {
+    'inps': 'RT03',
+    'enasarco': 'RT04',
+    'enpam': 'RT05',
+    'other': 'RT06'
+}
+
+TC_CODE = {
+    'inps': 'TC22',
+    'enasarco': 'TC07',
+    'enpam': 'TC09',
+}
 
 class WizardExportFatturapa(models.TransientModel):
     _inherit = "wizard.export.fatturapa"
-
+    
+    def getTipoRitenuta(self, wt_types, partner):
+        if wt_types == 'ritenuta':
+            if partner.is_company:
+                tipoRitenuta = 'RT02'
+            else:
+                tipoRitenuta = 'RT01'
+        else:
+            tipoRitenuta = WT_TAX_CODE[wt_types]
+        return tipoRitenuta
+        
     def setDatiGeneraliDocumento(self, invoice, body):
         res = super(WizardExportFatturapa, self).setDatiGeneraliDocumento(
             invoice, body)
-        ritenuta_lines = invoice.withholding_tax_line_ids.filtered(
-            lambda x: x.withholding_tax_id.wt_types == 'ritenuta')
-        if len(ritenuta_lines) > 1:
-            raise UserError(
-                _("More than one withholding tax in invoice!"))
+        ritenuta_lines = invoice.withholding_tax_line_ids
+        # if len(ritenuta_lines) > 1:
+        #     raise UserError(
+        #         _("More than one withholding tax in invoice!"))
         for wt_line in ritenuta_lines:
             if not wt_line.withholding_tax_id.causale_pagamento_id.code:
                 raise UserError(_('Missing payment reason for '
                                   'withholding tax %s!')
                                 % wt_line.withholding_tax_id.name)
-            body.DatiGenerali.DatiGeneraliDocumento.DatiRitenuta\
-                = DatiRitenutaType(
-                    TipoRitenuta="RT02" if invoice.partner_id.is_company
-                    else "RT01",  # RT02 persona giuridica
+            tipoRitenuta = self.getTipoRitenuta(
+                wt_line.withholding_tax_id.wt_types,
+                invoice.partner_id
+                )
+            body.DatiGenerali.DatiGeneraliDocumento.DatiRitenuta.append(
+                DatiRitenutaType(
+                    TipoRitenuta=tipoRitenuta,
                     ImportoRitenuta='%.2f' % float_round(wt_line.tax, 2),
                     AliquotaRitenuta='%.2f' % float_round(
                         wt_line.withholding_tax_id.tax, 2),
                     CausalePagamento=wt_line.withholding_tax_id.
                     causale_pagamento_id.code
-                )
-        enasarco_lines = invoice.withholding_tax_line_ids.filtered(
-            lambda x: x.withholding_tax_id.wt_types == 'enasarco')
-        for enas_line in enasarco_lines:
-            if enas_line.withholding_tax_id.use_daticassaprev_for_enasarco:
+                ))
+            if wt_line.withholding_tax_id.use_daticassaprev:
+                tax_id = wt_line.withholding_tax_id.daticassprev_tax_id
+                tax_kind = tax_id.kind_id.code
                 body.DatiGenerali.DatiGeneraliDocumento.\
                     DatiCassaPrevidenziale.append(
                         DatiCassaPrevidenzialeType(
-                            TipoCassa='TC07',
+                            TipoCassa=TC_CODE[wt_line.withholding_tax_id.wt_types],
                             AlCassa='%.2f' % float_round(
-                                enas_line.withholding_tax_id.tax, 2),
+                                wt_line.withholding_tax_id.tax, 2),
                             ImportoContributoCassa='%.2f' % float_round(
-                                enas_line.tax, 2),
+                                wt_line.tax, 2),
                             AliquotaIVA='0.00',
-                            Natura='N2'
+                            Natura=tax_kind,
                             )
                         )
         return res
 
-    def get_n2_tax_riepilogo(self, body):
+    def get_tax_riepilogo(self, body, tax_id):
         for riepilogo in body.DatiBeniServizi.DatiRiepilogo:
-            if float(riepilogo.AliquotaIVA) == 0 and riepilogo.Natura == 'N2':
+            if float(riepilogo.AliquotaIVA) == 0.0 \
+                and riepilogo.Natura == tax_id.kind_id.code:
                 return riepilogo
 
     def setDatiRiepilogo(self, invoice, body):
         res = super(WizardExportFatturapa, self).setDatiRiepilogo(
             invoice, body)
-        enasarco_lines_to_write = invoice.withholding_tax_line_ids.filtered(
-            lambda x: x.withholding_tax_id.wt_types == 'enasarco' and
-            x.withholding_tax_id.use_daticassaprev_for_enasarco
+        wt_lines_to_write = invoice.withholding_tax_line_ids.filtered(
+            lambda x: x.withholding_tax_id.wt_types not in ('ritenuta', 'other') 
+            and x.withholding_tax_id.use_daticassaprev
         )
-        if enasarco_lines_to_write:
-            n2_riepilogo = self.get_n2_tax_riepilogo(body)
-            enasarco_base = sum(enasarco_lines_to_write.mapped('tax'))
-            if n2_riepilogo:
-                base_amount = float(n2_riepilogo.ImponibileImporto)
-                base_amount += enasarco_base
-                n2_riepilogo.ImponibileImporto = '%.2f' % float_round(base_amount, 2)
+        for wt_line in wt_lines_to_write:
+            tax_id = wt_line.withholding_tax_id.daticassprev_tax_id
+            tax_riepilogo = self.get_tax_riepilogo(body, tax_id)
+            if tax_riepilogo:
+                base_amount = float(tax_riepilogo.ImponibileImporto)
+                base_amount += wt_line.tax
+                tax_riepilogo.ImponibileImporto = '%.2f' % float_round(
+                    base_amount, 2)
             else:
                 riepilogo = DatiRiepilogoType(
                     AliquotaIVA='0.00',
-                    ImponibileImporto='%.2f' % float_round(enasarco_base, 2),
+                    ImponibileImporto='%.2f' % float_round(wt_line.tax, 2),
                     Imposta='0.00',
-                    Natura='N2',
-                    RiferimentoNormativo='Escluso Art. 13 5C DPR 633/72',
+                    Natura=tax_id.kind_id.code,
+                    RiferimentoNormativo=tax_id.law_reference,
                 )
                 body.DatiBeniServizi.DatiRiepilogo.append(riepilogo)
         return res
@@ -91,18 +116,8 @@ class WizardExportFatturapa(models.TransientModel):
         DettaglioLinea = super(WizardExportFatturapa, self).setDettaglioLinea(
             line_no, line, body, price_precision, uom_precision
         )
-        for wt in line.invoice_line_tax_wt_ids:
-            if wt.wt_types == 'enasarco':
-                amount = wt.compute_tax(line.price_subtotal)['tax']
-                DettaglioLinea.AltriDatiGestionali.append(
-                    AltriDatiGestionaliType(
-                        TipoDato="CASSA-PREV",
-                        RiferimentoTesto=('ENASARCO TC07 (%s%%)' % wt.tax),
-                        RiferimentoNumero='%.2f' % float_round(amount, 2),
-                    )
-                )
-            else:
-                DettaglioLinea.Ritenuta = 'SI'
+        if any([wt for wt in line.invoice_line_tax_wt_ids]):
+            DettaglioLinea.Ritenuta = 'SI'
         return DettaglioLinea
 
     def setDatiPagamento(self, invoice, body):
