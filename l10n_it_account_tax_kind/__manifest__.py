8660edc6935f178991915443a365624aed57b961
Sergio Corato <sergiocorato@users.noreply.github.com>
2020-09-11 11:25:57 +0200

    [12.0][IMP] l10n it account tax kind fatturapa 1.6 (#1865)

--- l10n_it_account_tax_kind/__manifest__.py
+++ l10n_it_account_tax_kind/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Italian Localisation - Natura delle aliquote IVA',
-    'version': '12.0.1.0.2',
+    'version': '12.0.2.0.0',
     'development_status': 'Production/Stable',
     'category': 'Localisation/Italy',
     'author': "Odoo Community Association (OCA), Apulia Software s.r.l",
