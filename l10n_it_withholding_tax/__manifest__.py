0c9bc653d2435be05d940b0b3cc80a9a2a64e9d6
Ciro Urselli <c.urselli@apuliasoftware.it>
2020-09-11 14:19:00 +0200

    12.0 nuovi dati ritenuta fe (#1876)
    * [IMP] extend wt_types selection with new code and add tax_id for daticassa
    
    * [IMP] use causale_pagamento for all withholding tax type
    
    * [IMP] improve wizard_export_fatturapa with new 1.6 xml tack specific

--- l10n_it_withholding_tax/__manifest__.py
+++ l10n_it_withholding_tax/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Italian Withholding Tax',
-    'version': '12.0.1.2.0',
+    'version': '12.0.2.0.0',
     'category': 'Account',
     'author': 'Openforce, Odoo Italia Network, '
               'Odoo Community Association (OCA)',
