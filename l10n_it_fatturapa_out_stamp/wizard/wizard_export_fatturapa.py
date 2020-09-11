32caddb84aae7afa86784b85698c45bcdc67f32d
Sergio Corato <sergiocorato@users.noreply.github.com>
2020-09-11 11:33:15 +0200

    [12.0][IMP] importo bollo non più obbligatorio e-invoice 1.6 (#1866)

--- l10n_it_fatturapa_out_stamp/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out_stamp/wizard/wizard_export_fatturapa.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models
-from odoo.tools.translate import _
-from odoo.exceptions import UserError
-from odoo.tools.float_utils import float_round
+from odoo.tools.float_utils import float_round, float_is_zero
 from odoo.addons.l10n_it_fatturapa.bindings.fatturapa import (
     DatiBolloType
 )
@@ -16,13 +14,11 @@ class WizardExportFatturapa(models.TransientModel):
         res = super(WizardExportFatturapa, self).setDatiGeneraliDocumento(
             invoice, body)
         if invoice.tax_stamp:
-            if not invoice.company_id.tax_stamp_product_id:
-                raise UserError(_(
-                    "Tax Stamp Product not set for company %s"
-                ) % invoice.company_id.name)
-            stamp_price = invoice.company_id.tax_stamp_product_id.list_price
             body.DatiGenerali.DatiGeneraliDocumento.DatiBollo = DatiBolloType(
-                BolloVirtuale="SI",
-                ImportoBollo='%.2f' % float_round(stamp_price, 2),
-            )
+                BolloVirtuale="SI")
+            if invoice.company_id.tax_stamp_product_id:
+                stamp_price = invoice.company_id.tax_stamp_product_id.list_price
+                if not float_is_zero(stamp_price, precision_digits=2):
+                    body.DatiGenerali.DatiGeneraliDocumento.DatiBollo.ImportoBollo = \
+                        '%.2f' % float_round(stamp_price, 2)
         return res
