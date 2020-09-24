9147d9008ff773530f38ef7e5dd148277ec5d11c
eLBati <lb@takobi.online>
2020-09-24 11:40:09 +0200

    FIX license LGPL-3 conflicts

--- l10n_it_reverse_charge/models/account_invoice.py
+++ l10n_it_reverse_charge/models/account_invoice.py
@@ -2,7 +2,6 @@
 # Copyright 2017 Alex Comba - Agile Business Group
 # Copyright 2017 Lorenzo Battistini - Agile Business Group
 # Copyright 2017 Marco Calcagni - Dinamiche Aziendali srl
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models
 from odoo.exceptions import Warning as UserError
