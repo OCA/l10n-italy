ce8d17a06d413d42c886152cd4139c4f988c8ecb
eLBati <lb@takobi.online>
2020-09-24 11:40:09 +0200

    FIX license LGPL-3 conflicts

--- fiscal_epos_print/__manifest__.py
+++ fiscal_epos_print/__manifest__.py
@@ -2,11 +2,11 @@
 # © 2016 Alessio Gerace - Agile Business Group
 # © 2018-2019 Lorenzo Battistini
 # © 2019-2020 Roberto Fichera - Level Prime Srl
-# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'ITA - Driver per stampanti fiscali compatibili ePOS-Print XML',
-    'version': '12.0.1.1.9',
+    'version': '12.0.1.2.0',
     'category': 'Point Of Sale',
     'summary': 'ePOS-Print XML Fiscal Printer Driver - Stampanti Epson compatibili: '
                'FP81II, FP90III',
@@ -14,7 +14,7 @@
         'Odoo Community Association (OCA), Agile Business Group, '
         'Leonardo Donelli, TAKOBI, Level Prime Srl'
     ),
-    'license': 'GPL-3',
+    'license': 'AGPL-3',
     'website': 'https://github.com/OCA/l10n-italy',
     'depends': ['point_of_sale', 'pos_order_mgmt'],
     'data': [
