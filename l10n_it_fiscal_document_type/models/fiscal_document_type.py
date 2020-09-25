8a277ee9e4dbef1a9f850a8ebbf244eb5b9ee5b0
eLBati <lb@takobi.online>
2020-09-25 15:02:13 +0200

    IMP l10n_it_fiscal_document_type allowing to search records by code and showing code in relational fields

--- l10n_it_fiscal_document_type/models/fiscal_document_type.py
+++ l10n_it_fiscal_document_type/models/fiscal_document_type.py
@@ -35,3 +35,23 @@ class FiscalDocumentType(models.Model):
         for doc in self:
             doc.journal_ids.check_doc_type_relation()
         return res
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for doc_type in self:
+            res.append(
+                (doc_type.id, '[%s] %s' % (doc_type.code, doc_type.name)))
+        return res
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        if not args:
+            args = []
+        if name:
+            records = self.search([
+                '|', ('name', operator, name), ('code', operator, name)
+                ] + args, limit=limit)
+        else:
+            records = self.search(args, limit=limit)
+        return records.name_get()
