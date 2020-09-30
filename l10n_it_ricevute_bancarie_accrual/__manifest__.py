28c21dd45c5bd5dd6be1c8c245fa2724ef478030
sergiocorato <sergiocorato@gmail.com>
2020-09-30 12:21:06 +0200

    [IMP] version

--- l10n_it_ricevute_bancarie_accrual/__manifest__.py
+++ l10n_it_ricevute_bancarie_accrual/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'ITA - Ricevute bancarie con maturazione',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Accounting',
     'summary': 'Ricevute bancarie con maturazione',
     'author': 'Sergio Corato',
