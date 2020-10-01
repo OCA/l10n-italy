2737110509046cf9109345a73789a79d223d63b8
eLBati <lb@takobi.online>
2020-10-01 12:12:17 +0200

    Remove not used tests
    FIX SDI tests:
    00200 	The value '10.00000000' of element 'Percentuale' is not valid. riga: 93 - colonna: 59 - File non conforme al formato
    00305 	CF associato alla P.iva non rilevato in IPA: 00146089990 - 1.4.1.1.2 <IdCodice> non valido

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -699,7 +699,7 @@ class WizardExportFatturapa(models.TransientModel):
         if line.discount:
             res.append(ScontoMaggiorazioneType(
                 Tipo='SC',
-                Percentuale='%.8f' % float_round(line.discount, 8)
+                Percentuale='%.2f' % float_round(line.discount, 8)
             ))
         return res
 
