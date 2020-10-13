3d1ad056e2615767802ca569a8650046766f34ca
Francesco Apruzzese <cescoap@gmail.com>
2020-10-13 17:16:25 +0200

    [MIG] l10n_it_base_location_geonames_import: Migration to 11.0

--- l10n_it_base_location_geonames_import/__manifest__.py
+++ l10n_it_base_location_geonames_import/__manifest__.py
@@ -1,27 +1,10 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Agile Business Group (http://www.agilebg.com)
-#    @author Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+# Copyright 2020 Francesco Apruzzese <francesco.apruzzese@openforce.it>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Base Location Provinces Import',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Extra Tools',
     'license': 'AGPL-3',
     'summary': 'Import base_location entries (provinces) from Geonames',
@@ -30,8 +13,10 @@ This module extends base_location_geonames_import in order to correctly import
 Italian provinces
 """,
     'author': "Agile Business Group,Odoo Community Association (OCA)",
-    'website': 'http://www.agilebg.com',
-    'depends': ['base_location_geonames_import'],
+    'website': 'https://github.com/OCA/l10n-italy/',
+    'depends': [
+        'base_location_geonames_import',
+        ],
     'test': [
         'test/import.yml',
         ],
