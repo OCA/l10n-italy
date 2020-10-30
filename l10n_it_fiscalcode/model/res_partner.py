7e6cb2bc3678c102433cb98c9b07495928ce0e50
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-10-30 16:22:21 +0100

    [MIG] l10n_it_fiscalcode: Migration to 14.0

--- l10n_it_fiscalcode/model/res_partner.py
+++ l10n_it_fiscalcode/model/res_partner.py
@@ -1,12 +1,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    @api.multi
+    @api.constrains("fiscalcode")
     def check_fiscalcode(self):
         for partner in self:
             if not partner.fiscalcode:
@@ -21,15 +22,8 @@ class ResPartner(models.Model):
                     continue
                 if len(partner.fiscalcode) != 16:
                     # Check fiscalcode of a person
-                    return False
+                    msg = _("The fiscal code doesn't seem to be correct.")
+                    raise ValidationError(msg)
         return True
 
     fiscalcode = fields.Char("Fiscal Code", size=16, help="Italian Fiscal Code")
-
-    _constraints = [
-        (
-            check_fiscalcode,
-            "The fiscal code doesn't seem to be correct.",
-            ["fiscalcode"],
-        )
-    ]
