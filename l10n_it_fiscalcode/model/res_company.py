754b9d1420bf781d9ce76d1a0bdb812be59c0d92
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-10-30 16:22:21 +0100

    [MIG] l10n_it_fiscalcode: Migration to 14.0

--- l10n_it_fiscalcode/model/res_company.py
+++ l10n_it_fiscalcode/model/res_company.py
@@ -5,6 +5,7 @@ from odoo import fields, models
 
 class ResCompany(models.Model):
     _inherit = "res.company"
+
     fiscalcode = fields.Char(
         related="partner_id.fiscalcode", store=True, readonly=False
     )
