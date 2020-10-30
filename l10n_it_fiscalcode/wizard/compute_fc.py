619c57e75888653c8bae654e23d7cf6b82f2827a
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-10-30 16:08:59 +0100

    [IMP] l10n_it_fiscalcode: black, isort, prettier

--- l10n_it_fiscalcode/wizard/compute_fc.py
+++ l10n_it_fiscalcode/wizard/compute_fc.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 from odoo.osv import expression
 
@@ -15,82 +15,83 @@ except ImportError:
     _logger.warning(
         "codicefiscale library not found. "
         "If you plan to use it, please install the codicefiscale library"
-        " from https://pypi.python.org/pypi/codicefiscale")
+        " from https://pypi.python.org/pypi/codicefiscale"
+    )
 
 
 class WizardComputeFc(models.TransientModel):
-    _name = 'wizard.compute.fc'
+    _name = "wizard.compute.fc"
     _description = "Compute Fiscal Code"
-    _rec_name = 'fiscalcode_surname'
+    _rec_name = "fiscalcode_surname"
 
     fiscalcode_surname = fields.Char("Surname", required=True, size=64)
     fiscalcode_firstname = fields.Char("First name", required=True, size=64)
     birth_date = fields.Date("Date of birth", required=True)
-    birth_city = fields.Many2one('res.city.it.code.distinct',
-                                 required=True,
-                                 string="City of birth")
-    birth_province = fields.Many2one('res.country.state',
-                                     required=True,
-                                     string="Province")
-    sex = fields.Selection([('M', "Male"), ('F', "Female")],
-                           required=True,
-                           string="Sex")
+    birth_city = fields.Many2one(
+        "res.city.it.code.distinct", required=True, string="City of birth"
+    )
+    birth_province = fields.Many2one(
+        "res.country.state", required=True, string="Province"
+    )
+    sex = fields.Selection(
+        [("M", "Male"), ("F", "Female")], required=True, string="Sex"
+    )
 
     @api.multi
-    @api.onchange('birth_city')
+    @api.onchange("birth_city")
     def onchange_birth_city(self):
         self.ensure_one()
 
-        it = self.env.ref('base.it').id
+        it = self.env.ref("base.it").id
         res = {
-            'domain': {'birth_province': [('country_id', '=', it)]},
-            'value': {'birth_province': False}
+            "domain": {"birth_province": [("country_id", "=", it)]},
+            "value": {"birth_province": False},
         }
 
         if self.birth_city:
             # SMELLS: Add a foreign key in "res_city_it_code"
             #          instead using the weak link "code" <-> "province".
             #
-            city_ids = self.env['res.city.it.code'] \
-                .search([('name', '=', self.birth_city.name)])
-            provinces = city_ids.mapped('province')
-            province_ids = self.env['res.country.state'] \
-                .search([('country_id', '=', it), ('code', 'in', provinces)])
+            city_ids = self.env["res.city.it.code"].search(
+                [("name", "=", self.birth_city.name)]
+            )
+            provinces = city_ids.mapped("province")
+            province_ids = self.env["res.country.state"].search(
+                [("country_id", "=", it), ("code", "in", provinces)]
+            )
 
-            res['domain']['birth_province'] = \
-                expression.AND([
-                    res['domain']['birth_province'],
-                    [('id', 'in', province_ids.ids)]
-                ])
+            res["domain"]["birth_province"] = expression.AND(
+                [res["domain"]["birth_province"], [("id", "in", province_ids.ids)]]
+            )
 
             if len(province_ids) == 1:
-                res['value']['birth_province'] = province_ids.id
+                res["value"]["birth_province"] = province_ids.id
 
         return res
 
     @api.multi
-    @api.onchange('birth_province')
+    @api.onchange("birth_province")
     def onchange_birth_province(self):
         self.ensure_one()
 
-        res = {'domain': {'birth_city': []}}
+        res = {"domain": {"birth_city": []}}
 
         if not self.birth_city:
             if self.birth_province:
                 # SMELLS: Add a foreign key in "res_city_it_code"
                 #          instead using the weak link "code" <-> "province".
                 #
-                city_ids = self.env['res.city.it.code'] \
-                    .search([('province', '=', self.birth_province.code)])
-                names = city_ids.mapped('name')
-                distinct_city_ids = self.env['res.city.it.code.distinct'] \
-                    .search([('name', 'in', names)])
+                city_ids = self.env["res.city.it.code"].search(
+                    [("province", "=", self.birth_province.code)]
+                )
+                names = city_ids.mapped("name")
+                distinct_city_ids = self.env["res.city.it.code.distinct"].search(
+                    [("name", "in", names)]
+                )
 
-                res['domain']['birth_city'] = \
-                    expression.AND([
-                        res['domain']['birth_city'],
-                        [('id', 'in', distinct_city_ids.ids)]
-                    ])
+                res["domain"]["birth_city"] = expression.AND(
+                    [res["domain"]["birth_city"], [("id", "in", distinct_city_ids.ids)]]
+                )
 
         return res
 
@@ -117,84 +118,99 @@ class WizardComputeFc(models.TransientModel):
         - VED: reference to another city. This is assigned to cities that
                changed name and were then subject to other changes.
         """
-        cities = self.env['res.city.it.code'].search([
-            ('name', '=', birth_city), ('province', '=', birth_prov)],
-            order='creation_date ASC, var_date ASC, notes ASC')
+        cities = self.env["res.city.it.code"].search(
+            [("name", "=", birth_city), ("province", "=", birth_prov)],
+            order="creation_date ASC, var_date ASC, notes ASC",
+        )
         if not cities or len(cities) == 0:
-            return ''
+            return ""
         # Checks for any VED element
         newcts = None
         for ct in cities:
-            if ct.notes == 'VED':
-                newcts = self.env['res.city.it.code'].search([
-                    ('name', '=', ct.name_var)])
+            if ct.notes == "VED":
+                newcts = self.env["res.city.it.code"].search(
+                    [("name", "=", ct.name_var)]
+                )
                 break
         if newcts:
             cities = newcts
         return self._check_national_codes(birth_date, cities)
 
     def _check_national_codes(self, birth_date, cities):
-        nc = ''
+        nc = ""
         dtcostvar = None
         for ct in cities:
-            if (ct.creation_date and
-                    (not dtcostvar or not ct.creation_date or
-                        dtcostvar < ct.creation_date)):
+            if ct.creation_date and (
+                not dtcostvar or not ct.creation_date or dtcostvar < ct.creation_date
+            ):
                 dtcostvar = ct.creation_date
             if not ct.notes:
                 nc = ct.national_code
-            elif (ct.notes == 'ORA' and
-                    (not dtcostvar or not ct.var_date or
-                        dtcostvar < ct.var_date)):
-                if (not ct.var_date or ct.var_date <= birth_date):
+            elif ct.notes == "ORA" and (
+                not dtcostvar or not ct.var_date or dtcostvar < ct.var_date
+            ):
+                if not ct.var_date or ct.var_date <= birth_date:
                     nc = ct.national_code_var
                 elif not nc:
                     nc = ct.national_code
-                if (ct.var_date):
+                if ct.var_date:
                     dtcostvar = ct.var_date
-            elif (ct.notes == 'AGG' and
-                    (not dtcostvar or not ct.var_date or
-                        dtcostvar < ct.var_date)):
-                if (not ct.var_date or ct.var_date <= birth_date):
+            elif ct.notes == "AGG" and (
+                not dtcostvar or not ct.var_date or dtcostvar < ct.var_date
+            ):
+                if not ct.var_date or ct.var_date <= birth_date:
                     nc = ct.national_code_var
                 elif not nc:
                     nc = ct.national_code
-                if (ct.var_date):
+                if ct.var_date:
                     dtcostvar = ct.var_date
-            elif (ct.notes == 'AGP' and
-                    (not dtcostvar or not ct.var_date or
-                        dtcostvar < ct.var_date)):
+            elif ct.notes == "AGP" and (
+                not dtcostvar or not ct.var_date or dtcostvar < ct.var_date
+            ):
                 nc = ct.national_code
-                if (ct.var_date):
+                if ct.var_date:
                     dtcostvar = ct.var_date
-            elif (ct.notes == 'AGP' and
-                    (not dtcostvar or not ct.var_date or
-                        dtcostvar < ct.var_date)):
+            elif ct.notes == "AGP" and (
+                not dtcostvar or not ct.var_date or dtcostvar < ct.var_date
+            ):
                 nc = ct.national_code
 
         return nc
 
     @api.multi
     def compute_fc(self):
-        active_id = self._context.get('active_id')
-        partner = self.env['res.partner'].browse(active_id)
+        active_id = self._context.get("active_id")
+        partner = self.env["res.partner"].browse(active_id)
         for f in self:
-            if (not f.fiscalcode_surname or not f.fiscalcode_firstname or
-                    not f.birth_date or not f.birth_city or not f.sex):
-                raise UserError(_('One or more fields are missing'))
+            if (
+                not f.fiscalcode_surname
+                or not f.fiscalcode_firstname
+                or not f.birth_date
+                or not f.birth_city
+                or not f.sex
+            ):
+                raise UserError(_("One or more fields are missing"))
             nat_code = self._get_national_code(
-                f.birth_city.name, f.birth_province.code, f.birth_date)
+                f.birth_city.name, f.birth_province.code, f.birth_date
+            )
             if not nat_code:
-                raise UserError(_('National code is missing'))
-            c_f = build(f.fiscalcode_surname, f.fiscalcode_firstname,
-                        f.birth_date, f.sex, nat_code)
+                raise UserError(_("National code is missing"))
+            c_f = build(
+                f.fiscalcode_surname,
+                f.fiscalcode_firstname,
+                f.birth_date,
+                f.sex,
+                nat_code,
+            )
             if partner.fiscalcode and partner.fiscalcode != c_f:
-                raise UserError(_(
-                    'Existing fiscal code %(partner_fiscalcode)s is different '
-                    'from the computed one (%(compute)s). If you want to use'
-                    ' the computed one, remove the existing one' % {
-                        'partner_fiscalcode': partner.fiscalcode,
-                        'compute': c_f}))
+                raise UserError(
+                    _(
+                        "Existing fiscal code %(partner_fiscalcode)s is different "
+                        "from the computed one (%(compute)s). If you want to use"
+                        " the computed one, remove the existing one"
+                        % {"partner_fiscalcode": partner.fiscalcode, "compute": c_f}
+                    )
+                )
             partner.fiscalcode = c_f
-            partner.company_type = 'person'
-        return {'type': 'ir.actions.act_window_close'}
+            partner.company_type = "person"
+        return {"type": "ir.actions.act_window_close"}
