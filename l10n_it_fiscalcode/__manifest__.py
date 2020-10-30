320a70be34513173e880f637748a54b602c51f0f
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-10-30 16:08:59 +0100

    [IMP] l10n_it_fiscalcode: black, isort, prettier

--- l10n_it_fiscalcode/__manifest__.py
+++ l10n_it_fiscalcode/__manifest__.py
@@ -5,27 +5,27 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Italian Localization - Fiscal Code',
-    'version': '12.0.1.1.3',
-    'development_status': 'Production/Stable',
-    'category': 'Localisation/Italy',
-    'author': "Link IT s.r.l., "
-              "Apulia Software, "
-              "Odoo Italia Network, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/l10n-italy',
-    'license': 'AGPL-3',
-    'depends': ['base_vat'],
-    'external_dependencies': {
-        'python': ['codicefiscale'],
+    "name": "Italian Localization - Fiscal Code",
+    "version": "12.0.1.1.3",
+    "development_status": "Production/Stable",
+    "category": "Localisation/Italy",
+    "author": "Link IT s.r.l., "
+    "Apulia Software, "
+    "Odoo Italia Network, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "AGPL-3",
+    "depends": ["base_vat"],
+    "external_dependencies": {
+        "python": ["codicefiscale"],
     },
-    'data': [
+    "data": [
         "security/ir.model.access.csv",
-        'data/res.city.it.code.csv',
-        'view/fiscalcode_view.xml',
-        'view/report_invoice_document.xml',
-        'wizard/compute_fc_view.xml',
-        'view/company_view.xml'
-        ],
-    'installable': True
+        "data/res.city.it.code.csv",
+        "view/fiscalcode_view.xml",
+        "view/report_invoice_document.xml",
+        "wizard/compute_fc_view.xml",
+        "view/company_view.xml",
+    ],
+    "installable": True,
 }
--- l10n_it_fiscalcode/model/res_company.py
+++ l10n_it_fiscalcode/model/res_company.py
@@ -1,9 +1,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
     fiscalcode = fields.Char(
-        related='partner_id.fiscalcode', store=True, readonly=False)
+        related="partner_id.fiscalcode", store=True, readonly=False
+    )
