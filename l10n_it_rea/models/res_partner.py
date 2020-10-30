d8df92a0c0e5e58b2d4ea659429c006165642575
gianmarco <gconte@dinamicheaziendali.it>
2020-10-30 15:20:09 +0100

    [IMP] l10n_it_rea: black, isort, prettier

--- l10n_it_rea/models/res_partner.py
+++ l10n_it_rea/models/res_partner.py
@@ -4,20 +4,22 @@ from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    rea_office = fields.Many2one(
-        'res.country.state', string='Office Province')
-    rea_code = fields.Char('REA Code', size=20)
-    rea_capital = fields.Float('Share Capital')
+    rea_office = fields.Many2one("res.country.state", string="Office Province")
+    rea_code = fields.Char("REA Code", size=20)
+    rea_capital = fields.Float("Share Capital")
     rea_member_type = fields.Selection(
-        [('SU', 'Unique Member'),
-         ('SM', 'Multiple Members')], 'Member Type')
+        [("SU", "Unique Member"), ("SM", "Multiple Members")], "Member Type"
+    )
     rea_liquidation_state = fields.Selection(
-        [('LS', 'In liquidation'),
-         ('LN', 'Not in liquidation')], 'Liquidation State')
+        [("LS", "In liquidation"), ("LN", "Not in liquidation")], "Liquidation State"
+    )
 
     _sql_constraints = [
-        ('rea_code_uniq', 'unique (rea_office, rea_code, company_id)',
-         'The rea code code must be unique per company !'),
+        (
+            "rea_code_uniq",
+            "unique (rea_office, rea_code, company_id)",
+            "The rea code code must be unique per company !",
+        ),
     ]
