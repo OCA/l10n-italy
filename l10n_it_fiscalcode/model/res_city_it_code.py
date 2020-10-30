00395aa86aab9192582c1de19d0978ce4394f117
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-10-30 16:08:59 +0100

    [IMP] l10n_it_fiscalcode: black, isort, prettier

--- l10n_it_fiscalcode/model/res_city_it_code.py
+++ l10n_it_fiscalcode/model/res_city_it_code.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, tools
+from odoo import fields, models, tools
 
 
 class ResCityItCode(models.Model):
@@ -35,45 +35,43 @@ class ResCityItCode(models.Model):
     #
 
     """
-    To create res.city.it.code.csv:
-    http://www.agenziaentrate.gov.it/wps/content/Nsilib/Nsi/Strumenti/
-Codici+attivita+e+tributo/Codici+territorio/Comuni+italia+esteri/
-    - download the file named: Codici Comuni d’Italia - xls
-    - open it in LibreOffice and save it as .ods
-    - some date cells contain a "'" to be removed using Calc's menu
-      Data / Text to columns
-    - rows 216,1122 contain wrong written dates
-    - dates format must be yyyy-mm-dd
-    - add first column with numeric ids
-    - change first row with column names from res.city.it.code model
-      id,national_code,cadastre_code,province,name,notes,national_code_var,
-cadastre_code_var,province_var,name_var,creation_date,var_date
-    - save as csv in data/res.city.it.code.csv
+        To create res.city.it.code.csv:
+        http://www.agenziaentrate.gov.it/wps/content/Nsilib/Nsi/Strumenti/
+    Codici+attivita+e+tributo/Codici+territorio/Comuni+italia+esteri/
+        - download the file named: Codici Comuni d’Italia - xls
+        - open it in LibreOffice and save it as .ods
+        - some date cells contain a "'" to be removed using Calc's menu
+          Data / Text to columns
+        - rows 216,1122 contain wrong written dates
+        - dates format must be yyyy-mm-dd
+        - add first column with numeric ids
+        - change first row with column names from res.city.it.code model
+          id,national_code,cadastre_code,province,name,notes,national_code_var,
+    cadastre_code_var,province_var,name_var,creation_date,var_date
+        - save as csv in data/res.city.it.code.csv
     """
     _name = "res.city.it.code"
     _description = "National city codes"
 
-    national_code = fields.Char('National code', size=4)
-    cadastre_code = fields.Char(
-        'Belfiore cadastre code (not used anymore)',
-        size=4)
-    province = fields.Char('Province', size=5)
-    name = fields.Char('Name')
-    notes = fields.Char('Notes', size=4)
-    national_code_var = fields.Char('National code variation', size=4)
-    cadastre_code_var = fields.Char('Cadastre code variation', size=4)
-    province_var = fields.Char('Province variation', size=5)
-    name_var = fields.Char('Name variation', size=100)
-    creation_date = fields.Date('Creation date')
-    var_date = fields.Date('Variation date')
+    national_code = fields.Char("National code", size=4)
+    cadastre_code = fields.Char("Belfiore cadastre code (not used anymore)", size=4)
+    province = fields.Char("Province", size=5)
+    name = fields.Char("Name")
+    notes = fields.Char("Notes", size=4)
+    national_code_var = fields.Char("National code variation", size=4)
+    cadastre_code_var = fields.Char("Cadastre code variation", size=4)
+    province_var = fields.Char("Province variation", size=5)
+    name_var = fields.Char("Name variation", size=100)
+    creation_date = fields.Date("Creation date")
+    var_date = fields.Date("Variation date")
 
 
 class ResCityItCodeDistinct(models.Model):
-    _name = 'res.city.it.code.distinct'
+    _name = "res.city.it.code.distinct"
     _description = "National city codes distinct"
     _auto = False
 
-    name = fields.Char('Name', size=100)
+    name = fields.Char("Name", size=100)
 
     def init(self):
         tools.drop_view_if_exists(self.env.cr, self._table)
@@ -82,4 +80,5 @@ class ResCityItCodeDistinct(models.Model):
             CREATE OR REPLACE VIEW res_city_it_code_distinct AS (
             SELECT name, MAX(id) AS id FROM res_city_it_code
             GROUP BY name)
-            """)
+            """
+        )
--- l10n_it_fiscalcode/tests/test_fiscalcode.py
+++ l10n_it_fiscalcode/tests/test_fiscalcode.py
@@ -1,53 +1,65 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
 from odoo.exceptions import ValidationError
+from odoo.tests.common import TransactionCase
 
 
 class TestFiscalCode(TransactionCase):
-
     def setUp(self):
         super(TestFiscalCode, self).setUp()
 
-        self.partner = self.env.ref('base.res_partner_2')
-        self.rome_province = self.env.ref('base.state_it_rm')
+        self.partner = self.env.ref("base.res_partner_2")
+        self.rome_province = self.env.ref("base.state_it_rm")
 
     def test_fiscalcode_compute(self):
-        wizard = self.env['wizard.compute.fc'].with_context(
-            active_id=self.partner.id).create({
-                'fiscalcode_surname': 'ROSSI',
-                'fiscalcode_firstname': 'MARIO',
-                'birth_date': '1984-06-04',
-                'sex': 'M',
-                'birth_city': 10048,
-                'birth_province': self.rome_province.id
-            })
+        wizard = (
+            self.env["wizard.compute.fc"]
+            .with_context(active_id=self.partner.id)
+            .create(
+                {
+                    "fiscalcode_surname": "ROSSI",
+                    "fiscalcode_firstname": "MARIO",
+                    "birth_date": "1984-06-04",
+                    "sex": "M",
+                    "birth_city": 10048,
+                    "birth_province": self.rome_province.id,
+                }
+            )
+        )
         # ---- Compute FiscalCode
         wizard.compute_fc()
-        self.assertEqual(self.partner.fiscalcode, 'RSSMRA84H04H501X')
+        self.assertEqual(self.partner.fiscalcode, "RSSMRA84H04H501X")
 
     def test_fiscalcode_check(self):
         # Wrong FC
         with self.assertRaises(ValidationError):
-            self.env['res.partner'].create({
-                'name': 'Person',
-                'is_company': False,
-                'fiscalcode': '123',
-            })
+            self.env["res.partner"].create(
+                {
+                    "name": "Person",
+                    "is_company": False,
+                    "fiscalcode": "123",
+                }
+            )
         # Correct FC
-        self.env['res.partner'].create({
-            'name': 'Person',
-            'is_company': False,
-            'fiscalcode': 'RSSMRA84H04H501X',
-        })
+        self.env["res.partner"].create(
+            {
+                "name": "Person",
+                "is_company": False,
+                "fiscalcode": "RSSMRA84H04H501X",
+            }
+        )
         # Empty FC
-        self.env['res.partner'].create({
-            'name': 'Person',
-        })
+        self.env["res.partner"].create(
+            {
+                "name": "Person",
+            }
+        )
         # FC is VAT number
-        self.env['res.partner'].create({
-            'name': 'Person',
-            'company_name': 'Company',
-            'is_company': False,
-            'fiscalcode': '123456789',
-        })
+        self.env["res.partner"].create(
+            {
+                "name": "Person",
+                "company_name": "Company",
+                "is_company": False,
+                "fiscalcode": "123456789",
+            }
+        )
