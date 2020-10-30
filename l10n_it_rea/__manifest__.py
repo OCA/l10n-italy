fe65145c6730d86a2a6a93a72d1635d35b6cd141
gianmarco <gconte@dinamicheaziendali.it>
2020-10-30 16:11:50 +0100

    [MIG] l10n_it_rea: Migration to 14.0

--- l10n_it_rea/__manifest__.py
+++ l10n_it_rea/__manifest__.py
@@ -3,17 +3,18 @@
 # Copyright 2016 Andrea Gallina (Apulia Software)
 # Copyright 2018 Lorenzo Battistini
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# Copyright 2020 Gianmarco Conte (Dinamiche Aziendali Srl)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "ITA - Registro REA",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Localisation/Italy",
     "development_status": "Production/Stable",
     "summary": "Gestisce i campi del Repertorio Economico Amministrativo",
     "author": "Agile Business Group, Odoo Italia Network,"
     "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/l10n-italy/tree/12.0/l10n_it_rea",
+    "website": "https://github.com/OCA/l10n-italy",
     "license": "AGPL-3",
     "depends": ["account"],
     "data": [
