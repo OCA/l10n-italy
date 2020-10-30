227f69fd88207dff00772e296ffb766bd659ea06
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-10-30 14:42:36 +0100

    [MIG] l10n_it_ipa: Migration to 14.0
    [MIG] l10n_it_ipa: Migration to 14.0

--- l10n_it_ipa/__manifest__.py
+++ l10n_it_ipa/__manifest__.py
@@ -3,14 +3,14 @@
 # Copyright 2014 Associazione Odoo Italia
 # (<http://www.odoo-italia.org>).
 # Copyright 2016 Lorenzo Battistini - Agile Business Group
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "IPA Code (IndicePA)",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
-    "category": "Localisation/Italy",
-    "author": "KTec S.r.l,Odoo Community Association (OCA)," "Odoo Italia Network",
+    "category": "Localization/Italy",
+    "author": "KTec S.r.l,Odoo Community Association (OCA),Odoo Italia Network",
     "website": "https://github.com/OCA/l10n-italy",
     "license": "AGPL-3",
     "depends": ["base"],
