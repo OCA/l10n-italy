19376fc7acd2567e4280813c90195268ad6fa3ed
Alfredo Zamora <alfredo.zamora@agilebg.com>
2020-11-06 15:12:00 +0100

    [IMP] l10n_it_fiscal_document_type: black, isort, prettier

--- l10n_it_fiscal_document_type/__manifest__.py
+++ l10n_it_fiscal_document_type/__manifest__.py
@@ -4,23 +4,20 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Italian Localization - '
-            'Tipi di documento fiscale per dichiarativi',
-    'version': '12.0.1.2.1',
-    'category': 'Localisation/Italy',
-    'author': "Link It srl, Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/l10n-italy/tree/12.0/'
-               'l10n_it_fiscal_document_type',
-    'license': 'LGPL-3',
-    'depends': ['l10n_it_account'],
-    'data': [
-        'views/fiscal_document_type_view.xml',
-        'views/res_partner_view.xml',
-        'views/account_invoice_view.xml',
-        'views/account_view.xml',
-        'data/fiscal.document.type.csv',
-        "security/ir.model.access.csv"
+    "name": "Italian Localization - " "Tipi di documento fiscale per dichiarativi",
+    "version": "14.0.1.2.1",
+    "category": "Localisation/Italy",
+    "author": "Link It srl, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "LGPL-3",
+    "depends": ["l10n_it_account"],
+    "data": [
+        "views/fiscal_document_type_view.xml",
+        "views/res_partner_view.xml",
+        "views/account_invoice_view.xml",
+        "views/account_view.xml",
+        "data/fiscal.document.type.csv",
+        "security/ir.model.access.csv",
     ],
-
-    'installable': True
+    "installable": True,
 }
