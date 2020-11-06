e9d22bed936828d2c46cb18a4d820ab9707e0a22
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-11-06 11:52:24 +0100

    [IMP] l10n_it_fiscal_payment_term: black, isort, prettier

--- l10n_it_fiscal_payment_term/__manifest__.py
+++ l10n_it_fiscal_payment_term/__manifest__.py
@@ -4,21 +4,21 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Italian Localization - Termini fiscali di pagamento',
-    'version': '12.0.2.0.0',
-    'category': 'Localization/Italy',
-    'summary': 'Condizioni di pagamento delle fatture elettroniche',
-    'author': 'Davide Corio, Agile Business Group, Innoviu, '
-              'Odoo Italia Network, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-italy',
-    'license': 'LGPL-3',
-    'depends': [
-        'account',
+    "name": "Italian Localization - Termini fiscali di pagamento",
+    "version": "12.0.2.0.0",
+    "category": "Localization/Italy",
+    "summary": "Condizioni di pagamento delle fatture elettroniche",
+    "author": "Davide Corio, Agile Business Group, Innoviu, "
+    "Odoo Italia Network, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/fatturapa_data.xml',
-        'views/account_view.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "data/fatturapa_data.xml",
+        "views/account_view.xml",
     ],
-    'installable': True
+    "installable": True,
 }
