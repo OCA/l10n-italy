2179150a22c43fa503151084184b432f943380ba
gianmarco <gconte@dinamicheaziendali.it>
2020-11-13 11:16:20 +0100

    [IMP] l10n_it_account: black, isort, prettier

--- l10n_it_account/models/account_account.py
+++ l10n_it_account/models/account_account.py
@@ -4,15 +4,15 @@ from odoo import api, models
 
 
 class Account(models.Model):
-    _inherit = 'account.account'
+    _inherit = "account.account"
 
-    @api.constrains('group_id')
+    @api.constrains("group_id")
     def check_balance_sign_coherence(self):
         """
         Checks whether adding an account to (or removing it from) a group
         creates incoherencies in account groups' balance signs.
         """
-        groups = self.mapped('group_id')
+        groups = self.mapped("group_id")
         # Avoid check upon empty recordset to make it faster
         if groups:
             groups.check_balance_sign_coherence()
