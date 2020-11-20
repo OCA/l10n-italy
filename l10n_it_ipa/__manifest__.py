f9a36b9286197a30ef3b55b965232f73eebea31a
Alex Comba <alex.comba@agilebg.com>
2020-11-20 11:14:04 +0100

    [IMP] l10n_it_ipa: adapt module to https://www.odoo-italia.org/documentazione/12.0/sviluppo/guidelines.html (#1)

--- l10n_it_ipa/__manifest__.py
+++ l10n_it_ipa/__manifest__.py
@@ -6,8 +6,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    "name": "IPA Code (IndicePA)",
-    "version": "14.0.1.0.0",
+    "name": "ITA - Codice IPA",
+    "version": "14.0.1.0.1",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "author": "KTec S.r.l,Odoo Community Association (OCA),Odoo Italia Network",
