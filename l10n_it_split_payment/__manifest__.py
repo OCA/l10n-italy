cf3d4b5805816b858de85a0c0ec78b17603a9284
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-11-20 18:08:22 +0100

    [MIG] l10n_it_split_payment: Migration to 14.0

--- l10n_it_split_payment/__manifest__.py
+++ l10n_it_split_payment/__manifest__.py
@@ -1,16 +1,16 @@
 # Copyright 2015  Davide Corio <davide.corio@abstract.it>
 # Copyright 2015-2018  Lorenzo Battistini - Agile Business Group
-# Copyright 2016  Alessio Gerace - Agile Business Group
+# Copyright 2016  Alessio Gerace - Agile Business Group.
 # Copyright 2018  Ruben Tonetto (Associazione PNLUG - Gruppo Odoo)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Split Payment",
-    "version": "12.0.1.0.1",
+    "name": "ITA - Scissione pagamenti",
+    "version": "14.0.1.0.0",
     "category": "Localization/Italy",
-    "summary": "Split Payment",
-    "author": "Abstract, Agile Business Group, " "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/l10n-italy" "l10n_it_split_payment",
+    "summary": "Scissione pagamenti",
+    "author": "Abstract, Agile Business Group, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
     "license": "AGPL-3",
     "depends": [
         "account",
@@ -18,6 +18,12 @@
     "data": [
         "views/account_view.xml",
         "views/config_view.xml",
+    ],
+    "images": [
+        "static/fiscal_position.png",
+        "static/settings.png",
+        "static/SP.png",
+        "static/SP2.png",
     ],
     "installable": True,
 }
