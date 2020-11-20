9fe9c1caaafea438b8a1492819420eeb1c43a5c7
Marco Colombo <marco.colombo@gmail.com>
2020-11-20 16:22:41 +0100

    [ADD] l10n_fatturapa_out backporting qweb template

--- l10n_it_fatturapa_out/wizard/__init__.py
+++ l10n_it_fatturapa_out/wizard/__init__.py
@@ -8,5 +8,6 @@ _logger = logging.getLogger(__name__)
 # in wizard_export_fatturapa, so we guard the entire file
 try:
     from . import wizard_export_fatturapa
+    from . import efattura
 except ImportError:
     _logger.debug('Cannot `import pyxb`.')  # Avoid init error if not installed
