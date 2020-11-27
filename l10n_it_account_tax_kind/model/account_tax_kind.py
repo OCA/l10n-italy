cdd5938720affe1c337880e8f2565cb4b1f730dc
gianmarco <gconte@dinamicheaziendali.it>
2020-11-27 16:24:34 +0100

    [IMP] l10n_it_account_tax_kind: black, isort, prettier

--- l10n_it_account_tax_kind/model/account_tax_kind.py
+++ l10n_it_account_tax_kind/model/account_tax_kind.py
@@ -1,31 +1,30 @@
-
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class AccountTaxKind(models.Model):
 
-    _name = 'account.tax.kind'
-    _description = 'Tax exemption kind'
+    _name = "account.tax.kind"
+    _description = "Tax exemption kind"
 
-    code = fields.Char(string='Code', size=4, required=True)
-    name = fields.Char(string='Name', required=True)
+    code = fields.Char(string="Code", size=4, required=True)
+    name = fields.Char(string="Name", required=True)
 
     @api.multi
     def name_get(self):
         res = []
         for tax_kind in self:
-            res.append(
-                (tax_kind.id, '[%s] %s' % (tax_kind.code, tax_kind.name)))
+            res.append((tax_kind.id, "[{}] {}".format(tax_kind.code, tax_kind.name)))
         return res
 
     @api.model
-    def name_search(self, name='', args=None, operator='ilike', limit=100):
+    def name_search(self, name="", args=None, operator="ilike", limit=100):
         if not args:
             args = []
         if name:
-            records = self.search([
-                '|', ('name', operator, name), ('code', operator, name)
-                ] + args, limit=limit)
+            records = self.search(
+                ["|", ("name", operator, name), ("code", operator, name)] + args,
+                limit=limit,
+            )
         else:
             records = self.search(args, limit=limit)
         return records.name_get()
