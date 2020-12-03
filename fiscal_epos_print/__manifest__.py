09e6dd3768210139d92453b189725a155d2581b8
eLBati <lb@takobi.online>
2020-12-03 16:57:59 +0100

    ADD Lottery code handling

--- fiscal_epos_print/__manifest__.py
+++ fiscal_epos_print/__manifest__.py
@@ -1,6 +1,6 @@
 # Leonardo Donelli - Creativi Quadrati
 # © 2016 Alessio Gerace - Agile Business Group
-# © 2018-2019 Lorenzo Battistini
+# © 2018-2020 Lorenzo Battistini
 # © 2019-2020 Roberto Fichera - Level Prime Srl
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -16,6 +16,7 @@
     ),
     'license': 'AGPL-3',
     'website': 'https://github.com/OCA/l10n-italy',
+    'maintainers': ['eLBati'],
     'depends': ['point_of_sale', 'pos_order_mgmt'],
     'data': [
         'views/account.xml',
@@ -23,7 +24,8 @@
         'views/assets.xml',
     ],
     'qweb': [
-        'static/src/xml/pos.xml'
+        'static/src/xml/pos.xml',
+        'static/src/xml/lottery.xml',
     ],
     'installable': True,
     'auto_install': False,
