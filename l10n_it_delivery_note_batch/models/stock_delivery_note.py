ded7abd310b9eddc6c6c08ce6d755f1b8895c510
Alfredo Zamora <alfredo.zamora@agilebg.com>
2020-12-11 11:03:14 +0100

    [MIG] l10n_it_delivery_note_batch: Migration to 14.0

--- l10n_it_delivery_note_batch/models/stock_delivery_note.py
+++ l10n_it_delivery_note_batch/models/stock_delivery_note.py
@@ -5,10 +5,10 @@ from odoo import fields, models
 
 
 class StockDeliveryNote(models.Model):
-    _inherit = 'stock.delivery.note'
+    _inherit = "stock.delivery.note"
 
-    stock_picking_batch_id = \
-        fields.Many2one('stock.picking.batch',
-                        oldname="parent_stock_picking_batch_id",
-                        string="Batch Picking",
-                        readonly=True)
+    stock_picking_batch_id = fields.Many2one(
+        "stock.picking.batch",
+        string="Batch Picking",
+        readonly=True,
+    )
