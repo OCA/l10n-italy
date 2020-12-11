b68d2d001af518d163e9f8ee3b23a6464c760ef1
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-12-11 10:37:57 +0100

    [IMP] l10n_it_delivery_note_order_link: black, isort, prettier

--- l10n_it_delivery_note_order_link/models/sale_order.py
+++ l10n_it_delivery_note_order_link/models/sale_order.py
@@ -5,17 +5,19 @@ from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
-    delivery_note_ids = fields.Many2many('stock.delivery.note',
-                                         compute='_compute_delivery_notes')
-    delivery_note_count = fields.Integer(compute='_compute_delivery_notes')
+    delivery_note_ids = fields.Many2many(
+        "stock.delivery.note", compute="_compute_delivery_notes"
+    )
+    delivery_note_count = fields.Integer(compute="_compute_delivery_notes")
 
     @api.multi
     def _compute_delivery_notes(self):
         for order in self:
             delivery_notes = order.order_line.mapped(
-                'delivery_note_line_ids.delivery_note_id')
+                "delivery_note_line_ids.delivery_note_id"
+            )
 
             order.delivery_note_ids = delivery_notes
             order.delivery_note_count = len(delivery_notes)
--- l10n_it_delivery_note_order_link/models/stock_delivery_note.py
+++ l10n_it_delivery_note_order_link/models/stock_delivery_note.py
@@ -3,8 +3,8 @@ from odoo import fields, models
 
 
 class StockDeliveryNoteLine(models.Model):
-    _inherit = 'stock.delivery.note.line'
+    _inherit = "stock.delivery.note.line"
 
-    purchase_line_id = fields.Many2one('purchase.order.line',
-                                       related='move_id.purchase_line_id',
-                                       store=True)
+    purchase_line_id = fields.Many2one(
+        "purchase.order.line", related="move_id.purchase_line_id", store=True
+    )
