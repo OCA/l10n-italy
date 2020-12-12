23b69d622f42a86a42a9ca8f8d7077cd8c9b352a
Sergio Zanchetta <primes2h@gmail.com>
2020-12-12 17:10:16 +0100

    [FIX] show stamp duty parameters in manual mode

--- l10n_it_account_stamp/__manifest__.py
+++ l10n_it_account_stamp/__manifest__.py
@@ -2,12 +2,12 @@
 # Copyright 2018 Enrico Ganzaroli (enrico.gz@gmail.com)
 # Copyright 2018 Ermanno Gnan (ermannognan@gmail.com)
 # Copyright 2018 Lorenzo Battistini (https://github.com/eLBati)
-# Copyright 2018-2019 Sergio Zanchetta (https://github.com/primes2h)
+# Copyright 2018-2020 Sergio Zanchetta (https://github.com/primes2h)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Italian Localization - Imposta di bollo',
-    'version': '12.0.1.1.2',
+    'version': '12.0.1.1.3',
     'category': 'Localization/Italy',
     'summary': "Gestione automatica dell'imposta di bollo",
     'author': 'Ermanno Gnan, Sergio Corato, Enrico Ganzaroli, '
