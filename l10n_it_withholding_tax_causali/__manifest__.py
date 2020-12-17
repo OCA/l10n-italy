563c637e53cca6578ede3234fc416680cc0d7008
Marco Colombo <marco.colombo@gmail.com>
2020-12-17 18:22:48 +0100

    [14.0][MIG] l10n_it_withholding_tax_causali Migration to 14.0 from 12.0

--- l10n_it_withholding_tax_causali/__manifest__.py
+++ l10n_it_withholding_tax_causali/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2018 Lorenzo Battistini - Agile Business Group
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Causali pagamento per ritenute d'acconto",
-    "version": "12.0.2.0.0",
+    "name": "ITA - Causali pagamento per ritenute d'acconto",
+    "version": "14.0.2.0.0",
     "development_status": "Beta",
     "category": "Hidden",
     "website": "https://github.com/OCA/l10n-italy",
