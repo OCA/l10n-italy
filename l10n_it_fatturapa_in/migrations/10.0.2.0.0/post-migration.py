ce8047b44e1addc39341b1d651947cc43e8ac9c4
SimoRubi <simone.rubino@agilebg.com>
2020-12-18 11:15:45 +0100

    Adapt migration scripts to v10

--- l10n_it_fatturapa_in/migrations/10.0.2.0.0/post-migration.py
+++ l10n_it_fatturapa_in/migrations/10.0.2.0.0/post-migration.py
@@ -1,14 +1,45 @@
-from odoo import api, SUPERUSER_ID
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2020 Alex Comba - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+from psycopg2 import sql
 
 
-def migrate(cr, version):
+def create_withholding_data_lines(env):
+    """
+    Create ftpa_withholding_ids from ftpa_withholding_type
+    and ftpa_withholding_amount
+    """
+    column_wht_amount = openupgrade.get_legacy_name('ftpa_withholding_amount')
+    column_wht_type = openupgrade.get_legacy_name('ftpa_withholding_type')
+    exists = openupgrade.column_exists(env.cr, 'account_invoice', column_wht_amount)
+    mapping = {
+        'name': 'ai.{ftpa_withholding_type}'.format(
+            ftpa_withholding_type=column_wht_type),
+        'invoice_id': 'ai.id',
+        'create_uid': 'ai.create_uid',
+        'create_date': 'ai.create_date',
+        'write_date': 'ai.write_date',
+        'write_uid': 'ai.write_uid',
+    }
+    if exists:
+        mapping.update(
+            {'amount': 'ai.{ftpa_withholding_amount}'.format(
+                ftpa_withholding_amount=column_wht_amount)})
+    query = """
+        INSERT INTO withholding_data_line
+        ({columns})
+        SELECT {values}
+        FROM account_invoice AS ai
+        WHERE ai.{ftpa_withholding_type} IS NOT NULL;""".format(
+            columns=','.join(mapping.keys()),
+            values=','.join(mapping.values()),
+            ftpa_withholding_type=column_wht_type)
+    openupgrade.logged_query(env.cr, sql.SQL(query))
+
+
+@openupgrade.migrate()
+def migrate(env, version):
     if not version:
         return
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        invoices = env['account.invoice'].search([])
-        # in order to prevent error messages in old invoices,
-        # where ftpa_withholding_amount is 0
-        for invoice in invoices:
-            invoice.withholding_tax_amount = \
-                sum(invoice.ftpa_withholding_ids.mapped('amount'))
+    create_withholding_data_lines(env)
