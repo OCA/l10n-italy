7d6440c4738d3f5ebc14a1d67b3729e92ee5bdd3
alex_cam <alessandrocamilli@openforce.it>
2020-12-22 09:36:57 +0100

    [UPD][l10n_it_invoices_data_communication] remove intrastat dependance

--- l10n_it_invoices_data_communication/__manifest__.py
+++ l10n_it_invoices_data_communication/__manifest__.py
@@ -16,16 +16,17 @@
     'depends': [
         'account', 'l10n_it_fiscal_document_type', 'l10n_it_codici_carica',
         'l10n_it_fiscalcode', 'l10n_it_esigibilita_iva',
-        'l10n_it_account_tax_kind', 'l10n_it_account',
-        'intrastat_base'
+        'l10n_it_account_tax_kind', 'l10n_it_account'
     ],
     'data': [
         'security/ir.model.access.csv',
+        'data/res_country_data.xml',
         'wizard/compute_fiscal_document_type_view.xml',
         'wizard/split_big_communication_view.xml',
         'views/comunicazione.xml',
         'views/account.xml',
         'views/account_invoice_view.xml',
+        'views/res_country_view.xml',
         'wizard/export_file_view.xml',
         'security/security.xml',
     ],
