d5870dd02c9b12654780dceb21120bfcc3893082
eLBati <lb@takobi.online>
2021-01-11 10:32:11 +0100

    FIX l10n_it_pos_fatturapa also setting electronic_invoice_obliged_subject when enabling e-invoice for customer Otherwise, in backend, e-invoice fields would not appear

--- l10n_it_pos_fatturapa/models/res_partner.py
+++ l10n_it_pos_fatturapa/models/res_partner.py
@@ -10,6 +10,10 @@ class ResPartner(models.Model):
     @api.model
     def create_from_ui(self, partner):
         if 'electronic_invoice_subjected' in partner:
-            partner['electronic_invoice_subjected'] = \
+            electronic_invoice_subjected = \
                 partner['electronic_invoice_subjected'] == 'true'
+            partner['electronic_invoice_subjected'] = \
+                electronic_invoice_subjected
+            partner['electronic_invoice_obliged_subject'] = \
+                electronic_invoice_subjected
         return super(ResPartner, self).create_from_ui(partner)
