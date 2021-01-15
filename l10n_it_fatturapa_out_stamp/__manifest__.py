9946c24813b12020b3f2a81dcbd1a85280a5a7ca
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2021-01-15 11:22:25 +0100

    [IMP] l10n_it_fatturapa_out_stamp: black, isort, prettier

--- l10n_it_fatturapa_out_stamp/__manifest__.py
+++ l10n_it_fatturapa_out_stamp/__manifest__.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Lorenzo Battistini
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Italian Localization - Fattura elettronica - Integrazione bollo",
-    "summary": "Modulo ponte tra emissione fatture elettroniche e imposta di "
-               "bollo",
+    "summary": "Modulo ponte tra emissione fatture elettroniche e imposta di " "bollo",
     "version": "12.0.2.0.0",
     "development_status": "Beta",
     "category": "Hidden",
@@ -19,6 +17,5 @@
         "l10n_it_fatturapa_out",
         "l10n_it_account_stamp",
     ],
-    "data": [
-    ],
+    "data": [],
 }
--- l10n_it_fatturapa_out_stamp/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out_stamp/wizard/wizard_export_fatturapa.py
@@ -1,24 +1,22 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models
-from odoo.tools.float_utils import float_round, float_is_zero
-from odoo.addons.l10n_it_fatturapa.bindings.fatturapa import (
-    DatiBolloType
-)
+from odoo.tools.float_utils import float_is_zero, float_round
+
+from odoo.addons.l10n_it_fatturapa.bindings.fatturapa import DatiBolloType
 
 
 class WizardExportFatturapa(models.TransientModel):
     _inherit = "wizard.export.fatturapa"
 
     def setDatiGeneraliDocumento(self, invoice, body):
-        res = super(WizardExportFatturapa, self).setDatiGeneraliDocumento(
-            invoice, body)
+        res = super(WizardExportFatturapa, self).setDatiGeneraliDocumento(invoice, body)
         if invoice.tax_stamp:
             body.DatiGenerali.DatiGeneraliDocumento.DatiBollo = DatiBolloType(
-                BolloVirtuale="SI")
+                BolloVirtuale="SI"
+            )
             if invoice.company_id.tax_stamp_product_id:
                 stamp_price = invoice.company_id.tax_stamp_product_id.list_price
                 if not float_is_zero(stamp_price, precision_digits=2):
-                    body.DatiGenerali.DatiGeneraliDocumento.DatiBollo.ImportoBollo = \
-                        '%.2f' % float_round(stamp_price, 2)
+                    body.DatiGenerali.DatiGeneraliDocumento.DatiBollo.ImportoBollo = (
+                        "%.2f" % float_round(stamp_price, 2)
+                    )
         return res
