603b6074066966de09f84a25e554b5f9ca55e134
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2021-01-15 11:50:22 +0100

    [MIG] l10n_it_fatturapa_out_stamp: Migration to 14.0
    [UPD] Update l10n_it_fatturapa_out_stamp.pot
    
    [UPD] README.rst

--- l10n_it_fatturapa_out_stamp/__manifest__.py
+++ l10n_it_fatturapa_out_stamp/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2018 Lorenzo Battistini
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Italian Localization - Fattura elettronica - Integrazione bollo",
-    "summary": "Modulo ponte tra emissione fatture elettroniche e imposta di " "bollo",
-    "version": "12.0.2.0.0",
+    "name": "ITA - Fattura elettronica - Integrazione bollo",
+    "summary": "Modulo ponte tra emissione fatture elettroniche e imposta di bollo",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
-    "category": "Hidden",
+    "category": "Localization/Italy",
     "website": "https://github.com/OCA/l10n-italy",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "maintainers": ["eLBati"],
@@ -17,5 +17,5 @@
         "l10n_it_fatturapa_out",
         "l10n_it_account_stamp",
     ],
-    "data": [],
+    "data": ["views/account_invoice_it_dati_bollo.xml"],
 }
