54c5af8cffd441e1c1c4913ff8ee315e0fd63541
sergiocorato <sergiocorato@gmail.com>
2021-01-26 12:59:10 +0100

    [FIX][12.0] remove space

--- l10n_it_fatturapa_sale/models/sale_order_line.py
+++ l10n_it_fatturapa_sale/models/sale_order_line.py
@@ -4,7 +4,7 @@
 from odoo import api, fields, models
 
 
-class SaleOrderLine (models.Model):
+class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     related_documents = fields.One2many(
