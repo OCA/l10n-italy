ebd2c0bfce1dfc920932463dbb3b1befbb4d7f65
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2021-01-29 11:19:12 +0100

    [IMP] l10n_it_abicab: black, isort, prettier

--- l10n_it_abicab/__manifest__.py
+++ l10n_it_abicab/__manifest__.py
@@ -4,15 +4,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Italian localization - Codici bancari ABI/CAB',
-    'version': '12.0.1.1.1',
-    'category': 'Localization/Italy',
-    'development_status': 'Production/Stable',
-    'summary': 'Base Bank ABI/CAB codes',
-    'author': "Odoo Italia Network, Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/l10n-italy/tree/12.0/l10n_it_abicab',
-    'license': 'AGPL-3',
-    'depends': ['account'],
-    'data': ['views/abicab_view.xml'],
-    'installable': True,
+    "name": "Italian localization - Codici bancari ABI/CAB",
+    "version": "12.0.1.1.1",
+    "category": "Localization/Italy",
+    "development_status": "Production/Stable",
+    "summary": "Base Bank ABI/CAB codes",
+    "author": "Odoo Italia Network, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["views/abicab_view.xml"],
+    "installable": True,
 }
