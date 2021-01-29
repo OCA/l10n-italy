24b64d48b557e287b6323c8df8f757d4319879ab
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2021-01-29 11:19:12 +0100

    [IMP] l10n_it_abicab: black, isort, prettier

--- l10n_it_abicab/tests/test_abicab.py
+++ l10n_it_abicab/tests/test_abicab.py
@@ -5,24 +5,27 @@ from odoo.tests.common import TransactionCase
 
 
 class BankCase(TransactionCase):
-
     def setUp(self):
         super(BankCase, self).setUp()
-        self.partner1 = self.env['res.partner'].create({'name': 'Partner1'})
+        self.partner1 = self.env["res.partner"].create({"name": "Partner1"})
 
     def test_create_bank(self):
-        bank_model = self.env['res.bank']
-        pbank_model = self.env['res.partner.bank']
-        bank1 = bank_model.create({
-            'name': 'Bank1',
-            'abi': 'abi_1',
-            'cab': 'cab_1',
-            })
-        pbank1 = pbank_model.create({
-            'acc_number': '212',
-            'partner_id': self.partner1.id,
-            'bank_id': bank1.id,
-            })
+        bank_model = self.env["res.bank"]
+        pbank_model = self.env["res.partner.bank"]
+        bank1 = bank_model.create(
+            {
+                "name": "Bank1",
+                "abi": "abi_1",
+                "cab": "cab_1",
+            }
+        )
+        pbank1 = pbank_model.create(
+            {
+                "acc_number": "212",
+                "partner_id": self.partner1.id,
+                "bank_id": bank1.id,
+            }
+        )
         pbank1.onchange_bank_id()
-        self.assertEqual(pbank1.bank_abi, 'abi_1')
-        self.assertEqual(pbank1.bank_cab, 'cab_1')
+        self.assertEqual(pbank1.bank_abi, "abi_1")
+        self.assertEqual(pbank1.bank_cab, "cab_1")
