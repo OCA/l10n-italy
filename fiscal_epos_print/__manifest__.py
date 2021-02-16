2ceca922c316d3987936c8597b1c649ed96b1d61
eLBati <lb@takobi.online>
2021-02-16 10:38:06 +0100

    FIX fiscal_epos_print lottery_code could also be false

--- fiscal_epos_print/__manifest__.py
+++ fiscal_epos_print/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'ITA - Driver per stampanti fiscali compatibili ePOS-Print XML',
-    'version': '12.0.1.3.1',
+    'version': '12.0.1.3.2',
     'category': 'Point Of Sale',
     'summary': 'ePOS-Print XML Fiscal Printer Driver - Stampanti Epson compatibili: '
                'FP81II, FP90III',
