65c061389e9a41a5685819052798ab78da4668f5
Giovanni <giovanni@gslab.it>
2021-02-18 16:37:09 +0100

    move state from fatturapa_pec to fatturapa_out

--- l10n_it_fatturapa_pec/__manifest__.py
+++ l10n_it_fatturapa_pec/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'ITA - Fattura elettronica - Supporto PEC',
-    'version': '12.0.1.9.0',
+    'version': '12.0.1.9.1',
     'category': 'Localization/Italy',
     'summary': 'Invio fatture elettroniche tramite PEC',
     'author': 'Openforce Srls Unipersonale, Odoo Community Association (OCA)',
