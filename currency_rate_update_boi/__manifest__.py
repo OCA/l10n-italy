34ae26d87ef0058dd1d8c442bf19a13f8abe7b03
eLBati <lb@takobi.online>
2021-02-24 17:23:18 +0100

    ADD currency_rate_update_boi - Currency Rate Update: Bank of Italy

---
+++ currency_rate_update_boi/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Lorenzo Battistini @ TAKOBI
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Currency Rate Update: Banca d\'Italia',
+    'version': '12.0.1.0.0',
+    'category': 'Financial Management/Configuration',
+    'summary': 'Update exchange rates using www.bancaditalia.it',
+    'author':
+        'TAKOBI, '
+        'Odoo Community Association (OCA)',
+    'maintainers': ['eLBati'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'depends': [
+        'currency_rate_update',
+    ],
+    'data': [
+    ],
+}
