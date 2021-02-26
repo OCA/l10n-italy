a3d667f0ae03d55f7f400cf473c4e0a7186d8a4e
Borruso <gborruso@dinamicheaziendali.it>
2021-02-26 10:43:38 +0100

    [IMP] l10n_it_ricevute_bancarie: black, isort, prettier

--- l10n_it_ricevute_bancarie/report/distinta_qweb.py
+++ l10n_it_ricevute_bancarie/report/distinta_qweb.py
@@ -6,14 +6,14 @@ from odoo import api, models
 
 class DistintaReportQweb(models.AbstractModel):
 
-    _name = 'report.l10n_it_ricevute_bancarie.distinta_qweb'
+    _name = "report.l10n_it_ricevute_bancarie.distinta_qweb"
     _description = "C/O Slip Report"
 
     @api.multi
     def _get_report_values(self, docids, data=None):
         return {
-            'doc_ids': docids,
-            'doc_model': 'riba.distinta',
-            'docs': self.env['riba.distinta'].browse(docids),
-            'data': data,
+            "doc_ids": docids,
+            "doc_model": "riba.distinta",
+            "docs": self.env["riba.distinta"].browse(docids),
+            "data": data,
         }
