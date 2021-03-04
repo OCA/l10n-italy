935dd68311702c8c57297dd1396ccf2c200c0669
Marco Colombo <marco.colombo@gmail.com>
2021-03-04 19:14:51 +0100

    [IMP] l10n_it_fatturapa_in_rc: black, isort, prettier

--- l10n_it_fatturapa_in_rc/models/account_rc_type.py
+++ l10n_it_fatturapa_in_rc/models/account_rc_type.py
@@ -1,10 +1,10 @@
-from odoo import models, fields
+from odoo import fields, models
 
 
 class RCType(models.Model):
-    _inherit = 'account.rc.type'
+    _inherit = "account.rc.type"
 
     e_invoice_suppliers = fields.Boolean(
         "E-invoice suppliers",
-        help="Automatically used when importing e-invoices from Italian "
-             "suppliers")
+        help="Automatically used when importing e-invoices from Italian " "suppliers",
+    )
