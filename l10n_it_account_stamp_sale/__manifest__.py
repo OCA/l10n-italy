7071cf8c1b1517ac25ede8f27fed30696b195c42
Marco Colombo <marco.colombo@gmail.com>
2021-03-04 19:13:38 +0100

    [IMP] l10n_it_account_stamp_sale: black, isort, prettier

--- l10n_it_account_stamp_sale/__manifest__.py
+++ l10n_it_account_stamp_sale/__manifest__.py
@@ -2,19 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'ITA - Imposta di bollo - Vendite',
-    'summary': "Modulo ponte tra imposta di bollo e vendite",
-    'version': '12.0.1.0.1',
-    'category': 'Localization/Italy',
-    'license': 'AGPL-3',
-    'author': 'Agile Business Group, Odoo Community Association (OCA)',
-    'maintainers': ['tafaRU'],
-    'website': 'https://github.com/OCA/l10n-italy/tree/12.0/'
-                'l10n_it_account_stamp_sale',
-    'depends': [
-        'l10n_it_account_stamp',
-        'sale',
+    "name": "ITA - Imposta di bollo - Vendite",
+    "summary": "Modulo ponte tra imposta di bollo e vendite",
+    "version": "12.0.1.0.1",
+    "category": "Localization/Italy",
+    "license": "AGPL-3",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "maintainers": ["tafaRU"],
+    "website": "https://github.com/OCA/l10n-italy" "l10n_it_account_stamp_sale",
+    "depends": [
+        "l10n_it_account_stamp",
+        "sale",
     ],
-    'auto_install': True,
-    'installable': True,
+    "auto_install": True,
+    "installable": True,
 }
--- l10n_it_account_stamp_sale/models/sale_order.py
+++ l10n_it_account_stamp_sale/models/sale_order.py
@@ -6,13 +6,14 @@ from odoo import api, models
 
 class AccountInvoice(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.multi
     def action_invoice_create(self, grouped=False, final=False):
         invoice_ids = super(AccountInvoice, self).action_invoice_create(
-            grouped=grouped, final=final)
-        invoice_model = self.env['account.invoice']
+            grouped=grouped, final=final
+        )
+        invoice_model = self.env["account.invoice"]
         for invoice in invoice_model.browse(invoice_ids):
             invoice.tax_stamp = invoice.is_tax_stamp_applicable()
         return invoice_ids
