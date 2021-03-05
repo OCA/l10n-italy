cc8a0db40769a25c246ae5ab8f33f5c2db15d75a
Borruso <gborruso@dinamicheaziendali.it>
2021-03-05 15:42:56 +0100

    [IMP] l10n_it_pec: black, isort, prettier

--- l10n_it_pec/__manifest__.py
+++ l10n_it_pec/__manifest__.py
@@ -3,16 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Italian Localization - Email PEC',
-    'version': '12.0.1.0.1',
-    'category': 'Localization/Italy',
-    'summary': 'Aggiunge il campo email PEC al partner',
-    'author': "Odoo Italia Network,Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/l10n-italy/tree/12.0/l10n_it_pec',
-    'license': 'AGPL-3',
-    'depends': ['mail'],
-    'data': [
-        'views/partner_view.xml',
+    "name": "Italian Localization - Email PEC",
+    "version": "12.0.1.0.1",
+    "category": "Localization/Italy",
+    "summary": "Aggiunge il campo email PEC al partner",
+    "author": "Odoo Italia Network,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "data": [
+        "views/partner_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
