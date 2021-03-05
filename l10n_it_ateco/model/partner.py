4348e1f6aa4d4b179d5631f838e3115f235ffb42
Borruso <gborruso@dinamicheaziendali.it>
2021-03-05 15:34:39 +0100

    [MIG] l10n_it_ateco: Migration to 14.0

--- l10n_it_ateco/model/partner.py
+++ l10n_it_ateco/model/partner.py
@@ -1,8 +1,6 @@
-#
-#    Copyright (C) 2014 Abstract (<http://abstract.it>).
-#    Copyright (C) 2016 Ciro Urselli (<http://www.apuliasoftware.it>).
-#
-#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright (C) 2014 Abstract (<http://abstract.it>).
+# Copyright (C) 2016 Ciro Urselli (<http://www.apuliasoftware.it>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
