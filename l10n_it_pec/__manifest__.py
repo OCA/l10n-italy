69202d9f0b89d843f1dc961c9212e3ddc1209b0f
Borruso <gborruso@dinamicheaziendali.it>
2021-03-05 16:33:21 +0100

    [MIG] l10n_it_pec: Migration to 14.0

--- l10n_it_pec/__manifest__.py
+++ l10n_it_pec/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
 # Copyright 2019 Stefano Consolaro (Associazione PNLUG - Gruppo Odoo)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    "name": "Italian Localization - Email PEC",
-    "version": "12.0.1.0.1",
+    "name": "ITA - Email PEC",
+    "version": "14.0.1.0.0",
     "category": "Localization/Italy",
     "summary": "Aggiunge il campo email PEC al partner",
     "author": "Odoo Italia Network,Odoo Community Association (OCA)",
