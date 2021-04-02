7420a691d0183cae87399c43b744f90c236c3d39
Alfredo Zamora <alfredo.zamora@agilebg.com>
2021-04-02 13:50:23 +0200

    [IMP] account_vat_period_end_statement: black, isort, prettier

--- account_vat_period_end_statement/wizard/add_period.py
+++ account_vat_period_end_statement/wizard/add_period.py
@@ -3,38 +3,35 @@
 #  Copyright 2015 Associazione Odoo Italia (<http://www.odoo-italia.org>)
 #  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
-from odoo.tools.translate import _
+from odoo import api, fields, models
 from odoo.exceptions import UserError
+from odoo.tools.translate import _
 
 
 class AddPeriod(models.TransientModel):
 
-    _name = 'add.period.to.vat.statement'
+    _name = "add.period.to.vat.statement"
     _description = "Add period to VAT Statement"
 
-    period_id = fields.Many2one(
-        'date.range', 'Period', required=True)
+    period_id = fields.Many2one("date.range", "Period", required=True)
 
     @api.multi
     def add_period(self):
         self.ensure_one()
-        if 'active_id' not in self.env.context:
-            raise UserError(_('Current statement not found'))
-        statement_env = self.env['account.vat.period.end.statement']
+        if "active_id" not in self.env.context:
+            raise UserError(_("Current statement not found"))
+        statement_env = self.env["account.vat.period.end.statement"]
         wizard = self
         if wizard.period_id.vat_statement_id:
             raise UserError(
-                _('Period %s is associated to statement %s yet') %
-                (
-                    wizard.period_id.name,
-                    wizard.period_id.vat_statement_id.date)
+                _("Period %s is associated to statement %s yet")
+                % (wizard.period_id.name, wizard.period_id.vat_statement_id.date)
             )
-        statement_id = self.env.context['active_id']
+        statement_id = self.env.context["active_id"]
         wizard.period_id.vat_statement_id = statement_id
         statement = statement_env.browse(statement_id)
         statement.set_fiscal_year()
         statement.compute_amounts()
         return {
-            'type': 'ir.actions.act_window_close',
+            "type": "ir.actions.act_window_close",
         }
