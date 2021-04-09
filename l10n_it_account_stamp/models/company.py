dbc3e2bb4b7a6163d38a1e1f7ae8f19cec359e80
gianmarco <gconte@dinamicheaziendali.it>
2021-04-09 16:39:30 +0200

    [IMP] l10n_it_account_stamp: black, isort, prettier

--- l10n_it_account_stamp/models/company.py
+++ l10n_it_account_stamp/models/company.py
@@ -1,35 +1,38 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     tax_stamp_product_id = fields.Many2one(
-        'product.product', 'Tax Stamp Product',
-        help="Product used as Tax Stamp in customer invoices."
-        )
+        "product.product",
+        "Tax Stamp Product",
+        help="Product used as Tax Stamp in customer invoices.",
+    )
 
 
 class AccountConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     tax_stamp_product_id = fields.Many2one(
-        related='company_id.tax_stamp_product_id',
+        related="company_id.tax_stamp_product_id",
         string="Tax Stamp Product",
         help="Product used as Tax Stamp in customer invoices.",
-        readonly=False
-        )
+        readonly=False,
+    )
 
-    @api.onchange('company_id')
+    @api.onchange("company_id")
     def onchange_company_id(self):
         res = super(AccountConfigSettings, self).onchange_company_id()
         if self.company_id:
             company = self.company_id
             self.tax_stamp_product_id = (
-                company.tax_stamp_product_id and
-                company.tax_stamp_product_id.id or False)
+                company.tax_stamp_product_id
+                and company.tax_stamp_product_id.id
+                or False
+            )
         else:
             self.tax_stamp_product_id = False
         return res
--- l10n_it_account_stamp/models/product.py
+++ l10n_it_account_stamp/models/product.py
@@ -1,26 +1,31 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
+from odoo import _, api, exceptions, fields, models
+
 import odoo.addons.decimal_precision as dp
-from odoo import fields, models, api, exceptions, _
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    @api.constrains('stamp_apply_tax_ids', 'is_stamp')
+    @api.constrains("stamp_apply_tax_ids", "is_stamp")
     def _check_stamp_apply_tax(self):
         for template in self:
             if template.stamp_apply_tax_ids and not template.is_stamp:
                 raise exceptions.ValidationError(
                     _("The product %s must be a stamp to apply set taxes!")
-                    % template.name)
+                    % template.name
+                )
 
     stamp_apply_tax_ids = fields.Many2many(
-        'account.tax',
-        'product_tax_account_tax__rel',
-        'product_id', 'tax_id', string='Stamp taxes')
+        "account.tax",
+        "product_tax_account_tax__rel",
+        "product_id",
+        "tax_id",
+        string="Stamp taxes",
+    )
     stamp_apply_min_total_base = fields.Float(
-        'Stamp applicability min total base',
-        digits=dp.get_precision('Account'))
-    is_stamp = fields.Boolean('Is a stamp')
-    auto_compute = fields.Boolean('Auto-compute')
+        "Stamp applicability min total base", digits=dp.get_precision("Account")
+    )
+    is_stamp = fields.Boolean("Is a stamp")
+    auto_compute = fields.Boolean("Auto-compute")
