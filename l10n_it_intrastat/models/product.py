b65a2616a28c0b5ef57d7b9341632e2bd155b9e7
SimoRubi <simone.rubino@agilebg.com>
2021-04-22 14:40:45 +0200

    [IMP] l10n_it_intrastat: black, isort, prettier

--- l10n_it_intrastat/models/product.py
+++ l10n_it_intrastat/models/product.py
@@ -1,39 +1,41 @@
 # Copyright 2019 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProductCategory(models.Model):
-    _inherit = 'product.category'
+    _inherit = "product.category"
 
     intrastat_code_id = fields.Many2one(
-        'report.intrastat.code',
-        string="Nomenclature Code"
+        "report.intrastat.code", string="Nomenclature Code"
     )
     intrastat_type = fields.Selection(
         [
-            ('good', "Goods"),
-            ('service', "Service"),
-            ('misc', "Miscellaneous"),
-            ('exclude', "Exclude")
+            ("good", "Goods"),
+            ("service", "Service"),
+            ("misc", "Miscellaneous"),
+            ("exclude", "Exclude"),
         ],
-        string="Intrastat Type")
+        string="Intrastat Type",
+    )
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     intrastat_code_id = fields.Many2one(
-        comodel_name='report.intrastat.code',
-        string="Intrastat Code")
+        comodel_name="report.intrastat.code", string="Intrastat Code"
+    )
     intrastat_type = fields.Selection(
         selection=[
-            ('good', "Goods"),
-            ('service', "Service"),
-            ('misc', "Miscellaneous"),
-            ('exclude', "Exclude")],
-        string="Intrastat Type")
+            ("good", "Goods"),
+            ("service", "Service"),
+            ("misc", "Miscellaneous"),
+            ("exclude", "Exclude"),
+        ],
+        string="Intrastat Type",
+    )
 
     def get_intrastat_data(self):
         """
@@ -42,15 +44,12 @@ class ProductTemplate(models.Model):
         - Intrastat Code on product template
         - Intrastat Code on product category
         """
-        res = {
-            'intrastat_code_id': False,
-            'intrastat_type': False
-        }
+        res = {"intrastat_code_id": False, "intrastat_type": False}
         # From Product
         if self.intrastat_type:
-            res['intrastat_code_id'] = self.intrastat_code_id.id
-            res['intrastat_type'] = self.intrastat_type
+            res["intrastat_code_id"] = self.intrastat_code_id.id
+            res["intrastat_type"] = self.intrastat_type
         elif self.categ_id and self.categ_id.intrastat_code_id:
-            res['intrastat_code_id'] = self.categ_id.intrastat_code_id.id
-            res['intrastat_type'] = self.categ_id.intrastat_type
+            res["intrastat_code_id"] = self.categ_id.intrastat_code_id.id
+            res["intrastat_type"] = self.categ_id.intrastat_type
         return res
