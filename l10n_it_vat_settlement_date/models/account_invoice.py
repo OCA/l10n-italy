a3a47e3a8dcda12ce4ab6050175efbdd811cb32c
Marco Colombo <marco.colombo@gmail.com>
2021-04-26 23:02:38 +0200

    [FIX] support for l10n_it_reverse_charge

--- l10n_it_vat_settlement_date/models/account_invoice.py
+++ l10n_it_vat_settlement_date/models/account_invoice.py
@@ -16,9 +16,14 @@ class AccountInvoice(models.Model):
             if not invoice.date_vat_settlement:
                 invoice.write({"date_vat_settlement": self.date or self.date_invoice or fields.Date.context_today(self)})
             for move in invoice.move_id:
-                move.date_vat_settlement = invoice.date_vat_settlement
+                move.write({"date_vat_settlement": invoice.date_vat_settlement})
 
     @api.onchange("date_vat_settlement")
     def _onchange_date_vat_settlement(self):
         for move in self.move_id:
             move.write({"date_vat_settlement": self.date_vat_settlement})
+
+    def rc_inv_vals(self, partner, account, rc_type, lines, currency):
+        ret = super().rc_inv_vals(partner, account, rc_type, lines, currency)
+        ret['date_vat_settlement'] = self.date_vat_settlement
+        return ret
