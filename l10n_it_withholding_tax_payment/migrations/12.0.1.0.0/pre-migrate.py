f1117f677deee06b65ccd7569f52a513a6134a10
SimoRubi <simone.rubino@agilebg.com>
2021-04-26 11:06:13 +0200

    [IMP] l10n_it_withholding_tax_payment: black, isort, prettier

--- l10n_it_withholding_tax_payment/migrations/12.0.1.0.0/pre-migrate.py
+++ l10n_it_withholding_tax_payment/migrations/12.0.1.0.0/pre-migrate.py
@@ -2,16 +2,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
-
 from datetime import datetime
+
 from odoo.tools.misc import DEFAULT_SERVER_DATE_FORMAT
 
 _logger = logging.getLogger(__name__)
 
 
 def _fix_date_field(cr):
-    cr.execute("SELECT id, date FROM withholding_tax_move_payment"
-               " WHERE date IS NOT NULL;")
+    cr.execute(
+        "SELECT id, date FROM withholding_tax_move_payment" " WHERE date IS NOT NULL;"
+    )
 
     records = cr.fetchall()
     for record in records:
@@ -22,22 +23,29 @@ def _fix_date_field(cr):
             datetime.strptime(record_value, DEFAULT_SERVER_DATE_FORMAT)
 
         except (TypeError, ValueError):
-            _logger.error("The row in table 'withholding_tax_move_payment'"
-                          " with ID #{} and 'date' field valued"
-                          " as '{}' will be set as NULL..."
-                          .format(record_id, record_value))
+            _logger.error(
+                "The row in table 'withholding_tax_move_payment'"
+                " with ID #{} and 'date' field valued"
+                " as '{}' will be set as NULL...".format(record_id, record_value)
+            )
 
-            cr.execute("UPDATE withholding_tax_move_payment"
-                       " SET date = NULL WHERE id = %s;", (record_id, ))
+            cr.execute(
+                "UPDATE withholding_tax_move_payment" " SET date = NULL WHERE id = %s;",
+                (record_id,),
+            )
 
-    cr.execute("ALTER TABLE withholding_tax_move_payment"
-               " ALTER COLUMN date TYPE DATE USING date::DATE;")
+    cr.execute(
+        "ALTER TABLE withholding_tax_move_payment"
+        " ALTER COLUMN date TYPE DATE USING date::DATE;"
+    )
 
 
 def migrate(cr, version):
     if not version:
-        _logger.warning("Does not exist any previous version for this module. "
-                        "Skipping the migration...")
+        _logger.warning(
+            "Does not exist any previous version for this module. "
+            "Skipping the migration..."
+        )
 
         return
 
