c0a7b2893ce385308a346b284a39119e1196c453
eLBati <lb@takobi.online>
2021-04-28 11:57:33 +0200

    porting l10n_it_vat_statement_communication to 14.0

--- l10n_it_vat_statement_communication/__manifest__.py
+++ l10n_it_vat_statement_communication/__manifest__.py
@@ -1,18 +1,18 @@
 # Copyright 2017 Alessandro Camilli - Openforce
-# Copyright 2017-2019 Lorenzo Battistini
+# Copyright 2017-2021 Lorenzo Battistini
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "ITA - Comunicazione liquidazione IVA",
     "summary": "Comunicazione liquidazione IVA ed esportazione file xml"
     "conforme alle specifiche dell'Agenzia delle Entrate",
-    "version": "12.0.1.6.0",
+    "version": "14.0.1.0.0",
     "category": "Account",
-    "author": "Openforce di Camilli Alessandro, " "Odoo Community Association (OCA)",
+    "author": "Openforce di Camilli Alessandro, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-italy",
     "license": "AGPL-3",
     "depends": [
         "account_vat_period_end_statement",
-        "l10n_it_codici_carica",
+        "l10n_it_appointment_code",
         "l10n_it_fiscalcode",
     ],
     "data": [
