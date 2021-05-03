7192eefdb37e47ec2510016b670d767f019c7ec0
SimoRubi <simone.rubino@agilebg.com>
2021-05-03 12:42:36 +0200

    [IMP] l10n_it_account_stamp: Add test

--- l10n_it_account_stamp/tests/__init__.py
+++ l10n_it_account_stamp/tests/__init__.py
@@ -1,1 +1,5 @@
+#  Copyright 2021 Simone Rubino - Agile Business Group
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import test_account_stamp
 from . import test_account_stamp_invoicing
