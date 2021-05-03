a67e7b70c5adbeb53e105d9b7122ed01eb2740c3
SimoRubi <simone.rubino@agilebg.com>
2021-05-03 11:27:33 +0200

    [FIX] l10n_it_account_stamp: Method onchange_company_id does not exist in super() Remove redundant field attributes already inherited from related field UnboundLocalError: local variable 'posted' referenced before assignment

--- l10n_it_account_stamp/tests/test_account_stamp.py
+++ l10n_it_account_stamp/tests/test_account_stamp.py
@@ -4,59 +4,80 @@
 from odoo.addons.account.tests.account_test_classes import AccountingTestCase
 
 
-class TestAccountStamp (AccountingTestCase):
-
+class TestAccountStamp(AccountingTestCase):
     def setUp(self):
         super(TestAccountStamp, self).setUp()
-        self.partner_model = self.env['res.partner']
-        self.product_model = self.env['product.product']
-        self.invoice_model = self.env['account.invoice']
-        self.tax_model = self.env['account.tax']
-        self.account_model = self.env['account.account']
+        self.partner_model = self.env["res.partner"]
+        self.product_model = self.env["product.product"]
+        self.invoice_model = self.env["account.invoice"]
+        self.tax_model = self.env["account.tax"]
+        self.account_model = self.env["account.account"]
 
-        self.partner = self.partner_model.create({
-            'name': "Test partner for stamp",
-        })
-        self.product = self.product_model.create({
-            'name': "Test product for stamp",
-            'lst_price': 100,
-        })
-        self.receivable_type_id = self.ref('account.data_account_type_receivable')
-        self.account_receivable = self.account_model.search([
-            ('user_type_id', '=', self.receivable_type_id),
-        ], limit=1)
-        self.revenue_type_id = self.ref('account.data_account_type_revenue')
-        self.account_revenue = self.account_model.search([
-            ('user_type_id', '=', self.revenue_type_id),
-        ], limit=1)
+        self.partner = self.partner_model.create(
+            {
+                "name": "Test partner for stamp",
+            }
+        )
+        self.product = self.product_model.create(
+            {
+                "name": "Test product for stamp",
+                "lst_price": 100,
+            }
+        )
+        self.receivable_type_id = self.ref("account.data_account_type_receivable")
+        self.account_receivable = self.account_model.search(
+            [
+                ("user_type_id", "=", self.receivable_type_id),
+            ],
+            limit=1,
+        )
+        self.revenue_type_id = self.ref("account.data_account_type_revenue")
+        self.account_revenue = self.account_model.search(
+            [
+                ("user_type_id", "=", self.revenue_type_id),
+            ],
+            limit=1,
+        )
 
-        self.income_stamp_account = self.account_model.create({
-            'name': "Test account income stamp",
-            'code': 'TAIS',
-            'user_type_id': self.revenue_type_id,
-            'reconcile': True,
-        })
-        self.expense_stamp_account = self.account_model.create({
-            'name': "Test account expense stamp",
-            'code': 'TAES',
-            'user_type_id': self.receivable_type_id,
-            'reconcile': True,
-        })
+        self.income_stamp_account = self.account_model.create(
+            {
+                "name": "Test account income stamp",
+                "code": "TAIS",
+                "user_type_id": self.revenue_type_id,
+                "reconcile": True,
+            }
+        )
+        self.expense_stamp_account = self.account_model.create(
+            {
+                "name": "Test account expense stamp",
+                "code": "TAES",
+                "user_type_id": self.receivable_type_id,
+                "reconcile": True,
+            }
+        )
 
     def test_automatic_stamp(self):
-        invoice = self.invoice_model.create({
-            'partner_id': self.partner.id,
-            'name': 'Test invoice automatic stamp',
-            'account_id': self.account_receivable.id,
-            'type': 'out_invoice',
-            'invoice_line_ids': [(0, 0, {
-                'product_id': self.product.id,
-                'quantity': 1,
-                'price_unit': 121.0,
-                'name': 'Test invoice line for stamp',
-                'account_id': self.account_revenue.id,
-            })],
-        })
+        invoice = self.invoice_model.create(
+            {
+                "partner_id": self.partner.id,
+                "name": "Test invoice automatic stamp",
+                "account_id": self.account_receivable.id,
+                "type": "out_invoice",
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product.id,
+                            "quantity": 1,
+                            "price_unit": 121.0,
+                            "name": "Test invoice line for stamp",
+                            "account_id": self.account_revenue.id,
+                        },
+                    )
+                ],
+            }
+        )
         invoice.invoice_line_ids._onchange_product_id()
         invoice.compute_taxes()
         self.assertFalse(invoice.tax_stamp)
@@ -73,15 +94,15 @@ class TestAccountStamp (AccountingTestCase):
         stamp_product.property_account_income_id = self.income_stamp_account
         stamp_product.property_account_expense_id = self.expense_stamp_account
         invoice.action_invoice_open()
-        self.assertEqual(invoice.move_id.state, 'posted')
+        self.assertEqual(invoice.move_id.state, "posted")
 
         # Check that lines for the stamp have been created in the account move
         move_lines = invoice.move_id.line_ids
         income_stamp_line = move_lines.filtered(
-            lambda line: line.account_id
-            == stamp_product.property_account_income_id)
+            lambda line: line.account_id == stamp_product.property_account_income_id
+        )
         self.assertEqual(len(income_stamp_line), 1)
         expense_stamp_line = move_lines.filtered(
-            lambda line: line.account_id
-            == stamp_product.property_account_expense_id)
+            lambda line: line.account_id == stamp_product.property_account_expense_id
+        )
         self.assertEqual(len(expense_stamp_line), 1)
