09a583e36b2aaf3cad7d5fc26c671986d2ff3716
Marco Colombo <marco.colombo@gmail.com>
2021-05-26 12:21:52 +0200

    [IMP] l10n_it_fatturapa_out: black, isort, prettier

--- l10n_it_fatturapa_out/models/account.py
+++ l10n_it_fatturapa_out/models/account.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Lorenzo Battistini - Agile Business Group
 
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
 
@@ -11,12 +11,12 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     fatturapa_attachment_out_id = fields.Many2one(
-        'fatturapa.attachment.out', 'E-invoice Export File',
-        readonly=True, copy=False)
+        "fatturapa.attachment.out", "E-invoice Export File", readonly=True, copy=False
+    )
 
     has_pdf_invoice_print = fields.Boolean(
-        related='fatturapa_attachment_out_id.has_pdf_invoice_print',
-        readonly=True)
+        related="fatturapa_attachment_out_id.has_pdf_invoice_print", readonly=True
+    )
 
     def preventive_checks(self):
         # hook for preventive checks. Override and raise exception, in case
@@ -26,18 +26,22 @@ class AccountInvoice(models.Model):
     def action_invoice_cancel(self):
         for invoice in self:
             if invoice.fatturapa_attachment_out_id:
-                raise UserError(_(
-                    "Invoice %s has XML and can't be canceled. "
-                    "Delete the XML before."
-                ) % invoice.number)
+                raise UserError(
+                    _(
+                        "Invoice %s has XML and can't be canceled. "
+                        "Delete the XML before."
+                    )
+                    % invoice.number
+                )
         res = super(AccountInvoice, self).action_invoice_cancel()
         return res
 
     def get_first_non_zero_tax(self):
         for line in self.invoice_line_ids:
             if (
-                not line.display_type and line.price_subtotal and
-                len(line.invoice_line_tax_ids) == 1
+                not line.display_type
+                and line.price_subtotal
+                and len(line.invoice_line_tax_ids) == 1
             ):
                 return line.invoice_line_tax_ids[0]
         return False
--- l10n_it_fatturapa_out/models/attachment.py
+++ l10n_it_fatturapa_out/models/attachment.py
@@ -1,38 +1,49 @@
 # Copyright 2014 Davide Corio
 # Copyright 2016-2018 Lorenzo Battistini - Agile Business Group
 
-from odoo import fields, models, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class FatturaPAAttachment(models.Model):
     _name = "fatturapa.attachment.out"
     _description = "E-invoice Export File"
-    _inherits = {'ir.attachment': 'ir_attachment_id'}
-    _inherit = ['mail.thread']
-    _order = 'id desc'
+    _inherits = {"ir.attachment": "ir_attachment_id"}
+    _inherit = ["mail.thread"]
+    _order = "id desc"
 
     ir_attachment_id = fields.Many2one(
-        'ir.attachment', 'Attachment', required=True, ondelete="cascade")
+        "ir.attachment", "Attachment", required=True, ondelete="cascade"
+    )
     att_name = fields.Char(
-        string="E-invoice file name",
-        related='ir_attachment_id.name',
-        store=True)
+        string="E-invoice file name", related="ir_attachment_id.name", store=True
+    )
     out_invoice_ids = fields.One2many(
-        'account.invoice', 'fatturapa_attachment_out_id',
-        string="Out Invoices", readonly=True)
+        "account.invoice",
+        "fatturapa_attachment_out_id",
+        string="Out Invoices",
+        readonly=True,
+    )
     has_pdf_invoice_print = fields.Boolean(
         help="True if all the invoices have a printed "
-             "report attached in the XML, False otherwise.",
-        compute='_compute_has_pdf_invoice_print', store=True)
+        "report attached in the XML, False otherwise.",
+        compute="_compute_has_pdf_invoice_print",
+        store=True,
+    )
     invoice_partner_id = fields.Many2one(
-        'res.partner', string='Customer', store=True,
-        compute='_compute_invoice_partner_id')
+        "res.partner",
+        string="Customer",
+        store=True,
+        compute="_compute_invoice_partner_id",
+    )
 
-    _sql_constraints = [(
-        'ftpa_attachment_out_name_uniq',
-        'unique(att_name)',
-        'The name of the e-invoice file must be unique!')]
+    _sql_constraints = [
+        (
+            "ftpa_attachment_out_name_uniq",
+            "unique(att_name)",
+            "The name of the e-invoice file must be unique!",
+        )
+    ]
 
     @api.model
     def get_file_vat(self):
@@ -40,48 +51,43 @@ class FatturaPAAttachment(models.Model):
         if company.fatturapa_sender_partner:
             if not company.fatturapa_sender_partner.vat:
                 raise UserError(
-                    _('Partner %s TIN not set.')
+                    _("Partner %s TIN not set.")
                     % company.fatturapa_sender_partner.display_name
                 )
             vat = company.fatturapa_sender_partner.vat
         else:
             if not company.vat:
-                raise UserError(
-                    _('Company %s TIN not set.') % company.display_name)
+                raise UserError(_("Company %s TIN not set.") % company.display_name)
             vat = company.vat
-        vat = vat.replace(' ', '').replace('.', '').replace('-', '')
+        vat = vat.replace(" ", "").replace(".", "").replace("-", "")
         return vat
 
     def file_name_exists(self, file_id):
         vat = self.get_file_vat()
-        partial_fname = r'%s\_%s.' % (vat, file_id)  # escaping _ SQL
+        partial_fname = r"{}\_{}.".format(vat, file_id)  # escaping _ SQL
         # Not trying to perfect match file extension, because user could have
         # downloaded, signed and uploaded again the file, thus having changed
         # file extension
-        return bool(self.search(
-            [('datas_fname', '=like', '%s%%' % partial_fname)]))
+        return bool(self.search([("datas_fname", "=like", "%s%%" % partial_fname)]))
 
     @api.multi
-    @api.depends('out_invoice_ids')
+    @api.depends("out_invoice_ids")
     def _compute_invoice_partner_id(self):
         for att in self:
-            partners = att.mapped('out_invoice_ids.partner_id')
+            partners = att.mapped("out_invoice_ids.partner_id")
             if len(partners) == 1:
                 att.invoice_partner_id = partners.id
 
     @api.multi
-    @api.constrains('datas_fname')
+    @api.constrains("datas_fname")
     def _check_datas_fname(self):
         for att in self:
-            res = self.search([('datas_fname', '=', att.datas_fname)])
+            res = self.search([("datas_fname", "=", att.datas_fname)])
             if len(res) > 1:
-                raise UserError(
-                    _("File %s already present.") %
-                    att.datas_fname)
+                raise UserError(_("File %s already present.") % att.datas_fname)
 
     @api.multi
-    @api.depends(
-        'out_invoice_ids.fatturapa_doc_attachments.is_pdf_invoice_print')
+    @api.depends("out_invoice_ids.fatturapa_doc_attachments.is_pdf_invoice_print")
     def _compute_has_pdf_invoice_print(self):
         """Check if all the invoices related to this attachment
         have at least one attachment containing
@@ -89,8 +95,7 @@ class FatturaPAAttachment(models.Model):
         for attachment_out in self:
             for invoice in attachment_out.out_invoice_ids:
                 invoice_attachments = invoice.fatturapa_doc_attachments
-                if any([ia.is_pdf_invoice_print
-                        for ia in invoice_attachments]):
+                if any([ia.is_pdf_invoice_print for ia in invoice_attachments]):
                     continue
                 else:
                     attachment_out.has_pdf_invoice_print = False
@@ -104,12 +109,12 @@ class FatturaPAAttachment(models.Model):
     @api.multi
     def write(self, vals):
         res = super(FatturaPAAttachment, self).write(vals)
-        if 'datas' in vals and 'message_ids' not in vals:
+        if "datas" in vals and "message_ids" not in vals:
             for attachment in self:
                 attachment.message_post(
                     subject=_("E-invoice attachment changed"),
-                    body=_("User %s uploaded a new e-invoice file"
-                           ) % self.env.user.login
+                    body=_("User %s uploaded a new e-invoice file")
+                    % self.env.user.login,
                 )
         return res
 
@@ -118,4 +123,5 @@ class FatturaAttachments(models.Model):
     _inherit = "fatturapa.attachments"
 
     is_pdf_invoice_print = fields.Boolean(
-        help="This attachment contains the PDF report of the linked invoice")
+        help="This attachment contains the PDF report of the linked invoice"
+    )
