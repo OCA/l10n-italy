e55ed92397c00bdacee460cfa610fe654ea88597
Docker @ kuntrawant <matteomircoli@openforce.it>
2021-06-23 20:02:01 +0200

    [ADD][l10n_it_vat_registries] add module from mega merge

---
+++ l10n_it_vat_registries/models/account_journal.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+    tax_registry_id = fields.Many2one(
+        "account.tax.registry",
+        "VAT registry",
+        help="You can group several journals within 1 registry. In printing "
+        "wizard, you will be able to select the registry in order to load"
+        " that group of journals",
+    )
