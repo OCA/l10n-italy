5bf45694460685d681b5be50bc569c17c230b327
Andrea Cometa <a.cometa@apuliasoftware.it>
2021-07-07 12:42:08 +0200

    Aggiornamento licenza
    la licenza deve riportare AGPL così come indicato nella v12.0 e nella seguente PR https://github.com/OCA/l10n-italy/pull/1892/files

--- l10n_it_fiscal_document_type/__manifest__.py
+++ l10n_it_fiscal_document_type/__manifest__.py
@@ -9,7 +9,7 @@
     "category": "Localization/Italy",
     "author": "Link It srl, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-italy",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": ["l10n_it_account"],
     "data": [
         "views/fiscal_document_type_view.xml",
