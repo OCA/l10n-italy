ce5dc99fe01f12ab32b08763d6f505baf013fc49
Marco Colombo <marco.colombo@gmail.com>
2021-07-19 16:33:02 +0200

    [14.0][FIX] l10n_it_rea: Unicità codice REA solo per partner attivi
    Forward port of #2257, fix for #2256
    
    Co-authored-by: SimoRubi <simone.rubino@agilebg.com>

--- l10n_it_rea/__manifest__.py
+++ l10n_it_rea/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "ITA - Registro REA",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "category": "Localization/Italy",
     "development_status": "Production/Stable",
     "summary": "Gestisce i campi del Repertorio Economico Amministrativo",
