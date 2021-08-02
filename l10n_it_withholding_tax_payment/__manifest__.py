a6d8df51c0c4ce85e9b13850bffff6d48e4d3357
Docker @ kuntrawant <matteomircoli@openforce.it>
2021-08-02 18:05:20 +0200

    [ADD][l10n_it_withholding_tax_payment] add from mega merge

---
+++ l10n_it_withholding_tax_payment/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2015 Alessandro Camilli (<http://www.openforce.it>)
+# Copyright 2019 Matteo Bilotta
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "ITA - Ritenuta d'acconto - Pagamenti",
+    "summary": "Gestisce le ritenute sulle fatture e sui pagamenti",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Localization/Italy",
+    "website": "https://github.com/OCA/l10n-italy",
+    "author": "Openforce, Odoo Italia Network, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["account", "l10n_it_withholding_tax"],
+    "data": [
+        "data/sequence.xml",
+        "security/ir.model.access.csv",
+        "security/security.xml",
+        "views/withholding_tax.xml",
+        "wizard/create_move_payment_view.xml",
+    ],
+}
