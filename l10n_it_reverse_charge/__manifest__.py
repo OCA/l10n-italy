823814aecb5b95d34c42a37c776a5b4de04283dd
simonev <simone.vanin@agilebg.com>
2021-08-06 16:27:36 +0200

    [IMP] l10n_it_reverse_charge: black, isort, prettier

--- l10n_it_reverse_charge/__manifest__.py
+++ l10n_it_reverse_charge/__manifest__.py
@@ -5,25 +5,24 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'ITA - Inversione contabile',
-    'version': '12.0.1.2.4',
-    'category': 'Localization/Italy',
-    'summary': 'Inversione contabile',
-    'author': 'Odoo Italia Network, Odoo Community Association (OCA)',
-    'license': 'LGPL-3',
-    'website': 'https://github.com/OCA/l10n-italy/tree/'
-               '12.0/l10n_it_reverse_charge',
-    'depends': [
-        'account',
-        'account_cancel',
+    "name": "ITA - Inversione contabile",
+    "version": "12.0.1.2.4",
+    "category": "Localization/Italy",
+    "summary": "Inversione contabile",
+    "author": "Odoo Italia Network, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/l10n-italy" "12.0/l10n_it_reverse_charge",
+    "depends": [
+        "account",
+        "account_cancel",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/rc_type.xml',
-        'views/account_invoice_view.xml',
-        'views/account_fiscal_position_view.xml',
-        'views/account_rc_type_view.xml',
-        'security/reverse_charge_security.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "data/rc_type.xml",
+        "views/account_invoice_view.xml",
+        "views/account_fiscal_position_view.xml",
+        "views/account_rc_type_view.xml",
+        "security/reverse_charge_security.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
