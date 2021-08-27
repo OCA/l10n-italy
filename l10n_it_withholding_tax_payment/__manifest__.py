ee8f7c812d62f888cec3df3881f350a49c823a0f
SimoRubi <simone.rubino@agilebg.com>
2021-08-27 15:48:47 +0200

    [IMP] l10n_it_withholding_tax_payment: Readme

--- l10n_it_withholding_tax_payment/__manifest__.py
+++ l10n_it_withholding_tax_payment/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "ITA - Ritenuta d'acconto - Pagamenti",
     "summary": "Gestisce le ritenute sulle fatture e sui pagamenti",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "website": "https://github.com/OCA/l10n-italy",
