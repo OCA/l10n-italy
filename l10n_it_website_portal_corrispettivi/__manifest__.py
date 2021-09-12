9b27a582cfc3d227f3c357dfd114967cc261b7d8
eLBati <lb@takobi.online>
2021-09-12 07:44:08 +0200

    ADD l10n_it_website_portal_corrispettivi: Aggiunge ricevuta o fattura come opzione nel profilo dell'utente portale

---
+++ l10n_it_website_portal_corrispettivi/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Lorenzo Battistini @  TAKOBI
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "ITA - Ricevute - Portale",
+    "summary": "Aggiunge ricevuta o fattura come opzione nel profilo dell'utente"
+               " portale",
+    "version": "12.0.1.0.0",
+    "author": "Odoo Community Association (OCA), TAKOBI",
+    "category": "Localization/Italy",
+    "website": "https://github.com/OCA/l10n-italy/tree/12.0",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_corrispettivi",
+        "portal"
+    ],
+    "data": [
+        "views/l10n_it_website_portal_corrispettivi_templates.xml"
+    ],
+    "auto_install": True,
+}
