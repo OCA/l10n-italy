d1b8ba1690874cef4431fd8f7cd66d31754ecea2
SimoRubi <simone.rubino@agilebg.com>
2021-10-04 11:51:18 +0200

    [IMP] l10n_it_account_stamp: Usability for stamp in invoice. If stamp line can be added (invoice in draft): show button to add stamp line. If stamp line can't be added (invoice not in draft): show message explaining why it can't be added. If stamp line has already been added: show message saying it has already been added (useful if invoice has many lines). Button and messages update live while updating the invoice. Better messages to user for charging stamp to customer
    Co-authored-by:  Simone Rubino <simone.rubino@aion-tech.it>

--- l10n_it_account_stamp/models/account_move.py
+++ l10n_it_account_stamp/models/account_move.py
@@ -1,3 +1,4 @@
+# Copyright 2023 Simone Rubino - Aion Tech
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from odoo import _, api, fields, models
@@ -8,7 +9,15 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     tax_stamp = fields.Boolean(
-        "Tax Stamp", readonly=False, compute="_compute_tax_stamp", store=True
+        "Tax Stamp",
+        help="Tax stamp is applied to this invoice.",
+        readonly=False,
+        compute="_compute_tax_stamp",
+        store=True,
+    )
+    tax_stamp_line_present = fields.Boolean(
+        string="Stamp line is present in invoice",
+        compute="_compute_tax_stamp_line_present",
     )
     auto_compute_stamp = fields.Boolean(
         related="company_id.tax_stamp_product_id.auto_compute"
@@ -76,6 +85,7 @@ class AccountMove(models.Model):
             invoice_line_vals = {
                 "move_id": inv.id,
                 "product_id": stamp_product_id.id,
+                "is_stamp_line": True,
                 "name": stamp_product_id.description_sale,
                 "sequence": 99999,
                 "account_id": stamp_account.id,
@@ -94,10 +104,20 @@ class AccountMove(models.Model):
         return super()._move_autocomplete_invoice_lines_values()
 
     def is_tax_stamp_line_present(self):
+        self.ensure_one()
         for line in self.line_ids:
             if line.is_stamp_line:
                 return True
         return False
+
+    @api.depends(
+        "invoice_line_ids",
+        "invoice_line_ids.product_id",
+        "invoice_line_ids.product_id.is_stamp",
+    )
+    def _compute_tax_stamp_line_present(self):
+        for invoice in self:
+            invoice.tax_stamp_line_present = invoice.is_tax_stamp_line_present()
 
     def is_tax_stamp_product_present(self):
         product_stamp = self.invoice_line_ids.filtered(
@@ -183,7 +203,7 @@ class AccountMove(models.Model):
             move_line_tax_stamp_ids = account_move.line_ids.filtered(
                 lambda line: line.is_stamp_line
             )
-            move_line_tax_stamp_ids.unlink()
+            move_line_tax_stamp_ids.with_context(check_move_validity=False).unlink()
         return res
 
 
