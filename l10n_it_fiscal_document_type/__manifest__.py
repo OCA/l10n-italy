9159e728393011e894371352a8afaa4963318315
Docker @ kuntrawant <matteomircoli@openforce.it>
2021-10-06 11:59:23 +0200

    [UPD][l10n_it_fiscal_document_type] update from megamerge

--- l10n_it_fiscal_document_type/__manifest__.py
+++ l10n_it_fiscal_document_type/__manifest__.py
@@ -1,15 +1,15 @@
 # Copyright 2017 Alessandro Camilli
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # Copyright 2018 Lorenzo Battistini (https://github.com/eLBati)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "ITA - Tipi di documento fiscale per dichiarativi",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.2",
     "category": "Localization/Italy",
     "author": "Link It srl, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-italy",
-    "license": "AGPL-3",
+    "license": "AGPL-3",  # Depends on AGPL modules
     "depends": ["l10n_it_account"],
     "data": [
         "views/fiscal_document_type_view.xml",
