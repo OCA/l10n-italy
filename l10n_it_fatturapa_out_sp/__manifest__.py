f535acafb346194fb9f9868f64f97f9df1aaa829
Docker @ kuntrawant <matteomircoli@openforce.it>
2021-10-06 12:07:25 +0200

    [UPD][l10n_it_fatturapa_out_sp] update from megamerge

---
+++ l10n_it_fatturapa_out_sp/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Marco Colombo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "ITA - Fattura elettronica - Emissione - Scissione Pagamenti",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Localization/Italy",
+    "summary": "Scissione pagamenti in fatturapa",
+    "author": "Marco Colombo," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy" "l10n_it_fatturapa_out_sp",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_fatturapa_out",
+        "l10n_it_split_payment",
+    ],
+    "data": [
+        "view/invoice_it_template.xml",
+    ],
+    "installable": True,
+}
