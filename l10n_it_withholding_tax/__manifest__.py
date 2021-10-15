4a9968a9aaeb15fae0307f057df0c91278d01325
tafaRU <alex.comba@agilebg.com>
2021-10-15 11:48:52 +0200

    [FIX] l10n_it_withholding_tax: change name according to https://www.odoo-italia.org/documentazione/12.0/sviluppo/guidelines.html#titolo-del-modulo

--- l10n_it_withholding_tax/__manifest__.py
+++ l10n_it_withholding_tax/__manifest__.py
@@ -4,8 +4,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "ITA - Withholding Tax",
-    "version": "14.0.1.0.4",
+    "name": "ITA - Ritenute d'acconto",
+    "version": "14.0.1.0.5",
     "category": "Account",
     "author": "Openforce, Odoo Italia Network, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-italy",
