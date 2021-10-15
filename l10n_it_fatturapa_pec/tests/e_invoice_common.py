c5e5bb5ca22862e652c60719ff6a05ea9c57a94e
Borruso <gborruso@dinamicheaziendali.it>
2021-10-15 18:22:09 +0200

    [MIG] l10n_it_fatturapa_pec: Migration to 14.0
    Co-authored-by: SimoRubi <simone.rubino@agilebg.com>
    Co-authored-by: fredzamoabg

--- l10n_it_fatturapa_pec/tests/e_invoice_common.py
+++ l10n_it_fatturapa_pec/tests/e_invoice_common.py
@@ -8,8 +8,22 @@ class EInvoiceCommon(FatturaPACommon):
     def setUp(self):
         super(EInvoiceCommon, self).setUp()
         self.account_model = self.env["account.account"]
-        self.invoice_model = self.env["account.invoice"]
+        self.invoice_model = self.env["account.move"]
         self.wizard_model = self.env["wizard.export.fatturapa"]
+        self.tax_model = self.env["account.tax"]
+
+        self.env.company.vat = "IT06363391001"
+        self.env.company.fatturapa_art73 = True
+        self.env.company.partner_id.street = "Via Milano, 1"
+        self.env.company.partner_id.city = "Roma"
+        self.env.company.partner_id.state_id = self.env.ref("base.state_us_2").id
+        self.env.company.partner_id.zip = "00100"
+        self.env.company.partner_id.phone = "06543534343"
+        self.env.company.email = "info@yourcompany.example.com"
+        self.env.company.partner_id.country_id = self.env.ref("base.it").id
+        self.env.company.fatturapa_fiscal_position_id = self.env.ref(
+            "l10n_it_fatturapa.fatturapa_RF01"
+        ).id
 
         self.account_receivable_id = self.account_model.create(
             dict(
@@ -29,10 +43,10 @@ class EInvoiceCommon(FatturaPACommon):
         )
         self.partner = self.env.ref("l10n_it_fatturapa.res_partner_fatturapa_2")
         self.product = self.env.ref("product.product_product_5")
-        self.env.user.company_id.sdi_channel_id = self.env.ref(
+        self.env.company.sdi_channel_id = self.env.ref(
             "l10n_it_sdi_channel.sdi_channel_pec"
         )
-        self.env.user.company_id.sdi_channel_id.pec_server_id = self.env[
+        self.env.company.sdi_channel_id.pec_server_id = self.env[
             "ir.mail_server"
         ].create(
             {
@@ -43,20 +57,28 @@ class EInvoiceCommon(FatturaPACommon):
         )
 
     def _create_invoice(self):
+        tax = self.tax_model.create(
+            {
+                "name": "Tax 22.0%",
+                "amount": 22,
+                "amount_type": "percent",
+                "type_tax_use": "sale",
+            }
+        )
         invoice_line_data = {
             "product_id": self.product.id,
             "quantity": 1,
             "price_unit": 1,
             "account_id": self.account_income_id.id,
             "name": self.product.name,
-            "invoice_line_tax_ids": [(6, 0, [self.ref("l10n_it_fatturapa.tax_22")])],
+            "tax_ids": [(6, 0, [tax.id])],
         }
         return self.invoice_model.create(
             dict(
                 name="Test Invoice",
-                account_id=self.account_receivable_id.id,
                 invoice_line_ids=[(0, 0, invoice_line_data)],
                 partner_id=self.partner.id,
+                move_type="out_invoice",
             )
         )
 
@@ -66,7 +88,7 @@ class EInvoiceCommon(FatturaPACommon):
 
     def _create_e_invoice(self):
         invoice = self._create_invoice()
-        invoice.action_invoice_open()
+        invoice.action_post()
 
         wizard = self._get_export_wizard(invoice)
         action = wizard.exportFatturaPA()
@@ -77,7 +99,7 @@ class EInvoiceCommon(FatturaPACommon):
         return self.env["fetchmail.server"].create(
             {
                 "name": "Test PEC server",
-                "type": "pop",
+                "server_type": "pop",
                 "is_fatturapa_pec": True,
                 "server": "dummy",
                 "port": 110,
