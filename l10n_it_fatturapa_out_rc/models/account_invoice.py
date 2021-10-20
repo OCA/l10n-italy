aae37f2d91b016adc1b82edc7dc635aa5d2a36a8
tafaRU <alex.comba@agilebg.com>
2021-10-20 17:19:28 +0200

    [IMP] l10n_it_fatturapa_out_rc: black, isort, prettier

--- l10n_it_fatturapa_out_rc/models/account_invoice.py
+++ l10n_it_fatturapa_out_rc/models/account_invoice.py
@@ -1,4 +1,4 @@
-from odoo import api, models, _
+from odoo import _, api, models
 from odoo.exceptions import UserError
 
 
@@ -10,18 +10,23 @@ class Invoice(models.Model):
         if self.rc_self_invoice_id:
             rc_type = self.fiscal_position_id.rc_type_id
             if rc_type.fiscal_document_type_id:
-                self.rc_self_invoice_id.fiscal_document_type_id =\
+                self.rc_self_invoice_id.fiscal_document_type_id = (
                     rc_type.fiscal_document_type_id.id
+                )
             if self.fatturapa_attachment_in_id:
                 doc_id = self.fatturapa_attachment_in_id.name
             else:
                 doc_id = self.reference if self.reference else self.number
             self.rc_self_invoice_id.related_documents = [
-                (0, 0, {
-                    "type": "invoice",
-                    "name": doc_id,
-                    "date": self.date_invoice,
-                })
+                (
+                    0,
+                    0,
+                    {
+                        "type": "invoice",
+                        "name": doc_id,
+                        "date": self.date_invoice,
+                    },
+                )
             ]
         return res
 
@@ -29,11 +34,14 @@ class Invoice(models.Model):
     def action_invoice_draft(self):
         super().action_invoice_draft()
         for inv in self:
-            if not inv.env.context.get("rc_set_to_draft") and \
-                    inv.rc_purchase_invoice_id.state in ['draft', 'cancel']:
-                raise UserError(_(
-                    "Vendor invoice that has generated this self invoice isn't "
-                    "validated. "
-                    "Validate vendor invoice before."
-                ))
+            if not inv.env.context.get(
+                "rc_set_to_draft"
+            ) and inv.rc_purchase_invoice_id.state in ["draft", "cancel"]:
+                raise UserError(
+                    _(
+                        "Vendor invoice that has generated this self invoice isn't "
+                        "validated. "
+                        "Validate vendor invoice before."
+                    )
+                )
         return True
