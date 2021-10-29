5754feac2ed8e83ff204705e5f859f6588e94b8a
Docker @ kuntrawant <matteomircoli@openforce.it>
2021-10-29 13:15:41 +0200

    [FIX][l10n_it_fatturapa_out_dn] bump version

--- l10n_it_fatturapa_out_dn/__manifest__.py
+++ l10n_it_fatturapa_out_dn/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "ITA - Fattura elettronica - Emissione - DDT",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.0+PR202+PR25REPOTHEMULE",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "summary": "DDT in fatture elettroniche",
