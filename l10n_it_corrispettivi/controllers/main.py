978b679dcc3914776dff1b78a406afd14b9a2b55
tafaRU <alex.comba@agilebg.com>
2021-11-12 09:38:03 +0100

    [IMP] l10n_it_corrispettivi: black, isort, prettier

--- l10n_it_corrispettivi/controllers/main.py
+++ l10n_it_corrispettivi/controllers/main.py
@@ -4,8 +4,7 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
 class CorrispettiviPortal(CustomerPortal):
-
     def _show_report(self, model, report_type, report_ref, download=False):
-        if model._name == 'account.invoice' and model.corrispettivo:
-            report_ref = 'l10n_it_corrispettivi.account_corrispettivi'
+        if model._name == "account.invoice" and model.corrispettivo:
+            report_ref = "l10n_it_corrispettivi.account_corrispettivi"
         return super()._show_report(model, report_type, report_ref, download)
