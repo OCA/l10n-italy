dde9b01b6779c8df1ff1f39b8d96a305908c8f56
Borruso <gborruso@dinamicheaziendali.it>
2021-11-20 18:09:01 +0100

    [MIG] l10n_it_website_portal_ipa: Migration to 14.0

--- l10n_it_website_portal_ipa/__manifest__.py
+++ l10n_it_website_portal_ipa/__manifest__.py
@@ -1,11 +1,11 @@
 #  Copyright 2019 Simone Rubino - Agile Business Group
 #  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "ITA - Indice PA nel portale",
-    "summary": "Aggiunge l'indice PA (IPA) " "tra i dettagli dell'utente nel portale.",
-    "version": "12.0.1.1.1",
+    "name": "ITA - Codice IPA - Portale",
+    "summary": "Aggiunge l'indice PA (IPA) tra i dettagli dell'utente nel portale.",
+    "version": "14.0.1.0.0",
     "category": "Localization/Italy",
-    "website": "https://github.com/OCA/l10n-italy" "12.0/l10n_it_website_portal_ipa",
+    "website": "https://github.com/OCA/l10n-italy",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": [
