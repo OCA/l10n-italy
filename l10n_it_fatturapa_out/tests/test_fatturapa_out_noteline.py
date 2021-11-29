b931362cd917201048f75c2b8c70e0545af5580d
SimoRubi <simone.rubino@agilebg.com>
2021-11-29 16:01:14 +0100

    [FIX] l10n_it_fatturapa_out: Generate e-invoice when tax has kind_id

--- l10n_it_fatturapa_out/tests/test_fatturapa_out_noteline.py
+++ l10n_it_fatturapa_out/tests/test_fatturapa_out_noteline.py
@@ -194,3 +194,41 @@ class TestFatturaOutNoteLine(FatturaPACommon):
         aliquote = xml.findall(".//DatiRiepilogo/AliquotaIVA")
         self.assertEqual(len(aliquote), 1)
         self.assertEqual(aliquote[0].text, "22.00")
+
+    def test_5_note_tax_kind_id(self):
+        """test case:
+        - company default 22%
+        - one product 0%, tax having kind_id
+        - one note
+
+        expected result: one DatiRiepilogo with AliquotaIVA == 0%
+        """
+        self.env.company.account_sale_tax_id = self.tax_22
+
+        move_form = Form(
+            self.env["account.move"].with_context(default_move_type="out_invoice")
+        )
+        move_form.name = "INV/2016/0013"
+        move_form.invoice_date = fields.Date.from_string("2016-01-07")
+        move_form.partner_id = self.res_partner_fatturapa_0
+        with move_form.invoice_line_ids.new() as line_form:
+            line_form.product_id = self.product_product_10
+            line_form.tax_ids.remove(index=0)
+            line_form.tax_ids.add(self.tax_00_ns)
+        with move_form.invoice_line_ids.new() as line_form:
+            line_form.display_type = "line_note"
+            line_form.name = "just a note"
+            line_form.account_id = self.env["account.account"]
+        invoice = move_form.save()
+
+        res = self.run_wizard(invoice.id)
+        self.assertTrue(res)
+
+        attachment = self.attach_model.browse(res["res_id"])
+        xml_content = base64.decodebytes(attachment.datas)
+
+        parser = etree.XMLParser(remove_blank_text=True)
+        xml = etree.fromstring(xml_content, parser)
+        aliquote = xml.findall(".//DatiRiepilogo/AliquotaIVA")
+        self.assertEqual(len(aliquote), 1)
+        self.assertEqual(aliquote[0].text, "0.00")
