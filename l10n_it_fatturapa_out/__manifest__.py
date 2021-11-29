25c06847deaa57298c999937bd771aa7e18f17bb
SimoRubi <simone.rubino@agilebg.com>
2021-11-29 16:10:58 +0100

    [REF] l10n_it_fatturapa_out: Use variables in template instead of duplicating code

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "ITA - Fattura elettronica - Emissione",
-    "version": "14.0.2.0.11",
+    "version": "14.0.2.0.12",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "summary": "Emissione fatture elettroniche",
