a7e2bf0ae484ae79cfd6bad6be1da98437429396
Borruso <gborruso@dinamicheaziendali.it>
2021-12-14 11:29:05 +0100

    [IMP] l10n_it_mis_reports_pl_bs: black, isort, prettier

--- l10n_it_mis_reports_pl_bs/__manifest__.py
+++ l10n_it_mis_reports_pl_bs/__manifest__.py
@@ -1,20 +1,19 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'ITA - MIS builder - Bilancio civilistico',
-    'summary': 'Modelli "MIS Builder" per il conto economico e lo stato patrimoniale',
-    'author': 'Dinamiche Aziendali srl,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-italy',
-    'category': 'Reporting',
-    'version': '12.0.1.0.1',
-    'license': 'AGPL-3',
-    'depends': [
-        'mis_builder',
+    "name": "ITA - MIS builder - Bilancio civilistico",
+    "summary": 'Modelli "MIS Builder" per il conto economico e lo stato patrimoniale',
+    "author": "Dinamiche Aziendali srl," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "category": "Reporting",
+    "version": "12.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": [
+        "mis_builder",
     ],
-    'data': [
-        'data/mis_report_styles.xml',
-        'data/mis_report_pl.xml',
-        'data/mis_report_bs.xml',
+    "data": [
+        "data/mis_report_styles.xml",
+        "data/mis_report_pl.xml",
+        "data/mis_report_bs.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
