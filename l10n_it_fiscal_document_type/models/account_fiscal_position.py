bac6c9495bd36b106140739a2dbdac08bfd412be
Alessandro Camilli <alessandrocamilli@MBPdiAlssandro2.homenet.telecomitalia.it>
2022-01-15 19:06:14 +0100

    [DEL][l10n_it_fiscal_document_type] Get lastest version from l10n-italy

--- l10n_it_fiscal_document_type/models/account_fiscal_position.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class AccountFiscalPosition(models.Model):
-    _inherit = "account.fiscal.position"
-
-    fiscal_document_type_id = fields.Many2one(
-        "fiscal.document.type", string="Fiscal Document Type", readonly=False
-    )
--- l10n_it_fiscal_document_type/readme/DESCRIPTION.rst
+++
@@ -1,7 +0,0 @@
-**Italiano**
-
-Tabella con codici tipo documento previsti dall'Agenzia delle Entrate.
-
-**English**
-
-Document type table with codes as provided for by the Italian Revenue Agency.
