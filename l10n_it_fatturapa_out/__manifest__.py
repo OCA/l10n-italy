e3f2b5c1ab1c86d6af78638e8e5a2c771dae512d
Marco Colombo <marco.colombo@gmail.com>
2022-04-08 12:12:53 +0200

    [FIX] Adjust price in USD for products used in tests
    env.ref("product.product_product_10") is a demo data we use in tests
    
    Prior to commit 86febae278f08864e83017d43f6aa9d67165d664 in odoo
    the original price (14.0 USD) was taken as EUR (from the company
    currency), and converted to 16.38 USD when added to a invoice
    with currency USD. This module used to convert it back to 14.0 EUR.
    
    After 86febae278f08864e83017d43f6aa9d67165d664, the product is added
    to a invoice with currency == USD with the correct price, 14.00,
    which is later converted to 11.97 EUR by this module.

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "ITA - Fattura elettronica - Emissione",
-    "version": "14.0.2.0.15",
+    "version": "14.0.2.0.16",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "summary": "Emissione fatture elettroniche",
