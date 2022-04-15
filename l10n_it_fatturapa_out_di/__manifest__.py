8f3fd3e7a79789c8c428dea0d416c9d7c650f4e6
Marco Colombo <marco.colombo@gmail.com>
2022-04-15 11:06:46 +0200

    [FIX] l10n_it_fatturapa_out_di: fix test tags

--- l10n_it_fatturapa_out_di/__manifest__.py
+++ l10n_it_fatturapa_out_di/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "ITA - Fattura elettronica - Emissione - Dichiarazione d'intento",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "summary": "Dichiarazioni d'intento in fatturapa",
