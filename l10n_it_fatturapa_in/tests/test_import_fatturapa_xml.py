f9be5d8d7547396d3803beb9eeeb5593aad8f653
eLBati <lb@takobi.online>
2022-04-20 13:02:45 +0200

    Avoid check for IntegrityError, assuming _sql_constraints work in generale.
    Otherwise, tests would fail with
    
    odoo/api.py", line 1051, in get
    raise CacheMiss(record, field)
    odoo.exceptions.CacheMiss: ('fatturapa.attachment.in(ID,).xml_supplier_id', None)

--- l10n_it_fatturapa_in/tests/test_import_fatturapa_xml.py
+++ l10n_it_fatturapa_in/tests/test_import_fatturapa_xml.py
@@ -1,26 +1,7 @@
-from psycopg2 import IntegrityError
-
 from datetime import date
 
-from odoo.tools import mute_logger
 from .fatturapa_common import FatturapaCommon
 from odoo.exceptions import UserError
-
-
-class TestDuplicatedAttachment(FatturapaCommon):
-
-    def test_duplicated_attachment(self):
-        """Attachment name must be unique"""
-        # This test breaks the current transaction
-        # and every test executed after this in the
-        # same transaction would fail.
-        # Note that all the tests in TestFatturaPAXMLValidation
-        # are executed in the same transaction.
-        self.run_wizard('test_duplicated', 'IT02780790107_11005.xml')
-        with self.assertRaises(IntegrityError) as ie:
-            with mute_logger('odoo.sql_db'):
-                self.run_wizard('test_duplicated', 'IT02780790107_11005.xml')
-        self.assertEqual(ie.exception.pgcode, '23505')
 
 
 class TestFatturaPAXMLValidation(FatturapaCommon):
