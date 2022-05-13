03cedeb4976ed36148cf688be1d52cceb7ef311b
Simone Rubino <sir@takobi.online>
2022-05-13 18:24:02 +0200

    [IMP] l10n_it_fatturapa_pec: Notify the user with email if XML failed parsing

--- l10n_it_fatturapa_pec/tests/test_e_invoice_response.py
+++ l10n_it_fatturapa_pec/tests/test_e_invoice_response.py
@@ -128,6 +128,7 @@ class TestEInvoiceResponse(EInvoiceCommon):
             instance.retr.return_value = ('', [incoming_mail], '')
 
             with mute_logger(
+                    'odoo.addons.l10n_it_fatturapa_in.models.attachment',
                     'odoo.addons.l10n_it_fatturapa_pec.models.fetchmail'):
                 self.PEC_server.fetch_mail()
 
