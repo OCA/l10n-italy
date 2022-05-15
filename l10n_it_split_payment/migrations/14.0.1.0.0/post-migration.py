ebff80c3a152fed2b782cbcabafc1908da343d66
OmniaSolutions <omniagit@omniasolutions.eu>
2022-05-15 10:04:39 +0200

    Update post-migration.py

--- l10n_it_split_payment/migrations/14.0.1.0.0/post-migration.py
+++ l10n_it_split_payment/migrations/14.0.1.0.0/post-migration.py
@@ -20,7 +20,8 @@ def migrate(cr, installed_version):
         text_vals.add(text_val)
     text_vals = list(text_vals)
     line_ids = MoveLine.search([("name", "in", text_vals)]).ids
-    openupgrade.logged_query(
+    if line_ids:
+        openupgrade.logged_query(
         cr,
         """
 update account_move_line
