3831f9aef3527c5f34088e4a42c9eb0dd4dcdfb5
Simone Rubino <sir@takobi.online>
2022-05-20 13:01:22 +0200

    [IMP] l10n_it_fatturapa: Common objects and views for SdI files

--- l10n_it_fatturapa/controllers/main.py
+++ l10n_it_fatturapa/controllers/main.py
@@ -8,7 +8,9 @@ class FatturaElettronicaController(Controller):
     ], type='http', auth='user', website=True)
     def pdf_preview(self, attachment_id, **data):
         attach = request.env['ir.attachment'].browse(int(attachment_id))
-        html = attach.get_fattura_elettronica_preview()
+        fatturapa_attachment_model = request.env['fatturapa.attachment']
+        html = fatturapa_attachment_model \
+            .get_fattura_elettronica_preview(attach)
         pdf = request.env['ir.actions.report']._run_wkhtmltopdf(
             [html])
 
