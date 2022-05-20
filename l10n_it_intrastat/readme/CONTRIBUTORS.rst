dc876016495195b85fe14442d2cc50c4dec00cfa
Giovanni <giovanni@gslab.it>
2022-05-20 11:52:21 +0200

    * l10n_it_instrastat, fix subject to intrastat when invoicing from sales order

--- l10n_it_intrastat/readme/CONTRIBUTORS.rst
+++ l10n_it_intrastat/readme/CONTRIBUTORS.rst
@@ -3,3 +3,7 @@
 * Lara Baggio <lbaggio@linkgroup.it>
 * Glauco Prina <gprina@linkgroup.it>
 * Sergio Zanchetta <https://github.com/primes2h>
+
+* `Ooops <https://www.ooops404.com>`_:
+
+   * Giovanni Serra <giovanni@gslab.it>
--- l10n_it_intrastat/tests/test_intrastat.py
+++ l10n_it_intrastat/tests/test_intrastat.py
@@ -14,6 +14,8 @@ class TestIntrastat(AccountTestInvoicingCommon):
         cls.partner01 = cls.env.ref("base.res_partner_1")
         cls.product01 = cls.env.ref("product.product_product_10")
         cls.account_account_model = cls.env["account.account"]
+        cls.so_model = cls.env["sale.order"]
+        cls.fp_model = cls.env["account.fiscal.position"]
 
         cls.account_account_receivable = cls.account_account_model.create(
             {
@@ -46,6 +48,20 @@ class TestIntrastat(AccountTestInvoicingCommon):
             .copy(default={"company_id": cls.env.company.id})
         )
 
+    def _create_invoice_from_sale(self, sale):
+        payment = self.env["sale.advance.payment.inv"].create(
+            {"advance_payment_method": "delivered"}
+        )
+        sale_context = {
+            "active_id": sale.id,
+            "active_ids": sale.ids,
+            "active_model": "sale.order",
+            "open_invoices": True,
+        }
+        res = payment.with_context(sale_context).create_invoices()
+        invoice = self.env["account.move"].browse(res["res_id"])
+        return invoice
+
     def test_invoice_totals(self):
         invoice = self.init_invoice(
             "out_invoice",
@@ -64,3 +80,32 @@ class TestIntrastat(AccountTestInvoicingCommon):
             line.amount_currency for line in invoice.intrastat_line_ids
         )
         self.assertEqual(total_intrastat_amount, invoice.amount_untaxed)
+
+    def test_invoice_from_sale(self):
+        self.product01.invoice_policy = "order"
+        self.partner01.property_account_position_id = self.fp_model.create(
+            {
+                "name": "F.P subjected to intrastat",
+                "intrastat": True,
+            }
+        )
+        so = self.so_model.create(
+            {
+                "partner_id": self.partner01.id,
+                "partner_invoice_id": self.partner01.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product01.id,
+                            "product_uom_qty": 1,
+                            "product_uom": self.product01.uom_id.id,
+                        },
+                    ),
+                ],
+            }
+        )
+        so.action_confirm()
+        invoice = self._create_invoice_from_sale(so)
+        self.assertTrue(invoice.intrastat)
