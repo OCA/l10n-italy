408c222f5805fbe35a47bdd6fea1695859c55441
Simone Rubino <sir@takobi.online>
2022-05-23 17:02:07 +0200

    [REF] l10n_it_fatturapa_pec: Use common objects and views for SdI files

--- l10n_it_fatturapa_pec/models/fatturapa_attachment_out.py
+++ l10n_it_fatturapa_pec/models/fatturapa_attachment_out.py
@@ -6,8 +6,6 @@
 import logging
 import re
 
-from lxml import etree
-
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
@@ -20,9 +18,6 @@ RESPONSE_MAIL_REGEX = '[A-Z]{2}[a-zA-Z0-9]{11,16}_[a-zA-Z0-9]{,5}_[A-Z]{2}_' \
 class FatturaPAAttachmentOut(models.Model):
     _inherit = 'fatturapa.attachment.out'
 
-    last_sdi_response = fields.Text(
-        string='Last Response from Exchange System', default='No response yet',
-        readonly=True)
     sending_user = fields.Many2one("res.users", "Sending User", readonly=True)
 
     @api.multi
@@ -31,10 +26,10 @@ class FatturaPAAttachmentOut(models.Model):
         message_dict['res_id'] = 0
 
         regex = re.compile(RESPONSE_MAIL_REGEX)
-        attachments = [x for x in message_dict['attachments']
-                       if regex.match(x.fname)]
+        notifications = [x for x in message_dict['attachments']
+                         if regex.match(x.fname)]
 
-        if not attachments:
+        if not notifications:
             raise UserError(_(
                 "PEC message \"%s\" is coming from SDI but attachments do not "
                 "match SDI response format. Please check."
@@ -42,124 +37,14 @@ class FatturaPAAttachmentOut(models.Model):
                 message_dict['subject']
             ))
 
-        for attachment in attachments:
-            response_name = attachment.fname
-            message_type = response_name.split('_')[2]
-            if attachment.fname.lower().endswith('.zip'):
-                # not implemented, case of AT, todo
-                continue
-            root = etree.fromstring(attachment.content)
-            file_name = root.find('NomeFile')
-            fatturapa_attachment_out = False
+        sdi_channel_model = self.env['sdi.channel']
+        attachments = sdi_channel_model.receive_notification(
+            {
+                notification.fname: notification.content
+                for notification in notifications
+            },
+        )
 
-            if file_name is not None:
-                file_name = file_name.text
-                fatturapa_attachment_out = self.search(
-                    ['|',
-                     ('datas_fname', '=', file_name),
-                     ('datas_fname', '=', file_name.replace('.p7m', ''))])
-                if len(fatturapa_attachment_out) > 1:
-                    _logger.info('More than 1 out invoice found for incoming'
-                                 'message')
-                    fatturapa_attachment_out = fatturapa_attachment_out[0]
-                if not fatturapa_attachment_out:
-                    if message_type == 'MT':  # Metadati
-                        # out invoice not found, so it is an incoming invoice
-                        return message_dict
-                    else:
-                        _logger.info('Error: FatturaPA {} not found.'.format(
-                            file_name))
-                        # TODO Send a mail warning
-                        return message_dict
-
-            if fatturapa_attachment_out:
-                id_sdi = root.find('IdentificativoSdI')
-                receipt_dt = root.find('DataOraRicezione')
-                message_id = root.find('MessageId')
-                id_sdi = id_sdi.text if id_sdi is not None else False
-                receipt_dt = receipt_dt.text if receipt_dt is not None \
-                    else False
-                message_id = message_id.text if message_id is not None \
-                    else False
-                if message_type == 'NS':  # 2A. Notifica di Scarto
-                    error_list = root.find('ListaErrori')
-                    error_str = ''
-                    for error in error_list:
-                        error_str += "\n[%s] %s %s" % (
-                            error.find('Codice').text if error.find(
-                                'Codice') is not None else '',
-                            error.find('Descrizione').text if error.find(
-                                'Descrizione') is not None else '',
-                            error.find('Suggerimento').text if error.find(
-                                'Suggerimento') is not None else ''
-                        )
-                    fatturapa_attachment_out.write({
-                        'state': 'sender_error',
-                        'last_sdi_response': 'SdI ID: {}; '
-                        'Message ID: {}; Receipt date: {}; '
-                        'Error: {}'.format(
-                            id_sdi, message_id, receipt_dt, error_str)
-                    })
-                elif message_type == 'MC':  # 3A. Mancata consegna
-                    missed_delivery_note = root.find('Descrizione').text
-                    fatturapa_attachment_out.write({
-                        'state': 'recipient_error',
-                        'last_sdi_response': 'SdI ID: {}; '
-                        'Message ID: {}; Receipt date: {}; '
-                        'Missed delivery note: {}'.format(
-                            id_sdi, message_id, receipt_dt,
-                            missed_delivery_note)
-                    })
-                elif message_type == 'RC':  # 3B. Ricevuta di Consegna
-                    delivery_dt = root.find('DataOraConsegna').text
-                    fatturapa_attachment_out.write({
-                        'state': 'validated',
-                        'delivered_date': fields.Datetime.now(),
-                        'last_sdi_response': 'SdI ID: {}; '
-                        'Message ID: {}; Receipt date: {}; '
-                        'Delivery date: {}'.format(
-                            id_sdi, message_id, receipt_dt, delivery_dt)
-                    })
-                elif message_type == 'NE':  # 4A. Notifica Esito per PA
-                    esito_committente = root.find('EsitoCommittente')
-                    if esito_committente is not None:
-                        # more than one esito?
-                        esito = esito_committente.find('Esito')
-                        if esito is not None:
-                            if esito.text == 'EC01':
-                                state = 'accepted'
-                            elif esito.text == 'EC02':
-                                state = 'rejected'
-                            fatturapa_attachment_out.write({
-                                'state': state,
-                                'last_sdi_response': 'SdI ID: {}; '
-                                'Message ID: {}; Response: {}; '.format(
-                                    id_sdi, message_id, esito.text)
-                            })
-                elif message_type == 'DT':  # 5. Decorrenza Termini per PA
-                    description = root.find('Descrizione')
-                    if description is not None:
-                        fatturapa_attachment_out.write({
-                            'state': 'validated',
-                            'last_sdi_response': 'SdI ID: {}; '
-                            'Message ID: {}; Receipt date: {}; '
-                            'Description: {}'.format(
-                                id_sdi, message_id, receipt_dt,
-                                description.text)
-                        })
-                # not implemented - todo
-                elif message_type == 'AT':  # 6. Avvenuta Trasmissione per PA
-                    description = root.find('Descrizione')
-                    if description is not None:
-                        fatturapa_attachment_out.write({
-                            'state': 'accepted',
-                            'last_sdi_response': (
-                                'SdI ID: {}; Message ID: {}; Receipt date: {};'
-                                ' Description: {}'
-                            ).format(
-                                id_sdi, message_id, receipt_dt,
-                                description.text)
-                        })
-
-                message_dict['res_id'] = fatturapa_attachment_out.id
+        # Link the message to the last attachment updated
+        message_dict['res_id'] = attachments[-1].id
         return message_dict
