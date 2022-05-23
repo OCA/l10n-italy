daa08928696087026928c86c0bd06ab6774147b6
Simone Rubino <sir@takobi.online>
2022-05-23 14:57:59 +0200

    [REF] l10n_it_fatturapa_pec: Use SdI channel's generic method to receive FE from SdI

--- l10n_it_fatturapa_pec/models/company.py
+++ l10n_it_fatturapa_pec/models/company.py
@@ -4,12 +4,6 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    e_invoice_user_id = fields.Many2one(
-        "res.users",
-        "E-bill creator",
-        help="This user will be used at supplier e-bill creation.",
-        default=lambda self: self.env.user.id,
-    )
     email_from_for_fatturaPA = fields.Char(
         string="Sender Email Address",
         related="sdi_channel_id.pec_server_id.email_from_for_fatturaPA",
@@ -25,10 +19,6 @@ class ResCompany(models.Model):
 class AccountConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    e_invoice_user_id = fields.Many2one(
-        related="company_id.e_invoice_user_id",
-        readonly=False,
-    )
     email_from_for_fatturaPA = fields.Char(
         string="Sender Email Address",
         related="company_id.sdi_channel_id.pec_server_id.email_from_for_fatturaPA",
