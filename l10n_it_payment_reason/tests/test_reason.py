78bce3d8fee16b524e34f22b4176b48855e4d018
Marco Colombo <marco.colombo@gmail.com>
2022-05-27 17:27:53 +0200

    [IMP] verifica codice con Schema dello SdI per fatturapa

--- l10n_it_payment_reason/tests/test_reason.py
+++ l10n_it_payment_reason/tests/test_reason.py
@@ -1,6 +1,8 @@
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 
+from odoo.addons.l10n_it_account.tools.account_tools import fpa_schema_get_enum
+
 
 class TestReasons(TransactionCase):
     def setUp(self):
@@ -21,3 +23,19 @@ class TestReasons(TransactionCase):
                 )
             ],
         )
+
+    def test_compare_with_fpa_schema(self):
+        """Check that the values we define in this module are
+        the same as those defined in FPA xsd"""
+
+        my_codes = self.reason_model.search([]).mapped("code")
+
+        # from fatturapa xml Schema
+        xsd_codes = [
+            code for code, descr in fpa_schema_get_enum("CausalePagamentoType")
+        ]
+
+        # XXX hardcoded - obsolete code, that is still supported by Schema
+        xsd_codes.remove("Z")
+
+        self.assertCountEqual(my_codes, xsd_codes)
