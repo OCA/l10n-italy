4a1f20a0183be16e95852f0140a3f2a12757a735
Simone Rubino <sir@takobi.online>
2022-06-21 12:15:03 +0200

    [IMP] l10n_it_fatturapa_out: Allow a specific group to force attachment.out state The group and its view are migrated from l10n_it_fatturapa_pec

---
+++ l10n_it_fatturapa_out/migrations/16.0.1.3.0/pre-migration.py
@@ -0,0 +1,22 @@
+#  Copyright 2022 Simone Rubino - TAKOBI
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+def migrate(cr, version):
+    # Move the group and its view from l10n_it_fatturapa_pec
+    openupgrade.rename_xmlids(
+        cr,
+        [
+            (
+                "l10n_it_fatturapa_pec.group_force_e_inv_export_state",
+                "l10n_it_fatturapa_out.group_force_e_inv_export_state",
+            ),
+            (
+                "l10n_it_fatturapa_pec"
+                ".view_fatturapa_out_pec_attachment_form_statusbar",
+                "l10n_it_fatturapa_out.fatturapa_attachment_out_form_statusbar",
+            ),
+        ],
+    )
