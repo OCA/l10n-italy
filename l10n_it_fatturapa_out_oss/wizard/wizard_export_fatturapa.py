7bd7fedbb2b4aee57f00cbb28226f5447890b210
eLBati <lb@takobi.online>
2022-06-21 12:10:34 +0200

    REF l10n_it_fatturapa_out_oss using existing field oss_country_id and removing oss_year_ids: applicability of OSS taxes should be done upstream

--- l10n_it_fatturapa_out_oss/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out_oss/wizard/wizard_export_fatturapa.py
@@ -17,10 +17,7 @@ class WizardExportFatturapa(models.TransientModel):
             line_no, line, body, price_precision, uom_precision)
 
         invoice_line_tax = line.invoice_line_tax_ids[0]
-        if invoice_line_tax.oss_subjected and line.invoice_id.company_id.oss_year_ids.\
-            filtered(
-                lambda x: x.year == str(line.invoice_id.date.year) and x.oss_subjected
-                ) and invoice_line_tax.kind_id:
+        if invoice_line_tax.oss_country_id and invoice_line_tax.kind_id:
             dati_gestionali = AltriDatiGestionaliType(
                 TipoDato="OSS",
                 RiferimentoTesto=encode_for_export(
@@ -34,17 +31,14 @@ class WizardExportFatturapa(models.TransientModel):
 
     def setDatiRiepilogo(self, invoice, body):
         super().setDatiRiepilogo(invoice, body)
-
-        if invoice.company_id.oss_year_ids.filtered(
-                lambda x: x.year == str(invoice.date.year) and x.oss_subjected):
-            for line in invoice.invoice_line_ids:
-                if any(
-                    y.oss_subjected and y.kind_id for y in line.invoice_line_tax_ids
-                ):
-                    body.DatiBeniServizi.DatiRiepilogo[0].Imposta = '0.00'
-                    body.DatiBeniServizi.DatiRiepilogo[0].AliquotaIVA = '0.00'
-                    body.DatiBeniServizi.DatiRiepilogo[0].Natura = \
-                        line.invoice_line_tax_ids[0].kind_id.code
-                    body.DatiBeniServizi.DatiRiepilogo[0].RiferimentoNormativo = \
-                        encode_for_export(
-                            line.invoice_line_tax_ids[0].law_reference, 100)
+        for line in invoice.invoice_line_ids:
+            if any(
+                y.oss_country_id and y.kind_id for y in line.invoice_line_tax_ids
+            ):
+                body.DatiBeniServizi.DatiRiepilogo[0].Imposta = '0.00'
+                body.DatiBeniServizi.DatiRiepilogo[0].AliquotaIVA = '0.00'
+                body.DatiBeniServizi.DatiRiepilogo[0].Natura = \
+                    line.invoice_line_tax_ids[0].kind_id.code
+                body.DatiBeniServizi.DatiRiepilogo[0].RiferimentoNormativo = \
+                    encode_for_export(
+                        line.invoice_line_tax_ids[0].law_reference, 100)
