f11b1b400e2a2d0fe5def25c663e1822c48b13ee
Alberto Re <alberto.re@agilebg.com>
2022-06-28 16:25:47 +0200

    [FIX] l10n_it_fatturapa_out: prevent reset to draft if XML is present

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "ITA - Fattura elettronica - Emissione",
-    "version": "14.0.2.1.0",
+    "version": "14.0.2.1.1",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "summary": "Emissione fatture elettroniche",
