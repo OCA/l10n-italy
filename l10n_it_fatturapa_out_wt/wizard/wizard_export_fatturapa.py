61722eb00d857238724e8c5a510004932a485ff1
Ciro Urselli <c.urselli@apuliasoftware.it>
2022-06-29 15:17:31 +0200

    [IMP] improve and refactor function out_wt

--- l10n_it_fatturapa_out_wt/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out_wt/wizard/wizard_export_fatturapa.py
@@ -1,10 +1,63 @@
-from odoo import models
+from odoo import models, api
+from odoo.tools.float_utils import float_round
+from odoo.addons.l10n_it_account.tools.account_tools import encode_for_export
+from odoo.addons.l10n_it_fatturapa_out.wizard.wizard_export_fatturapa import format_numbers
 
-from .efattura import EFatturaOut
+WT_TAX_CODE = {
+    "inps": "RT03", 
+    "enasarco": "RT04", 
+    "enpam": "RT05", 
+    "other": "RT06"
+}
 
+def get_withholding_type(wt_types, partner):
+        if wt_types == "ritenuta":
+            if partner.is_company:
+                withholding_type = "RT02"
+            else:
+                withholding_type = "RT01"
+        else:
+            withholding_type = WT_TAX_CODE[wt_types]
+        return withholding_type
 
 class WizardExportFatturapa(models.TransientModel):
+
     _inherit = "wizard.export.fatturapa"
 
-    def _get_efattura_class(self):
-        return EFatturaOut
+    @api.model
+    def getAllTaxes(self, invoice):
+        res = super(WizardExportFatturapa, self).getAllTaxes(invoice)
+        wt_lines_to_write = invoice.withholding_tax_line_ids.filtered(
+            lambda x: x.withholding_tax_id.wt_types not in ("ritenuta", "other")
+            and x.withholding_tax_id.use_daticassaprev
+        )
+        for wt_line in wt_lines_to_write:
+            tax_id = wt_line.withholding_tax_id.daticassprev_tax_id
+            key = "{}_{}".format("0.00", tax_id.kind_id.code)
+            if key in res:
+                base_amount = float(res[key]["ImponibileImporto"]) + wt_line.tax
+                res[key]["ImponibileImporto"] = float_round(base_amount, 2)
+            else:
+                res[key] = {
+                    "AliquotaIVA": format_numbers(0.0),
+                    "Natura": tax_id.kind_id.code,
+                    # 'Arrotondamento':'',
+                    "ImponibileImporto": float_round(wt_line.tax, 2),
+                    "Imposta": 0.0,
+                    "EsigibilitaIVA": tax_id.payability,
+                }
+                if tax_id.law_reference:
+                    res[key]["RiferimentoNormativo"] = encode_for_export(
+                        tax_id.law_reference, 100
+                    )
+        return res
+
+    @api.model
+    def getTemplateValues(self, template_values):
+        """
+        Entry point for other modules to override values
+        (and helper functions) passed to template
+        """
+        template_values = super().getTemplateValues(template_values)
+        template_values.update({"get_withholding_type": get_withholding_type})
+        return template_values
