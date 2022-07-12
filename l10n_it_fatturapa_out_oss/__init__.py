98d5983b818bf6f945a0349cd392ceffeca8cce7
eLBati <lb@takobi.online>
2022-07-12 15:08:41 +0200

    ADD riferimento normativo to OSS taxes

--- l10n_it_fatturapa_out_oss/__init__.py
+++ l10n_it_fatturapa_out_oss/__init__.py
@@ -6,5 +6,6 @@ def _l10n_it_fatturapa_out_oss_post_init(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     oss_taxes = env["account.tax"].search([("oss_country_id", "!=", False)])
     oss_taxes.write({
-        "kind_id": env.ref("l10n_it_account_tax_kind.n3_2").id
+        "kind_id": env.ref("l10n_it_account_tax_kind.n3_2").id,
+        "law_reference": "Art. 41 D.L. 331/1993",
     })
