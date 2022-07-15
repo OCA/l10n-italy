4aa0479ccea8daf3622907fdb24e38cbf5dcfe92
Marco Colombo <marco.colombo@gmail.com>
2022-07-15 15:54:31 +0200

    [14.0][IMP] l10n_it_fatturapa_out_rc: make template data overridable

--- l10n_it_fatturapa_out_rc/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out_rc/wizard/wizard_export_fatturapa.py
@@ -1,17 +1,12 @@
 # Copyright 2021 Alex Comba - Agile Business Group
+# Copyright 2022 Marco Colombo - Phi srl - <marco.colombo@phi.technology>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
 
-from .efattura import EFatturaOut
-
 
 class WizardExportFatturapa(models.TransientModel):
     _inherit = "wizard.export.fatturapa"
-
-    @api.model
-    def _get_efattura_class(self):
-        return EFatturaOut
 
     @api.model
     def getSign(self, invoice):
@@ -22,6 +17,21 @@ class WizardExportFatturapa(models.TransientModel):
         ] and invoice.fiscal_document_type_id.code not in ["TD04", "TD08"]:
             sign = -1
         return sign
+
+    @api.model
+    def getTemplateValues(self, template_values):
+        template_values = super().getTemplateValues(template_values)
+        template_values.update({"get_sign": self.getSign})
+        return template_values
+
+    @api.model
+    def getPayments(self, invoice):
+        payments = super().getPayments(invoice)
+        sign = self.getSign(invoice)
+        for payment in payments:
+            payment.amount_currency *= sign
+            payment.debit *= sign
+        return payments
 
     @api.model
     def getImportoTotale(self, invoice):
