d77a894257b097c1b62b08a2b3a40e0f171816a8
Marco Colombo <marco.colombo@gmail.com>
2022-07-15 15:53:28 +0200

    [14.0][IMP] l10n_it_fatturapa_out: make template data overridable

--- l10n_it_fatturapa_out/__manifest__.py
+++ l10n_it_fatturapa_out/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "ITA - Fattura elettronica - Emissione",
-    "version": "14.0.2.1.1",
+    "version": "14.0.2.2.0",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "summary": "Emissione fatture elettroniche",
