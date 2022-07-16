5080b81f6e196ebe36d469955a38d14bb11bff2e
Marco Colombo <marco.colombo@gmail.com>
2022-07-16 12:04:11 +0200

    [14.0][FIX] l10n_it_reverse_charge: porting mistake
    'name' is now what 'number' used to be. We should use 'ref'

--- l10n_it_reverse_charge/__manifest__.py
+++ l10n_it_reverse_charge/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "ITA - Inversione contabile",
-    "version": "14.0.1.1.1",
+    "version": "14.0.1.1.2",
     "category": "Localization/Italy",
     "summary": "Inversione contabile",
     "author": "Odoo Italia Network, Odoo Community Association (OCA)",
