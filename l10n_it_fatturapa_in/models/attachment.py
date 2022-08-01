e273076335bf46fbf83db65ec9006cfcfd57cbf4
eLBati <lb@takobi.online>
2022-08-01 11:14:58 +0200

    [8.0][FIX] l10n_it_fatturapa_in: do not block upgrade in case of XML files with exceptions

--- l10n_it_fatturapa_in/models/attachment.py
+++ l10n_it_fatturapa_in/models/attachment.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import base64
+import logging
 from openerp import fields, models, api, _
 from openerp.exceptions import ValidationError
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT as DF
+
+_logger = logging.getLogger(__name__)
 
 SELF_INVOICE_TYPES = ("TD16", "TD17", "TD18", "TD19", "TD20", "TD21")
 
@@ -82,23 +86,40 @@ class FatturaPAAttachmentIn(models.Model):
     @api.depends('ir_attachment_id.datas')
     def _compute_xml_data(self):
         for att in self:
+            att.xml_supplier_id = False
+            att.invoices_number = 0
+            att.invoices_total = 0
+            att.invoices_date = False
+            att.is_self_invoice = False
             wiz_obj = self.env['wizard.import.fatturapa'] \
                 .with_context(from_attachment=att)
-            fatt = wiz_obj.get_invoice_obj(att)
-            cedentePrestatore = fatt.FatturaElettronicaHeader.CedentePrestatore
-            partner_id = wiz_obj.getCedPrest(cedentePrestatore)
-            att.xml_supplier_id = partner_id
-            att.invoices_number = len(fatt.FatturaElettronicaBody)
-            att.invoices_total = 0
-            att.is_self_invoice = False
-            for invoice_body in fatt.FatturaElettronicaBody:
-                att.invoices_total += float(
-                    invoice_body.DatiGenerali.DatiGeneraliDocumento.
-                    ImportoTotaleDocumento or 0
+            try:
+                fatt = wiz_obj.get_invoice_obj(att)
+                cedentePrestatore = fatt.FatturaElettronicaHeader.CedentePrestatore
+                partner_id = wiz_obj.getCedPrest(cedentePrestatore)
+                att.xml_supplier_id = partner_id
+                att.invoices_number = len(fatt.FatturaElettronicaBody)
+                att.invoices_total = 0
+                att.is_self_invoice = False
+                invoices_date = []
+                for invoice_body in fatt.FatturaElettronicaBody:
+                    att.invoices_total += float(
+                        invoice_body.DatiGenerali.DatiGeneraliDocumento.
+                        ImportoTotaleDocumento or 0
+                    )
+                    invoice_date = invoice_body.DatiGenerali\
+                        .DatiGeneraliDocumento.Data.strftime(DF)
+                    if invoice_date not in invoices_date:
+                        invoices_date.append(invoice_date)
+                    if invoice_body.DatiGenerali.DatiGeneraliDocumento\
+                            .TipoDocumento in SELF_INVOICE_TYPES:
+                        att.is_self_invoice = True
+                att.invoices_date = ' '.join(invoices_date)
+            except Exception as e:
+                _logger.error(
+                    _("Impossible to execute _compute_xml_data for %s: %s")
+                    % (att.display_name, e)
                 )
-                if invoice_body.DatiGenerali.DatiGeneraliDocumento.TipoDocumento \
-                        in SELF_INVOICE_TYPES:
-                    att.is_self_invoice = True
 
     @api.multi
     @api.depends('in_invoice_ids', 'invoices_number')
