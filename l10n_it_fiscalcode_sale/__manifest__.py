e0907472c2e5c305b014d9f95381c1d467a12d3c
Simone Rubino <sir@takobi.online>
2022-08-05 16:05:16 +0200

    Manually fix manifest website key

--- l10n_it_fiscalcode_sale/__manifest__.py
+++ l10n_it_fiscalcode_sale/__manifest__.py
@@ -6,7 +6,8 @@
     "version": "12.0.1.0.0",
     "development_status": "Beta",
     "category": "Hidden",
-    "website": "https://github.com/OCA/l10n-italy",
+    "website": "https://github.com/OCA/l10n-italy"
+               "/tree/12.0/l10n_it_fiscalcode_sale",
     "author": "TAKOBI, Nextev Srl, Odoo Community Association (OCA)",
     "maintainers": ["eLBati"],
     "license": "AGPL-3",
