25f5ddd916a640dc03a5f049c3f30268aa0f4167
Simone Rubino <sir@takobi.online>
2022-08-05 17:27:06 +0200

    Upgrade development status due to Beta dependencies Pre-commit error is: l10n_it_account_tax_kind (Production/Stable) depends on l10n_it_account (Beta) l10n_it_causali_pagamento (Production/Stable) depends on l10n_it_account (Beta) l10n_it_vat_registries (Production/Stable) depends on account_tax_balance (Beta) l10n_it_vat_registries (Production/Stable) depends on l10n_it_account (Beta)

--- l10n_it_account/__manifest__.py
+++ l10n_it_account/__manifest__.py
@@ -9,6 +9,7 @@
     'name': 'ITA - Contabilità base',
     'summary': 'Modulo base usato come dipendenza di altri moduli contabili',
     'version': '12.0.1.4.5',
+    "development_status": "Production/Stable",
     'category': 'Hidden',
     'author': "Agile Business Group, Abstract, "
               "Odoo Community Association (OCA)",
