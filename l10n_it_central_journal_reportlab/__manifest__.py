e80b7129b2e87f586e2f111203a0a0820699ac48
Borruso <gborruso@dinamicheaziendali.it>
2022-09-02 18:09:34 +0200

    [ADD] l10n_it_central_journal_reportlab: aggiunto modulo libro giornale stampa reportlab

---
+++ l10n_it_central_journal_reportlab/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Gianmarco Conte (gconte@dinamicheaziendali.it)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "ITA - Libro giornale - Reportlab",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Localization/Italy",
+    "author": "Gianmarco Conte - Dinamiche Aziendali srl, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "AGPL-3",
+    "depends": ["account", "date_range"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/print_giornale.xml",
+        "views/date_range_view.xml",
+    ],
+    "installable": True,
+}
