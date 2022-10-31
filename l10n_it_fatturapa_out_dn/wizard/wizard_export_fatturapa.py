ac10ab4ff1cd55103556c5713b05e7eec857c11e
Simone Rubino <sir@takobi.online>
2022-10-31 16:04:09 +0100

    [REF] l10n_it_fatturapa_out_dn: Move logic to Python

--- l10n_it_fatturapa_out_dn/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out_dn/wizard/wizard_export_fatturapa.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Marco Colommbo
+# Copyright 2022 Simone Rubino - TAKOBI
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
@@ -25,3 +26,56 @@ class WizardExportFatturapa(models.TransientModel):
         help="Include DN data: The field must be entered when a transport "
         "document associated with a deferred invoice is present",
     )
+
+    def getDatiDDT(self, invoice):
+        """
+        Get the data for rendering DatiDDT.
+
+        :return: a list of dictionaries, with one dictionary per involved DdT.
+        Each dictionary has shape:
+        {
+            '_delivery_note': <stock.delivery.note record of the involved the DdT>,
+
+            'NumeroDDT': <string representing the DdT>,
+
+            'DataDDT': <date of the DdT>,
+
+            '_invoice_lines': (optional)
+                <account.move.line records of invoice lines involved in the DdT>,
+            'RiferimentoNumeroLinea': (optional)
+                <string containing numbers of the invoice lines involved in the DdT>,
+        }
+        """
+        self.ensure_one()
+        dati_ddt_list = list()
+        for delivery_note in invoice.delivery_note_ids:
+            ddt_data = {
+                "_delivery_note": delivery_note,
+                "NumeroDDT": delivery_note.name,
+                "DataDDT": delivery_note.date,
+            }
+            e_invoice_lines = self.get_e_invoice_lines(invoice)
+            e_invoice_delivery_note_lines = e_invoice_lines.filtered(
+                lambda l: l.delivery_note_id == delivery_note
+            )
+            if e_invoice_delivery_note_lines:
+                # RiferimentoNumeroLinea should not be populated
+                # if all the lines of the invoice
+                # are linked to this delivery_note,
+                # but we can't verify this condition
+                # because only note lines (not product lines)
+                # are linked to a delivery note.
+                e_invoice_lines_list = list(e_invoice_lines)
+                lines_refs_list = [
+                    e_invoice_lines_list.index(line) + 1  # NumeroLinea is 1-based
+                    for line in e_invoice_delivery_note_lines
+                ]
+                ddt_data.update(
+                    {
+                        "_invoice_lines": e_invoice_delivery_note_lines,
+                        "RiferimentoNumeroLinea": lines_refs_list,
+                    }
+                )
+
+            dati_ddt_list.append(ddt_data)
+        return dati_ddt_list
