6c302b02eb1ec575e796f7204db38bc2315be29b
Marco Colombo <marco.colombo@gmail.com>
2022-11-02 00:06:12 +0100

    [MIG] l10n_it_account_tax_kind: Migration to 16.0

--- l10n_it_account_tax_kind/model/account_tax_kind.py
+++ l10n_it_account_tax_kind/model/account_tax_kind.py
@@ -1,28 +1,16 @@
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class AccountTaxKind(models.Model):
     _name = "account.tax.kind"
     _description = "Tax exemption kind"
+    _rec_names_search = ["code", "name"]
 
-    code = fields.Char(string="Code", size=4, required=True)
-    name = fields.Char(string="Name", required=True)
+    code = fields.Char(size=4, required=True)
+    name = fields.Char(required=True)
 
     def name_get(self):
         res = []
         for tax_kind in self:
             res.append((tax_kind.id, "[{}] {}".format(tax_kind.code, tax_kind.name)))
         return res
-
-    @api.model
-    def name_search(self, name="", args=None, operator="ilike", limit=100):
-        if not args:
-            args = []
-        if name:
-            records = self.search(
-                ["|", ("name", operator, name), ("code", operator, name)] + args,
-                limit=limit,
-            )
-        else:
-            records = self.search(args, limit=limit)
-        return records.name_get()
