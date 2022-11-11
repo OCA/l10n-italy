9ab2ef4e94de80c083292b41c8cb238a395d2456
Arcadio <pinto.a@stesi.eu>
2022-11-11 14:29:18 +0100

    Aggiunta modulo l10n_it_fatturapa_out_di_edi

--- l10n_it_fatturapa_out_di_edi/__manifest__.py
+++ l10n_it_fatturapa_out_di_edi/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2021 Marco Colombo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "ITA - Fattura elettronica - Emissione - Dichiarazione d'intento",
-    "version": "14.0.1.0.2",
+    "name": "ITA - Fattura elettronica - Emissione - Dichiarazione d'intento EDI",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "summary": "Dichiarazioni d'intento in fatturapa",
@@ -10,7 +10,7 @@
     "website": "https://github.com/OCA/l10n-italy",
     "license": "AGPL-3",
     "depends": [
-        "l10n_it_fatturapa_out",
+        "l10n_it_edi",
         "l10n_it_declaration_of_intent",
     ],
     "data": [
