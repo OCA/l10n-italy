56856cbc7ce012081f1d280ff8f1fd2c4a8a35a6
sergiocorato <sergiocorato@gmail.com>
2022-12-02 10:13:15 +0100

    [IMP] assets_management: black, isort, prettier

--- assets_management/wizard/asset_generate_depreciation.py
+++ assets_management/wizard/asset_generate_depreciation.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models
 
 
 class WizardAssetsGenerateDepreciations(models.TransientModel):
-    _name = 'wizard.asset.generate.depreciation'
+    _name = "wizard.asset.generate.depreciation"
     _description = "Generate Asset Depreciations"
 
     @api.model
@@ -15,10 +15,8 @@ class WizardAssetsGenerateDepreciations(models.TransientModel):
 
     @api.model
     def get_default_date_dep(self):
-        fiscal_year = self.env['account.fiscal.year'].get_fiscal_year_by_date(
-            fields.Date.today(),
-            company=self.env.user.company_id,
-            miss_raise=False
+        fiscal_year = self.env["account.fiscal.year"].get_fiscal_year_by_date(
+            fields.Date.today(), company=self.env.user.company_id, miss_raise=False
         )
         if fiscal_year:
             return fiscal_year.date_to
@@ -26,20 +24,20 @@ class WizardAssetsGenerateDepreciations(models.TransientModel):
 
     @api.model
     def get_default_type_ids(self):
-        return [(6, 0, self.env['asset.depreciation.type'].search([]).ids)]
+        return [(6, 0, self.env["asset.depreciation.type"].search([]).ids)]
 
     asset_ids = fields.Many2many(
-        'asset.asset',
+        "asset.asset",
         string="Assets",
     )
 
     category_ids = fields.Many2many(
-        'asset.category',
+        "asset.category",
         string="Categories",
     )
 
     company_id = fields.Many2one(
-        'res.company',
+        "res.company",
         default=get_default_company_id,
         string="Company",
     )
@@ -51,10 +49,10 @@ class WizardAssetsGenerateDepreciations(models.TransientModel):
     )
 
     type_ids = fields.Many2many(
-        'asset.depreciation.type',
+        "asset.depreciation.type",
         default=get_default_type_ids,
         required=True,
-        string="Depreciation Types"
+        string="Depreciation Types",
     )
 
     @api.multi
@@ -69,28 +67,25 @@ class WizardAssetsGenerateDepreciations(models.TransientModel):
         deps = self.get_depreciations().with_context(dep_date=self.date_dep)
         dep_lines = deps.generate_depreciation_lines(self.date_dep)
         deps.post_generate_depreciation_lines(dep_lines)
-        if self._context.get('reload_window'):
-            return {
-                'type': 'ir.actions.client',
-                'tag': 'reload'
-            }
+        if self._context.get("reload_window"):
+            return {"type": "ir.actions.client", "tag": "reload"}
 
     def get_depreciations(self):
         self.ensure_one()
         domain = self.get_depreciations_domain()
-        return self.env['asset.depreciation'].search(domain)
+        return self.env["asset.depreciation"].search(domain)
 
     def get_depreciations_domain(self):
         domain = [
-            ('amount_residual', '>', 0),
-            ('date_start', '!=', False),
-            ('date_start', '<', self.date_dep),
-            ('type_id', 'in', self.type_ids.ids),
+            ("amount_residual", ">", 0),
+            ("date_start", "!=", False),
+            ("date_start", "<", self.date_dep),
+            ("type_id", "in", self.type_ids.ids),
         ]
         if self.asset_ids:
-            domain += [('asset_id', 'in', self.asset_ids.ids)]
+            domain += [("asset_id", "in", self.asset_ids.ids)]
         if self.category_ids:
-            domain += [('asset_id.category_id', 'in', self.category_ids.ids)]
+            domain += [("asset_id.category_id", "in", self.category_ids.ids)]
         if self.company_id:
-            domain += [('company_id', '=', self.company_id.id)]
+            domain += [("company_id", "=", self.company_id.id)]
         return domain
