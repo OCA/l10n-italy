c299a43d4e31adcee9b18bd94548cf3b01859b3d
Borruso <gborruso@dinamicheaziendali.it>
2022-12-02 13:46:59 +0100

    [MIG] l10n_it_pos_fiscalcode: Migration to 16.0

--- l10n_it_pos_fiscalcode/__manifest__.py
+++ l10n_it_pos_fiscalcode/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "ITA - POS - Codice fiscale",
     "summary": "Gestione codice fiscale del cliente all'interno "
     "dell'interfaccia del POS",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Point Of Sale",
     "website": "https://github.com/OCA/l10n-italy",
@@ -18,8 +18,9 @@
         "point_of_sale",
         "l10n_it_fiscalcode",
     ],
-    "qweb": ["static/src/xml/pos.xml"],
-    "data": [
-        "views/assets.xml",
-    ],
+    "assets": {
+        "point_of_sale.assets": [
+            "l10n_it_pos_fiscalcode/static/src/xml/pos.xml",
+        ],
+    },
 }
