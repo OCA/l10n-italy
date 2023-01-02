a03fc547382476e99d9de3c565be5b24904467b1
Simone Rubino <sir@takobi.online>
2023-01-02 11:45:12 +0100

    [REF] l10n_it_fatturapa_out_welfare: In tests, avoid sequence conflict

--- l10n_it_fatturapa_out_welfare/__manifest__.py
+++ l10n_it_fatturapa_out_welfare/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "ITA - Fattura elettronica - Cassa previdenziale",
     "summary": "Registrazione della cassa previdenziale "
                "nelle fatture elettroniche in uscita",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "development_status": "Beta",
     "category": "Localization/Italy",
     "website": "https://github.com/OCA/l10n-italy",
