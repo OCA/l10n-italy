bf5f8c7c418990b0c647c5c9a8ca0aa8f37d9548
Simone Rubino <sir@takobi.online>
2023-01-04 11:25:10 +0100

    [IMP] l10n_it_fatturapa_sdicoop: pre-commit execution

--- l10n_it_fatturapa_sdicoop/__manifest__.py
+++ l10n_it_fatturapa_sdicoop/__manifest__.py
@@ -2,20 +2,20 @@
 #  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'ITA - Fattura elettronica - Supporto SDICoop',
-    'version': '12.0.2.2.0',
-    'category': 'Localization/Italy',
-    'summary': 'Invio fatture elettroniche tramite SDICoop',
-    'author': 'TAKOBI, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-italy'
-               '/tree/12.0/l10n_it_fatturapa_sdicoop',
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_it_fatturapa_out',
-        'l10n_it_fatturapa_in',
-        'l10n_it_sdi_channel',
+    "name": "ITA - Fattura elettronica - Supporto SDICoop",
+    "version": "12.0.2.2.0",
+    "category": "Localization/Italy",
+    "summary": "Invio fatture elettroniche tramite SDICoop",
+    "author": "TAKOBI, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy"
+    "/tree/12.0/l10n_it_fatturapa_sdicoop",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_fatturapa_out",
+        "l10n_it_fatturapa_in",
+        "l10n_it_sdi_channel",
     ],
-    'data': [
-        'data/sdi_channel_data.xml',
+    "data": [
+        "data/sdi_channel_data.xml",
     ],
 }
