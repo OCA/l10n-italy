69bd6e800f573b8635df7f76a6472297eba0a25f
Tony Masci <45100916+TonyMasciI@users.noreply.github.com>
2023-01-13 10:48:49 +0100

    [MIG] l10n_it_ricevute_bancarie: Migration to 16.0

---
+++ l10n_it_riba/report/slip_qweb.py
@@ -0,0 +1,18 @@
+# © 2016 Andrea Cometa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class SlipReportQweb(models.AbstractModel):
+
+    _name = "report.l10n_it_riba.slip_qweb"
+    _description = "RiBa Slip Report"
+
+    def _get_report_values(self, docids, data=None):
+        return {
+            "doc_ids": docids,
+            "doc_model": "riba.slip",
+            "docs": self.env["riba.slip"].browse(docids),
+            "data": data,
+        }
