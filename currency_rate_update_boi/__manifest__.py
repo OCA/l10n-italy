c6d8d142ab1fe1a3d93fe9338c68e73921d698f5
Roberto Fichera <robyf@tekno-soft.it>
2023-02-17 14:41:36 +0100

    [14.0][MIG] currency_rate_update_boi: Migration to 14.0

--- currency_rate_update_boi/__manifest__.py
+++ currency_rate_update_boi/__manifest__.py
@@ -2,22 +2,19 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Currency Rate Update: Banca d\'Italia',
-    'version': '12.0.1.0.0',
-    'category': 'Financial Management/Configuration',
-    'summary': 'Update exchange rates using www.bancaditalia.it',
-    'author':
-        'TAKOBI, '
-        'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-italy'
-               '/tree/12.0/currency_rate_update_boi',
-    'maintainers': ['eLBati'],
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'depends': [
-        'currency_rate_update',
+    "name": "Currency Rate Update: Banca d'Italia",
+    "version": "14.0.1.0.0",
+    "category": "Financial Management/Configuration",
+    "summary": "Update exchange rates using www.bancaditalia.it",
+    "author": "TAKOBI, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy"
+    "/tree/12.0/currency_rate_update_boi",
+    "maintainers": ["eLBati"],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "currency_rate_update",
     ],
-    'data': [
-    ],
+    "data": [],
 }
