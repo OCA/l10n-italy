04f4b093e277f3951bbfa54fb4165c6b117761fa
matteo.tognini <matteo.tognini@nextev.it>
2023-03-06 10:33:02 +0100

    [12.0][ADD] add l10n_it_riba_sale_commission module

---
+++ l10n_it_riba_sale_commission/wizard/wizard_riba_commissions_settle.py
@@ -0,0 +1,38 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+from datetime import date, timedelta
+
+
+class SaleCommissionMakeSettle(models.TransientModel):
+    _inherit = "sale.commission.make.settle"
+
+    def _get_agent_lines(self, agent, date_to_agent):
+        """
+            Exclude outstanding invoices, those with Ri.Ba subject to collection payment
+            if at least safety days haven't passed since expiration date and those that
+            have manually set the flag 'no_commission' (for example if it has been
+            outstanding for years now).
+        """
+        # rimuove le righe delle fatture che hanno impostato flag "no_commission"
+        # e che abbiano Ri.Ba come metodo di pagamento da quelle recuperate
+        # con il metodo originale
+        agent_lines = (
+            super()
+            ._get_agent_lines(agent, date_to_agent)
+            .filtered(lambda al: not al.invoice_id.no_commission)
+            .filtered(lambda r: r.invoice_payment_term_id.riba)
+        )
+        for line in agent_lines:
+            riba_mv_line = self.env['riba.distinta.move.line'].search([
+                ('move_line_id.invoice_id', '=', line.invoice.id)
+            ])
+            riba_type = riba_mv_line.riba_line_id.type
+            if line.commission.invoice_state == 'paid' and (
+                    line.invoice.is_unsolved or (
+                    (line.invoice.date_due + timedelta(
+                        days=riba_mv_line.riba_line_id.config_id.safety_days)
+                        > date.today())
+                    and riba_type == 'sbf')):
+                agent_lines -= line
+        return agent_lines
