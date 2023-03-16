a2f7de948e34b36c5538b0d37ec065df08c3d65d
Alberto Carollo <baba75@gmail.com>
2023-03-16 10:25:55 +0100

    [IMP] l10n_it_website_sale_fiscalcode: black, isort, prettier

--- l10n_it_website_sale_fiscalcode/__manifest__.py
+++ l10n_it_website_sale_fiscalcode/__manifest__.py
@@ -3,21 +3,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Website Sale FiscalCode',
-    'category': 'e-commerce',
-    'author': "Agile Business Group,"
-              "Odoo Community Association (OCA)",
-    'version': '12.0.1.1.3',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/l10n-italy'
-               '/tree/12.0/l10n_it_website_sale_fiscalcode',
-    'depends': [
-        'website_sale',
-        'l10n_it_fiscalcode'
+    "name": "Website Sale FiscalCode",
+    "category": "e-commerce",
+    "author": "Agile Business Group," "Odoo Community Association (OCA)",
+    "version": "12.0.1.1.3",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-italy"
+    "/tree/12.0/l10n_it_website_sale_fiscalcode",
+    "depends": ["website_sale", "l10n_it_fiscalcode"],
+    "data": [
+        "views/templates.xml",
     ],
-    'data': [
-        'views/templates.xml',
-    ],
-    'installable': True,
-    'auto_install': True,
+    "installable": True,
+    "auto_install": True,
 }
