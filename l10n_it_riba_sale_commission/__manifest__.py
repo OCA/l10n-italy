187bd0fd0699f3afc8ee5308843fe6b07942d25b
odooNextev <odoo@nextev.it>
2023-03-22 08:56:09 +0100

    [14.0][ADD] l10n_it_riba_sale_commission

---
+++ l10n_it_riba_sale_commission/__manifest__.py
@@ -0,0 +1,21 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "ITA - Integrazione tra RiBa e provvigioni su vendite",
+    "summary": "Modulo ponte tra provvigioni agenti e RiBa",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Localization/Italy",
+    "website": "https://github.com/OCA/l10n-italy",
+    "author": "Nextev Srl, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_commission",
+        "l10n_it_ricevute_bancarie",
+    ],
+    "data": [
+        "views/invoice_no_commission.xml",
+        "views/configuration_riba_view.xml",
+    ],
+}
