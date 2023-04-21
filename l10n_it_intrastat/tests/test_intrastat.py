76b310691bb6592307d3ff7cdcf6ef6623ee7743
Patrick T <patricktrabocchi@openforce.it>
2023-04-21 15:58:20 +0200

    [IMP] l10n_it_intrastat: update method to compute intrastat line and add helper methods for invoice
    Update method to prepare instrastat line amount converting line subtotal to currency
    amount of partner country, raising an exception if currency is not active or no
    currency rate found when currency conversion is mandatory
    
    Add method to get last currency rate or near currency rate about invoice date and
    method to check if invoice amount need to be converted in partner currency

--- l10n_it_intrastat/tests/test_intrastat.py
+++ l10n_it_intrastat/tests/test_intrastat.py
@@ -1,6 +1,10 @@
 # Copyright 2019 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import datetime
+
+from odoo import fields
+from odoo.exceptions import UserError
 from odoo.tests import tagged
 
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
@@ -15,6 +19,8 @@ class TestIntrastat(AccountTestInvoicingCommon):
         cls.product01 = cls.env.ref("product.product_product_10")
         cls.account_account_model = cls.env["account.account"]
         cls.fp_model = cls.env["account.fiscal.position"]
+        cls.partner_model = cls.env["res.partner"]
+        cls.res_currency_rate_model = cls.env["res.currency.rate"]
 
         cls.account_account_receivable = cls.account_account_model.create(
             {
@@ -36,6 +42,29 @@ class TestIntrastat(AccountTestInvoicingCommon):
 
         cls.partner01.property_account_receivable_id = cls.account_account_receivable
         cls.partner01.property_account_payable_id = cls.account_account_payable
+
+        partner_not_euro_country = cls.env["res.country"].search(
+            [("name", "ilike", "poland")], limit=1
+        )
+        partner_not_euro_country.currency_id.active = True
+        cls.partner_not_euro = cls.partner_model.create(
+            {
+                "name": "AMAZON EU s.a.r.l. (Poland)",
+                "vat": "PL5262907815",
+                "street": "Ul. Rondo Daszynskiego 1",
+                "city": "Warsav",
+                "zip": "00000",
+                "country_id": partner_not_euro_country.id,
+            }
+        )
+
+        cls.res_currency_rate_model.create(
+            {
+                "currency_id": partner_not_euro_country.currency_id.id,
+                "name": fields.Date.to_string(fields.Date.today()),
+                "rate": "1.0",
+            }
+        )
 
         # Demo tax is in another company than current user's company.
         # We can't change this tax's company because
@@ -82,3 +111,112 @@ class TestIntrastat(AccountTestInvoicingCommon):
         invoice.action_post()
         invoice.compute_intrastat_lines()
         self.assertEqual(invoice.intrastat, True)
+
+    def test_currency_not_active_raise_exception(self):
+        self.partner_not_euro.country_id.currency_id.active = False
+        with self.assertRaises(UserError):
+            invoice = self.invoice_model.create(
+                {
+                    "partner_id": self.partner_not_euro.id,
+                    "journal_id": self.sales_journal.id,
+                    "account_id": self.account_account_receivable.id,
+                    "date_invoice": fields.Date.today(),
+                    "intrastat": True,
+                    "invoice_line_ids": [
+                        (
+                            0,
+                            0,
+                            {
+                                "name": "service",
+                                "product_id": self.product01.id,
+                                "account_id": self.account_account_receivable.id,
+                                "quantity": 1,
+                                "price_unit": 100,
+                                "invoice_line_tax_ids": [(6, 0, {self.tax22.id})],
+                            },
+                        )
+                    ],
+                }
+            )
+
+        invoice.compute_taxes()
+        invoice.action_invoice_open()
+
+        # Compute intrastat lines
+        invoice.compute_intrastat_lines()
+
+    def test_no_currency_rate_raise_exception(self):
+        currency_rate_date = fields.Date.today() - datetime.timedelta(days=1)
+        with self.assertRaises(UserError):
+            invoice = self.invoice_model.create(
+                {
+                    "partner_id": self.partner_not_euro.id,
+                    "journal_id": self.sales_journal.id,
+                    "account_id": self.account_account_receivable.id,
+                    "date_invoice": currency_rate_date,
+                    "intrastat": True,
+                    "invoice_line_ids": [
+                        (
+                            0,
+                            0,
+                            {
+                                "name": "service",
+                                "product_id": self.product01.id,
+                                "account_id": self.account_account_receivable.id,
+                                "quantity": 1,
+                                "price_unit": 100,
+                                "invoice_line_tax_ids": [(6, 0, {self.tax22.id})],
+                            },
+                        )
+                    ],
+                }
+            )
+
+            invoice.compute_taxes()
+            invoice.action_invoice_open()
+
+            # Compute intrastat lines
+            invoice.compute_intrastat_lines()
+
+    def test_invoice_totals_partner_no_euro(self):
+        invoice = self.invoice_model.create(
+            {
+                "partner_id": self.partner_not_euro.id,
+                "journal_id": self.sales_journal.id,
+                "account_id": self.account_account_receivable.id,
+                "date_invoice": fields.Date.today(),
+                "intrastat": True,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "service",
+                            "product_id": self.product01.id,
+                            "account_id": self.account_account_receivable.id,
+                            "quantity": 1,
+                            "price_unit": 100,
+                            "invoice_line_tax_ids": [(6, 0, {self.tax22.id})],
+                        },
+                    )
+                ],
+            }
+        )
+
+        invoice.compute_taxes()
+        invoice.action_invoice_open()
+
+        # Compute intrastat lines
+        invoice.compute_intrastat_lines()
+        self.assertEqual(invoice.intrastat, True)
+        # Amount Control
+        total_intrastat_amount = sum(
+            line.amount_currency for line in invoice.intrastat_line_ids
+        )
+        invoice_amount_currency = invoice.currency_id._convert(
+            invoice.amount_untaxed,
+            self.partner_not_euro.country_id.currency_id,
+            invoice.company_id,
+            invoice.date_invoice or fields.Date.today(),
+        )
+        self.assertEqual(total_intrastat_amount, invoice_amount_currency)
