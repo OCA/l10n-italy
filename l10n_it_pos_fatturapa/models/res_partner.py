0e5993fe500b3c2d26eadcbf2bd1f924b6da7a6b
Gianluca Milano <gm@gianlucamilano.com>
2023-05-15 10:48:09 +0200

    [FIX] pre-commit

--- l10n_it_pos_fatturapa/models/res_partner.py
+++ l10n_it_pos_fatturapa/models/res_partner.py
@@ -9,9 +9,10 @@ class ResPartner(models.Model):
     @api.model
     def create_from_ui(self, partner):
         if "electronic_invoice_subjected" in partner:
-            electronic_invoice_subjected = (
-                partner["electronic_invoice_subjected"] == True
+            electronic_invoice_subjected = bool(
+                partner.get("electronic_invoice_subjected")
             )
+
             partner.update(
                 {
                     "electronic_invoice_subjected": electronic_invoice_subjected,
