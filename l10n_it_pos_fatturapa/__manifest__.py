11974fec3ffec595da4cd811bd0be0929f6c56b5
Gianluca Milano <gm@gianlucamilano.com>
2023-05-18 10:21:13 +0200

    [IMP] l10n_it_pos_fatturapa: black, isort, prettier

--- l10n_it_pos_fatturapa/__manifest__.py
+++ l10n_it_pos_fatturapa/__manifest__.py
@@ -1,17 +1,14 @@
-# Copyright 2019 Roberto Fichera
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "ITA - POS - Fattura elettronica",
-    "summary": "Gestione dati fattura elettronica del cliente all'interno "
-               "dell'interfaccia del POS",
-    "version": "12.0.1.0.2",
+    "summary": "Gestione dati fattura elettronica del cliente all'interno"
+    "dell'interfaccia del POS",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
     "category": "Point Of Sale",
-    "website": "https://github.com/OCA/l10n-italy"
-               "/tree/12.0/l10n_it_pos_fatturapa",
-    "author": "Roberto Fichera, Odoo Community Association (OCA)",
-    "maintainers": ["robyf70"],
+    "website": "https://github.com/OCA/l10n-italy",
+    "author": "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
@@ -21,10 +18,8 @@
         "l10n_it_pos_fiscalcode",
         "l10n_it_fatturapa",
     ],
-    'qweb': [
-        'static/src/xml/pos.xml'
-    ],
-    'data': [
-        'views/assets.xml',
+    "qweb": ["static/src/xml/pos_dump.xml"],
+    "data": [
+        "views/assets.xml",
     ],
 }
--- l10n_it_pos_fatturapa/models/res_partner.py
+++ l10n_it_pos_fatturapa/models/res_partner.py
@@ -1,19 +1,22 @@
-# Copyright 2019 Roberto Fichera
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.model
     def create_from_ui(self, partner):
-        if 'electronic_invoice_subjected' in partner:
-            electronic_invoice_subjected = \
-                partner['electronic_invoice_subjected'] == 'true'
-            partner['electronic_invoice_subjected'] = \
-                electronic_invoice_subjected
-            partner['electronic_invoice_obliged_subject'] = \
-                electronic_invoice_subjected
+        if "electronic_invoice_subjected" in partner:
+            electronic_invoice_subjected = bool(
+                partner.get("electronic_invoice_subjected")
+            )
+
+            partner.update(
+                {
+                    "electronic_invoice_subjected": electronic_invoice_subjected,
+                    "electronic_invoice_obliged_subject": electronic_invoice_subjected,
+                }
+            )
         return super(ResPartner, self).create_from_ui(partner)
