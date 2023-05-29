0315ba5e5a601765552bee38fce4d70a3fd78dcc
Marco Colombo <marco.colombo@gmail.com>
2023-05-29 10:55:08 +0200

    fix: remove access to all models, allow selection of reports

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -40,9 +40,13 @@ class WizardExportFatturapa(models.TransientModel):
             ("binding_model_id", "=", model_name),
         ]
 
-    report_print_menu = fields.Many2one(
-        comodel_name="ir.actions.report",
-        domain=_domain_ir_values,
+    def _get_selection(self):
+        reports = self.env["ir.actions.actions"].sudo().search(self._domain_ir_values())
+        ret = [(str(r.id), r.name) for r in reports]
+        return ret
+
+    report_print_menu = fields.Selection(
+        selection="_get_selection",
         help="This report will be automatically included in the created XML",
     )
 
@@ -283,7 +287,12 @@ class WizardExportFatturapa(models.TransientModel):
         return action
 
     def generate_attach_report(self, inv):
-        report_model = self.report_print_menu
+        try:
+            report_id = int(self.report_print_menu)
+        except ValueError as exc:
+            raise UserError(_("Print report not found")) from exc
+
+        report_model = self.env["ir.actions.report"].sudo().browse(report_id)
         attachment, attachment_type = report_model._render_qweb_pdf(
             report_model, inv.ids
         )
@@ -297,7 +306,7 @@ class WizardExportFatturapa(models.TransientModel):
                 "mimetype": "application/x-pdf",
             }
         )
-        inv.write(
+        inv.sudo().write(
             {
                 "fatturapa_doc_attachments": [
                     (
