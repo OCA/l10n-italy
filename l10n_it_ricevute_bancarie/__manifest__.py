272048e0d15ec50a013ada58c931789e1ec17b6a
matteoopenf <matteomircoli@openforce.it>
2023-05-30 18:14:42 +0200

    [FIX][l10n_it_ricevute_bancarie] fix issue https://github.com/OCA/l10n-italy/issues/3216 and add depens from account_due_list

--- l10n_it_ricevute_bancarie/__manifest__.py
+++ l10n_it_ricevute_bancarie/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     "name": "ITA - Ricevute bancarie",
-    "version": "14.0.1.2.3",
+    "version": "14.0.1.3.0",
     "development_status": "Beta",
     "author": "Odoo Community Association (OCA)",
     "category": "Localization/Italy",
@@ -18,6 +18,7 @@
     "license": "AGPL-3",
     "depends": [
         "account",
+        "account_due_list",
         "l10n_it_fatturapa_out",
         "l10n_it_fiscalcode",
         "base_iban",
