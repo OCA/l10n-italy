7e6f22def70cbc7231e94dd83a14d0990a60bf61
matteo.tognini <matteo.tognini@nextev.it>
2023-05-30 14:19:33 +0200

    [IMP] l10n_it_fiscalcode_sale: black, isort, prettier

--- l10n_it_fiscalcode_sale/__manifest__.py
+++ l10n_it_fiscalcode_sale/__manifest__.py
@@ -3,10 +3,10 @@
 {
     "name": "ITA - Codice fiscale nel preventivo",
     "summary": "Mostra il codice fiscale del cliente nella stampa del preventivo",
-    "version": "16.0.1.0.0",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
     "category": "Hidden",
-    "website": "https://github.com/OCA/l10n-italy",
+    "website": "https://github.com/OCA/l10n-italy" "/tree/12.0/l10n_it_fiscalcode_sale",
     "author": "TAKOBI, Nextev Srl, Odoo Community Association (OCA)",
     "maintainers": ["eLBati"],
     "license": "AGPL-3",
