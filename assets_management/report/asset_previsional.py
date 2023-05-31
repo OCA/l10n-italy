0882aab4efd9d7d632cf49c7391d7e115edf9368
sergiocorato <sergiocorato@gmail.com>
2023-05-31 11:10:12 +0200

    [14.0][FIX] assets_management view html report

--- assets_management/report/asset_previsional.py
+++ assets_management/report/asset_previsional.py
@@ -8,7 +8,6 @@ from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.tools.float_utils import float_is_zero
 from odoo.tools.misc import format_amount
-from odoo.tools.safe_eval import safe_eval
 
 
 def format_date(rec, field_name, fmt):
@@ -100,10 +99,8 @@ class Report(models.TransientModel):
         """
         self.ensure_one()
         report_type = report_type or "qweb-pdf"
-        if report_type in ("qweb-pdf", "xlsx"):
+        if report_type in ("qweb-pdf", "xlsx", "qweb-html"):
             res = self.do_print(report_type)
-        elif report_type == "qweb-html":
-            res = self.view_report()
         elif report_type:
             raise ValidationError(
                 _("No report has been defined for type `{}`.").format(report_type)
@@ -118,24 +115,12 @@ class Report(models.TransientModel):
         self.ensure_one()
         if report_type == "qweb-pdf":
             xml_id = "assets_management.report_asset_previsional_pdf"
+        elif report_type == "qweb-html":
+            xml_id = "assets_management.report_asset_previsional_html"
         else:
             xml_id = "assets_management.report_asset_previsional_xlsx"
         report = self.env.ref(xml_id)
         return report.report_action(self)
-
-    def view_report(self):
-        """Launches view for HTML report"""
-        self.ensure_one()
-        xmlid = "assets_management.act_client_asset_previsional_report"
-        [act] = self.env.ref(xmlid).read()
-        ctx = act.get("context", {})
-        if isinstance(ctx, str):
-            ctx = safe_eval(ctx)
-        # Call update twice to force 'active_id(s)' values to be overridden
-        ctx.update(dict(self._context))
-        ctx.update(active_id=self.id, active_ids=self.ids)
-        act["context"] = ctx
-        return act
 
     @api.model
     def get_html(self, given_context=None):
