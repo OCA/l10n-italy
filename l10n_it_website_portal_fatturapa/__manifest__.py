23584ff9538d67e89a368b1abca6a581c947fc06
Borruso <gborruso@dinamicheaziendali.it>
2023-06-26 14:43:37 +0200

    [MIG] l10n_it_website_portal_fatturapa: Migration to 16.0

--- l10n_it_website_portal_fatturapa/__manifest__.py
+++ l10n_it_website_portal_fatturapa/__manifest__.py
@@ -4,11 +4,11 @@
 {
     "name": "Italian Localization - Fattura elettronica - Portale",
     "summary": "Add fatturapa fields and checks in frontend user's details",
-    "version": "12.0.1.3.0",
+    "version": "16.0.1.0.0",
     "author": "Odoo Community Association (OCA)",
     "category": "Localization/Italy",
     "website": "https://github.com/OCA/l10n-italy"
-    "/tree/12.0/l10n_it_website_portal_fatturapa",
+    "/tree/16.0/l10n_it_website_portal_fatturapa",
     "license": "AGPL-3",
     "depends": [
         "l10n_it_fatturapa",
@@ -16,5 +16,11 @@
         "l10n_it_website_portal_ipa",
     ],
     "data": ["views/l10n_it_website_portal_fatturapa_templates.xml"],
+    "assets": {
+        "web.assets_frontend": [
+            "l10n_it_website_portal_fatturapa/static/"
+            "src/js/l10n_it_website_portal_fatturapa.js",
+        ],
+    },
     "auto_install": True,
 }
