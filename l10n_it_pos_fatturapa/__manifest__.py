29a81cab55c01a95673ad98553c42aa4997bf41b
Gianluca Milano <gm@gianlucamilano.com>
2023-06-29 11:58:31 +0200

    [IMP] l10n_it_pos_fatturapa: black, isort, prettier

--- l10n_it_pos_fatturapa/__manifest__.py
+++ l10n_it_pos_fatturapa/__manifest__.py
@@ -4,12 +4,11 @@
 {
     "name": "ITA - POS - Fattura elettronica",
     "summary": "Gestione dati fattura elettronica del cliente all'interno "
-               "dell'interfaccia del POS",
+    "dell'interfaccia del POS",
     "version": "12.0.1.0.2",
     "development_status": "Beta",
     "category": "Point Of Sale",
-    "website": "https://github.com/OCA/l10n-italy"
-               "/tree/12.0/l10n_it_pos_fatturapa",
+    "website": "https://github.com/OCA/l10n-italy" "/tree/12.0/l10n_it_pos_fatturapa",
     "author": "Roberto Fichera, Odoo Community Association (OCA)",
     "maintainers": ["robyf70"],
     "license": "AGPL-3",
@@ -21,10 +20,8 @@
         "l10n_it_pos_fiscalcode",
         "l10n_it_fatturapa",
     ],
-    'qweb': [
-        'static/src/xml/pos.xml'
-    ],
-    'data': [
-        'views/assets.xml',
+    "qweb": ["static/src/xml/pos.xml"],
+    "data": [
+        "views/assets.xml",
     ],
 }
--- l10n_it_pos_fatturapa/models/res_partner.py
+++ l10n_it_pos_fatturapa/models/res_partner.py
@@ -5,15 +5,14 @@ from odoo import api, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.model
     def create_from_ui(self, partner):
-        if 'electronic_invoice_subjected' in partner:
-            electronic_invoice_subjected = \
-                partner['electronic_invoice_subjected'] == 'true'
-            partner['electronic_invoice_subjected'] = \
-                electronic_invoice_subjected
-            partner['electronic_invoice_obliged_subject'] = \
-                electronic_invoice_subjected
+        if "electronic_invoice_subjected" in partner:
+            electronic_invoice_subjected = (
+                partner["electronic_invoice_subjected"] == "true"
+            )
+            partner["electronic_invoice_subjected"] = electronic_invoice_subjected
+            partner["electronic_invoice_obliged_subject"] = electronic_invoice_subjected
         return super(ResPartner, self).create_from_ui(partner)
