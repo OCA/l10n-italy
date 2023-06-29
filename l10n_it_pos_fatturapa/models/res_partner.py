ddb3367304bb5156fb575cffdb09fb0dfb250bb7
Gianluca Milano <gm@gianlucamilano.com>
2023-06-29 12:09:26 +0200

    [MIG] l10n_it_pos_fatturapa: Migration to 14.0

--- l10n_it_pos_fatturapa/models/res_partner.py
+++ l10n_it_pos_fatturapa/models/res_partner.py
@@ -1,4 +1,3 @@
-# Copyright 2019 Roberto Fichera
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
@@ -10,9 +9,14 @@ class ResPartner(models.Model):
     @api.model
     def create_from_ui(self, partner):
         if "electronic_invoice_subjected" in partner:
-            electronic_invoice_subjected = (
-                partner["electronic_invoice_subjected"] == "true"
+            electronic_invoice_subjected = bool(
+                partner.get("electronic_invoice_subjected")
             )
-            partner["electronic_invoice_subjected"] = electronic_invoice_subjected
-            partner["electronic_invoice_obliged_subject"] = electronic_invoice_subjected
+
+            partner.update(
+                {
+                    "electronic_invoice_subjected": electronic_invoice_subjected,
+                    "electronic_invoice_obliged_subject": electronic_invoice_subjected,
+                }
+            )
         return super(ResPartner, self).create_from_ui(partner)
