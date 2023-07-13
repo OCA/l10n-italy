41920b7baf6e4d28b517fd0382c0b013ab1fff72
sergiocorato <sergiocorato@gmail.com>
2023-07-13 19:31:33 +0200

    [14.0][ADD] new module account_disable_auto_install_l10n_it_edi

---
+++ account_disable_auto_install_l10n_it_edi/__init__.py
@@ -0,0 +1,1 @@
+from .patch import _auto_install_l10n
---
+++ account_disable_auto_install_l10n_it_edi/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Sergio Corato
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Remove auto installable modules l10n_it_edi*",
+    "summary": "Awful patch to do not auto-install l10n_it_edi* modules",
+    "version": "14.0.1.0.0",
+    "category": "Tools",
+    "maintainers": ["sergiocorato"],
+    "author": "Sergio Corato, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "installable": True,
+    "depends": ["account"],
+    "license": "AGPL-3",
+}
