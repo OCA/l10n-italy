5fa13cb7c08e8584f3223d7dd58a52e1aea154e5
Simone Rubino <sir@takobi.online>
2023-08-18 10:59:43 +0200

    [REF] l10n_it_fiscalcode: Extract person Fiscal Code check

--- l10n_it_fiscalcode/model/res_partner.py
+++ l10n_it_fiscalcode/model/res_partner.py
@@ -1,9 +1,22 @@
+# Copyright 2023 Simone Rubino - TAKOBI
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from codicefiscale import isvalid
 
 from odoo import _, models, fields, api
 from odoo.exceptions import ValidationError
+
+
+def _check_person_fiscal_code(fiscal_code):
+    """`fiscal_code` is the Fiscal Code of a Person."""
+    if len(fiscal_code) != 16:
+        # Check fiscalcode length of a person
+        msg = _("The fiscal code must have 16 characters.")
+        raise ValidationError(msg)
+    if not isvalid(fiscal_code):
+        # Check fiscalcode validity
+        msg = _("The fiscal code isn't valid.")
+        raise ValidationError(msg)
 
 
 class ResPartner(models.Model):
@@ -13,7 +26,8 @@ class ResPartner(models.Model):
     @api.constrains('fiscalcode')
     def check_fiscalcode(self):
         for partner in self:
-            if not partner.fiscalcode:
+            fiscal_code = partner.fiscalcode
+            if not fiscal_code:
                 # Because it is not mandatory
                 continue
             elif partner.company_type == 'person':
@@ -23,14 +37,7 @@ class ResPartner(models.Model):
                     # the user might insert VAT in fiscalcode field.
                     # Perform the same check as Company case
                     continue
-                if len(partner.fiscalcode) != 16:
-                    # Check fiscalcode length of a person
-                    msg = _("The fiscal code must have 16 characters.")
-                    raise ValidationError(msg)
-                if not isvalid(partner.fiscalcode):
-                    # Check fiscalcode validity
-                    msg = _("The fiscal code isn't valid.")
-                    raise ValidationError(msg)
+                _check_person_fiscal_code(fiscal_code)
         return True
 
     fiscalcode = fields.Char(
