e6cf7c44d702685fdaeb57f10b926029a9a33513
PicchiSeba <picchiseba+git@gmail.com>
2023-08-28 14:26:17 +0200

    [IMP][l10n_it_intrastat] add some missing intrastat codes

--- l10n_it_intrastat/__manifest__.py
+++ l10n_it_intrastat/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "ITA - Intrastat",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "category": "Account",
     "summary": "Riclassificazione merci e servizi per dichiarazioni Intrastat",
     "author": "Openforce, Link IT srl, Agile Business Group, "
