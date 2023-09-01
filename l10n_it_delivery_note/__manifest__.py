5aecd4fb201c3d6cb0133e2a31ada4fa72ea9405
Borruso <gborruso@dinamicheaziendali.it>
2023-09-01 12:10:29 +0200

    [UPD] l10n_it_delivery_note: aggiornato maintainers

--- l10n_it_delivery_note/__manifest__.py
+++ l10n_it_delivery_note/__manifest__.py
@@ -16,7 +16,7 @@
     "version": "16.0.1.0.3",
     "category": "Localization/Italy",
     "license": "AGPL-3",
-    "maintainers": ["As400it"],
+    "maintainers": ["MarcoCalcagni"],
     "depends": [
         "delivery",
         "l10n_it_delivery_note_base",
