def43e4807d37058b84f59c25f643e93fbd67863
Borruso <gborruso@dinamicheaziendali.it>
2023-09-01 11:44:37 +0200

    [ADD] l10n_it_delivery_note: gross_weight and net_weight computation from pickings
    Co-authored-by: TheMule71 <marco.colombo@gmail.com>

--- l10n_it_delivery_note/models/stock_delivery_note.py
+++ l10n_it_delivery_note/models/stock_delivery_note.py
@@ -169,7 +169,13 @@ class StockDeliveryNote(models.Model):
         domain=_domain_volume_uom,
         states=DONE_READONLY_STATE,
     )
-    gross_weight = fields.Float(string="Gross weight", states=DONE_READONLY_STATE)
+    gross_weight = fields.Float(
+        string="Gross weight",
+        store=True,
+        readonly=False,
+        compute="_compute_weights",
+        states=DONE_READONLY_STATE,
+    )
     gross_weight_uom_id = fields.Many2one(
         "uom.uom",
         string="Gross weight UoM",
@@ -177,7 +183,13 @@ class StockDeliveryNote(models.Model):
         domain=_domain_weight_uom,
         states=DONE_READONLY_STATE,
     )
-    net_weight = fields.Float(string="Net weight", states=DONE_READONLY_STATE)
+    net_weight = fields.Float(
+        string="Net weight",
+        store=True,
+        readonly=False,
+        compute="_compute_weights",
+        states=DONE_READONLY_STATE,
+    )
     net_weight_uom_id = fields.Many2one(
         "uom.uom",
         string="Net weight UoM",
@@ -308,6 +320,30 @@ class StockDeliveryNote(models.Model):
     def _compute_get_pickings(self):
         for note in self:
             note.pickings_picker = note.picking_ids
+
+    @api.depends("picking_ids")
+    def _compute_weights(self):
+        for note in self:
+            # fill gross & net weight from pickings
+            gross_weight = net_weight = 0.0
+            if note.picking_ids:
+                # this is the unit used for shipping_weight
+                weight_uom = self.env[
+                    "product.template"
+                ]._get_weight_uom_id_from_ir_config_parameter()
+                for pick in note.picking_ids:
+                    gross_weight += weight_uom._compute_quantity(
+                        pick.shipping_weight, note.gross_weight_uom_id
+                    )
+                    net_weight += weight_uom._compute_quantity(
+                        pick.shipping_weight, note.net_weight_uom_id
+                    )
+            note.gross_weight = gross_weight
+            note.net_weight = net_weight
+
+    @api.onchange("picking_ids")
+    def _onchange_picking_ids(self):
+        self._compute_weights()
 
     def _inverse_set_pickings(self):
         for note in self:
