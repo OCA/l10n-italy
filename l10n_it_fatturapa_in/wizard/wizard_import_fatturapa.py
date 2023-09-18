9ec9a04216f690531162b8fbd053c5f435331c3b
Simone Rubino <simone.rubino@aion-tech.it>
2023-09-18 16:36:04 +0200

    [REF] l10n_it_fatturapa_in: Allow override for invoice partner

--- l10n_it_fatturapa_in/wizard/wizard_import_fatturapa.py
+++ l10n_it_fatturapa_in/wizard/wizard_import_fatturapa.py
@@ -1830,6 +1830,11 @@ class WizardImportFatturapa(models.TransientModel):
             new_price_precision.sudo().write({"digits": original_precision})
             new_cr.commit()
 
+    def _get_invoice_partner_id(self, fatt):
+        cedentePrestatore = fatt.FatturaElettronicaHeader.CedentePrestatore
+        partner_id = self.getCedPrest(cedentePrestatore)
+        return partner_id
+
     def importFatturaPA(self):
         self.ensure_one()
 
@@ -1870,7 +1875,7 @@ class WizardImportFatturapa(models.TransientModel):
 
             cedentePrestatore = fatt.FatturaElettronicaHeader.CedentePrestatore
             # 1.2
-            partner_id = self.getCedPrest(cedentePrestatore)
+            partner_id = self._get_invoice_partner_id(fatt)
             # 1.3
             TaxRappresentative = fatt.FatturaElettronicaHeader.RappresentanteFiscale
             # 1.5
