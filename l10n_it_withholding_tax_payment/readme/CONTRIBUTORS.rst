7d53ca313a73d18721ae75973520b3476a1428c2
matteo.tognini <matteo.tognini@nextev.it>
2023-09-25 12:15:47 +0200

    [MIG] l10n_it_withholding_tax_payment: Migration to 16.0

--- l10n_it_withholding_tax_payment/readme/CONTRIBUTORS.rst
+++ l10n_it_withholding_tax_payment/readme/CONTRIBUTORS.rst
@@ -1,3 +1,4 @@
 * Alessandro Camilli <alessandrocamilli@openforce.it>
 * Lorenzo Battistini <lorenzo.battistini@agilebg.com>
 * Matteo Bilotta <mbilotta@linkgroup.it>
+* Nextev <web@nextev.it>
--- l10n_it_withholding_tax_payment/wizard/create_move_payment.py
+++ l10n_it_withholding_tax_payment/wizard/create_move_payment.py
@@ -25,10 +25,10 @@ class WizardWtMovePaymentCreate(models.TransientModel):
     def generate(self):
         wt_move_payment_obj = self.env["withholding.tax.move.payment"]
         wt_payment = wt_move_payment_obj.generate_from_moves(self.wt_move_ids)
-        view = self.env["ir.model.data"].get_object_reference(
-            "l10n_it_withholding_tax_payment", "view_withholding_move_payment_form"
+        view = self.env["ir.model.data"]._xmlid_lookup(
+            "l10n_it_withholding_tax_payment.view_withholding_move_payment_form"
         )
-        view_id = view[1] or False
+        view_id = view[2] or False
 
         return {
             "name": _("Withholding Tax Payment"),
