e96f433b72f9db77f5f425e17d0f4b09615d2c0c
tafaRU <alex.comba@agilebg.com>
2023-09-27 10:48:40 +0200

    [IMP] l10n_it_delivery_note: add hook to prepare vals needed to create DN

--- l10n_it_delivery_note/wizard/delivery_note_create.py
+++ l10n_it_delivery_note/wizard/delivery_note_create.py
@@ -67,6 +67,28 @@ class StockDeliveryNoteCreateWizard(models.TransientModel):
     def _onchange_partner(self):
         self.partner_shipping_id = self.partner_id
 
+    def _prepare_delivery_note_vals(self, sale_order_id):
+        return {
+            "partner_sender_id": self.partner_sender_id.id,
+            "partner_id": self.partner_id.id,
+            "partner_shipping_id": self.partner_shipping_id.id,
+            "type_id": self.type_id.id,
+            "date": self.date,
+            "delivery_method_id": self.partner_id.property_delivery_carrier_id.id,
+            "transport_condition_id": sale_order_id.default_transport_condition_id.id
+            or self.partner_id.default_transport_condition_id.id
+            or self.type_id.default_transport_condition_id.id,
+            "goods_appearance_id": sale_order_id.default_goods_appearance_id.id
+            or self.partner_id.default_goods_appearance_id.id
+            or self.type_id.default_goods_appearance_id.id,
+            "transport_reason_id": sale_order_id.default_transport_reason_id.id
+            or self.partner_id.default_transport_reason_id.id
+            or self.type_id.default_transport_reason_id.id,
+            "transport_method_id": sale_order_id.default_transport_method_id.id
+            or self.partner_id.default_transport_method_id.id
+            or self.type_id.default_transport_method_id.id,
+        }
+
     def confirm(self):
         self.check_compliance(self.selected_picking_ids)
 
@@ -74,30 +96,7 @@ class StockDeliveryNoteCreateWizard(models.TransientModel):
         sale_order_id = sale_order_ids and sale_order_ids[0] or self.env["sale.order"]
 
         delivery_note = self.env["stock.delivery.note"].create(
-            {
-                "partner_sender_id": self.partner_sender_id.id,
-                "partner_id": self.partner_id.id,
-                "partner_shipping_id": self.partner_shipping_id.id,
-                "type_id": self.type_id.id,
-                "date": self.date,
-                "delivery_method_id": self.partner_id.property_delivery_carrier_id.id,
-                "transport_condition_id": sale_order_id
-                and sale_order_id.default_transport_condition_id.id
-                or self.partner_id.default_transport_condition_id.id
-                or self.type_id.default_transport_condition_id.id,
-                "goods_appearance_id": sale_order_id
-                and sale_order_id.default_goods_appearance_id.id
-                or self.partner_id.default_goods_appearance_id.id
-                or self.type_id.default_goods_appearance_id.id,
-                "transport_reason_id": sale_order_id
-                and sale_order_id.default_transport_reason_id.id
-                or self.partner_id.default_transport_reason_id.id
-                or self.type_id.default_transport_reason_id.id,
-                "transport_method_id": sale_order_id
-                and sale_order_id.default_transport_method_id.id
-                or self.partner_id.default_transport_method_id.id
-                or self.type_id.default_transport_method_id.id,
-            }
+            self._prepare_delivery_note_vals(sale_order_id)
         )
 
         self.selected_picking_ids.write({"delivery_note_id": delivery_note.id})
