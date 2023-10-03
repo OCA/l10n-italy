4ddf71c15da90d2dc55f3d78d3c06e65b4477863
Simone Rubino <simone.rubino@aion-tech.it>
2023-10-03 16:40:41 +0200

    [MIG] l10n_it_mis_reports_pl_bs: Migration to 16.0

--- l10n_it_mis_reports_pl_bs/__manifest__.py
+++ l10n_it_mis_reports_pl_bs/__manifest__.py
@@ -1,11 +1,13 @@
+# Copyright 2023 Simone Rubino - Aion Tech
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
     "name": "ITA - MIS builder - Bilancio civilistico",
     "summary": 'Modelli "MIS Builder" per il conto economico e lo stato patrimoniale',
     "author": "Dinamiche Aziendali srl, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-italy",
     "category": "Localization/Italy",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "maintainers": ["As400it"],
     "depends": [
--- l10n_it_mis_reports_pl_bs/readme/CONTRIBUTORS.rst
+++ l10n_it_mis_reports_pl_bs/readme/CONTRIBUTORS.rst
@@ -1,1 +1,4 @@
 * Marco Calcagni at Dinamiche Aziendali <mcalcagni@dinamicheaziendali.it>
+* `Aion Tech <https://aiontech.company/>`_:
+
+  * Simone Rubino <simone.rubino@aion-tech.it>
