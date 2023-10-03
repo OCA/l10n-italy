38d3396f22e8751a91cb3435a5deb30b902ef49f
Simone Rubino <simone.rubino@aion-tech.it>
2023-10-03 12:10:58 +0200

    [REF] l10n_it_bill_of_entry: Tests must not be imported

--- l10n_it_bill_of_entry/__manifest__.py
+++ l10n_it_bill_of_entry/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "ITA - Bolle doganali",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Localization/Italy",
     "author": "Agile Business Group, CQ Creativi Quadrati, TAKOBI, "
     "Odoo Community Association (OCA)",
