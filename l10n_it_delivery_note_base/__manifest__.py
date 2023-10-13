a431c5afd54b9911511ca6bef698b4db56297a91
Marco Calcagni <mcalcagni@dinamicheaziendali.it>
2023-10-13 09:56:30 +0200

    [16] change of mainteiner name on manifest

--- l10n_it_delivery_note_base/__manifest__.py
+++ l10n_it_delivery_note_base/__manifest__.py
@@ -15,7 +15,7 @@
     "version": "16.0.1.0.0",
     "category": "Localization/Italy",
     "license": "AGPL-3",
-    "maintainers": ["As400it", "Byloth"],
+    "maintainers": ["MarcoCalcagni", "Byloth"],
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
