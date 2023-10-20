22c1c5857d73d23c80947931801637d61528e673
As400it <148192303+As400it@users.noreply.github.com>
2023-10-20 12:14:55 +0200

    Update of Maintainers
    Cambio dei mainteiners per aggiunta di Borruso. Calcagni ha cambiato nome da As400it a MarcoCalcagni

--- l10n_it_delivery_note_batch/__manifest__.py
+++ l10n_it_delivery_note_batch/__manifest__.py
@@ -8,10 +8,10 @@
     "summary": "Crea i DDT partendo da gruppi di prelievi",
     "author": "Marco Colombo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-italy" "l10n_it_delivery_note_batch",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "category": "Localization/Italy",
     "license": "AGPL-3",
-    "maintainers": ["As400it", "TheMule71"],
+    "maintainers": ["MarcoCalcagni", "TheMule71", "Borruso"],
     "depends": [
         "stock",
         "stock_picking_batch",
