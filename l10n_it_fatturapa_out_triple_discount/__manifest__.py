d85268de7be4feac88799910daf464da1bc5602a
eLBati <elbaddy@gmail.com>
2023-10-26 12:24:07 +0200

    [IMP] : black, isort, prettier

--- l10n_it_fatturapa_out_triple_discount/__manifest__.py
+++ l10n_it_fatturapa_out_triple_discount/__manifest__.py
@@ -2,14 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-
     "name": "ITA - Fattura elettronica - Integrazione sconto triplo",
-    "summary": "Modulo ponte tra emissione " "fatture elettroniche e sconto triplo",
+    "summary": "Modulo ponte tra emissione fatture elettroniche e sconto triplo",
     "version": "14.0.1.0.0",
     "development_status": "Beta",
     "category": "Hidden",
-    'website': 'https://github.com/OCA/l10n-italy'
-               '/tree/12.0/l10n_it_fatturapa_out_triple_discount',
+    "website": "https://github.com/OCA/l10n-italy"
+    "/tree/12.0/l10n_it_fatturapa_out_triple_discount",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "auto_install": True,
