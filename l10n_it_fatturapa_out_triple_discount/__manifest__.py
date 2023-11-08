7d2615af8ee214601b3e82ab8613ba15e4129e47
matteo.tognini <matteo.tognini@nextev.it>
2023-11-08 17:03:18 +0100

    [MIG]l10n_it_fatturapa_out_triple_discount: Migration to 14.0

--- l10n_it_fatturapa_out_triple_discount/__manifest__.py
+++ l10n_it_fatturapa_out_triple_discount/__manifest__.py
@@ -4,11 +4,10 @@
 {
     "name": "ITA - Fattura elettronica - Integrazione sconto triplo",
     "summary": "Modulo ponte tra emissione " "fatture elettroniche e sconto triplo",
-    "version": "12.0.2.0.1",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
     "category": "Hidden",
-    "website": "https://github.com/OCA/l10n-italy"
-    "/tree/12.0/l10n_it_fatturapa_out_triple_discount",
+    "website": "https://github.com/OCA/l10n-italy",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "auto_install": True,
