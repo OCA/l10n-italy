0e4a4d7e87ccc9d9767130f8e982b455972f88f0
Simone Rubino <simone.rubino@aion-tech.it>
2023-11-15 10:34:06 +0100

    [MIG] l10n_it_fatturapa_out_triple_discount: Migration to 16.0

--- l10n_it_fatturapa_out_triple_discount/tests/__init__.py
+++ l10n_it_fatturapa_out_triple_discount/tests/__init__.py
@@ -1,6 +1,3 @@
-# Copyright 2019 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-# tests disabled because unstable
-# TODO https://github.com/OCA/l10n-italy/issues/1588
-# from . import test_fatturapa_triple_discount
+from . import test_fatturapa_triple_discount
--- l10n_it_fatturapa_out_triple_discount/wizards/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out_triple_discount/wizards/wizard_export_fatturapa.py
@@ -1,31 +1,56 @@
 # Copyright 2019 Simone Rubino - Agile Business Group
+# Copyright 2023 Simone Rubino - Aion Tech
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models
-from odoo.tools.float_utils import float_round
-
-from odoo.addons.l10n_it_fatturapa.bindings.fatturapa import ScontoMaggiorazioneType
+from odoo.tools import float_is_zero
 
 
-class WizardExportFatturapa(models.TransientModel):
+class WizardExportFatturaPA(models.TransientModel):
     _inherit = "wizard.export.fatturapa"
 
-    def setDettaglioLinea(self, line_no, line, body, price_precision, uom_precision):
-        res = super(WizardExportFatturapa, self).setDettaglioLinea(
-            line_no, line, body, price_precision, uom_precision
-        )
-        if line.discount2 or line.discount3:
-            DettaglioLinea = body.DatiBeniServizi.DettaglioLinee[-1]
-            if line.discount2:
-                DettaglioLinea.ScontoMaggiorazione.append(
-                    ScontoMaggiorazioneType(
-                        Tipo="SC", Percentuale="%.2f" % float_round(line.discount2, 8)
+    def _get_efattura_class(self):
+        efattura_class = super()._get_efattura_class()
+
+        class EInvoiceOutTripleDiscount(efattura_class):
+            def get_template_values(self):
+                template_values = super().get_template_values()
+
+                format_numbers_two = template_values["format_numbers_two"]
+
+                def get_triple_ScontoMaggiorazione_values_list(line):
+                    ScontoMaggiorazione_values_list = []
+
+                    discount_fields = line._get_multiple_discount_field_names()
+
+                    discounts_digits = line.fields_get(
+                        allfields=discount_fields,
+                        attributes=[
+                            "digits",
+                        ],
                     )
-                )
-            if line.discount3:
-                DettaglioLinea.ScontoMaggiorazione.append(
-                    ScontoMaggiorazioneType(
-                        Tipo="SC", Percentuale="%.2f" % float_round(line.discount3, 8)
-                    )
-                )
-        return res
+                    for discount_field in discount_fields:
+                        discount_perc = line[discount_field]
+                        all_digits, precision_digits = discounts_digits[discount_field][
+                            "digits"
+                        ]
+                        is_discount_zero = float_is_zero(
+                            discount_perc,
+                            precision_digits=precision_digits,
+                        )
+                        if not is_discount_zero:
+                            ScontoMaggiorazione_values = {
+                                "Tipo": "SC",
+                                "Percentuale": format_numbers_two(discount_perc),
+                            }
+                            ScontoMaggiorazione_values_list.append(
+                                ScontoMaggiorazione_values
+                            )
+                    return ScontoMaggiorazione_values_list
+
+                template_values[
+                    "get_triple_ScontoMaggiorazione_values_list"
+                ] = get_triple_ScontoMaggiorazione_values_list
+                return template_values
+
+        return EInvoiceOutTripleDiscount
