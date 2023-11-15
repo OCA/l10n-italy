965825f9975d01247c59e929038df6f593c0110b
Simone Rubino <simone.rubino@aion-tech.it>
2023-11-15 10:34:06 +0100

    [MIG] l10n_it_fatturapa_out_triple_discount: Migration to 16.0

--- l10n_it_fatturapa_out_triple_discount/tests/test_fatturapa_triple_discount.py
+++ l10n_it_fatturapa_out_triple_discount/tests/test_fatturapa_triple_discount.py
@@ -1,48 +1,37 @@
 # Copyright 2019 Simone Rubino - Agile Business Group
+# Copyright 2023 Simone Rubino - Aion Tech
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import codecs
+import base64
 
-from odoo.addons.l10n_it_fatturapa_out.tests.fatturapa_common import FatturaPACommon
+from odoo.tests import Form, tagged
+
+from .fatturapa_common import Common
 
 
-class TestInvoiceTripleDiscount(FatturaPACommon):
-    def setUp(self):
-        super(TestInvoiceTripleDiscount, self).setUp()
-
+@tagged("post_install", "-at_install")
+class TestEInvoiceTripleDiscount(Common):
     def test_xml_export_triple_discount(self):
-        self.set_sequences(13, "2016-01-07")
-        invoice = self.invoice_model.create(
-            {
-                "date_invoice": "2016-01-07",
-                "partner_id": self.res_partner_fatturapa_0.id,
-                "journal_id": self.sales_journal.id,
-                "account_id": self.a_recv.id,
-                "payment_term_id": self.account_payment_term.id,
-                "user_id": self.user_demo.id,
-                "type": "out_invoice",
-                "currency_id": self.EUR.id,
-                "invoice_line_ids": [
-                    (
-                        0,
-                        0,
-                        {
-                            "account_id": self.a_sale.id,
-                            "product_id": self.product_product_10.id,
-                            "name": "Mouse\nOptical",
-                            "quantity": 1,
-                            "uom_id": self.product_uom_unit.id,
-                            "price_unit": 10,
-                            "discount": 50,
-                            "discount2": 50,
-                            "discount3": 50,
-                            "invoice_line_tax_ids": [(6, 0, {self.tax_22.id})],
-                        },
-                    )
-                ],
-            }
+        partner = self.res_partner_fatturapa_0
+        partner.vat = "IT00146089990"
+        partner.fiscalcode = "00146089990"
+        invoice = self.init_invoice(
+            "out_invoice",
+            invoice_date="2016-01-07",
+            partner=partner,
+            products=self.product_product_10,
+            taxes=self.tax_22,
         )
-        invoice.action_invoice_open()
+        invoice_form = Form(invoice)
+        invoice_form.invoice_payment_term_id = self.account_payment_term
+        with invoice_form.invoice_line_ids.edit(0) as line:
+            line.name = "Mouse\nOptical"
+            line.price_unit = 10
+            line.discount = 50
+            line.discount2 = 50
+            line.discount3 = 50
+        invoice = invoice_form.save()
+        invoice.action_post()
         res = self.run_wizard(invoice.id)
 
         self.assertTrue(res)
@@ -50,9 +39,12 @@ class TestInvoiceTripleDiscount(FatturaPACommon):
         self.set_e_invoice_file_id(attachment, "IT06363391001_00001.xml")
 
         # XML doc to be validated
-        xml_content = codecs.decode(attachment.datas, "base64")
-        self.check_content(
-            xml_content,
-            "IT06363391001_00001.xml",
-            module_name="l10n_it_fatturapa_out_triple_discount",
+        xml_content = base64.decodebytes(attachment.datas)
+        xml_tree = self.get_xml_tree_from_string(xml_content)
+        expected_xml = self.getFile("IT06363391001_00001.xml")[1]
+        expected_xml_content = base64.decodebytes(expected_xml)
+        expected_xml_tree = self.get_xml_tree_from_string(expected_xml_content)
+        self.assertXmlTreeEqual(
+            xml_tree,
+            expected_xml_tree,
         )
