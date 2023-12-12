29cdb68705b241d5dff290fa3b46727e200584e6
Simone Rubino <simone.rubino@aion-tech.it>
2023-12-12 16:42:56 +0100

    [REF] assets_management: Remove unused actions Actions usage removed in https://github.com/OCA/l10n-italy/commit/4b1f23af4b8d44f38207f8ff2f5b0f6df391dfc7

--- assets_management/__manifest__.py
+++ assets_management/__manifest__.py
@@ -28,7 +28,6 @@
         "report/templates/asset_journal.xml",
         "report/templates/asset_previsional.xml",
         "report/reports.xml",
-        "views/action_client.xml",
         "views/asset_menuitems.xml",
         "views/account_move.xml",
         "views/asset.xml",
