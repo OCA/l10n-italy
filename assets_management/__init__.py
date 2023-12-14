4df2c2b109763c287a307679a4ddbcdda7442d56
Simone Rubino <simone.rubino@aion-tech.it>
2023-12-14 10:56:43 +0100

    [MOV] assets_management: Rename to l10n_it_asset_management

--- assets_management/__init__.py
+++
@@ -1,7 +0,0 @@
-# Author(s): Silvio Gregorini (silviogregorini@openforce.it)
-# Copyright 2019 Openforce Srls Unipersonale (www.openforce.it)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import models
-from . import report
-from . import wizard
