73e8a428bb1399b47f387527068d123441d1876f
eLBati <elbaddy@gmail.com>
2024-01-10 16:46:16 +0100

    FIX l10n_it_fatturapa_out_oss - TypeError: _prepare_tax_vals() takes 4 positional arguments but 5 were given

--- l10n_it_fatturapa_out_oss/wizard/l10n_eu_oss_wizard.py
+++ l10n_it_fatturapa_out_oss/wizard/l10n_eu_oss_wizard.py
@@ -4,7 +4,9 @@ from odoo import models
 class L10nEuOssWizard(models.TransientModel):
     _inherit = "l10n.eu.oss.wizard"
 
-    def _prepare_tax_vals(self, country_id, tax_id, rate):
-        vals = super(L10nEuOssWizard, self)._prepare_tax_vals(country_id, tax_id, rate)
+    def _prepare_tax_vals(self, country, tax, rate, tax_group):
+        vals = super(L10nEuOssWizard, self)._prepare_tax_vals(
+            country, tax, rate, tax_group
+        )
         vals["kind_id"] = self.env.ref("l10n_it_account_tax_kind.n3_2").id
         return vals
