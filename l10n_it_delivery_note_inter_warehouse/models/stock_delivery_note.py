a1eb8e2ba3e2c3a813c2d72ce3b49f35874e006b
Alessio Renda <renda855@gmail.com>
2024-01-11 17:34:32 +0100

    [FIX] l10n_it_delivery_note_inter_warehouse: remove pickings

--- l10n_it_delivery_note_inter_warehouse/models/stock_delivery_note.py
+++ l10n_it_delivery_note_inter_warehouse/models/stock_delivery_note.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Ooops404
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import fields, models
 
 
 class StockDeliveryNote(models.Model):
@@ -10,7 +10,8 @@ class StockDeliveryNote(models.Model):
     def update_detail_lines(self):
         res = super().update_detail_lines()
         for note in self:
-            inter_wh_note_id = note.picking_ids[-1].mapped(
+            # Safe operation to get last element
+            inter_wh_note_id = fields.first(note.picking_ids[::-1]).mapped(
                 "move_lines.inter_warehouse_picking_id.delivery_note_id"
             )
             if inter_wh_note_id:
