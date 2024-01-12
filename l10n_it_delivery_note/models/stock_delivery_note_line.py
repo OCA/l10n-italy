906dcb5d47bcac51c97ace2992bc5444cd3b84a8
Borruso <gborruso@dinamicheaziendali.it>
2024-01-12 11:25:25 +0100

    [FIX] l10n_it_delivery_note: fix qty line

--- l10n_it_delivery_note/models/stock_delivery_note_line.py
+++ l10n_it_delivery_note/models/stock_delivery_note_line.py
@@ -122,7 +122,7 @@ class StockDeliveryNoteLine(models.Model):
                 "move_id": move.id,
                 "name": name,
                 "product_id": move.product_id.id,
-                "product_qty": move.product_uom_qty,
+                "product_qty": move.quantity_done,
                 "product_uom_id": move.product_uom.id,
             }
 
