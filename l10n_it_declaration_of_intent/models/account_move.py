1ebb77cff242648899c535f02c0aafdf7d055342
matteo.tognini <matteo.tognini@nextev.it>
2024-01-24 15:33:10 +0100

    [IMP]l10n_it_fatturapa_out: campo causale nel modello fattura ed esportazione xml

--- l10n_it_declaration_of_intent/models/account_move.py
+++ l10n_it_declaration_of_intent/models/account_move.py
@@ -143,7 +143,7 @@ class AccountMove(models.Model):
                     # Link declaration to invoice
                     self.declaration_of_intent_ids = [(4, declaration.id)]
                     if is_sale_document:
-                        cmt = self.narration or ""
+                        cmt = self.causale or ""
                         msg = (
                             "Vostra dichiarazione d'intento nr %s del %s, "
                             "nostro protocollo nr %s del %s, "
@@ -164,7 +164,7 @@ class AccountMove(models.Model):
                                 cmt += "\n\n" + msg
                             else:
                                 cmt = msg
-                        self.narration = cmt
+                        self.causale = cmt
 
     def _prepare_declaration_line(self, amount, lines, tax):
         """Dictionary used to create declaration line for this invoice."""
--- l10n_it_fatturapa_out/tests/test_fatturapa_xml_validation.py
+++ l10n_it_fatturapa_out/tests/test_fatturapa_xml_validation.py
@@ -140,7 +140,7 @@ class TestFatturaPAXMLValidation(FatturaPACommon):
                 "user_id": self.user_demo.id,
                 "move_type": "out_invoice",
                 "currency_id": self.EUR.id,
-                "narration": "prima riga\nseconda riga",
+                "causale": "prima riga\nseconda riga",
                 "invoice_line_ids": [
                     (
                         0,
@@ -203,7 +203,7 @@ class TestFatturaPAXMLValidation(FatturaPACommon):
                 "user_id": self.user_demo.id,
                 "move_type": "out_invoice",
                 "currency_id": self.EUR.id,
-                "narration": "prima riga\nseconda riga",
+                "causale": "prima riga\nseconda riga",
                 "invoice_line_ids": [
                     (
                         0,
@@ -406,7 +406,7 @@ class TestFatturaPAXMLValidation(FatturaPACommon):
                 "user_id": self.user_demo.id,
                 "move_type": "out_invoice",
                 "currency_id": self.EUR.id,
-                "narration": "first line\n\nsecond line",
+                "causale": "first line\n\nsecond line",
                 "invoice_line_ids": [
                     (
                         0,
