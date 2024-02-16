775aa4aea1913e8ccbb86c6899f80502f8b8141d
eLBati <elbaddy@gmail.com>
2024-02-16 15:12:59 +0100

    IMP l10n_it_vat_registries: ordering option

--- l10n_it_vat_registries/models/account_tax_registry.py
+++ l10n_it_vat_registries/models/account_tax_registry.py
@@ -13,6 +13,13 @@ class AccountTaxRegistry(models.Model):
         required=True,
         default=lambda self: self.env.company,
     )
+    entry_order = fields.Selection(
+        [
+            ("date_name", "Date - Number"),
+            ("journal_date_name", "Journal - Date - Number"),
+        ],
+        default="date_name",
+    )
     journal_ids = fields.One2many(
         "account.journal", "tax_registry_id", "Journals", readonly=True
     )
