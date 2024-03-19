559dbea6c100a50a06d89281e85fb22756eb2962
Simone Rubino <simone.rubino@aion-tech.it>
2024-03-19 09:35:56 +0100

    [IMP] l10n_it_vat_settlement_date: pre-commit execution

--- l10n_it_vat_settlement_date/models/account_tax.py
+++ l10n_it_vat_settlement_date/models/account_tax.py
@@ -1,15 +1,14 @@
 # Copyright (c) 2021 Marco Colombo (https://github/TheMule71)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import models
 
 
 class AccountTax(models.Model):
     _inherit = "account.tax"
 
-
     def _account_tax_ids_with_moves(self):
-        """ Return all account.tax ids for which there is at least
+        """Return all account.tax ids for which there is at least
         one account.move.line in the context period
         for the user company.
 
@@ -37,14 +36,19 @@ class AccountTax(models.Model):
             )
         """
         from_date, to_date, company_id, target_move = self.get_context_values()
-        self.env.cr.execute(
-            req, (company_id, from_date, to_date, company_id))
+        self.env.cr.execute(req, (company_id, from_date, to_date, company_id))
         return [r[0] for r in self.env.cr.fetchall()]
-
 
     def get_move_line_partial_domain(self, from_date, to_date, company_id):
         return [
-            '|', '&', ('date_vat_settlement', '>=', from_date), ('date_vat_settlement', '<=', to_date),
-                 '&', ('date_vat_settlement', '=', None), '&', ('date', '<=', to_date), ('date', '>=', from_date),
-            ('company_id', '=', company_id),
+            "|",
+            "&",
+            ("date_vat_settlement", ">=", from_date),
+            ("date_vat_settlement", "<=", to_date),
+            "&",
+            ("date_vat_settlement", "=", None),
+            "&",
+            ("date", "<=", to_date),
+            ("date", ">=", from_date),
+            ("company_id", "=", company_id),
         ]
--- l10n_it_vat_settlement_date/readme/DESCRIPTION.rst
+++
@@ -1,8 +0,0 @@
-**Italiano**
-
-    Aggiunge la data competenza IVA per le fatture fornitori.
-
-
-**English**
-
-    Add settlement date for bills.
