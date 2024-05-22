33aabae39b3406d2df208f7059ba43765d269e1d
sergiocorato <sergiocorato@gmail.com>
2024-05-22 15:38:32 +0200

    [FIX] add a warning in l10n_it_intrastat_statement when there are invoices in draft or cancel state

--- l10n_it_intrastat_statement/models/__init__.py
+++ l10n_it_intrastat_statement/models/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+from . import account
 from . import config
 from . import intrastat_statement_section
 from . import intrastat_statement
