ce54c1ae3cde08d856c1fcbbd2602e28ab2bbc76
Simone Rubino <simone.rubino@aion-tech.it>
2024-08-21 17:47:43 +0200

    [COV] l10n_it_riba: Set commercial partner's bank

--- l10n_it_riba/tests/__init__.py
+++ l10n_it_riba/tests/__init__.py
@@ -4,6 +4,7 @@
 #
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import test_account_move
 from . import test_menu
 from . import test_riba
 from . import riba_common
