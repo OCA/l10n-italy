46673b1622910976211eb8125e5e286b6f98d601
Simone Rubino <simone.rubino@aion-tech.it>
2024-09-04 16:10:32 +0200

    [REF] l10n_it_vat_registries: Use common tax amounts overrides
    The overrides have been moved to l10n_it_account

--- l10n_it_vat_registries/models/account.py
+++ l10n_it_vat_registries/models/account.py
@@ -7,27 +7,3 @@ class AccountTax(models.Model):
     _inherit = "account.tax"
 
     exclude_from_registries = fields.Boolean(string="Exclude from VAT registries")
-
-    def get_balance_domain(self, state_list, type_list):
-        domain = super().get_balance_domain(state_list, type_list)
-        if self.env.context.get("vat_registry_journal_ids"):
-            domain.append(
-                (
-                    "move_id.journal_id",
-                    "in",
-                    self.env.context["vat_registry_journal_ids"],
-                )
-            )
-        return domain
-
-    def get_base_balance_domain(self, state_list, type_list):
-        domain = super().get_base_balance_domain(state_list, type_list)
-        if self.env.context.get("vat_registry_journal_ids"):
-            domain.append(
-                (
-                    "move_id.journal_id",
-                    "in",
-                    self.env.context["vat_registry_journal_ids"],
-                )
-            )
-        return domain
