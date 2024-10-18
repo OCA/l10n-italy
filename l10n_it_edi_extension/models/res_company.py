0184a5514d4fb675fb57860b052c718e3c774c42
Borruso <gborruso@dinamicheaziendali.it>
2024-10-18 10:28:07 +0200

    [ADD] l10n_it_edi_extension: adding tags in xml

---
+++ l10n_it_edi_extension/models/res_company.py
@@ -0,0 +1,24 @@
+# Copyright 2025 Giuseppe Borruso - Dinamiche Aziendali srl
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompanyInherit(models.Model):
+    _inherit = "res.company"
+
+    l10n_edi_it_art73 = fields.Boolean(
+        string="Art. 73",
+        help="Indicates whether the document has been issued according to "
+        "methods and terms laid down in a ministerial decree under "
+        "the terms of Article 73 of Italian Presidential Decree "
+        "633/72 (this enables the company to issue in the same "
+        "year several documents with same number)",
+    )
+    l10n_edi_it_admin_ref = fields.Char(string="Public Administration Reference Code")
+    l10n_edi_it_stable_organization = fields.Many2one(
+        "res.partner",
+        string="Stable Organization",
+        help="The fields must be entered only when the seller/provider is "
+        "non-resident, with a stable organization in Italy",
+    )
