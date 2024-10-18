0dfe9268746750d391535ec21bb76ee51a48a949
Borruso <gborruso@dinamicheaziendali.it>
2024-10-18 10:28:07 +0200

    [ADD] l10n_it_edi_extension: adding tag Causale in xml

--- l10n_it_edi_extension/models/account_move.py
+++ l10n_it_edi_extension/models/account_move.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
+from odoo.tools import html2plaintext
 
 
 class AccountMoveInherit(models.Model):
@@ -40,3 +41,31 @@ class AccountMoveInherit(models.Model):
             "url": self.l10n_it_edi_attachment_preview_link,
             "target": "new",
         }
+
+    # -------------------------------------------------------------------------
+    # Helpers
+    # -------------------------------------------------------------------------
+
+    def _l10n_it_edi_get_values(self, pdf_values=None):
+        res = super()._l10n_it_edi_get_values(pdf_values)
+
+        causale_list = []
+        if self.narration:
+            try:
+                narration_text = html2plaintext(self.narration)
+            except Exception:
+                narration_text = ""
+
+            # max length of Causale is 200
+            for causale in narration_text.split("\n"):
+                if not causale:
+                    continue
+                causale_list_200 = [
+                    causale[i : i + 200] for i in range(0, len(causale), 200)
+                ]
+                for causale200 in causale_list_200:
+                    causale_list.append(causale200)
+
+        res["causale"] = causale_list
+
+        return res
