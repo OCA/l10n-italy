eea23f0422a5d148b92f78400cb6c158b46637f1
Borruso <gborruso@dinamicheaziendali.it>
2024-12-23 09:54:49 +0100

    [ADD] l10n_it_edi_extension: aggiunta PR https://github.com/OCA/l10n-italy/pull/4411

---
+++ l10n_it_edi_extension/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2024 Giuseppe Borruso <gborruso@dinamicheaziendali.it>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import controllers
+from . import models
