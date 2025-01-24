e2c1002ce1989130dfb7f13c72e6046a303070e9
Borruso <gborruso@dinamicheaziendali.it>
2025-01-24 10:28:52 +0100

    [ADD] l10n_it_account: aggiunta PR https://github.com/OCA/l10n-italy/pull/4555

---
+++ l10n_it_account/__manifest__.py
@@ -0,0 +1,43 @@
+# Copyright 2015 Abstract srl (<http://www.abstract.it>)
+# Copyright 2015-2017 Agile Business Group (<http://www.agilebg.com>)
+# Copyright 2015 Link It Spa (<http://www.linkgroup.it/>)
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# Copyright 2018 Lorenzo Battistini <https://github.com/eLBati>
+# Copyright 2020 Gianmarco Conte <gconte@dinamicheaziendali.it>
+# Copyright 2022 Marco Colombo <marco.colombo@phi.technology>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "ITA - Contabilità base",
+    "summary": "Modulo base usato come dipendenza di altri moduli contabili",
+    "version": "18.0.1.0.0",
+    "development_status": "Production/Stable",
+    "category": "Hidden",
+    "author": "Agile Business Group, Abstract, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "AGPL-3",
+    "depends": [
+        "account_fiscal_year",
+        "account_tax_balance",
+        "date_range",
+        "web",
+        "l10n_it",
+    ],
+    "data": [
+        "views/account_menuitem.xml",
+        "views/partner_view.xml",
+        "views/res_config_settings_views.xml",
+        "reports/account_reports_view.xml",
+    ],
+    "assets": {
+        "web.report_assets_common": [
+            "l10n_it_account/static/src/css/*.css",
+        ]
+    },
+    "installable": True,
+    "external_dependencies": {
+        "python": [
+            "openupgradelib",
+        ],
+    },
+}
---
+++ l10n_it_account/readme/CONTRIBUTORS.md
@@ -0,0 +1,9 @@
+- Davide Corio
+- Lorenzo Battistini \<<lorenzo.battistini@agilebg.com>\>
+- Alex Comba \<<alex.comba@agilebg.com>\>
+- Sergio Zanchetta \<<https://github.com/primes2h>\>
+- Gianmarco Conte - Dinamiche Aziendali Sr
+  \<<gconte@dinamicheaziendali.it>\>
+- Marco Colombo - Phi S.r.l. \<<marco.colombo@phi.technology>\>
+- [TAKOBI](https://takobi.online):
+  - Simone Rubino \<<sir@takobi.online>\>
