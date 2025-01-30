c2c59d3454bb7cee35675f11d7b6e8ced822acc8
Gianluigi Tiesi <sherpya@gmail.com>
2025-01-30 15:03:46 +0100

    [FIX] fixed manual declaration of intent selection
    the form allows only add declarations of intent,
    this fix clears the set before adding new ones
    
    closes #4576

--- l10n_it_declaration_of_intent/wizard/select_declarations.py
+++ l10n_it_declaration_of_intent/wizard/select_declarations.py
@@ -46,6 +46,8 @@ class SelectManuallyDeclarations(models.TransientModel):
         if not invoice_id:
             return res
         invoice = self.env["account.move"].browse(invoice_id)
+        declaration_of_intent_ids = [(5, 0)]
         for declaration in self.declaration_ids:
-            invoice.declaration_of_intent_ids = [(4, declaration.id)]
+            declaration_of_intent_ids.append((4, declaration.id))
+        invoice.declaration_of_intent_ids = declaration_of_intent_ids
         return True
