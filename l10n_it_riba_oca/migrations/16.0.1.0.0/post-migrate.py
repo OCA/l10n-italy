fe52ed9547fb0f1561f229e37e19b8be684a87de
gianmarco <gconte@dinamicheaziendali.it>
2025-05-05 10:26:09 +0200

    [MIG] test migrazione riba oca per v16 + v18

---
+++ l10n_it_riba_oca/migrations/16.0.1.0.0/post-migrate.py
@@ -0,0 +1,41 @@
+#  Copyright 2023 Simone Rubino - AionTech
+#  Copyright 2024 Nextev Srl
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+NEW_MODULE_NAME = "l10n_it_riba_oca"
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+
+    openupgrade.map_values(
+        env.cr,
+        openupgrade.get_legacy_name("state"),
+        "state",
+        [
+            ("draft", "draft"),
+            ("confirmed", "confirmed"),
+            ("accredited", "credited"),
+            ("paid", "paid"),
+            ("unsolved", "past_due"),
+            ("cancel", "cancel"),
+        ],
+        table="riba_slip_line",
+    )
+
+    openupgrade.map_values(
+        env.cr,
+        openupgrade.get_legacy_name("state"),
+        "state",
+        [
+            ("draft", "draft"),
+            ("accepted", "accepted"),
+            ("accredited", "credited"),
+            ("paid", "paid"),
+            ("unsolved", "past_due"),
+            ("cancel", "cancel"),
+        ],
+        table="riba_slip",
+    )
