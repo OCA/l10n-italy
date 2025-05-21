c5b745ea3b55474b2fb737bbec84e90faf75c05d
Lorenzo Allegrucci <lorenzoallegrucci@openforce.it>
2025-05-21 16:51:45 +0200

    [IMP] l10n_it_rea: pre-commit auto fixes

--- l10n_it_rea/__manifest__.py
+++ l10n_it_rea/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     "name": "ITA - Registro REA",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Localization/Italy",
     "development_status": "Production/Stable",
     "summary": "Gestisce i campi del Repertorio Economico Amministrativo",
