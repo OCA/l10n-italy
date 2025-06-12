a8f788ead1a27b20812c47c669a9aed58a55dfab
matteo.tognini <matteo.tognini@nextev.it>
2025-06-12 13:47:23 +0200

    [FIX]l10n_it_account_stamp: migration script fix

--- l10n_it_account_stamp/migrations/18.0.1.0.0/post-migration.py
+++ l10n_it_account_stamp/migrations/18.0.1.0.0/post-migration.py
@@ -7,8 +7,8 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(cr, version):
     openupgrade.load_data(
-        cr, "l10n_it_account_stamp", "18.0.1.0.0/noupdate_changes.xml"
+        cr, "l10n_it_account_stamp", "migrations/18.0.1.0.0/noupdate_changes.xml"
     )
     openupgrade.delete_record_translations(
-        cr, "l10n_it_account_stamp", ["l10n_it_account_stamp_2_euro"]
+        cr.cr, "l10n_it_account_stamp", ["l10n_it_account_stamp_2_euro"]
     )
--- l10n_it_account_stamp/migrations/18.0.1.1.0/post-migration.py
+++ l10n_it_account_stamp/migrations/18.0.1.1.0/post-migration.py
@@ -4,10 +4,9 @@
 from openupgradelib import openupgrade
 
 
-@openupgrade.migrate()
 def migrate(env, version):
     openupgrade.logged_query(
-        env.cr,
+        env,
         """
         UPDATE account_move_line aml
         SET
