776a58faab80f1c42ad33057602633bc728d4f4d
tafaRU <alex.comba@agilebg.com>
2025-09-01 15:07:50 +0200

    [IMP] l10n_it__account_stamp: force new pip ver.
    See https://github.com/OCA/l10n-italy/issues/4887 for futher info.

--- l10n_it_account_stamp/__manifest__.py
+++ l10n_it_account_stamp/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "ITA - Imposta di bollo",
-    "version": "18.0.1.1.1",
+    "version": "18.0.1.1.2",
     "category": "Localization/Italy",
     "summary": "Gestione automatica dell'imposta di bollo",
     "author": "Ermanno Gnan, Sergio Corato, Enrico Ganzaroli, "
