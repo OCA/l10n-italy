17ab60e82287a3774c3e82ad7fccb732153247de
matteo.tognini <matteo.tognini@nextev.it>
2026-02-03 13:55:29 +0100

    [IMP]l10n_it_riba_oca: riba policy expenses

--- l10n_it_riba_oca/tests/__init__.py
+++ l10n_it_riba_oca/tests/__init__.py
@@ -5,6 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import test_account_move
+from . import test_different_payment_terms
 from . import test_menu
 from . import test_riba
 from . import riba_common
