4d2e243a57a27ff436012266966a2fc63f66f679
VBNext <valentina.balgera@nextev.it>
2026-02-05 02:08:18 +0100

    Add asset_situation and asset_situation_xlsx imports

--- l10n_it_asset_management/report/__init__.py
+++ l10n_it_asset_management/report/__init__.py
@@ -6,3 +6,5 @@ from . import asset_journal
 from . import asset_journal_xlsx
 from . import asset_previsional
 from . import asset_previsional_xlsx
+from . import asset_situation
+from . import asset_situation_xlsx
