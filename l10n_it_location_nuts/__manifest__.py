4ce9b5789775e3090c5326dc9c468e9d59613f7b
Borruso <gborruso@dinamicheaziendali.it>
2026-02-08 12:16:24 +0100

    [IMP] l10n_it_location_nuts: pre-commit auto fixes

--- l10n_it_location_nuts/__manifest__.py
+++ l10n_it_location_nuts/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "18.0.1.0.0",
     "category": "Localization/Europe",
     "website": "https://github.com/OCA/l10n-italy",
-    "author": "Agile Business Group, " "Odoo Community Association (OCA)",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
