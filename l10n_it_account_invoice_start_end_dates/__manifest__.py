53f7793ca350f0910e4e56f1f94cb3668f7eeac5
eLBati <elbaddy@gmail.com>
2026-02-09 14:55:38 +0100

    ADD l10n_it_account_invoice_start_end_dates: Set start/end dates on Italian tax move lines for partially/totally deductible VAT

---
+++ l10n_it_account_invoice_start_end_dates/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2026 Lorenzo Battistini
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "ITA - Account Invoice Start End Dates",
+    "version": "18.0.1.0.0",
+    "category": "Hidden",
+    "license": "LGPL-3",
+    "summary": "Set start/end dates on Italian tax move lines"
+    " for partially/totally deductible VAT",
+    "author": "Lorenzo Battistini, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "maintainers": ["eLBati"],
+    "depends": [
+        "account_invoice_start_end_dates",
+        "l10n_it",
+    ],
+    "auto_install": True,
+    "installable": True,
+}
