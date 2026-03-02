2f400095c48d9c0839c13a7375f348e3f93620e1
TennyMKT <andrea.martinelli@mkt.it>
2026-03-02 12:00:05 +0100

    l10n_it_delivery_note: FIX backorder

--- l10n_it_delivery_note/models/stock_picking.py
+++ l10n_it_delivery_note/models/stock_picking.py
@@ -492,6 +492,7 @@ class StockPicking(models.Model):
         """When we make a backorder of a picking the delivery note lines needed
         to be updated otherwise stock_delivery_note_line_move_uniq constraint is raised"""
         backorders = super()._create_backorder()
-        for backorder in backorders:
-            backorder.backorder_id.delivery_note_id.update_detail_lines()
+        if backorders:
+            for backorder in backorders:
+                backorder.backorder_id.delivery_note_id.update_detail_lines()
         return backorders
