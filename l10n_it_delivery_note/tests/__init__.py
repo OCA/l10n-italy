6ccb66cab821039a1eb841425cd1978631bb387f
NextEvTeo <matteo.tognini@nextev.it>
2026-03-20 11:45:05 +0100

    [IMP] l10n_it_delivery_note: electronic invoicing DatiDdt

--- l10n_it_delivery_note/tests/__init__.py
+++ l10n_it_delivery_note/tests/__init__.py
@@ -1,4 +1,5 @@
 from . import delivery_note_common
+from . import test_edi_delivery_note
 from . import test_stock_delivery_note_invoicing
 from . import test_stock_delivery_note
 from . import test_stock_delivery_note_sequence
